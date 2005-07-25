fb63825c8b166e5f9e15d179b311bf45c683684f
dschanen <dschanen>
2005-07-25 20:50:09 +0000

    Initial checkin of inputfields files
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@144 f1ce5500-ea2b-45e0-a110-28dffb978352

---
+++ inputfields/run_inputfields.bash
@@ -0,0 +1,44 @@
+#######################################################################
+#
+# Script to run the standalone hoc program.  
+# Tested with BASH.  Not tested with Korn shell or Bourne(sh) shell.
+# Edit to change run
+#
+#######################################################################
+# Useful on compilers which have OpenMP
+# OMP_NUM_THREADS=2
+#######################################################################
+# Select a run, comment out the rest
+# RUN_CASE=atex 
+# RUN_CASE=arm 
+# RUN_CASE=bomex 
+  RUN_CASE=dycoms2_rf01 
+# RUN_CASE=dycoms2_rf02_d 
+# RUN_CASE=dycoms2_rf02_nd 
+# RUN_CASE=fire 
+# RUN_CASE=nov11_altocu
+#######################################################################
+# Check for necessary namelists.  If files exist, then
+# copy them over to the general input files.
+
+ INPUTFIELDS_IN=$RUN_CASE'_inputfields.in'
+
+ if [ ! -e "$INPUTFIELDS_IN" ] ; then
+	echo $INPUTFIELDS_IN " does not exist"
+	exit 1
+ fi
+
+ if [ -e 'inputfields.in' ] ; then
+	rm -f 'inputfields.in'
+ fi
+
+ ln -s $INPUTFIELDS_IN 'inputfields.in'
+
+
+#######################################################################
+#
+# Run HOC
+./hoc_inputfields
+
+# remove the temporary error.in file
+ rm -f 'inputfields.in'
