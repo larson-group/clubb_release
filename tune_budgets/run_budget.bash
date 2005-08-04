8c892fbf159fecfbe3adbf6dc31c738f51272425
dschanen <dschanen>
2005-08-04 21:32:30 +0000

    New budget tuner.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@216 f1ce5500-ea2b-45e0-a110-28dffb978352

---
+++ tune_budgets/run_budget.bash
@@ -0,0 +1,43 @@
+#######################################################################
+#
+# Tested with BASH.  Not tested with Korn shell or Bourne(sh) shell.
+# Edit to change run
+#
+#######################################################################
+# Useful on compilers which have OpenMP
+# OMP_NUM_THREADS=2
+#######################################################################
+# Select a run, comment out the rest
+# RUN_CASE=atex 
+# RUN_CASE=arm 
+# RUN_CASE=bomex 
+# RUN_CASE=dycoms2_rf01 
+# RUN_CASE=dycoms2_rf02_d 
+# RUN_CASE=dycoms2_rf02_nd 
+  RUN_CASE=fire 
+# RUN_CASE=nov11_altocu
+#######################################################################
+# Check for necessary namelists.  If files exist, then
+# copy them over to the general input files.
+
+ BUDGET_IN=$RUN_CASE'_budget.in'
+
+ if [ ! -e "$BUDGET_IN" ] ; then
+	echo $BUDGET_IN " does not exist"
+	exit 1
+ fi
+
+ if [ -e 'budget.in' ] ; then
+	rm -f 'budget.in'
+ fi
+
+ ln -s $BUDGET_IN 'budget.in'
+
+
+#######################################################################
+#
+# Run HOC
+./hoc_tuner_budget_terms
+
+# remove the temporary error.in file
+ rm -f 'budget.in'
