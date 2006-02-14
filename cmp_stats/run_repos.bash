89938a65fa6568b459e5bf8a77fd0d96bf42b2b4
dschanen <dschanen>
2006-02-14 21:41:37 +0000

    Added directory for doing our stats comparison plots, and scripts.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@648 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: d6349682afe08ab69e627bf10e2c17ac1b951997

---
+++ cmp_stats/run_repos.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+#######################################################################
+# $Id: run_repos.bash,v 1.1 2006-02-14 21:41:26 dschanen Exp $
+#
+# Script to run the standalone hoc program for all models, with 
+# repository (non-static) constants.
+# Tested with bash v2.  Might work with Ksh.
+#
+#######################################################################
+# Useful on multiprocessor machines with OpenMP capable Fortran
+# OMP_NUM_THREADS=2
+#######################################################################
+
+EXIT_CODE=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0 [7]=0 [8]=0 [9]=0 [10]=0 )
+
+RUN_CASE=(arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
+ dycoms2_rf02_nd dycoms2_rf02_so fire nov11_altocu wangara )
+
+# This will loop over all runs in sequence 
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+#######################################################################
+# Check for necessary namelists.  If files exist, then
+# copy them over to the general input files.
+
+ STANDALONE_IN='../standalone/standalone_'"${RUN_CASE[$x]}"'.in'
+ MODEL_IN='../model/'"${RUN_CASE[$x]}"'_model.in'
+ STATS_IN="${RUN_CASE[$x]}"'_stats.in'
+
+ if [ ! -e "$STANDALONE_IN" ] ; then
+	echo $STANDALONE_IN " does not exist"
+	exit 1
+ fi
+
+ if [ -e 'standalone.in' ] ; then
+	rm -f 'standalone.in'
+ fi
+
+ ln -s $STANDALONE_IN 'standalone.in'
+ cat $MODEL_IN $STATS_IN > "${RUN_CASE[$x]}"'_hoc.in'
+
+#######################################################################
+#
+# State which case is being run
+ echo "Running ""${RUN_CASE[$x]}"
+# Run HOC 
+ RESULT=`./hoc_standalone 2>&1 |grep 'normal'`
+#echo $RESULT
+ if [ -z "$RESULT" ]; then
+	EXIT_CODE[$x]=-1
+ fi
+
+# remove the namelists
+ rm -f 'standalone.in'
+ rm -f $RUN_CASE'_hoc.in'
+
+done
+
+# Print the results & copy files
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+  if [ "${EXIT_CODE[$x]}" != 0 ]; then
+	echo "${RUN_CASE[$x]}"' failure'
+  else
+	cp "${RUN_CASE[$x]}"_zt.??? repos_const/
+	cp "${RUN_CASE[$x]}"_zm.??? repos_const/
+  fi
+done
---
+++ cmp_stats/run_std.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+#######################################################################
+# $Id: run_std.bash,v 1.1 2006-02-14 21:41:26 dschanen Exp $
+#
+# Script to run the standalone hoc program for all models, with the
+# static set of constants.
+# Tested with bash v2.  Might work with Ksh.
+#
+#######################################################################
+# Useful on multiprocessor machines with OpenMP capable Fortran
+# OMP_NUM_THREADS=2
+#######################################################################
+
+EXIT_CODE=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0 [7]=0 [8]=0 [9]=0 [10]=0 )
+
+RUN_CASE=(arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
+ dycoms2_rf02_nd dycoms2_rf02_so fire nov11_altocu wangara )
+
+# This will loop over all runs in sequence 
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+#######################################################################
+# Check for necessary namelists.  If files exist, then
+# copy them over to the general input files.
+
+ CONST_IN='std_const/std_const_nml.in'
+ MODEL_IN='../model/'"${RUN_CASE[$x]}"'_model.in'
+ STATS_IN="${RUN_CASE[$x]}"'_stats.in'
+
+ echo "&model" > "standalone.in"
+ echo "run_file = \"${RUN_CASE[$x]}"'_hoc.in'\" >> "standalone.in"
+ echo "stdout = .true." >> "standalone.in"
+ echo "/"\n >> "standalone.in"
+ cat $CONST_IN >> "standalone.in"
+ cat $MODEL_IN $STATS_IN > "${RUN_CASE[$x]}"'_hoc.in'
+
+#######################################################################
+#
+# State which case is being run
+ echo "Running ""${RUN_CASE[$x]}"
+# Run HOC 
+ RESULT=`./hoc_standalone 2>&1 |grep 'normal'`
+#echo $RESULT
+ if [ -z "$RESULT" ]; then
+	EXIT_CODE[$x]=-1
+ fi
+
+# remove the namelists
+ rm -f 'standalone.in'
+ rm -f $RUN_CASE'_hoc.in'
+
+done
+
+# Print the results & copy files
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+  if [ "${EXIT_CODE[$x]}" != 0 ]; then
+	echo "${RUN_CASE[$x]}"' failure'
+  else
+	cp "${RUN_CASE[$x]}"_zt.??? std_const/
+	cp "${RUN_CASE[$x]}"_zm.??? std_const/
+  fi
+done
