c5d753dfe1654a19cede4a81355a0b167fe490be
dschanen <dschanen>
2006-04-21 22:21:39 +0000

    Moved files to HOC_parameterization, see further comments there. arrays.F and grid.h were no longer needed, and were removed.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@747 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: c05f6a1622a9619d9c25e8a55ae1dcf7b895d41b

--- src/grid.h
+++
@@ -1,5 +0,0 @@
-! $Id: grid.h,v 1.2 2005-08-23 21:11:10 dschanen Exp $
-        integer, parameter :: NMAX = 200      ! max number of levels for grid
-
-        real, parameter :: UNDEFINED = 9.999e10
-
