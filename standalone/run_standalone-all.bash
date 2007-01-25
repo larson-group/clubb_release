92cd55f3d0173a81acb48d9df39300c16a46c7d1
dschanen <dschanen>
2007-01-25 23:49:13 +0000

    Added mpace to standalone-all.bash;
    Put Chris Golaz's best constant paramters into all the standalone files.
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@1074 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ca1b5c7372c84cd826f6e3cf1af0823d313a99a7

--- standalone/run_standalone-all.bash
+++ standalone/run_standalone-all.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################################
-# $Id: run_standalone-all.bash,v 1.20 2006-07-19 21:28:15 dschanen Exp $
+# $Id: run_standalone-all.bash,v 1.21 2007-01-25 23:49:13 dschanen Exp $
 #
 # Script to run the standalone hoc program for all models.
 # Tested with bash v2.  Might work with Ksh.
@@ -11,10 +11,12 @@
 #######################################################################
 
 EXIT_CODE=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0 [7]=0 [8]=0 [9]=0 \
- [10]=0 [11]=0 )
+ [10]=0 [11]=0 [12]=0 )
 
-RUN_CASE=(arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
- dycoms2_rf02_nd dycoms2_rf02_so fire nov11_altocu jun25_altocu wangara )
+RUN_CASE=( \
+	arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
+ 	dycoms2_rf02_nd dycoms2_rf02_so fire nov11_altocu jun25_altocu 
+	wangara mpace )
 
 # This will loop over all runs in sequence 
 for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
