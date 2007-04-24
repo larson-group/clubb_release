c9e92804e82e8405e94f33ac1976943067ba15f6
dschanen <dschanen>
2007-04-24 22:07:25 +0000

    Adjusted to require a command line argument.  Most people seemed to be using it that way anyway, and now we don't have to add new cases to this file.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@1266 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: cb39d6f13c95a471bba8dd69a04f6a7ea4048755

--- standalone/run_standalone.bash
+++ standalone/run_standalone.bash
@@ -1,69 +1,57 @@
 #!/bin/bash
 #######################################################################
-# $Id: run_standalone.bash,v 1.27 2007-02-21 20:10:10 mjfalk Exp $
+# $Id: run_standalone.bash,v 1.28 2007-04-24 22:07:25 dschanen Exp $
 #
+# Description:
 # Script to run the standalone hoc program.  
 # Tested with bash v2.  Might work with Ksh.
 #
 #######################################################################
-# Useful on multiprocessor machines with OpenMP capable Fortran
-# OMP_NUM_THREADS=2
+# Useful variable on multiprocessor machines with OpenMP capable 
+# Fortran, uncomment and set to use.  
+# The hoc_standalone program is not parallel, but many LAPACK/BLAS 
+# libraries are.
+#
+# export OMP_NUM_THREADS=2
 #######################################################################
-# Select a run, comment out the rest
+
 if [ -z $1 ]; then
-
-# RUN_CASE=arm 
-# RUN_CASE=atex 
-# RUN_CASE=bomex 
-# RUN_CASE=cobra
-# RUN_CASE=dycoms2_rf01 
-# RUN_CASE=dycoms2_rf02_do
-# RUN_CASE=dycoms2_rf02_ds
-# RUN_CASE=dycoms2_rf02_nd 
-# RUN_CASE=dycoms2_rf02_so
-# RUN_CASE=fire 
-RUN_CASE=gabls2 
-# RUN_CASE=jun25_altocu
-# RUN_CASE=mpace
-# RUN_CASE=nov11_altocu
-# RUN_CASE=rico
-# RUN_CASE=wangara  
-
-#######################################################################
-# Or alternatively specify it as a command line argument in the form
-# run_standalone.bash <CASE>.
+	echo "Usage: run_standalone.bash <CASE>"
+	exit
 else
- RUN_CASE=$1
+	RUN_CASE=$1
 fi
 
 
 #######################################################################
 # Check for necessary namelists.  If files exist, then
 # copy them over to the general input files.
+#######################################################################
 
- STANDALONE_IN='standalone_'$RUN_CASE'.in'
- MODEL_IN='../model/'$RUN_CASE'_model.in'
- STATS_IN='../stats/'$RUN_CASE'_stats.in'
+STANDALONE_IN='standalone_'$RUN_CASE'.in'
+MODEL_IN='../model/'$RUN_CASE'_model.in'
+STATS_IN='../stats/'$RUN_CASE'_stats.in'
 
- if [ ! -e "$STANDALONE_IN" ] ; then
+if [ ! -e "$STANDALONE_IN" ] ; then
 	echo $STANDALONE_IN " does not exist"
 	exit 1
- fi
+fi
 
- if [ -e 'standalone.in' ] ; then
+if [ -e 'standalone.in' ] ; then
 	rm -f 'standalone.in'
- fi
+fi
 
- ln -s $STANDALONE_IN 'standalone.in'
- cat $MODEL_IN $STATS_IN > $RUN_CASE'_hoc.in'
+ln -s $STANDALONE_IN 'standalone.in'
+cat $MODEL_IN $STATS_IN > $RUN_CASE'_hoc.in'
 
 #######################################################################
-#
 # State which case is being run
- echo "Running" $RUN_CASE
-# Run HOC
- ../bin/hoc_standalone
+#######################################################################
+echo "Running" $RUN_CASE
 
-# remove the namelists
- rm -f 'standalone.in'
- rm -f $RUN_CASE'_hoc.in'
+# Run the HOC model
+../bin/hoc_standalone
+
+# Remove the namelists
+rm -f 'standalone.in'
+rm -f $RUN_CASE'_hoc.in'
