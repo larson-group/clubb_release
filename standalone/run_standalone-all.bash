907cd9818f549dd479fb928548bcb6dd669dbfb0
vlarson <vlarson>
2007-10-19 16:50:12 +0000

    Vince Larson increased number of exit codes to accommodate new mpace_a entry. 19 Oct 2007.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@1521 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: bbf3fcd0e122ab9130a671a881e637f0c5b80b63

--- standalone/run_standalone-all.bash
+++ standalone/run_standalone-all.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################################
-# $Id: run_standalone-all.bash,v 1.27 2007-09-21 16:21:19 mjfalk Exp $
+# $Id: run_standalone-all.bash,v 1.28 2007-10-19 16:50:12 vlarson Exp $
 #
 # Script to run the standalone hoc program for all models.
 # Tested with bash v2.  Might work with Ksh.
@@ -11,11 +11,11 @@
 #######################################################################
 
 EXIT_CODE=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0 [7]=0 [8]=0 [9]=0 \
- [10]=0 [11]=0 [12]=0 [13]=0 [14]=0 )
+ [10]=0 [11]=0 [12]=0 [13]=0 [14]=0 [15]=0 )
 
 RUN_CASE=( \
 	arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
-	dycoms2_rf02_nd dycoms2_rf02_so fire gabls2 jun25_altocu\ 
+	dycoms2_rf02_nd dycoms2_rf02_so fire gabls2 jun25_altocu\
         mpace_a mpace_b nov11_altocu rico wangara )
 
 # Since everyone seems to like to add new cases without adding exit codes,
