2ceb0f6d660c49eab52dc8afac013231a150a811
nielsenb <nielsenb>
2008-03-25 19:02:27 +0000

    File used to automatically generate HOC code browser.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@1833 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 2b754f1dad381937c7e6d08c649a56a503e7995a

---
+++ utilities/hoc_browser/browser.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "******browser.bash started******" >> ~/nightly_plots.log
+
+#nielsenb - We need to clean up the directory for the code browser too
+rm -f ~/hoc_v2.2_tuner/hocbrowser/call_from/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/call_to/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/html_code/bugs_rad/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/html_code/latin_hc/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/html_code/main/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/ind/*.html
+rm -f ~/hoc_v2.2_tuner/hocbrowser/*.html
+#end nielsenb's changes
+
+cd ~/f90tohtml/nwp_codes/ && \
+perl hoc_prepare.pl && \
+../f90tohtml hoc.f2h && \
+cd ~/hoc_v2.2_tuner/ && \
+
+#nielsenb - There is no "main" folder, so instead make a Main_index.html that
+#mirrors the src index. This prevents 404 errors on main browser page
+cp ~/hoc_v2.2_tuner/hocbrowser/ind/src_index.html ~/hoc_v2.2_tuner/hocbrowser/ind/main_index.html
+#end nielsenb's changes
+
+#cvs ci -m"Automated Generation" hocbrowser/ && \
+echo "******browser.bash ended successfully******" >> ~/nightly_plots.log
