fcbfe9741b97c6d737f3fda51cfaaab755775383
ajsmith4 <ajsmith4>
2008-04-07 17:25:33 +0000

    Removing the CLEX-9 cases (clex9_oct14 and clex9_nov02) from run_standalone-all.bash.  These cases are not included in the nightly plots, so it does not appear to be necessary to included them here.  Also, I am still working on setting them up, so it is probably better to wait until they are finished before re-adding them.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@1892 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 96e62393d129805227284ff535edc005efda88ab

--- standalone/run_standalone-all.bash
+++ standalone/run_standalone-all.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################################
-# $Id: run_standalone-all.bash,v 1.29 2008-03-25 18:11:29 ajsmith4 Exp $
+# $Id: run_standalone-all.bash,v 1.30 2008-04-07 17:25:33 ajsmith4 Exp $
 #
 # Script to run the standalone hoc program for all models.
 # Tested with bash v2.  Might work with Ksh.
@@ -11,10 +11,10 @@
 #######################################################################
 
 EXIT_CODE=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0 [7]=0 [8]=0 [9]=0 \
- [10]=0 [11]=0 [12]=0 [13]=0 [14]=0 [15]=0 [16]=0 [17]=0 )
+ [10]=0 [11]=0 [12]=0 [13]=0 [14]=0 [15]=0 )
 
 RUN_CASE=( \
-	arm atex bomex clex9_nov02 clex9_oct14 dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
+	arm atex bomex dycoms2_rf01 dycoms2_rf02_do dycoms2_rf02_ds\
 	dycoms2_rf02_nd dycoms2_rf02_so fire gabls2 jun25_altocu\
         mpace_a mpace_b nov11_altocu rico wangara )
 
