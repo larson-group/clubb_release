d6f85cc08b74185364ce5131e4fe52fe39977b88
dschanen <dschanen>
2008-05-14 22:13:02 +0000

    Changed to match up with the standalone script, which takes an argument for the model case.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2073 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 9afadae63d674730bcc2c45efaa3826d409c98ad

--- inputfields/run_inputfields.bash
+++ inputfields/run_inputfields.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 #######################################################################
+# $Id: run_inputfields.bash,v 1.9 2008-05-14 22:13:02 dschanen Exp $
 #
-# Script to run the standalone hoc program.  
+# Script to run the hoc inputfields program.  
 # Tested with BASH.  Not tested with Korn shell or Bourne(sh) shell.
 # Edit to change run
 #
@@ -9,19 +10,16 @@
 # Useful on compilers which have OpenMP
 # OMP_NUM_THREADS=2
 #######################################################################
-# Select a run, comment out the rest
-  RUN_CASE=atex 
-# RUN_CASE=arm 
-# RUN_CASE=bomex 
-# RUN_CASE=dycoms2_rf01 
-# RUN_CASE=dycoms2_rf02_do
-# RUN_CASE=dycoms2_rf02_ds
-# RUN_CASE=dycoms2_rf02_nd
-# RUN_CASE=fire 
-# RUN_CASE=nov11_altocu
-#######################################################################
 # Check for necessary namelists.  If files exist, then
 # copy them over to the general input files.
+#######################################################################
+if [ -z $1 ]; then
+	echo "Usage: run_inputfields.bash <MODEL CASE>"
+	exit
+else
+	RUN_CASE=$1
+fi
+
 
 INPUTFIELDS_IN=$RUN_CASE'_inputfields.in'
 MODEL_IN='../model/'$RUN_CASE'_model.in'
