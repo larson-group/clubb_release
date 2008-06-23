ff5f05812be18433e725647b6ba9ef967b50e436
faschinj <faschinj>
2008-06-23 14:17:27 +0000

    Added the abilitiy to set the control file, start time, end time, and tolerance.
    Also added additional comments
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2289 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 6a9dbb640987358788e2b02bba146f7995723614

--- utilities/run_comparisons/find_grads_differences.m
+++ utilities/run_comparisons/find_grads_differences.m
@@ -1,11 +1,18 @@
-function [] = find_grads_differences();
+function [] = find_grads_differences( ctl_file, t1, t2, tol )
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% find_grads_differences()
+% find_grads_differences( string, integer, integer, integer )
+% Example: find_grads_differences( 'nov11_altocu.ctl', 1, 240, 2 )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Written by Michael Falk, December 2006-March 2007
+% Modified by Joshua Fasching, June 2008
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Input parameters: none
+% Input parameters: 
+%       ctl_file - .ctl file for the data being compared
+%       t1      - Lower time bound of data
+%       t2      - Upper time bound of data
+%       tol     - Digits of Precision ( Higher # means finer tolerance )
+%
 % Input files: Three GrADS .ctl files in separate directories
 % Output parameters: none
 % Output files: none
@@ -14,6 +21,8 @@ function [] = find_grads_differences();
 %           header_read.m
 %           read_grads_hoc_endian.m
 %           get_profile.m
+%
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This script takes three separate model runs with identically-named
 % files in separate directories (following Michael Falk's directory
@@ -22,27 +31,25 @@ function [] = find_grads_differences();
 %
 % It accomplishes this by taking a time average over a given period for
 % each file and comparing a vertical profile of each variable over that
-% time period.  If the two profiles are not identical, the script will
+% time period.  If the two profiles are not within tolerance, the script will
 % print a message to the screen.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% The three directories containing three different gabls_zt file sets.
-path1  = ['/home/mjfalk/hoc_working/standalone/gabls63/'];
-path2  = ['/home/mjfalk/hoc_working/standalone/gabls63_test/'];
-path3  = ['/home/mjfalk/hoc_working/standalone/gabls63_test2/'];
-smfile = 'gabls_zt.ctl';
+% The three directories containing three different ctlfile file sets.
+path1  = ['/home/faschinj/hoc_v2.2_tuner/standalone/'];
+path2  = ['/home/faschinj/previous/hoc_v2.2_tuner/standalone/'];
+path3  = ['/home/faschinj/previous/hoc_v2.2_tuner/standalone/'];
 
-[filename1,nz1,z1,ntimesteps1,numvars1,list_vars1] = header_read([path1,smfile]);
-[filename2,nz2,z2,ntimesteps2,numvars2,list_vars2] = header_read([path2,smfile]);
-[filename3,nz3,z3,ntimesteps3,numvars3,list_vars3] = header_read([path3,smfile]);
-
-% The times over which to average for the comparison
-t1 = 1;
-t2 = 140;
+% Read information from .ctl files
+[filename1,nz1,z1,ntimesteps1,numvars1,list_vars1] = header_read([path1,ctl_file]);
+[filename2,nz2,z2,ntimesteps2,numvars2,list_vars2] = header_read([path2,ctl_file]);
+[filename3,nz3,z3,ntimesteps3,numvars3,list_vars3] = header_read([path3,ctl_file]);
 
 % Loop over all variables
 for var=1:numvars3
     var1 = deblank(list_vars3(var,:));
+    
+    % Get profile for variable averaged over t1 to t2
     sim_profile1 = get_profile(path1,filename1, ...
         numvars1,list_vars1,nz1,t1,t2,var1,1);
     sim_profile2 = get_profile(path2,filename2, ...
@@ -50,18 +57,24 @@ for var=1:numvars3
     sim_profile3 = get_profile(path3,filename3, ...
         numvars3,list_vars3,nz3,t1,t2,var1,1);
 
-    diff1 = (sim_profile2 - sim_profile1);
-    diff2 = (sim_profile3 - sim_profile2);
-    diff3 = (sim_profile3 - sim_profile1);
-
+    % Determine the mean difference between profiles
+    diff1 = abs( mean(sim_profile2 - sim_profile1) );
+    diff2 = abs( mean(sim_profile3 - sim_profile2) );
+    diff3 = abs( mean(sim_profile3 - sim_profile1) );
+    
+    % Determine the tolerance between profiles
+    tolerance1 = abs( mean( [sim_profile2 ; sim_profile1] ) * 1.0*10^(-tol) );
+    tolerance2 = abs( mean( [sim_profile3 ; sim_profile2] ) * 1.0*10^(-tol) );
+    tolerance3 = abs( mean( [sim_profile3 ; sim_profile1] ) * 1.0*10^(-tol) );
+    
 % Print results, if differences exist
-    if (sum(diff1) ~= 0)
-        disp (['File 1 and File 2 disagree in field ',var1]);
+    if ( diff1 > tolerance1 )
+        disp ( ['File 1 and File 2 disagree in field ', var1] );
     end
-    if (sum(diff2) ~= 0)
-        disp (['File 2 and File 3 disagree in field ',var1]);
+    if ( diff2 > tolerance2 )
+        disp ( ['File 2 and File 3 disagree in field ', var1] );
     end
-    if (sum(diff3) ~= 0)
-        disp (['File 1 and File 3 disagree in field ',var1]);
+    if ( diff3 > tolerance3 )
+        disp ( ['File 1 and File 3 disagree in field ', var1] );
     end
 end
