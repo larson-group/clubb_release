816c49fa0ca31d572a1b66ac906d7d47ca7de41d
senkbeir <senkbeir>
2008-07-22 17:39:58 +0000

    Added a script to go from latex to html and pdf.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2519 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 18eaffbe9a82f05f26882cda02cfa0e8cd3b03b1

---
+++ doc/make_latex_docs.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+###############################################################################
+# make_latex_docs.bash
+# Author: Ryan Senkbeil
+#
+# Converts latex documents to html and pdf.
+###############################################################################
+
+# For every file that ends with .tex
+for file in *.tex
+do
+	# Get the filename without the extension
+	BaseFileName=`basename $file .tex`
+
+	# Compile the file
+	latex $file
+
+	# Create a PostScript file
+	dvips -Pcmz -o $BaseFileName.ps $BaseFileName
+
+	# Create PDF file
+	ps2pdf $BaseFileName.ps
+	
+	# Create HTML file
+	latex2html $file
+	# Remove header from index.html
+	vim -E -s $BaseFileName/index.html <<-EOF
+   		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
+		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
+		:/<!--Table of Child-Links-->/,/<!--End of Table of Child-Links-->/d
+   		:update
+   		:quit
+	EOF
+
+	# Move the index file to ../
+	mv $BaseFileName/index.html $BaseFileName.html
+	
+	# Clean up
+	rm -rf $BaseFileName.aux $BaseFileName.dvi $BaseFileName.log $BaseFileName
+done
+
