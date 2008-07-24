7ea9ccfed21b8449da30b20b6ddd5dd6e64ced58
dschanen <dschanen>
2008-07-24 17:12:19 +0000

    Removed carriage returns with dos2unix
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2548 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: a99b9e4c2ea4d94a8850d155468e1a61f1b44aec

--- src/NR/ran_state.f90
+++ src/NR/ran_state.f90
@@ -1,4 +1,4 @@
-! $Id: ran_state.f90,v 1.1 2008-07-24 16:51:57 dschanen Exp $
+! $Id: ran_state.f90,v 1.2 2008-07-24 17:12:19 dschanen Exp $
 !   From _Numerical Recipes in Fortran 90_
 !   (C) 1988-1996 Numerical Recipes Software
 MODULE ran_state
