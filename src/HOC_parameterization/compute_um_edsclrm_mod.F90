d97fe4f56ac57582b58d5f83577534e1274492c9
griffinb <griffinb>
2008-08-13 16:52:10 +0000

    I added a complete description to compute_um_edsclrm_mod.F90.  I also update the _lhs and _rhs subroutines accordingly, including adding stats to the _rhs subroutine.  This code is still not hooked up to the rest of the code.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2723 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: b3e38b2a9919d9f62047b36955b0d6b5f3c93b40

--- src/HOC_parameterization/compute_um_edsclrm_mod.F90
+++ src/HOC_parameterization/compute_um_edsclrm_mod.F90
@@ -1,5 +1,5 @@
 !------------------------------------------------------------------------
-! $Id: compute_um_edsclrm_mod.F90,v 1.10 2008-08-11 19:29:03 griffinb Exp $
+! $Id: compute_um_edsclrm_mod.F90,v 1.11 2008-08-13 16:52:10 griffinb Exp $
 !------------------------------------------------------------------------
 module compute_um_edsclrm_mod
 
@@ -17,18 +17,175 @@ subroutine compute_um_edsclrm( solve_type, xpwp_sfc, &
                                Kh_zm, dt,   &
                                xm, xpwp,  &
                                err_code )
-!       Description:
-!         Prognoses a horizontal wind component or other 
-!         eddy diffusivity variable.
-!         Diagnoses the turbulent flux.
-!         Uses a Crank-Nicholson time-stepping algorithm.
 
-!       References:
-!       Eqn. 8 & 9 on p. 3545 of 
-!       ``A PDF-Based Model for Boundary Layer Clouds. Part I:
-!         Method and Model Description'' Golaz, et al. (2002)
-!       JAS, Vol. 59, pp. 3540--3551.
-!------------------------------------------------------------------------
+! Description:
+! Solves the horizontal wind or eddy-scalar time-tendency equation, and 
+! diagnoses the turbulent flux.  A Crank-Nicholson time-stepping algorithm is
+! used in solving the turbulent advection term and in diagnosing the turbulent
+! flux.
+!
+! The rate of change of an eddy-scalar quantity, xm, is:
+!
+! d(xm)/dt = - w * d(xm)/dz - d(x'w')/dz + xm_forcings.
+!
+!
+! The Turbulent Advection Term
+! ----------------------------
+!
+! The above equation contains a turbulent advection term:
+!
+! - d(x'w')/dz;
+!
+! where the momentum flux, x'w', is closed using a down gradient approach:
+!
+! x'w' = - K_zm * d(xm)/dz.
+!
+! The turbulent advection term becomes:
+!
+! + d [ K_zm * d(xm)/dz ] / dz;
+!
+! which is the same as an eddy-diffusion term.  Thus, the turbulent advection
+! term is treated and solved in the same way that an eddy-diffusion term would 
+! be solved.  The term is discretized as follows:
+!
+! The values of xm are found on the thermodynamic levels, while the values of 
+! K_zm are found on the momentum levels.  The derivatives (d/dz) of xm are taken
+! over the intermediate momentum levels.  At the intermediate momentum levels, 
+! d(xm)/dz is multiplied by K_zm.  Then, the derivative of the whole 
+! mathematical expression is taken over the central thermodynamic level, which 
+! yields the desired result.
+!
+! ---xm(kp1)----------------------------------------------- t(k+1)
+!
+! =============d(xm)/dz===K_zm(k)========================== m(k)
+!
+! ---xm(k)---------------------------d[K_zm*d(xm)/dz]/dz--- t(k)
+!
+! =============d(xm)/dz===K_zm(km1)======================== m(k-1)
+!
+! ---xm(km1)----------------------------------------------- t(k-1)
+!
+! The vertical indices t(k+1), m(k), t(k), m(k-1), and t(k-1) correspond with 
+! altitudes zt(k+1), zm(k), zt(k), zm(k-1), and zt(k-1), respectively.  The 
+! letter "t" is used for thermodynamic levels and the letter "m" is used for 
+! momentum levels.
+!
+! dzt(k)   = 1 / ( zm(k) - zm(k-1) )
+! dzm(k)   = 1 / ( zt(k+1) - zt(k) )
+! dzm(k-1) = 1 / ( zt(k) - zt(k-1) )
+!
+! The vertically discretized form of the term is written out as:
+!
+! + dzt(k) * [   K_zm(k) * dzm(k) * ( xm(k+1) - xm(k) ) 
+!              - K_zm(k-1) * dzm(k-1) * ( xm(k) - xm(k-1) ) ].
+!
+! For this equation, a Crank-Nicholson (semi-implicit) diffusion scheme is used
+! to solve the d [ K_zm * d(xm)/dz ] / dz eddy-diffusion term.  The discretized 
+! implicit form of the term is written out as:
+!
+! + (1/2)*dzt(k) * [   K_zm(k) * dzm(k) * ( xm(k+1,<t+1>) - xm(k,<t+1>) )
+!                    - K_zm(k-1) * dzm(k-1) * ( xm(k,<t+1>) - xm(k-1,<t+1>) ) ].
+!
+! Note:  When the implicit term is brought over to the left-hand side, the 
+!        sign is reversed and the leading "+" in front of the term is changed 
+!        to a "-".
+!
+! The discretized explicit form of the term is written out as:
+!
+! + (1/2)*dzt(k) * [   K_zm(k) * dzm(k) * ( xm(k+1,<t>) - xm(k,<t>) )
+!                    - K_zm(k-1) * dzm(k-1) * ( xm(k,<t>) - xm(k-1,<t>) ) ].
+!
+! Timestep index (t) stands for the index of the current timestep, while 
+! timestep index (t+1) stands for the index of the next timestep, which is being
+! advanced to in solving the d(xm)/dt equation.
+!
+!
+! Boundary Conditions:
+!
+! An eddy-scalar quantity is not allowed to flux out the upper boundary.  Thus,
+! a zero-flux boundary condition is used for the upper boundary in the eddy
+! diffusion equation.
+!
+! The lower boundary condition is much more complicated.  It is neither a 
+! zero-flux nor a fixed-point boundary condition.  Rather, it is a fixed-flux 
+! boundary condition.  This term is a turbulent advection term, but with the
+! eddy-scalars, the only value of x'w' relevant in solving the d(xm)/dt equation
+! is the value of x'w' at the surface (written as x'w'|_sfc).
+!
+! Since x'w' = - K_zm * d(xm)/dz,
+!
+! x'w'|_sfc = - K_zm(1) * dzm(1) * ( xm(2) - xm(1) )
+!
+! The lower boundary condition, which in this case is applied to the d(xm)/dt 
+! equation at level 2, is discretized as follows:
+!
+! ---xm(3)------------------------------------------------- t(3)
+!
+! =============d(xm)/dz===K_zm(2)========================== m(2)
+!
+! ---xm(2)---------------------------d[K_zm*d(xm)/dz]/dz--- t(2)
+!
+! =============[ x'w'|_sfc = - K_zm(1) * d(xm)/dz ]======== m(1) (surface)
+!
+! ---xm(1)------------------------------------------------- t(1)
+! 
+! The vertically discretized form of the term is written out as:
+!
+! + dzt(2) * [ K_zm(2) * dzm(2) * ( xm(3) - xm(2) ) + x'w'|_sfc ];
+!
+! which can be re-written as:
+!
+! + dzt(2) * [ K_zm(2) * dzm(2) * ( xm(3) - xm(2) ) ]  +  dzt(2) * x'w'|_sfc ].
+!
+! For this equation, a Crank-Nicholson (semi-implicit) diffusion scheme is used
+! to solve the d [ K_zm * d(xm)/dz ] / dz eddy-diffusion term.  The discretized
+! implicit form of the term is written out as:
+!
+! + (1/2)*dzt(2) * [ K_zm(2) * dzm(2) * ( xm(3,<t+1>) - xm(2,<t+1>) ) ].
+!
+! Note:  When the implicit term is brought over to the left-hand side, the
+!        sign is reversed and the leading "+" in front of the term is changed
+!        to a "-".
+!
+! The discretized explicit form of the term is written out as:
+!
+! + (1/2)*dzt(2) * [ K_zm(2) * dzm(2) * ( xm(3,<t>) - xm(2,<t>) ) ]
+!    + dzt(2) * x'w'|_sfc.
+!
+! Note:  The x'w'|_sfc portion of the term written above has been pulled away 
+!        from the rest of the explicit form written above because the (1/2) 
+!        factor due to Crank-Nicholson time_stepping does not apply to it, as 
+!        there isn't an implicit portion for x'w'|_sfc.
+!
+! Timestep index (t) stands for the index of the current timestep, while
+! timestep index (t+1) stands for the index of the next timestep, which is being
+! advanced to in solving the d(xm)/dt equation.
+!
+! The lower boundary condition for the implicit and explicit portions of the 
+! term, without the x'w'|_sfc portion of the term, can easily be invoked by 
+! using the zero-flux boundary conditions found in the generalized diffusion 
+! function (function diffusion_zt_lhs) used for many other equations in this 
+! model.  The dzt(2) * x'w'|_sfc term needs to be added onto the explicit term
+! after this function has been called.  However, all other equations in this 
+! model that use zero-flux diffusion have level 1 as the level to which the
+! lower boundary condition is applied.  Thus, an adjuster will have to be used
+! at level 2 to call diffusion_zt_lhs with level 1 as the input level (the last
+! variable being passed in during the function call).  However, the other
+! variables passed in (K_zm, gr%dzt, and gr%dzm variables) will have to be 
+! passed in as solving for level 2.
+!
+! The value of xm(1) will just be set equal to itself and not effect the rest
+! of the xm array during solving.  However, the value of xm(1) can be found
+! after solving, since x'w'|_sfc = - K_zm(1) * dzm(1) * ( xm(2) - xm(1) ):
+!
+! xm(1)  =  xm(2)  +  x'w'|_sfc / ( K_zm(1) * dzm(1) ).
+
+! References:
+! Eqn. 8 & 9 on p. 3545 of 
+! ``A PDF-Based Model for Boundary Layer Clouds. Part I:
+!   Method and Model Description'' Golaz, et al. (2002)
+! JAS, Vol. 59, pp. 3540--3551.
+!-----------------------------------------------------------------------
 
 use grid_class, only: & 
     gr ! Variable(s)
@@ -414,29 +571,9 @@ end subroutine compute_uv_tndcy
 subroutine compute_um_edsclrm_lhs( solve_type, dt, wm_zt, Kh_zm, lhs )
 
 ! Description:
-! Calculate the implicit portion of the eddy-scalar time-tendency equation.
-!
-! The rate of change of an eddy-scalar quantity, xm, is:
-!
-! d(xm)/dt  =  - w * d(xm)/dz  -  d(x'w')/dz  +  xm_forcings;
-!
-! where the momentum flux, x'w', is calculated as:
-!
-! x'w'  =  - K * d(xm)/dz.
-!
-! The first equation becomes:
-!
-! d(xm)/dt  =  - w * d(xm)/dz  +  d [ K * d(xm)/dz ] / dz  +  xm_forcings.
-!
-! A Crank-Nicholson (semi-implicit) diffusion scheme is used to solve the 
-! d [ K * d(xm)/dz ] / dz diffusion term.
-!
-! However, at the lower boundary level (usually at the surface), the first 
-! equation becomes:
-!
-! ( d(xm)/dt )|_sfc  =  - d( x'w'|_sfc ) / dz.
-!
-! At the lower boundary level, this term is treated completely explicitly.
+! Calculate the implicit portion of the horizontal wind or eddy-scalar 
+! time-tendency equation.  See the description in subroutine compute_um_edsclrm
+! for more details.
 
 ! References:
 !-----------------------------------------------------------------------
@@ -491,6 +628,7 @@ real, dimension(3,gr%nnzp), intent(out) :: &
 
 ! Local Variables
 integer :: k, km1  ! Array indices
+integer :: diff_k_in
 
 real, dimension(3) :: tmp
 integer :: ixm_ma, ixm_ta
@@ -520,11 +658,18 @@ do k = 2, gr%nnzp-1, 1
    = lhs(kp1_tdiag:km1_tdiag,k)  &
    + term_ma_zt_lhs( wm_zt(k), gr%dzt(k), k )
 
-   ! LHS eddy diffusion of xm term (or the turbulent advection of x'w' term).
+   ! LHS turbulent advection term (solved as an eddy-diffusion term).
+   if ( k == 2 ) then
+      ! The lower boundary condition needs to be applied here at level 2.
+      diff_k_in = 1
+   else
+      diff_k_in = k
+   endif
    lhs(kp1_tdiag:km1_tdiag,k)  &
    = lhs(kp1_tdiag:km1_tdiag,k)  &
-   + (1.0/2.0) * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0, & 
-                                   gr%dzm(km1), gr%dzm(k), gr%dzt(k), k )
+   + (1.0/2.0)  &
+   * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0,  & 
+                       gr%dzm(km1), gr%dzm(k), gr%dzt(k), diff_k_in )
 
    ! LHS time tendency.
    lhs(k_tdiag,k)  &
@@ -543,9 +688,10 @@ do k = 2, gr%nnzp-1, 1
       endif
 
       if ( ixm_ta > 0 ) then
-         tmp(1:3) &
-         = (1.0/2.0) * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0, &
-                                         gr%dzm(km1), gr%dzm(k), gr%dzt(k), k )
+         tmp(1:3)  &
+         = (1.0/2.0)  &
+         * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0,  &
+                             gr%dzm(km1), gr%dzm(k), gr%dzt(k), diff_k_in )
          ztscr04(k) = -tmp(3)
          ztscr05(k) = -tmp(2)
          ztscr06(k) = -tmp(1)
@@ -570,33 +716,13 @@ return
 end subroutine compute_um_edsclrm_lhs
 
 !===============================================================================
-subroutine compute_um_edsclrm_rhs( dt, Kh_zm, xm, xm_forcing, &
-                                   xpwp_sfc, rhs )
+subroutine compute_um_edsclrm_rhs( solve_type, dt, Kh_zm, xm,  &
+                                   xm_forcing, xpwp_sfc, rhs )
 
 ! Description:
-! Calculate the implicit portion of the eddy-scalar time-tendency equation.
-!
-! The rate of change of an eddy-scalar quantity, xm, is:
-!
-! d(xm)/dt  =  - w * d(xm)/dz  -  d(x'w')/dz  +  xm_forcings;
-!
-! where the momentum flux, x'w', is calculated as:
-!
-! x'w'  =  - K * d(xm)/dz.
-!
-! The first equation becomes:
-!
-! d(xm)/dt  =  - w * d(xm)/dz  +  d [ K * d(xm)/dz ] / dz  +  xm_forcings.
-!
-! A Crank-Nicholson (semi-implicit) diffusion scheme is used to solve the 
-! d [ K * d(xm)/dz ] / dz diffusion term.
-!
-! However, at the lower boundary level (usually at the surface), the first 
-! equation becomes:
-!
-! ( d(xm)/dt )|_sfc  =  - d( x'w'|_sfc ) / dz.
-!
-! At the lower boundary level, this term is treated completely explicitly.
+! Calculate the explicit portion of the horizontal wind or eddy-scalar 
+! time-tendency equation.  See the description in subroutine compute_um_edsclrm
+! for more details.
 
 ! References:
 !-----------------------------------------------------------------------
@@ -610,9 +736,22 @@ use stats_precision, only:  &
 use diffusion, only:  & 
     diffusion_zt_lhs ! Procedure(s)
 
+use stats_variables, only: &
+    ium_ta,  & ! Variable(s)
+    ivm_ta,  &
+    zt,      &
+    l_stats_samp
+
+use stats_type, only: &
+    stat_begin_update_pt,  & ! Procedure(s)
+    stat_modify_pt
+
 implicit none
 
 ! Input Variables
+character(len=*), intent(in) :: &
+  solve_type ! Desc. of what is being solved for
+
 real(kind=time_precision), intent(in) :: & 
   dt           ! Model timestep                                   [s]
 
@@ -630,9 +769,21 @@ real, dimension(gr%nnzp), intent(out) :: &
 
 ! Local Variables
 integer :: k, kp1, km1  ! Array indices
+integer :: diff_k_in
 
 ! For use in Crank-Nicholson eddy diffusion.
 real, dimension(3) :: rhs_diff
+
+integer :: ixm_ta
+
+select case ( trim( solve_type ) )
+   case ( "um" )
+      ixm_ta = ium_ta
+   case ( "vm" )
+      ixm_ta = ivm_ta
+   case default  ! Eddy scalars
+      ixm_ta = 0
+end select
 
 
 ! Initialize the RHS vector.
@@ -644,10 +795,17 @@ do k = 2, gr%nnzp-1, 1
    km1 = max( k-1, 1 )
    kp1 = min( k+1, gr%nnzp )
 
-   ! RHS eddy diffusion of xm term (or the turbulent advection of x'w' term).
-   rhs_diff(1:3) & 
-   = (1.0/2.0) * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0, &
-                                   gr%dzm(km1), gr%dzm(k), gr%dzt(k), k )
+   ! RHS turbulent advection term (solved as an eddy-diffusion term).
+   if ( k == 2 ) then
+      ! The lower boundary condition needs to be applied here at level 2.
+      diff_k_in = 1
+   else
+      diff_k_in = k
+   endif
+   rhs_diff(1:3)  & 
+   = (1.0/2.0)  &
+   * diffusion_zt_lhs( Kh_zm(k), Kh_zm(km1), 0.0,  &
+                       gr%dzm(km1), gr%dzm(k), gr%dzt(k), diff_k_in )
    rhs(k)   =   rhs(k) & 
               - rhs_diff(3) * xm(km1) &
               - rhs_diff(2) * xm(k)   &
@@ -659,15 +817,38 @@ do k = 2, gr%nnzp-1, 1
    ! RHS time tendency
    rhs(k) = real( rhs(k) + ( 1.0 / dt ) * xm(k) )
 
+   if ( l_stats_samp ) then
+
+      ! Statistics:  explicit contributions for um or vm.
+
+      if ( ixm_ta > 0 ) then
+         call stat_begin_update_pt( ixm_ta, k, & 
+                rhs_diff(3) * xm(km1) &
+              + rhs_diff(2) * xm(k)   &
+              + rhs_diff(1) * xm(kp1), zt )
+      endif
+
+   endif  ! lstats_samp
+
 enddo
 
 
 ! Boundary Condition
 ! Lower Boundary
-! The surface value of x'w' is computed elsewhere in the code.  If the value of 
-! xm at the surface is positive, x'w' at the surface will be negative, and 
-! vice-versa.
-rhs(1) = + gr%dzt(1) * xpwp_sfc  +  real( ( 1.0 / dt ) * xm(1) )
+rhs(2) = rhs(2) + gr%dzt(2) * xpwp_sfc
+rhs(1) = real( ( 1.0 / dt ) * xm(1) )
+
+if ( l_stats_samp ) then
+
+   ! Statistics:  explicit contributions for um or vm.
+
+   if ( ixm_ta > 0 ) then
+      call stat_modify_pt( ixm_ta, 2, &
+           + gr%dzt(2) * xpwp_sfc, zt )
+   endif
+
+endif  ! lstats_samp
+
 
 ! Upper Boundary
 rhs(gr%nnzp) = real( ( 1.0 / dt ) * xm(gr%nnzp) )
