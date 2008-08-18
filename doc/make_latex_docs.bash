c7837fc100411705fba12aaba6064d65ac221749
dschanen <dschanen>
2008-08-18 17:53:48 +0000

    Added a rule about starting the executable part of the code with a !----- Begin Code ----- to make the code more clear.
    I also modified the portion of the document describing indenting so that in
    pdf at least, you can see the spacing with underscores.
    
    Finally, I changed the script to call tth instead of latex2html.  This will
    produce a better single html file from a text file than vim editing the
    index file produced by latex2html (including rendering p0 without a gif).
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@2732 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: d795278e153e611e0d644d5036d907658cec0ae0

--- doc/make_latex_docs.bash
+++ doc/make_latex_docs.bash
@@ -32,18 +32,21 @@ for file in *.tex ; do
 	ps2pdf $BaseFileName.ps
 
 	# Create HTML file
-	latex2html $file
+	# -dschanen changed this to use TtH rather than latex2html
+	# 18 Aug 2008
+#	latex2html $file
+ 	tth $file
 	# Remove header from index.html
-	vim -E -s $BaseFileName/index.html <<-EOF
-  		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
-		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
-		:/<!--Table of Child-Links-->/,/<!--End of Table of Child-Links-->/d
-   		:update
-   		:quit
-	EOF
+#	vim -E -s $BaseFileName/index.html <<-EOF
+#  		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
+#		:/<!--Navigation Panel-->/,/<!--End of Navigation Panel-->/d
+#		:/<!--Table of Child-Links-->/,/<!--End of Table of Child-Links-->/d
+#   		:update
+#   		:quit
+#	EOF
 
 	# Move the index file to ../
-	mv $BaseFileName/index.html $BaseFileName.html
+#	mv $BaseFileName/index.html $BaseFileName.html
 	
 	# Clean up
 	rm -rf $BaseFileName.aux $BaseFileName.dvi $BaseFileName.log $BaseFileName
