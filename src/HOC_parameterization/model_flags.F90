aab08dc7a961325ceca64a060f0f47dd68a58f74
faschinj <faschinj>
2008-08-26 13:46:57 +0000

    Fixed indentation.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/hoc_v2.2_tuner/trunk@2805 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 86ec57087d736ecc71f8cf670f613f0aaee6c3b4

--- src/HOC_parameterization/model_flags.F90
+++ src/HOC_parameterization/model_flags.F90
@@ -29,27 +29,27 @@ module model_flags
   logical, public :: & 
     l_bugsrad,       & ! BUGSrad interactive radiation scheme
     l_kk_rain,       & ! Khairoutdinov and Kogan (2000) drizzle scheme. - Brian
-    l_icedfs,       & ! Simplified ice scheme
+    l_icedfs,        & ! Simplified ice scheme
     l_coamps_micro,  & ! COAMPS rain microphysics
     l_cloud_sed,     & ! Cloud water droplet sedimentation. - Brian
     l_uv_nudge,      & ! For wind speed nudging. - Michael Falk
-    l_tke_aniso        ! For anisotropic turbulent kinetic energy, 
+    l_tke_aniso        ! For anisotropic turbulent kinetic energy,
                        !   i.e. TKE = 1/2 (u'^2 + v'^2 + w'^2)
 
-!$omp threadprivate(l_bugsrad, l_kk_rain, l_icedfs)
-!$omp threadprivate(l_coamps_micro, l_cloud_sed, l_uv_nudge)
-!$omp threadprivate(l_tke_aniso)
+    !$omp threadprivate(l_bugsrad, l_kk_rain, l_icedfs)
+    !$omp threadprivate(l_coamps_micro, l_cloud_sed, l_uv_nudge)
+    !$omp threadprivate(l_tke_aniso)
 
-  contains
-!-----------------------------------------------------------------------
+contains
+  !-----------------------------------------------------------------------
   subroutine setup_model_flags & 
              ( l_bugsrad_in, l_kk_rain_in, l_cloud_sed_in,  & 
                l_icedfs_in, l_coamps_micro_in, & 
                l_uv_nudge_in, l_tke_aniso_in )
 
-! Description:
-!   Setup model flags
-!-----------------------------------------------------------------------
+  ! Description:
+  !   Setup model flags
+  !-----------------------------------------------------------------------
     use constants, only:  & 
       fstderr ! Variable(s)
 
@@ -61,7 +61,7 @@ module model_flags
       l_icedfs_in, l_coamps_micro_in, l_uv_nudge_in, & 
       l_tke_aniso_in
 
-!-----------------------------------------------------------------------
+    !-----------------------------------------------------------------------
     l_bugsrad      = l_bugsrad_in
     l_kk_rain      = l_kk_rain_in
     l_cloud_sed    = l_cloud_sed_in
@@ -70,10 +70,10 @@ module model_flags
     l_uv_nudge     = l_uv_nudge_in
     l_tke_aniso    = l_tke_aniso_in
 
-        ! Make sure only one microphysical scheme is enabled.
-!       if ( .not.( l_kk_rain .and. l_coamps_micro ) .and. &
-!            .not.( l_kk_rain .and. l_icedfs ) .and. &
-!            .not.( l_icedfs .and. l_coamps_micro ) ) then
+    ! Make sure only one microphysical scheme is enabled.
+    !if ( .not.( l_kk_rain .and. l_coamps_micro ) .and. &
+    !     .not.( l_kk_rain .and. l_icedfs ) .and. &
+    !     .not.( l_icedfs .and. l_coamps_micro ) ) then
 
     if ( count( (/l_kk_rain, l_coamps_micro, l_icedfs/) ) > 1 ) then
 
