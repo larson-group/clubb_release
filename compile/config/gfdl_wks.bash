7470fffb34f3f0369729912ede8354235b5c060e
dschanen <dschanen>
2009-01-23 22:36:25 +0000

    Initial setup for mkmf.  While everything seems to work there are two things left to do: 1) The warnings are not enabled yet 2) All the old config files need to be converted over to bash scripts.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/hoc_v2.2_tuner/trunk@3189 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 8e1101c691a59dd006998c0a857426415f1e427b

---
+++ compile/config/gfdl_wks.bash
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------------
+#
+# Machine specific configuration to compile CLUBB on GFDL workstations:
+#
+# - Intel fortran compiler version 9.1.045
+# - netcdf library 3.6.3 compiled using compiled using Intel C++ and Fortran
+#   compilers version 9.1.045. The library is located in
+#   /home/cjg/local/desktop/netcdf
+# - standard lapack and blas libraries located in /usr/lib
+#
+# ------------------------------------------------------------------------------
+
+# Load desired version of Intel compiler
+source /usr/local/intel/fc/9.1.045/bin/ifortvars.sh
+
+# Fortran 90 compiler and linker
+FC=ifort
+LD=ifort
+
+# Define include directories. 
+# Need location of include and *.mod files for the netcdf library
+INCLUDE="-I/home/cjg/local/desktop/netcdf/include"
+
+# Compilation flags: optimization, architecture, etc.
+FFLAGS="-O2"
+#FFLAGS="-g"
+
+# Linker flags: specifiy location and libraries for CLUBB, netcdf and LAPACK
+LDFLAGS="-L../lib -lhoc_bugsrad -lhoc_param \
+ -L/home/cjg/local/desktop/netcdf/lib -lnetcdf \
+ -L/usr/lib -llapack -lblas"
+
+# Define path to directories
+dir="/home/cjg/clubb/clubb_dev/scripts"      # dir where this script resides
+bindir="/home/cjg/clubb/clubb_dev/code/bin"  # dir for Makefile and executable
+objdir="/home/cjg/clubb/clubb_dev/code/obj"  # dir for *.o and *.mod files
+libdir="/home/cjg/clubb/clubb_dev/code/lib"  # dir for *.a library files
+
+# Directories that contain modified source code. If a file is present
+# under both src/ and mods/ the one in mods/ will take precedence when
+# building tha makefile. Useful for development purposes.
+hoc_param_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_param"
+hoc_standalone_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_standalone"
+
+# Location of 'mkmf' utility
+mkmf=$dir/mkmf
+
+# gmake command to use and options: '-j 2' enables parallel compilation
+gmake="gmake -j 2"
+
---
+++ compile/config/linux_ia32_g95_debug.bash
@@ -0,0 +1,89 @@
+# Configuration file for Linux machine using g95
+# An MS Windows machine with MSYS or Cygwin should be similar
+
+# Fortran 95 compiler and linker
+FC=g95
+LD=g95
+
+# Define path to directories
+dir="$HOME/clubb/compile" # dir where this script resides
+bindir="$HOME/clubb/bin"  # dir for Makefile and executable
+objdir="$HOME/clubb/obj"  # dir for *.o and *.mod files
+libdir="$HOME/clubb/lib"  # dir for *.a library files
+srcdir="$HOME/clubb/src"  # dir where the source files reside
+
+
+# It is sometimes helpful to turn on floating-point trapping for the 
+#  standalone program, but this will not work when using the tuner.
+# In g95: `setenv G95_FPU_INVALID TRUE' or `export G95_FPU_INVALID=TRUE'
+# These are the options for debugging symbols, bounds checking, IEEE-754,
+# and a special g95 flag for debugging, respectively. 
+DEBUG="-g -fbounds-check -mieee-fp -ftrace=full"
+
+# == Warnings ==
+# This is the set of preferred warnings to use when compiling CLUBB with g95.
+# Warnings ignored are:
+# 	142 - Nonblock DO statement is obsolescent 
+# 	165 - Implicit interface is called
+# 	167 - PRIVATE module procedure is never invoked
+WARNINGS="-Wall -Wextra -Wno=142,165,167 -pedantic"
+
+# == Machine specific flags ==
+# Note: some of these are 64 bit architectures, so make sure NetCDF is
+# compiled accordingly.
+#ARCH="-march=pentium4 -msse2 -mfpmath=sse" # Old P4s
+#ARCH="-march=nocona -msse3 -mfpmath=sse" # New P4s
+#ARCH="-march=nocona -msse3 -mfpmath=sse -r8"# New P4s, double precision
+ARCH="-march=k8 -msse3 -mfpmath=sse" # New Opterons
+
+# == Optimization ==
+# These are all pretty conservative options. Check your compiler manual
+# for information on using more aggressive techniques (inlining, etc.)
+OPTIMIZE="-O3"
+
+# == NETCDF library ==
+NETCDF="/usr/local/netcdf-g95"
+
+# == LAPACK libraries ==
+#LAPACK="-L/usr/local/lib -llapack -lblas" #  The netlib reference LAPACK/BLAS
+LAPACK="-L/usr/lib64 -llapack -L/usr/local/atlas/lib -lf77blas -lcblas -latlas" # ATLAS BLAS (faster)
+
+# == Linking Flags ==
+# Use -s to strip (no debugging); 
+# Use -L<library path> -l<lib> to link in an external library
+# Use -Wl,-rpath <library path> to set a search path for shared libs
+LDFLAGS="-L$libdir -Wl,-rpath,$libdir -lclubb_param -lclubb_bugsrad -L$NETCDF/lib -lnetcdf $LAPACK"
+
+# == Compiler flags ==
+# You will need to `make clean' if you change these
+FFLAGS="$ARCH $DEBUG"
+
+# Preprocessing Directives:
+#   -DNETCDF enables netCDF output
+#   -Dradoffline and -Dnooverlap (see bugsrad documentation)
+# Define include directories. 
+# Need location of include and *.mod files for the netcdf library
+CPPFLAGS="-DNETCDF -I$NETCDF/include -Dnooverlap -Dradoffline"
+
+# == Static library processing ==
+AR=ar
+ARFLAGS=cru
+RANLIB=ranlib
+
+# == Shared library processing ==
+SHARED=$FC
+SHAREDFLAGS="-fPIC -shared"
+
+
+# Directories that contain modified source code. If a file is present
+# under both src/ and mods/ the one in mods/ will take precedence when
+# building tha makefile. Useful for development purposes.
+#clubb_param_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_param"
+#clubb_standalone_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_standalone"
+
+# Location of 'mkmf' utility
+mkmf=$dir/mkmf
+
+# gmake command to use and options: '-j 2' enables parallel compilation
+gmake="gmake -j 2"
+
