d99377d28b67a0009acdc6a7f2e812295d9ba6fd
dschanen <dschanen>
2009-01-26 15:59:34 +0000

    Corrected reference to libhoc_param.a to be libclubb_param.a and made the location of netCDF a variable.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/hoc_v2.2_tuner/trunk@3195 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 748195d8633cdd26032526722e352e20fc6948d6

--- compile/config/gfdl_wks.bash
+++ compile/config/gfdl_wks.bash
@@ -17,30 +17,31 @@ source /usr/local/intel/fc/9.1.045/bin/ifortvars.sh
 FC=ifort
 LD=ifort
 
+# Define path to directories
+dir="/home/cjg/clubb/clubb_dev/scripts"      # dir where this script resides
+bindir="/home/cjg/clubb/clubb_dev/code/bin"  # dir for Makefile and executable
+objdir="/home/cjg/clubb/clubb_dev/code/obj"  # dir for *.o and *.mod files
+libdir="/home/cjg/clubb/clubb_dev/code/lib"  # dir for *.a library files
 # Define include directories. 
 # Need location of include and *.mod files for the netcdf library
-INCLUDE="-I/home/cjg/local/desktop/netcdf/include"
+NETCDF="/home/cjg/local/desktop/netcdf"
+CPPFLAGS="-DNETCDF -I$NETCDF/include -Dnooverlap -Dradoffline"
 
 # Compilation flags: optimization, architecture, etc.
 FFLAGS="-O2"
 #FFLAGS="-g"
 
 # Linker flags: specifiy location and libraries for CLUBB, netcdf and LAPACK
-LDFLAGS="-L../lib -lhoc_bugsrad -lhoc_param \
+LDFLAGS="-L../lib -lclubb_bugsrad -lclubb_param \
  -L/home/cjg/local/desktop/netcdf/lib -lnetcdf \
  -L/usr/lib -llapack -lblas"
 
-# Define path to directories
-dir="/home/cjg/clubb/clubb_dev/scripts"      # dir where this script resides
-bindir="/home/cjg/clubb/clubb_dev/code/bin"  # dir for Makefile and executable
-objdir="/home/cjg/clubb/clubb_dev/code/obj"  # dir for *.o and *.mod files
-libdir="/home/cjg/clubb/clubb_dev/code/lib"  # dir for *.a library files
 
 # Directories that contain modified source code. If a file is present
 # under both src/ and mods/ the one in mods/ will take precedence when
 # building tha makefile. Useful for development purposes.
-hoc_param_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_param"
-hoc_standalone_mods="/home/cjg/clubb/clubb_dev/code/src_mods/hoc_standalone"
+clubb_param_mods="/home/cjg/clubb/clubb_dev/code/src_mods/clubb_param"
+clubb_standalone_mods="/home/cjg/clubb/clubb_dev/code/src_mods/clubb_standalone"
 
 # Location of 'mkmf' utility
 mkmf=$dir/mkmf
