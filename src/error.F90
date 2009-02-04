cdb75a33f0de5f7668174f0aa5a84fd1697fc37c
dschanen <dschanen>
2009-02-04 16:14:22 +0000

    Changed input/tuner/rand_seed.dat to input_misc/tuner/rand_seed.dat.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/hoc_v2.2_tuner/trunk@3219 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ddd5f5961eb1f106d238cbdb728eab2021f72a96

--- src/error.F90
+++ src/error.F90
@@ -303,7 +303,7 @@ module error
     ! Attempt to generate a pseudo-random seed using a file 
     ! generated from /dev/random.  File is an ASCII text file 
     ! and can be edited manually.
-    call read_random_seed( "../input/tuner/rand_seed.dat" )
+    call read_random_seed( "../input_misc/tuner/rand_seed.dat" )
 
   end if  ! l_read_files
   !-----------------------------------------------------------------------
