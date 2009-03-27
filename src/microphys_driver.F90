b8973d0b171bd4d816f3df1173aec0cac07c55cd
vlarson@uwm.edu <vlarson@uwm.edu>
2009-03-27 17:36:34 +0000

    I added a comment that our calculation of thlm_mc after the call to the Morrison microphysics is not entirely consistent, because grid-box mean and within-cloud values are used within the same formula.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3338 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 554fed3ccb7886d40d604cffe6068deecd41e555

--- src/microphys_driver.F90
+++ src/microphys_driver.F90
@@ -850,6 +850,10 @@ module microphys_driver
       ! Update thetal based on absolute temperature
       !   This assumes the Morrison microphysics will only adjust thetal within
       !   cloud, as do the modifications to M2005MICRO_GRAUPEL
+      ! This formula is somewhat inconsistent, because T_in_K is the within-cloud updated
+      !   value from the Morrison scheme, whereas the other quantities are grid-box means.
+      !   This may cause non-conservation of heat. However, it is not easy to fix this 
+      !   without Latin hypercube sampling.
       where ( cf > 0.01 ) 
         thlm_mc = ( T_in_K2thlm( T_in_K, exner, rcm_tmp ) - thlm ) / real( dt ) * cf
       else where
