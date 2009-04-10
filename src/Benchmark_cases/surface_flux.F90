263e6aba8ec0d9e96a9c4c05329e4c6a88b7fbb0
dschanen@uwm.edu <dschanen@uwm.edu>
2009-04-10 01:49:13 +0000

    I re-formatted surface_flux to be consistent with spacing and added coding standards Begin Code comments.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3388 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 8e0923cd28d0badb1d808bd0b1d9dee95afce07b

--- src/Benchmark_cases/surface_flux.F90
+++ src/Benchmark_cases/surface_flux.F90
@@ -1,9 +1,12 @@
 !$Id$
+!-------------------------------------------------------------------------------
 module surface_flux
-!
-!       This module contains generalized subroutines for determining surface
-!       fluxes.
-!
+!  Description:
+!    This module contains generalized subroutines for determining surface
+!    fluxes.
+!  References:
+!    None
+!-------------------------------------------------------------------------------
   implicit none
 
   public :: compute_momentum_flux, compute_ubar, compute_wprtp_sfc, &
@@ -13,93 +16,119 @@ module surface_flux
 
   contains
 
+!-------------------------------------------------------------------------------
   real function compute_ubar( um_sfc, vm_sfc )
-    !
-    !  Description: This function determins the value of ubar
-    !               based on the momentum at the surface.
-    !
-    !-----------------------------------------------------------
-    
+!
+!  Description:
+!    This function determins the value of ubar based on the momentum at the surface.
+!  References:
+!    None
+!-------------------------------------------------------------------------------
+
     implicit none
 
-    ! Input Variable(s)
-    real, intent(in) :: um_sfc, & ! u wind at zt(2) [m/s]
-                        vm_sfc    ! v wind at zt(2) [m/s]
+    ! External
+    intrinsic :: max, sqrt
 
-
+    ! Constant paramter(s)
     real, parameter :: ubmin = 0.25
+
+    ! Input Variable(s)
+    real, intent(in) :: &
+      um_sfc, & ! u wind at zt(2) [m/s]
+      vm_sfc    ! v wind at zt(2) [m/s]
+
+    ! ---- Begin Code ----
 
     compute_ubar = max( ubmin, sqrt( um_sfc**2 + vm_sfc**2 ) )
 
   end function compute_ubar
 
-  !-----------------------------------------------------------
+!-------------------------------------------------------------------------------
   subroutine compute_momentum_flux( um_sfc, vm_sfc, ubar, ustar, &
                                     upwp_sfc, vpwp_sfc )
-    !
-    !  Description: This subroutine computes the momentum fluxes
-    !               upwp_sfc and vpwp_sfc.
-    !
-    !-----------------------------------------------------------
+!
+! Description:
+!   This subroutine computes the momentum fluxes upwp_sfc and vpwp_sfc.
+! References:
+!   None
+!-------------------------------------------------------------------------------
 
     implicit none
 
     ! Input
-    real, intent(in) :: um_sfc, & ! u wind at zt(2) [m/s]
-                        vm_sfc, & ! v wind at zt(2) [m/s]
-                        ustar, &  ! Surface friction velocity [m/s]
-                        ubar      
+    real, intent(in) :: &
+      um_sfc, & ! u wind at zt(2) [m/s]
+      vm_sfc, & ! v wind at zt(2) [m/s]
+      ustar,  & ! Surface friction velocity [m/s]
+      ubar
 
     ! Output
-    real, intent(out) :: upwp_sfc, &  ! Turbulent upward flux of u-momentum [(m/s)^2]
-                         vpwp_sfc      ! Turbulent upward flux of v-momentum [(m/s)^2]
+    real, intent(out) :: &
+      upwp_sfc, &  ! Turbulent upward flux of u-momentum [(m/s)^2]
+      vpwp_sfc     ! Turbulent upward flux of v-momentum [(m/s)^2]
 
-
-
+    ! ---- Begin Code ----
 
     ! Compute momentum fluxes
 
     upwp_sfc = -um_sfc * ustar**2 / ubar
     vpwp_sfc = -vm_sfc * ustar**2 / ubar
 
+    return
   end subroutine compute_momentum_flux
 
+!-------------------------------------------------------------------------------
   real function compute_wpthlp_sfc( Cd, ubar, thlm_sfc, Tsfc, exner_sfc )
-  !
-  !  Description: This function determins the surface flux of heat.
-  !
-  !----------------------------------------------------------------------
+!
+! Description:
+!   This function determins the surface flux of heat.
+! References:
+!   None
+!-------------------------------------------------------------------------------
     implicit none
+
     ! Intent(in)
-    real, intent(in) :: Cd, &     ! Coefficient
-                        ubar, &
-                        thlm_sfc, & ! Theta_l at zt(2) [K]
-                        Tsfc, &     ! Surface temperature [K]
-                        exner_sfc   ! Exner function at surface [-]
+    real, intent(in) :: &
+      Cd,       & ! Coefficient
+      ubar,     &
+      thlm_sfc, & ! Theta_l at zt(2) [K]
+      Tsfc,     & ! Surface temperature [K]
+      exner_sfc   ! Exner function at surface [-]
+
+    ! ---- Begin Code ----
 
     compute_wpthlp_sfc = -Cd * ubar * ( thlm_sfc - Tsfc / exner_sfc )
 
-
+    return
   end function compute_wpthlp_sfc
 
 
-  !-----------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   real function compute_wprtp_sfc ( Cd, ubar, rtm_sfc, adjustment )
-    !
-    !  Description: This function determines the surface flux of moisture.
-    !
-    !--------------------------------------------------------------------
-  use saturation, only:  sat_mixrat_liq
 
-  implicit none
-        ! Input(s)
-        real, intent(in) :: Cd, &
-                            ubar, &
-                            rtm_sfc, &  ! Total Water mixing ratio at zt(2) [kg/kg]
-                            adjustment
+!
+!  Description:
+!    This function determines the surface flux of moisture.
+!  References:
+!    None
+!-------------------------------------------------------------------------------
+    use saturation, only:  sat_mixrat_liq
 
-        compute_wprtp_sfc  = -Cd * ubar * ( rtm_sfc - adjustment )
+    implicit none
 
+    ! Input(s)
+    real, intent(in) :: &
+      Cd, &
+      ubar, &
+      rtm_sfc, &  ! Total Water mixing ratio at zt(2) [kg/kg]
+      adjustment
 
+    ! ---- Begin Code ----
+
+    compute_wprtp_sfc  = -Cd * ubar * ( rtm_sfc - adjustment )
+
+    return
   end function compute_wprtp_sfc
+
 end module surface_flux
