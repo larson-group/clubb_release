53a28ebaa6ac180ba77996cab08168e6a0b51819
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-07-08 18:39:34 +0000

    Variable listing now returned to main script.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3745 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 7664f878f45e1ef908221b49723bf54cd2c3e48c

--- postprocessing/plotgen3/PlotCreator.m
+++ postprocessing/plotgen3/PlotCreator.m
@@ -14,19 +14,25 @@ optargin = size(varargin,2);
 %'/PATH/TO/FILE', 'timeseries or profile', 'varname', 'title', 'units', 'lineWidth', 'lineType', 'lineColor'
 
 %This means we can easily figure out the number of lines on the plot by dividing by 8
-numLines = optargin / 8;
+numLines = optargin / 9;
 
 %Loop through each line
 for i=1:numLines
 	filePath = varargin{1 * i};
 	plotType = varargin{2 * i};
-	varExpression = varargin{3 * i};
-	plotTitle = varargin{4 * i};
-	varUnits = varargin{5 * i};
-	lineWidth = varargin{6 * i};
-	lineType = varargin{7 * i};
-	lineColor = varargin{8 * i};
+	varName = varargin{3 * i};
+	varExpression = varargin{4 * i};
+	plotTitle = varargin{5 * i};
+	varUnits = varargin{6 * i};
+	lineWidth = varargin{7 * i};
+	lineType = varargin{8 * i};
+	lineColor = varargin{9 * i};
 
 	%Determine the variables that need to be read in
 	varsToRead = ParseVariablesFromExpression(varExpression);
+
+	%Read in the necessary variables
+	for j=1:size(varsToRead,2);
+		varsToRead(j)
+	end
 end
