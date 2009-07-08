627f72ef8ee5230aac54b75690a82cebbfb227c2
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-07-08 17:43:24 +0000

    Initial commit of MATLAB script that will generate plots for plotgen3. Currently does nothing but parse arguments.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3743 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 0b6f50ac6cec303565db0ef4d45d9c745545c10c

---
+++ postprocessing/plotgen3/PlotCreator.m
@@ -0,0 +1,29 @@
+function PlotCreator( plotNum, startTime, endTime, startHeight, endHeight, varargin )
+
+%Display the variables that were passed in (debug output)
+plotNum
+startTime
+endTime
+startHeight
+endHeight
+
+%Figure out the number of optional arguments passed in
+optargin = size(varargin,2);
+
+%Optional argument format is as follows:
+%'/PATH/TO/FILE', 'timeseries or profile', 'varname', 'title', 'units', 'lineWidth', 'lineType', 'lineColor'
+
+%This means we can easily figure out the number of lines on the plot by dividing by 8
+numLines = optargin / 8;
+
+%Loop through each line
+for i=1:numLines
+	filePath = varargin{1 * i}
+	plotType = varargin{2 * i}
+	varName = varargin{3 * i}
+	plotTitle = varargin{4 * i}
+	varUnits = varargin{5 * i}
+	lineWidth = varargin{6 * i}
+	lineType = varargin{7 * i}
+	lineColor = varargin{8 * i}
+end
