116de72625aeb43d48a649d2b2ba22124370edb5
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-07-08 21:07:12 +0000

    Added location for MATLAB to output plots.
    Updated to reflect newest argument structure.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3751 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5d202b788f9c44a0a9856bb281639e243a40a4a0

--- postprocessing/plotgen3/PlotCreator.m
+++ postprocessing/plotgen3/PlotCreator.m
@@ -1,7 +1,8 @@
-function PlotCreator( plotNum, startTime, endTime, startHeight, endHeight, varargin )
+function PlotCreator( plotNum, plotType, startTime, endTime, startHeight, endHeight, varargin )
 
 %Display the variables that were passed in (debug output)
 plotNum
+plotType
 startTime
 endTime
 startHeight
@@ -14,19 +15,21 @@ optargin = size(varargin,2);
 %'/PATH/TO/FILE', 'timeseries or profile', 'varname', 'title', 'units', 'lineWidth', 'lineType', 'lineColor'
 
 %This means we can easily figure out the number of lines on the plot by dividing by 8
-numLines = optargin / 9;
+numLines = optargin / 8;
+
+%Create a blank plot of the proper type so we have somewhere to draw lines
+%if strcmp(
 
 %Loop through each line on the plot
 for i=1:numLines
 	filePath = varargin{1 * i};
-	plotType = varargin{2 * i};
-	varName = varargin{3 * i};
-	varExpression = varargin{4 * i};
-	plotTitle = varargin{5 * i};
-	varUnits = varargin{6 * i};
-	lineWidth = varargin{7 * i};
-	lineType = varargin{8 * i};
-	lineColor = varargin{9 * i};
+	varName = varargin{2 * i};
+	varExpression = varargin{3 * i};
+	plotTitle = varargin{4 * i};
+	varUnits = varargin{5 * i};
+	lineWidth = varargin{6 * i};
+	lineType = varargin{7 * i};
+	lineColor = varargin{8 * i};
 
 	%Determine the type of file being read in
 	extension = DetermineExtension(filePath);
@@ -53,6 +56,8 @@ for i=1:numLines
 
 	%Now evaluate the expression using the read in values,
 	eval(['valueToPlot =', varExpression, ';']);
-
+	valueToPlot
 	%At this point, the value of the expression is contained in valueToPlot
+
+
 end
