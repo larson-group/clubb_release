e15e036a3e78009cdeb90dafa44528bad81a220c
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-07-13 21:37:22 +0000

    Relatively untested NetCDF support.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3771 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 75cc33d85b3c5497fff03809b15d2c3f473e3ada

--- postprocessing/plotgen3/VariableReadNC.m
+++ postprocessing/plotgen3/VariableReadNC.m
@@ -1,3 +1,36 @@
 function [varData] = VariableReadNC( variableToRead, filePath )
 
-varData = 1;
+%Read in some necessary information about the GRaDS file
+[dataFileName, nz, z, numTimesteps, dt, numVars, listofparams] = header_read_expanded_netcdf(filePath);
+
+%Calculate start and end timesteps, correct if they are invalid
+t_start = ceil(startTime / dt);
+if ( t_start < 1 )
+	t_start = 1;
+elseif ( t_start > numTimesteps )
+	t_start = numTimesteps;
+end
+
+t_end = ceil(endTime / dt);
+if ( t_end < 1 )
+	t_end = 1;
+elseif ( t_end > numTimesteps )
+	t_end = numTimesteps;
+end
+
+%Append the data file name to the path of the passed in file
+lastSlash =  max(regexp( filePath, '\/' ));
+dataFilePath = filePath(1:lastSlash);
+
+%Set a default value if the passed in variable is not found
+varData(1:nz) = 0;
+levData = 0;
+
+for i = 1:numVars
+	%See if the variable we found is the variable we are interested in
+	if ( strcmp( strtrim(listofparams(i, :)), variableToRead ) )
+		varData = read_grads_hoc_endian([dataFilePath, dataFileName], 'ieee-le', nz, t_start, t_end, i, numVars);
+	end
+end
+
+levData = z;
