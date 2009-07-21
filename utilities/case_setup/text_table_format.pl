27ed162162158fd40e37c78f5b7c912faf67d682
fasching@uwm.edu <fasching@uwm.edu>
2009-07-21 16:27:13 +0000

    I made this file a little more readable.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3827 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: dc45f480253ae867c375e58aeff26106e715c469

--- utilities/case_setup/text_table_format.pl
+++ utilities/case_setup/text_table_format.pl
@@ -23,42 +23,42 @@ foreach $file (@ARGV){
 	close FILE;
 
 	# Delimit table by single spaces (Essentially setting up the 2-D array.
-	foreach $s (@filedata){
-		$s =~ s/^\s+//;	
-        	@{$s} = split /\s+/,$s;
+	foreach $line (@filedata){
+		$line =~ s/^\s+//;      # Remove all leading spaces
+        	@{$line} = split /\s+/,$line; # Split up line into entries, delimited by spaces
 	}
         
 	# Array of the longest element in each column
 	@longest = 0;
         
 	# Iterate through each column determining which element in the column is the longest.
-	for $j (0 .. $#{$filedata[0]}){	
-	  	for $i ( 0 .. $#filedata ) {
-			if(length($filedata[$i][$j]) > $longest[$j]){
-				$longest[$j] = length($filedata[$i][$j]);
+	for $col (0 .. $#{$filedata[0]}){	# For every column
+	  	for $row ( 0 .. $#filedata ) {  # For every row
+			if( length($filedata[$row][$col]) > $longest[$col] ){ 
+				$longest[$col] = length($filedata[$row][$col]);
+			}
 		}
-	}
 
-	# Pad out for approriate spacing between columns
-  	$longest[$j]++;
-  	$longest[$j]++;
-  	$longest[$j]++;
+		# Pad out for approriate spacing between columns
+  		$longest[$col]++;
+  		$longest[$col]++;
+  		$longest[$col]++;
 	}	
 
 	# Open the same file for writing
 	open FILE, ">", $file;
 
 	# Construct the corrected table in memory
-	for $j (0 .. $#{$filedata[0]}){	
-  		for $i ( 0 .. $#filedata ) {
-        		$filedata[$i][$j] = $filedata[$i][$j] . ' ' x ($longest[$j] - length($filedata[$i][$j]))
+	for $col (0 .. $#{$filedata[0]}){	# For every column
+  		for $row ( 0 .. $#filedata ) {  # For every row
+        		$filedata[$row][$col] = $filedata[$row][$col] . ' ' x ($longest[$col] - length($filedata[$row][$col]))
   		}
 	}	
 	
 	# Write out the table to the file
-	foreach $a (@filedata){
-		foreach $b (@{$a}){
-			print FILE "$b";
+	foreach $line (@filedata){
+		foreach $entry (@{$line}){
+			print FILE $entry;
 		}
 		print FILE "\n";
 	}
