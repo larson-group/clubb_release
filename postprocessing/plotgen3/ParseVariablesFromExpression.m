e2b4c952bb7143d74e77d5877337af9c1d473d6f
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-08-03 21:31:34 +0000

    It is now possible to pass in complex MATLAB expressions. This opens up the use of functions such as 'sqrt' as well as to ability to use coefficients and constants in calculations.
    Essentially, now the 'expression' field in the case files can be
    _any_ valid MATLAB expression.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3918 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 248e9b995a2b618d2f9f51ca6ce0d773ada5492b

--- postprocessing/plotgen3/ParseVariablesFromExpression.m
+++ postprocessing/plotgen3/ParseVariablesFromExpression.m
@@ -1,3 +1,21 @@
 function [variables] = ParseVariablesFromExpression( expression )
 
-variables =  regexp( expression, '[A-Za-z0-9_]*', 'match' );
+varsToCheck =  regexp( expression, '[A-Za-z0-9_]*', 'match' );
+
+variables = [];
+
+
+%Now, loop through all the variables found, if it is not a
+%MATLAB keyword, assume it is a variable and add it to the list
+for i=1:size(varsToCheck,2)
+	%Current use contains a collection of all of the current
+	%ways a variable is already in use, this keeps us from
+	%interpreting something like 'sqrt' as a variable
+	currentUse = which('-all', char(varsToCheck(i)));
+
+	%Make sure the parsed out variable name is both a valid variable
+	%name and not already in use
+	if (isvarname(char(varsToCheck(i))) && isempty(currentUse))
+		variables = [variables varsToCheck(i)];
+	end
+end
