85868187eb5ed9942e4f947b1c1a4b12190002aa
bmg2@uwm.edu <bmg2@uwm.edu>
2009-08-05 06:56:40 +0000

    I added file spec_hum_to_mixing_ratio.F90, which contains two subroutines. The first is subroutine flux_spec_hum_to_mixing_ratio, which takes a flux given in terms of specific humidity, w'q_t', and converts it to a flux in terms of mixing ratio, w'r_t'.  The second is subroutine force_spec_hum_to_mixing_ratio, which takes a forcing given in terms of specific humidity, d(q_tm)/dt|_f, and converts it to a forcing in terms of mixing ratio, d(r_tm)/dt|_f.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3931 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 879f1ca4671fe833bb620b7a781925f6d03e42c0

---
+++ src/Benchmark_cases/spec_hum_to_mixing_ratio.F90
@@ -0,0 +1,95 @@
+!-----------------------------------------------------------------------
+! $Id$
+!===============================================================================
+module spec_hum_to_mixing_ratio
+
+  ! Description:
+  ! This module contains subroutines that take various moisture variables that
+  ! were given in terms of specific humidity and converts them to terms of
+  ! mixing ratio.
+
+  implicit none
+
+  private ! Default Scope
+
+  public :: flux_spec_hum_to_mixing_ratio, &
+            force_spec_hum_to_mixing_ratio
+
+  contains
+
+  !=============================================================================
+  subroutine flux_spec_hum_to_mixing_ratio( rtm_zm, wpqtp, wprtp )
+
+    ! Description:
+    ! This subroutine takes a flux given in terms of specific humidity, w'q_t',
+    ! and converts it to a flux given in terms of mixing ratio, w'r_t'.
+    !
+    ! This relationship is given by the equation:
+    !
+    ! w'r_t' = ( 1 + r_tm )^2 * w'q_t';
+    !
+    ! where r_tm is the mean total water mixing ratio.  Higher-order terms,
+    ! which were comparatively small in magnitude, were neglected for purposes
+    ! of linearizing the equation.
+    !
+    ! This subroutine has been written very generally, in order to allow for
+    ! this conversion at any vertical level.  However, the most likely
+    ! application of this is at the surface, taking a specified surface flux in
+    ! terms of specific humidity, w'q_t'|_sfc, and converting it to a surface
+    ! flux in terms of mixing ratio, w'r_t'|_sfc.
+    
+    !-------------------------------------------------------------------
+
+    implicit none
+
+    ! Input Variables
+    real, intent(in) ::  &
+      rtm_zm, & ! r_tm interpolated to momentum level (k)   [kg/kg]
+      wpqtp     ! w'q_t' flux at momentum level (k)         [(kg/kg) m/s]
+
+    ! Output Variable
+    real, intent(out) ::  &
+      wprtp     ! w'r_t' flux at momentum level (k)         [(kg/kg) m/s]
+
+    ! Solve for flux in terms of total water mixing ratio.
+    wprtp = ( 1.0 + rtm_zm )**2 * wpqtp
+
+    return
+  end subroutine flux_spec_hum_to_mixing_ratio
+
+  !=============================================================================
+  subroutine force_spec_hum_to_mixing_ratio( rtm, qtm_forcing, rtm_forcing )
+
+    ! Description:
+    ! This subroutine takes a forcing given in terms of specific humidity,
+    ! d(q_tm)/dt|_f, and converts it to a forcing given in terms of mixing
+    ! ratio, d(r_tm)/dt|_f.
+    !
+    ! This relationship is given by the equation:
+    !
+    ! d(r_tm)/dt|_f = ( 1 + r_tm )^2 * d(q_tm)/dt|_f;
+    !
+    ! where r_tm is the mean total water mixing ratio.
+
+    !-------------------------------------------------------------------
+
+    implicit none
+
+    ! Input Variables
+    real, intent(in) ::  &
+      rtm,        & ! r_tm at thermodynamic level (k)            [kg/kg]
+      qtm_forcing   ! d(q_tm)/dt|_f at thermodynamic level (k)   [(kg/kg)/s]
+
+    ! Output Variable
+    real, intent(out) ::  &
+      rtm_forcing   ! d(r_tm)/dt|_f at thermodynamic level (k)   [(kg/kg)/s]
+
+    ! Solve for forcing in terms of total water mixing ratio.
+    rtm_forcing = ( 1.0 + rtm )**2 * qtm_forcing
+
+    return
+  end subroutine force_spec_hum_to_mixing_ratio
+
+!===============================================================================
+
+end module spec_hum_to_mixing_ratio
