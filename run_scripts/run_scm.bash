39575e58504d75585c44606d376490435340f42d
eihoppe@uwm.edu <eihoppe@uwm.edu>
2009-08-10 14:45:55 +0000

    Added the capability to specify a grid to the run_scm script. Also, the echo that reports which case is being run was fixed--the RUN_CASE variable was never set, so run_scm.bash would just echo "Running" rather than "Running arm", for example.
    In addition, added a note for the grid options in the README, similar to the notes for the run_scm_all.bash script.
    
    This is in response to comment:52:ticket:125.
    
    ~EIHoppe//20090810
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@3979 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 32f071ed7c5c974e795bd00dc9b137fb8a239c3e

--- run_scripts/run_scm.bash
+++ run_scripts/run_scm.bash
@@ -15,6 +15,9 @@
 # export OMP_NUM_THREADS=2
 #######################################################################
 
+ZT_GRID_TEST=false
+ZM_GRID_TEST=false
+
 # Figure out the directory where the script is located
 scriptPath=`dirname $0`
 
@@ -25,10 +28,82 @@ restoreDir=`pwd`
 cd $scriptPath
 
 if [ -z $1 ]; then
-	echo "Usage: "$0" <MODEL CASE> [PARAMETER FILE] [STATS FILE]"
+	echo "Usage: "$0" [--zm_grid_test OR --zt_grid_test grid_levels grid_file]" \
+			" <MODEL CASE> [PARAMETER FILE] [STATS FILE]"
 	exit
 else
+	# Loop through to pull out options passed into the run.
+	# This is done as a loop to facilitate additional options later on,
+	# if necessary.
+	while [ -n "$(echo $1 | grep "-")" ]; do
+		case $1 in
+			--zt_grid_test ) ZT_GRID_TEST=true
+				if [ "$2" == "" ]; then
+					echo "Option '--zt_grid_test': The number of levels in the grid" \
+					"needs to be entered following '--zt_grid_test'."
+					exit 1
+				elif [ -n "$(echo $2 | grep "-")" ]; then
+					echo "Option '--zt_grid_test': The number of grid levels" \
+					"needs to follow '--zt_grid_test', not another option."
+					exit 1
+				elif [ "$3" == "" ]; then
+					echo "Option '--zt_grid_test': The path to the grid file" \
+					"needs to be entered following '--zt_grid_test'."
+					exit 1
+				elif [ -n "$(echo $3 | grep "-")" ]; then
+					echo "Option '--zt_grid_test': The path to the grid file" \
+					"needs to be entered following '--zt_grid_test'," \
+					"not another option."
+					exit 1
+				elif [ $ZM_GRID_TEST == true ]; then
+					echo "Only --zt_grid_test or --zm_grid_test may be used, not both."
+					exit 1
+				else
+					test_grid_nz=$2
+					test_grid_name=$3
+					test_grid_name=`echo $test_grid_name | sed 's/\//\\\\\//g'`
+					echo "Running case using specified zt grid."
+					shift
+					shift
+				fi;;
+
+			--zm_grid_test ) ZM_GRID_TEST=true
+				if [ "$2" == "" ]; then
+					echo "Option '--zm_grid_test': The number of levels in the grid" \
+					"needs to be entered following '--zm_grid_test'."
+					exit 1
+				elif [ -n "$(echo $2 | grep "-")" ]; then
+					echo "Option '--zm_grid_test': The number of grid levels" \
+					"needs to follow '--zm_grid_test', not another option."
+					exit 1
+				elif [ "$3" == "" ]; then
+					echo "Option '--zm_grid_test': The path to the grid file" \
+					"needs to be entered following '--zm_grid_test'."
+					exit 1
+				elif [ -n "$(echo $3 | grep "-")" ]; then
+					echo "Option '--zm_grid_test': The path to the grid file" \
+					"needs to be entered following '--zm_grid_test'," \
+					"not another option."
+					exit 1
+				elif [ $ZT_GRID_TEST == true ]; then
+					echo "Only --zt_grid_test or --zm_grid_test may be used, not both."
+					exit 1
+				else
+					test_grid_nz=$2
+					test_grid_name=$3
+					test_grid_name=`echo $test_grid_name | sed 's/\//\\\\\//g'`
+					echo "Running case using specified zm grid."
+					shift
+					shift
+				fi;;
+			esac
+		# Shift moves the parameters up one. Example: $2 -> $1, etc.
+		# This is so we only have to check $1 for every iteration.
+		shift
+	done
+
 	MODEL_FILE='../input/case_setups/'$1'_model.in'
+	RUN_CASE=$1
 
 	if [ -z $2 ]; then
 		PARAMS_FILE="../input/tunable_parameters.in"
@@ -69,7 +144,37 @@ if [ ! -e "$STATS_FILE" ]; then
 	exit 1
 fi
 
-cat $PARAMS_FILE $MODEL_FILE $STATS_FILE > 'clubb.in'
+##########################################################################
+# Set up the namelists for the CLUBB model.
+# If we are using the --zt_grid_test or --zm_grid_test options,
+# we need to modify the input files according to the options passed in.
+##########################################################################
+
+if [ $ZT_GRID_TEST == true ]; then
+	cat $PARAMS_FILE > 'clubb.in'
+	cat $MODEL_FILE | sed -e 's/^nzmax\s*=\s*.*//g' \
+		-e 's/^grid_type\s*=\s*.*//g' \
+		-e 's/^zm_grid_fname\s*=\s*.*//g' \
+		-e "s/^zt_grid_fname\s*=\s*.*//g" \
+		-e 's/^\&model_setting/\&model_setting\n \
+		nzmax = '$test_grid_nz'\n \
+		zt_grid_fname ='\'$test_grid_name\''\n \
+		grid_type = 2\n/g' >> 'clubb.in'
+	cat $STATS_FILE >> 'clubb.in'
+elif [ $ZM_GRID_TEST == true ]; then
+	cat $PARAMS_FILE > 'clubb.in'
+	cat $MODEL_FILE | sed -e 's/nzmax\s*=\s*.*//g' \
+		-e 's/grid_type\s*=\s*.*//g' \
+		-e 's/zt_grid_fname\s*=\s*.*//g' \
+		-e 's/zm_grid_fname\s*=\s*.*//g'
+		-e 's/^\&model_setting/\&model_setting\n \
+		nzmax = '$test_grid_nz'\n \
+		zm_grid_fname ='\'$test_grid_name\''\n \
+		grid_type = 3\n/g' >> 'clubb.in'
+	cat $STATS_FILE >> 'clubb.in'
+else
+	cat $PARAMS_FILE $MODEL_FILE $STATS_FILE > 'clubb.in'
+fi
 
 #######################################################################
 # State which case is being run
