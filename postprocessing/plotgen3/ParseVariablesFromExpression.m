ad3c782c50c06d25b7530ba0669ca0c00f442915
nielsenb@uwm.edu <nielsenb@uwm.edu>
2009-08-12 21:36:14 +0000

    Removed preallocation of string arrays. This returns multi-variable expression support back to functional status.
    A fix for preallocation should be looked in to at some point.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4000 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 3bc0f28de286d87059a5f91070f451533c90ac5a

--- postprocessing/plotgen3/ParseVariablesFromExpression.m
+++ postprocessing/plotgen3/ParseVariablesFromExpression.m
@@ -3,7 +3,7 @@ function [variables] = ParseVariablesFromExpression( expression )
 varsToCheck = regexp( expression, '[A-Za-z0-9_]*', 'match' );
 
 %We need to preallocate arrays for speed
-variables = cell(1,size(varsToCheck,2));
+%variables = cell(1,size(varsToCheck,2));
 
 varsFound = 0;
 
@@ -13,14 +13,16 @@ for i=1:size(varsToCheck,2)
 	%Current use contains a collection of all of the current
 	%ways a variable is already in use, this keeps us from
 	%interpreting something like 'sqrt' as a variable
-	currentUse = which('-all', char(varsToCheck(i)));
+	varToCheck = char(varsToCheck(i));
+	
+	currentUse = which('-all', varToCheck);
 
 	%Make sure the parsed out variable name is both a valid variable
 	%name and not already in use
-	if (isvarname(char(varsToCheck(i))) && isempty(currentUse))
+	if (isvarname(varToCheck) && isempty(currentUse))
 		varsFound = varsFound + 1;
 		variables(varsFound) = varsToCheck(i);
 	end
 end
 
-variables = variables(1,varsFound); %remove excess elements
+%variables = variables(1,varsFound); %remove excess elements
