440b863f5b99959771957a6b27d197a72c21046d
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-03-24 00:15:23 +0000

    Added Q1 and Q2 plots that match Laura Davies' poster: http://users.monash.edu.au/%7Eladavies/Davies_S9.pdf
    Adjusted axis to better match poster and case setup document.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4443 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 44fe83fcade62be944b7c063914fa7e5f6a0e478

--- postprocessing/output_scripts/twp_ice/twp_ice_output_plot.m
+++ postprocessing/output_scripts/twp_ice/twp_ice_output_plot.m
@@ -150,6 +150,9 @@ if ( exist(profilefilepath) )
 	hold all
 end 
 
+%Scale the axis
+ylim([100 20000])
+
 hold off
 
 legend( h, legend_text, 'Location', 'NorthEast' )
@@ -227,6 +230,9 @@ if ( exist(profilefilepath) )
 	hold all
 end 
 
+%Scale the axis
+ylim([100 20000])
+
 hold off
 
 legend( h, legend_text, 'Location', 'NorthEast' )
@@ -237,6 +243,164 @@ grid
 
 %PDF
 output_file_name = [ 'twp_ice_suppressed_monsoon_verify.pdf' ];
+print( '-dpdf', '-append', output_file_name )
+
+plot_index = 1;
+t_start = 1;
+t_end = t_start + 120;
+for i=0:99
+	profilefilepath = ['submission/', 'profiles.CLUBB_p', sprintf('%02d',i), '.nc'];
+
+	if ( exist(profilefilepath) )
+		profilefile = netcdf(profilefilepath,'nowrite');
+		file_time = profilefile{'time'}(:);
+
+		%Convert time to  julian days
+		file_time = file_time ./ 24;
+		file_time = file_time + 20;
+
+		file_w = profilefile{'Q1'}(:);
+		file_height = profilefile{'h'}(:);
+
+		%Average
+		w_avg = zeros(1, nz);
+		for t=t_start:t_end
+			w_avg = w_avg + file_w(t,:);
+		end
+		w_avg = w_avg ./ (t_end - t_start);
+
+		h(plot_index) = plot(w_avg, file_height(1,:));
+
+		axis_label =  sprintf('%02d',i);
+		legend_text(plot_index,1:length(axis_label)) = axis_label;
+
+		hold all
+
+		plot_index = plot_index + 1;
+	end 
+end
+
+%Now plot the best estimate	
+profilefilepath = ['submission/', 'profiles.CLUBB_best_estimate', '.nc'];
+
+if ( exist(profilefilepath) )
+	profilefile = netcdf(profilefilepath,'nowrite');
+	file_time = profilefile{'time'}(:);
+
+	%Convert time to  julian days
+	file_time = file_time ./ 24;
+	file_time = file_time + 20;
+
+	file_w = profilefile{'Q1'}(:);
+	file_height = profilefile{'h'}(:);
+
+	%Average
+	w_avg = zeros(1, nz);
+	for t=t_start:t_end
+		w_avg = w_avg + file_w(t,:);
+	end
+	w_avg = w_avg ./ (t_end - t_start);
+
+	h(plot_index) = plot(w_avg, file_height(1,:));
+
+	axis_label =  'best estimate';
+	legend_text(plot_index,1:length(axis_label)) = axis_label;
+
+	hold all
+end 
+
+%Scale the axis
+ylim([100 15000])
+
+hold off
+
+legend( h, legend_text, 'Location', 'NorthEast' )
+xlabel('Q1    [k/day]')
+ylabel('Height    [m]')
+title('Q1 Verification [Monsoon] (5-min timestep)')
+grid
+
+%PDF
+output_file_name = [ 'twp_ice_q1_active_verify.pdf' ];
+print( '-dpdf', '-append', output_file_name )
+
+plot_index = 1;
+t_start = 144;
+t_end = t_start + 120;
+for i=0:99
+	profilefilepath = ['submission/', 'profiles.CLUBB_p', sprintf('%02d',i), '.nc'];
+
+	if ( exist(profilefilepath) )
+		profilefile = netcdf(profilefilepath,'nowrite');
+		file_time = profilefile{'time'}(:);
+
+		%Convert time to  julian days
+		file_time = file_time ./ 24;
+		file_time = file_time + 20;
+
+		file_w = profilefile{'Q2'}(:);
+		file_height = profilefile{'h'}(:);
+
+		%Average
+		w_avg = zeros(1, nz);
+		for t=t_start:t_end
+			w_avg = w_avg + file_w(t,:);
+		end
+		w_avg = w_avg ./ (t_end - t_start);
+
+		h(plot_index) = plot(w_avg, file_height(1,:));
+
+		axis_label =  sprintf('%02d',i);
+		legend_text(plot_index,1:length(axis_label)) = axis_label;
+
+		hold all
+
+		plot_index = plot_index + 1;
+	end 
+end
+
+%Now plot the best esimate
+profilefilepath = ['submission/', 'profiles.CLUBB_best_estimate', '.nc'];
+
+if ( exist(profilefilepath) )
+	profilefile = netcdf(profilefilepath,'nowrite');
+	file_time = profilefile{'time'}(:);
+
+	%Convert time to  julian days
+	file_time = file_time ./ 24;
+	file_time = file_time + 20;
+
+	file_w = profilefile{'Q2'}(:);
+	file_height = profilefile{'h'}(:);
+
+	%Average
+	w_avg = zeros(1, nz);
+	for t=t_start:t_end
+		w_avg = w_avg + file_w(t,:);
+	end
+	w_avg = w_avg ./ (t_end - t_start);
+
+	h(plot_index) = plot(w_avg, file_height(1,:));
+
+	axis_label =  'best estimate';
+	legend_text(plot_index,1:length(axis_label)) = axis_label;
+
+	hold all
+end 
+
+%Scale the axis
+ylim([100 15000]) 
+
+hold off
+
+legend( h, legend_text, 'Location', 'NorthEast' )
+xlabel('Q2    [K/day]')
+ylabel('Height    [m]')
+title('Q2 Verification [Suppressed Monsoon] (5-min timestep)')
+grid
+
+%PDF
+output_file_name = [ 'twp_ice_q2_suppressed_verify.pdf' ];
 print( '-dpdf', '-append', output_file_name )
 
 %Now do profile verification
