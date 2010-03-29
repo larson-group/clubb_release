0752d0bf6f817163d4c4f4ed2bd89de5dbb5c3ed
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-03-29 21:05:53 +0000

    Script now uses an input directory instead of sharing ensembles with the ensemble_run.bash and create_output.bash scripts.
    Removed use of relative path to convert_arm_input_data.m and related
    definition of CLUBB path. I do not see a way to make this work with
    a single call to command line MATLAB, so I am not sure why I committed
    it in the first place.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4457 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: e9bd5a1bfe1d2201c94613e7e34f4a52ab766449

--- run_scripts/ensemble_run/ensemble_setup.bash
+++ run_scripts/ensemble_run/ensemble_setup.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-clubbDir="~/clubb"
-
-for ENSEMBLE in ensembles/*
+for ENSEMBLE in input/*
 do
 	echo "Running $ENSEMBLE..."
-	ensNum=${ENSEMBLE##ensembles/ensmem}
+	ensNum=${ENSEMBLE##input/ensmem}
 
 	#Create a directory to store the output
 	folderName=ensemble$ensNum
@@ -14,7 +12,7 @@ do
 	#Create the sounding and forcing data
 	dataName="twpice_p$ensNum"_layer.dat
 	cp "$ENSEMBLE/$dataName" .
-	sudo -u matlabuser matlab -nodisplay -nodesktop -r $clubbDir/utilities/case_setup/twp_ice/convert_arm_input_data"('twp_ice', '$dataName', 0, 1004.4 )"
+	sudo -u matlabuser matlab -nodisplay -nodesktop -r convert_arm_input_data"('twp_ice', '$dataName', 0, 1004.4 )"
 	mv -f "twp_ice_sounding.in" "output/$folderName"
 	mv -f "twp_ice_forcings.in" "output/$folderName"
 	mv -f "twp_ice_surface.in" "output/$folderName"
