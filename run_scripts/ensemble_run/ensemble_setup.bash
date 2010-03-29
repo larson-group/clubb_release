5d99ece4590c241aa0a5df71cf5b1437609280b4
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-03-29 21:26:19 +0000

    Now uses ensembles directory instead of sharing the output directory with the ensemble_run.bash script.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4460 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 69b7e78fa44e83cf1f89c46195c70630ce225c94

--- run_scripts/ensemble_run/ensemble_setup.bash
+++ run_scripts/ensemble_run/ensemble_setup.bash
@@ -7,14 +7,14 @@ do
 
 	#Create a directory to store the output
 	folderName=ensemble$ensNum
-	mkdir "output/$folderName"
+	mkdir "ensembles/$folderName"
 
 	#Create the sounding and forcing data
 	dataName="twpice_p$ensNum"_layer.dat
 	cp "$ENSEMBLE/$dataName" .
 	sudo -u matlabuser matlab -nodisplay -nodesktop -r convert_arm_input_data"('twp_ice', '$dataName', 0, 1004.4 )"
-	mv -f "twp_ice_sounding.in" "output/$folderName"
-	mv -f "twp_ice_forcings.in" "output/$folderName"
-	mv -f "twp_ice_surface.in" "output/$folderName"
+	mv -f "twp_ice_sounding.in" "ensembles/$folderName"
+	mv -f "twp_ice_forcings.in" "ensembles/$folderName"
+	mv -f "twp_ice_surface.in" "ensembles/$folderName"
 	rm $dataName
 done
