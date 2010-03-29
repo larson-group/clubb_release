bb6b1dbfcbcc39179feebf2f86221f3462a1a2bf
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-03-29 21:08:26 +0000

    The caseName variable is now used properly.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4458 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 4463e33021fd78d982c44ee75647b51f61f25d4c

--- run_scripts/ensemble_run/ensemble_run.bash
+++ run_scripts/ensemble_run/ensemble_run.bash
@@ -25,7 +25,7 @@ do
 	rm -f ../../output/"$caseName"_sfc.dat
 
 	#Run CLUBB
-	../../run_scripts/run_scm.bash twp_ice
+	../../run_scripts/run_scm.bash $caseName
 
 	#Harvest the output files
 	cp ../../output/twp_ice_zt.ctl output/"$folderName"
