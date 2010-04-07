074c0df86988e1cf4e6e74a93988dcd816ed3fa5
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-04-07 16:17:24 +0000

    Updated README to reflect required MATLAB files.
    create_output.bash no longer needs to know the CLUBB path as it does
    not change directories.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4481 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 59a60b19f81e628a7f14b1c0df10056db6c6f47d

--- run_scripts/ensemble_run/create_output.bash
+++ run_scripts/ensemble_run/create_output.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clubbDir="/home/nielsenb/clubb"
+
 
 for ENSEMBLE in output/*
 do
@@ -8,15 +8,9 @@ do
 
 	folderName=ensemble$ensNum
 
-	returnDir=`pwd`
-
-	cd $clubbDir/postprocessing/output_scripts/twp_ice
-
 	#Convert some output files
 	echo "quit" | (sudo -u matlabuser matlab -nodisplay -nodesktop -r twp_ice_profiles_creator"( $ensNum )")
 	echo "quit" | (sudo -u matlabuser matlab -nodisplay -nodesktop -r twp_ice_timeseries_creator"( $ensNum )")
 
-	cd $returnDir
-
-	mv "$clubbDir"/postprocessing/output_scripts/twp_ice/*.nc submission
+	mv ../../postprocessing/output_scripts/twp_ice/*.nc submission
 done
