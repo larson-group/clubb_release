1fb4e23e204fa6497f03d6f7b12fdc469a68948b
nielsenb@uwm.edu <nielsenb@uwm.edu>
2010-06-04 16:13:24 +0000

    Added support for reading a 1D (timeseries) from a file which also contains data with a different dimensionality.
    Corrected support for creating a timeseries from time/height data.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4562 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5107a2f7656a50a79c4df911373d8ffb39127cad

--- postprocessing/plotgen/read_netcdf_hoc_timeseries.m
+++ postprocessing/plotgen/read_netcdf_hoc_timeseries.m
@@ -1,4 +1,3 @@
-
 function avg_field = read_netcdf_hoc_timeseries(filename,nz,t1,t2,varnum,numvars)
 
 % Reads and time-averages profiles from 3D NETCDF *.nc files.
@@ -18,11 +17,21 @@ cleanupHandler = onCleanup(@()netcdf.close(fid));
 %Preallocate arrays for speed
 avg_field(t1:t2) = 0.0;
 
-% Read in and average profiles over all timesteps
-for t=t1:t2
-   field = netcdf.getVar(fid,varnum);
-   new_field(1:nz,1) = squeeze(field(1,1,1:nz,t));
-   avg_field(t) = new_field;
+%Check to see how many dimensions the variable has
+[varname, xtype, dimids, numatts] = netcdf.inqVar(fid,varnum);
+
+field = netcdf.getVar(fid,varnum);
+if max(size(dimids)) == 1
+   for t=t1:t2
+      new_field = field(t);
+      avg_field(t) = new_field;
+   end
+else
+   % Read in and average profiles over all timesteps
+   for t=t1:t2
+      new_field(1:nz,1) = squeeze(field(1,1,1:nz,t));
+      avg_field(t) = avg(new_field);
+   end
 end
 
 end
