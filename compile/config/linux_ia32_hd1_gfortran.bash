6f41cc83c4478e5fa4af4dd87a46e22946f09c94
roehl@uwm.edu <roehl@uwm.edu>
2010-07-29 17:24:40 +0000

    Added a config file to compile CLUBB on hd1
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4677 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: deeeb6b78fb1263883891441f45b5886e8e73083

---
+++ compile/config/linux_ia32_hd1_gfortran.bash
@@ -0,0 +1,71 @@
+\# $Id: linux_ia32_gfortran.bash 3371 2009-04-07 16:39:56Z dschanen@uwm.edu $
+# Configuration file for a Linux machine using GNU compiler collection Fortran
+# Note that the version of gfortran that comes with RHEL5 cannot compile clubb.
+# However, following options did work on Ubuntu 8.04 LTS (with the packaged
+# versions of netcdf and netcdf-dev)
+
+
+# Fortran 95 compiler and linker
+FC=/usr/local/gcc/4.4.3/bin/gfortran
+LD=/usr/local/gcc/4.4.3/bin/gfortran
+
+# Define path to directories
+dir=`pwd` # dir where this script resides
+bindir="$dir/../bin"  # dir for Makefile and executable
+objdir="$dir/../obj"  # dir for *.o and *.mod files
+libdir="$dir/../lib"  # dir for *.a library files
+srcdir="$dir/../src"  # dir where the source files reside
+
+
+# It is sometimes helpful to turn on floating-point trapping for the 
+#  standalone program, but this will not work when using the tuner.
+# These are the options for debugging symbols, bounds checking & IEEE-754 
+# floating point arithmetic
+DEBUG="-g -fbounds-check -mieee-fp"
+
+# == Machine specific flags ==
+# Note: some of these are 64 bit architectures, so make sure NetCDF is
+# compiled accordingly.
+ARCH="-march=native -msse3 -mfpmath=sse"
+
+# == Optimization ==
+OPTIMIZE="-O2"
+
+# == NetCDF Location ==
+NETCDF="/sharedapps/LS/vlarson_group/local/netcdf"
+
+# == LAPACK libraries ==
+#LAPACK="-llapack -lblas" # The netlib reference LAPACK/BLAS
+LAPACK="-L/sharedapps/LS/vlarson_group/local/lib -llapack -lblas -L/sharedapps/LS/vlarson_group/local/atlas/lib -lf77blas -lcblas -latlas"
+
+# == Linking Flags ==
+# Use -s to strip (no debugging); 
+# Use -L<library path> -l<lib> to link in an external library
+# Use -Wl,-rpath <library path> to set a search path for shared libs
+LDFLAGS="-L$NETCDF/lib -lnetcdf -lnetcdff $LAPACK"
+
+# == Compiler flags ==
+# You will need to `make clean' if you change these
+FFLAGS="$ARCH $DEBUG"
+
+# Preprocessing Directives:
+#   -DNETCDF enables netCDF output
+#   -Dradoffline and -Dnooverlap (see BUGSrad documentation)
+# You will need to `make clean' if you change these
+# Use -I<include path> to set a module or header file directory
+CPPFLAGS="-DNETCDF -I$NETCDF/include -D__GFORTRAN__ -Dnooverlap -Dradoffline"
+
+# == Static library processing ==
+AR=ar
+ARFLAGS=cru
+RANLIB=ranlib
+
+# == Shared library processing ==
+SHARED=$FC
+SHAREDFLAGS="-fPIC -shared"
+
+# Location of 'mkmf' utility
+mkmf=$dir/mkmf
+
+# gmake command to use and options: '-j 2' enables parallel compilation
+gmake="make"
