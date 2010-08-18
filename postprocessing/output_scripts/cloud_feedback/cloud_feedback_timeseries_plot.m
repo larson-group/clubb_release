dbef9124832f36f9dc265addebfb26ed38df8205
meyern@uwm.edu <meyern@uwm.edu>
2010-08-18 20:24:41 +0000

    Further changes for ticket #300, improving the output scripts for the CGILS cases.  This update changes the way the netcdf files are read for creation of the verify PDFs.  The script now uses the built-in netcdf capabilities of Matlab instead of relying on unsupported addon scripts and functions.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4704 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: b8887092663080b870498e69071dc592574d28f1

--- postprocessing/output_scripts/cloud_feedback/cloud_feedback_timeseries_plot.m
+++ postprocessing/output_scripts/cloud_feedback/cloud_feedback_timeseries_plot.m
@@ -1,11 +1,5 @@
 function[] = cloud_feedback_timeseries_plot(nc_path, verify_path);
 
-addpath '/home/meyernr/mexnc2/mexnc/'
-addpath '/home/meyernr/snctools'
-addpath '/home/meyernr/netcdf_toolbox/netcdf_toolbox/netcdf/' -end
-addpath '/home/meyernr/netcdf_toolbox/netcdf_toolbox/netcdf/nctype/' -end
-addpath '/home/meyernr/netcdf_toolbox/netcdf_toolbox/netcdf/ncutility/' -end
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %set(gca, 'ColorOrder', [0 0 1; 0 0.6 0; 1 0 0],'LineStyleOrder',{'-','--','o'},'NextPlot','ReplaceChildren');
@@ -15,32 +9,35 @@ curr_case = 'cloud_feedback_s12_p2k';
 sec_per_hour = 3600;
 mm_per_m = 1000;
 nz = 41;
-domain_top = 5950;
 
 t_start = 1;
 t_end = t_start + 719;
 
-timeseries_vars_to_plot = ['cldtot'; 'tglwp '; 'precw '; 'tsair '; 'ps    '; 'prect '; 'lh    '; 'sh    '; 'fsns  '; 'flns  '; 'fsnt  '; 'flnt  '; 'flntc '; 'fsntc '; 'fsnsc '; 'flnsc ']
-press = ['p     ']
+timeseries_vars_to_plot = ['cldtot'; 'tglwp '; 'precw '; 'tsair '; 'ps    '; 'prect '; 'lh    '; 'sh    '; 'fsns  '; 'flns  '; 'fsnt  '; 'flnt  '; 'flntc '; 'fsntc '; 'fsnsc '; 'flnsc '];
+sfcfilepath = [nc_path, curr_case, '_scm_UWM_CLUBB_v2.nc'];
+press = ['p     '];
 
-profile_vars_to_plot = [ 'T     '; 'ql    '; 'qv    '; 'cloud '; 'tdt_lw'; 'tdt_sw'; 'tdt_ls'; 'qdt_ls' ]
-% Plot the timeseries variables
-for i=1:size(timeseries_vars_to_plot,1);
-	minVal = 0;
-	maxVal = 0;
+profile_vars_to_plot = [ 'T     '; 'ql    '; 'qv    '; 'cloud '; 'tdt_lw'; 'tdt_sw'; 'tdt_ls'; 'qdt_ls' ];
 
-	var_to_plot = strtrim(timeseries_vars_to_plot(i, 1:size(timeseries_vars_to_plot,2)))
+if ( exist(sfcfilepath) )
+	% Plot the timeseries variables
+	for i=1:size(timeseries_vars_to_plot,1);
+		minVal = 0;
+		maxVal = 0;
 
-	sfcfilepath = [nc_path, curr_case, '_scm_UWM_CLUBB_v2.nc'];	
+		var_to_plot = strtrim(timeseries_vars_to_plot(i, 1:size(timeseries_vars_to_plot,2)))	
 
-	if ( exist(sfcfilepath) )
-		sfcfile = netcdf(sfcfilepath,'nowrite');
 
-		file_time = sfcfile{'time'}(:);
+		sfcfile = netcdf.open(sfcfilepath,'NC_NOWRITE');
+
+		varid = netcdf.inqVarID(sfcfile,'time');
+		file_time = netcdf.getVar(sfcfile,varid);
 		%file_time = file_time ./ 5; % Timestep (output time in minutes)
 		%file_time = file_time ./ 60;
 
-		file_var = sfcfile{var_to_plot}(:);
+		% Read in variable
+		varid = netcdf.inqVarID(sfcfile, var_to_plot);
+		file_var = netcdf.getVar(sfcfile, varid);
 
 		plot (file_time, file_var);
 
@@ -54,81 +51,76 @@ for i=1:size(timeseries_vars_to_plot,1);
 		if (max(max(file_var)) > maxVal)
 			maxVal = max(max(file_var));
 		end
-	end 
 
-	hold off
 
-	%Determine the unit
-	units = nc_attget( sfcfilepath, var_to_plot, 'unit' );
-	
-	ylabel([var_to_plot, ' ', '[', units, ']'])
-	xlabel('Time   [h]')
-	title(['Cloud Feedback ', var_to_plot, ' verification '])
-	grid
+		hold off
 
-	%Scale the axis
-	percentage = 95;
-			
-	% Normalize units to a factor of 1.
-	percentage = percentage / 100;
-			
-	equiv_space = 0.01;
-					
-	if ( maxVal == minVal )
-   		ymin = minVal - equiv_space;
-   		ymax = maxVal + equiv_space;
-	else
-   		ydiff = maxVal - minVal;
-   		yrange = ydiff / percentage;
-   		ymedian = ( minVal + maxVal ) / 2;
-   		ymin = ymedian - yrange / 2;
-   		ymax = ymedian + yrange / 2;
-	end
+		%Determine the unit
+		attname = netcdf.inqAttName(sfcfile, varid, 0);
+		units = netcdf.getAtt( sfcfile, varid, attname );
 
-	axis([ min(min(file_time)) max(max(file_time)) ymin ymax ])
+		% Set labels for axes and plot title
+		ylabel([var_to_plot, ' ', '[', units, ']']);
+		xlabel('Time   [h]');
+		title(['Cloud Feedback ', var_to_plot, ' verification ']);
+		grid;
 
-	%PDF
-	output_file_name = [ verify_path, curr_case, '_', var_to_plot, '_verify.pdf' ];
-	print( '-dpdf', '-append', output_file_name )
-end
-
-% Read in the pressure to use as the y-axis in the profile plots
-profilefilepath = [nc_path, curr_case, '_scm_UWM_CLUBB_v2.nc'];
-pressure = zeros(1,nz)
-if( exist(profilefilepath) )
-	var_to_plot = strtrim(press(1))
-	profilefile = netcdf(profilefilepath,'nowrite');
-	file_time = profilefile{'time'}(:);
-	pressure = profilefile{var_to_plot}(:)
-	% Invert the pressure array?
-end
-% Plot the profiles as an average of the variable over the entire run time
-for i=1:size(profile_vars_to_plot,1);
-	plot_index = 1;
-
-	minVal = 0;
-	maxVal = 0;
-
-	var_to_plot = strtrim(profile_vars_to_plot(i, 1:size(profile_vars_to_plot,2)))
-
-	profilefilepath = [nc_path, curr_case, '_scm_UWM_CLUBB_v2.nc'];
-
-	if ( exist(profilefilepath) )
-		profilefile = netcdf(profilefilepath,'nowrite');
-		file_time = profilefile{'time'}(:);
-		%file_time = file_time ./ 5; % Timestep (output time in minutes)
-		%file_time = file_time ./ 60;
+		%Scale the axis
+		percentage = 95;
 		
-		file_var = profilefile{var_to_plot}(:);
-
-		for h=1:nz
-			file_height(h) = (domain_top / nz) * (h - 1);
+		% Normalize units to a factor of 1.
+		percentage = percentage / 100;
+		
+		equiv_space = 0.01;
+					
+		if ( maxVal == minVal )
+   			ymin = minVal - equiv_space;
+   			ymax = maxVal + equiv_space;
+		else
+   			ydiff = maxVal - minVal;
+   			yrange = ydiff / percentage;
+   			ymedian = ( minVal + maxVal ) / 2;
+   			ymin = ymedian - yrange / 2;
+   			ymax = ymedian + yrange / 2;
 		end
 
+		axis([ min(min(file_time)) max(max(file_time)) ymin ymax ]);
+
+		%PDF
+		output_file_name = [ verify_path, curr_case, '_', var_to_plot, '_verify.pdf' ];
+		print( '-dpdf', '-append', output_file_name );
+	end
+
+	% Read in the pressure to use as the y-axis in the profile plots
+	pressure = zeros(1,nz);
+
+	var_to_plot = strtrim(press(1))
+	profilefile = netcdf.open(sfcfilepath,'NC_NOWRITE');
+	varid = netcdf.inqVarID(sfcfile, 'p');
+	pressure = netcdf.getVar(sfcfile, varid);
+
+	% Plot the profiles as an average of the variable over the entire run time
+	for i=1:size(profile_vars_to_plot,1);
+		plot_index = 1;
+
+		minVal = 0;
+		maxVal = 0;
+
+		var_to_plot = strtrim(profile_vars_to_plot(i, 1:size(profile_vars_to_plot,2)))
+
+		profilefile = netcdf.open(sfcfilepath,'NC_NOWRITE');
+		varid = netcdf.inqVarID(sfcfile,'time');
+		file_time = netcdf.getVar(sfcfile,varid);
+		%file_time = file_time ./ 5; % Timestep (output time in minutes)
+		%file_time = file_time ./ 60;
+
+		varid = netcdf.inqVarID(sfcfile, var_to_plot);
+		file_var = netcdf.getVar(sfcfile, varid);
+
 		%Average
-		var_avg = zeros(1, nz);
+		var_avg = zeros(nz, 1);
 		for t=t_start:t_end
-			var_avg = var_avg + file_var(t,:);
+			var_avg = var_avg + file_var(:,t);
 		end
 		var_avg = var_avg ./ (t_end - t_start);
 
@@ -146,40 +138,43 @@ for i=1:size(profile_vars_to_plot,1);
 		end
 
 		plot_index = plot_index + 1;
-	end 
 
-	hold off
+		hold off
 
-	%Determine the unit
-	units = nc_attget( profilefilepath, var_to_plot, 'unit' );
+		%Determine the unit
+		attname = netcdf.inqAttName(sfcfile, varid, 0);
+		units = netcdf.getAtt( sfcfile, varid, attname );
 
-	xlabel([var_to_plot, ' [', units, ']']); 
-	ylabel('Pressure [mb]')
-	title([curr_case, ' ', var_to_plot, ' verification'])
-	grid
+		% Set labels for axes and plot title
+		xlabel([var_to_plot, ' [', units, ']']); 
+		ylabel('Pressure [mb]');
+		title([curr_case, ' ', var_to_plot, ' verification']);
+		grid;
 
-	%Scale the axis
-	percentage = 95;
-			
-	% Normalize units to a factor of 1.
-	percentage = percentage / 100;
-			
-	equiv_space = 0.01;
-					
-	if ( maxVal == minVal )
-   		xmin = minVal - equiv_space;
-   		xmax = maxVal + equiv_space;
-	else
-   		xdiff = maxVal - minVal;
-   		xrange = xdiff / percentage;
-   		xmedian = ( minVal + maxVal ) / 2;
-   		xmin = xmedian - xrange / 2;
-   		xmax = xmedian + xrange / 2;
+		%Scale the axis
+		percentage = 95;
+		
+		% Normalize units to a factor of 1.
+		percentage = percentage / 100;
+		
+		equiv_space = 0.01;
+				
+		if ( maxVal == minVal )
+   			xmin = minVal - equiv_space;
+   			xmax = maxVal + equiv_space;
+		else
+   			xdiff = maxVal - minVal;
+   			xrange = xdiff / percentage;
+   			xmedian = ( minVal + maxVal ) / 2;
+   			xmin = xmedian - xrange / 2;
+   			xmax = xmedian + xrange / 2;
+		end
+		% Reverse the scale for pressure on the y-axis and set axes
+		set(gca, 'YDir', 'reverse');
+		axis([ xmin xmax min(min(pressure)) max(max(pressure)) ]);
+
+		%PDF
+		output_file_name = [ verify_path, curr_case, '_', var_to_plot, '_verify.pdf' ];
+		print( '-dpdf', '-append', output_file_name );
 	end
-	set(gca, 'YDir', 'reverse')
-	axis([ xmin xmax min(min(pressure)) max(max(pressure)) ])
-
-	%PDF
-	output_file_name = [ verify_path, curr_case, '_', var_to_plot, '_verify.pdf' ];
-	print( '-dpdf', '-append', output_file_name )
 end
