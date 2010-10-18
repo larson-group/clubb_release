907e15737edfb408ed7d29245a818aab98e8089b
dschanen@uwm.edu <dschanen@uwm.edu>
2010-10-18 21:28:37 +0000

    Fixed a typo in the comments, and ran findent on the file.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4816 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: cc1921ea1526127b61c666bf6375db4c86e7a2ca

--- src/CLUBB_core/parameter_indices.F90
+++ src/CLUBB_core/parameter_indices.F90
@@ -7,7 +7,7 @@ module parameter_indices
 !       parameter by hand like this.
 
 !       Adding new parameters is relatively simple.  First, the
-!       parameter should be added in the common block of the parametersle
+!       parameter should be added in the common block of the parameters
 !       module so it can be used in other parts of the code. Each
 !       variable needs a unique number in this module, and nparams must
 !       be incremented for the new variable.  Next, the params_list
@@ -20,12 +20,12 @@ module parameter_indices
 !       have the parameter added to them.
 !-----------------------------------------------------------------------
 
-implicit none
+  implicit none
 
-private ! Default Scope
+  private ! Default Scope
 
-integer, parameter, public ::  & 
-nparams = 52 ! Total tunable parameters
+  integer, parameter, public ::  & 
+    nparams = 52 ! Total tunable parameters
 
 !***************************************************************
 !                    ***** IMPORTANT *****
@@ -35,63 +35,63 @@ nparams = 52 ! Total tunable parameters
 !                    ***** IMPORTANT *****
 !***************************************************************
 
-integer, parameter, public :: & 
-iC1             =  1, & 
-iC1b            =  2, & 
-iC1c            =  3, & 
-iC2             =  4, & 
-iC2b            =  5, & 
-iC2c            =  6, & 
-iC2rt           =  7, & 
-iC2thl          =  8, & 
-iC2rtthl        =  9, & 
-iC4             = 10, & 
-iC5             = 11, & 
-iC6rt           = 12, & 
-iC6rtb          = 13, & 
-iC6rtc          = 14, & 
-iC6thl          = 15, & 
-iC6thlb         = 16, & 
-iC6thlc         = 17, & 
-iC7             = 18, & 
-iC7b            = 19, & 
-iC7c            = 20, & 
-iC8             = 21, & 
-iC8b            = 22, & 
-iC10            = 23, & 
-iC11            = 24, & 
-iC11b           = 25, & 
-iC11c           = 26, & 
-iC12            = 27, & 
-iC13            = 28, & 
-iC14            = 29
+  integer, parameter, public :: & 
+    iC1             =  1, & 
+    iC1b            =  2, & 
+    iC1c            =  3, & 
+    iC2             =  4, & 
+    iC2b            =  5, & 
+    iC2c            =  6, & 
+    iC2rt           =  7, & 
+    iC2thl          =  8, & 
+    iC2rtthl        =  9, & 
+    iC4             = 10, & 
+    iC5             = 11, & 
+    iC6rt           = 12, & 
+    iC6rtb          = 13, & 
+    iC6rtc          = 14, & 
+    iC6thl          = 15, & 
+    iC6thlb         = 16, & 
+    iC6thlc         = 17, & 
+    iC7             = 18, & 
+    iC7b            = 19, & 
+    iC7c            = 20, & 
+    iC8             = 21, & 
+    iC8b            = 22, & 
+    iC10            = 23, & 
+    iC11            = 24, & 
+    iC11b           = 25, & 
+    iC11c           = 26, & 
+    iC12            = 27, & 
+    iC13            = 28, & 
+    iC14            = 29
 
-integer, parameter, public :: & 
-ic_K            = 30, & 
-ic_K1           = 31, & 
-inu1            = 32, & 
-ic_K2           = 33, & 
-inu2            = 34, & 
-ic_K6           = 35, & 
-inu6            = 36, & 
-ic_K8           = 37, & 
-inu8            = 38, & 
-ic_K9           = 39, & 
-inu9            = 40, & 
-ic_Krrainm      = 41, & 
-inu_r           = 42, & 
-ic_Ksqd         = 43, &
-inu_hd          = 44
+  integer, parameter, public :: & 
+    ic_K            = 30, & 
+    ic_K1           = 31, & 
+    inu1            = 32, & 
+    ic_K2           = 33, & 
+    inu2            = 34, & 
+    ic_K6           = 35, & 
+    inu6            = 36, & 
+    ic_K8           = 37, & 
+    inu8            = 38, & 
+    ic_K9           = 39, & 
+    inu9            = 40, & 
+    ic_Krrainm      = 41, & 
+    inu_r           = 42, & 
+    ic_Ksqd         = 43, &
+    inu_hd          = 44
 
-integer, parameter, public :: & 
-igamma_coef     = 45, & 
-igamma_coefb    = 46, & 
-igamma_coefc    = 47, & 
-imu             = 48, & 
-ibeta           = 49, & 
-ilmin_coef      = 50, & 
-itaumin         = 51, & 
-itaumax         = 52
+  integer, parameter, public :: & 
+    igamma_coef     = 45, & 
+    igamma_coefb    = 46, & 
+    igamma_coefc    = 47, & 
+    imu             = 48, & 
+    ibeta           = 49, & 
+    ilmin_coef      = 50, & 
+    itaumin         = 51, & 
+    itaumax         = 52
 
 end module parameter_indices
 !-----------------------------------------------------------------------
