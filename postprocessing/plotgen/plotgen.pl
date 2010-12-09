02f8ab2cacc003a49e18a5d97ea7c421e95bbc9f
roehl@uwm.edu <roehl@uwm.edu>
2010-12-09 01:00:49 +0000

    Fixed version number
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@4914 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ef4b2ce51a58913944a4144205a128d89327492c

--- postprocessing/plotgen/plotgen.pl
+++ postprocessing/plotgen/plotgen.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###########################################################################
-# Plotgen v3.41
+# Plotgen v3.50
 #
 # Documentation is available here:
 # http://larson-group.com/twiki/bin/view.pl/Documentation/CarsonDoc/Plotgen3
