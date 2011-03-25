c759be0374a796cf483ab0deb3a37d3eeef2d360
bmg2@uwm.edu <bmg2@uwm.edu>
2011-03-25 06:39:57 +0000

    I have completed the coding for all of the general quadrivariate and trivariate mixed moments.  All have been tested by comparing their results to the results of my MATLAB code.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5061 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 8663d05d01352ec207edea2f77b06bd557f30017

--- src/KK_micro/PDF_integrals_all_MM.F90
+++ src/KK_micro/PDF_integrals_all_MM.F90
@@ -8,7 +8,10 @@ module PDF_integrals_all_mixed_moments
             quadrivar_NNLL_MM_const_x1, &
             quadrivar_NNLL_MM_const_x2, &
             quadrivar_NNLL_MM_const_x1_x2, &
-            trivar_NNL_MM
+            trivar_NNL_MM, &
+            trivar_NNL_MM_const_x1, &
+            trivar_NNL_MM_const_x2, &
+            trivar_NNL_MM_const_x1_x2
 
   contains
 
@@ -510,6 +513,237 @@ module PDF_integrals_all_mixed_moments
   end function trivar_NNL_MM
 
   !=============================================================================
+  function trivar_NNL_MM_const_x1( mu_x1, mu_x2, mu_x3_n, &
+                                   sigma_x2, sigma_x3_n, rho_x2x3_n, &
+                                   x1_bar, x2_alpha_x3_beta_bar, &
+                                   alpha_exp, beta_exp, a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use constants_clubb, only:  &
+        pi_dp  ! Constant(s)
+
+    use KK_utilities, only:  &
+        factorial, & ! Procedure(s)
+        Dv_fnc
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      sigma_x2,   & ! Standard deviation of x2 (ith PDF component)          [-]
+      sigma_x3_n, & ! Standard deviation of ln x3 (ith PDF component)       [-]
+      rho_x2x3_n    ! Correlation between x2 and ln x3 (ith PDF component)  [-]
+
+    double precision, intent(in) :: &
+      x1_bar,               & ! Mean of x1 (overall)                        [-]
+      x2_alpha_x3_beta_bar    ! Mean of x2^alpha x3^beta                    [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp      ! Exponent beta, corresponding to x3                    [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      trivar_NNL_MM_const_x1
+
+    ! Local Variables
+    integer :: &
+      q    !
+
+    double precision ::  &
+      sigma_sum, & !
+      s_c          !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do q = 0, b_exp, 1
+
+       s_c = ( mu_x2 / sigma_x2 )  &
+             + rho_x2x3_n * sigma_x3_n * beta_exp * q
+
+       sigma_sum  &
+       = sigma_sum  &
+       + 1.0 / sqrt( 2.0*pi_dp )  &
+         * ( mu_x1 - x1_bar )**a_exp  &
+         * factorial( b_exp )  &
+           / ( factorial( b_exp - q ) * factorial( q ) )  &
+         * ( - x2_alpha_x3_beta_bar )**(b_exp-q)  &
+         * sigma_x2**(alpha_exp*q)  &
+         * exp( mu_x3_n * beta_exp * q  &
+                + 0.5 * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0  &
+                - 0.25 * s_c**2.0 )  &
+         * gamma( alpha_exp*q + 1 )  &
+         * Dv_fnc( -(alpha_exp*q + 1), -s_c )
+
+    enddo
+
+    trivar_NNL_MM_const_x1 = sigma_sum
+
+    return
+
+  end function trivar_NNL_MM_const_x1
+
+  !=============================================================================
+  function trivar_NNL_MM_const_x2( mu_x1, mu_x2, mu_x3_n, &
+                                   sigma_x1, sigma_x3_n, rho_x1x3_n, &
+                                   x1_bar, x2_alpha_x3_beta_bar, &
+                                   alpha_exp, beta_exp, a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use KK_utilities, only:  &
+        factorial    ! Procedure(s)
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      sigma_x1,   & ! Standard deviation of x1 (ith PDF component)          [-]
+      sigma_x3_n, & ! Standard deviation of ln x3 (ith PDF component)       [-]
+      rho_x1x3_n    ! Correlation between x1 and ln x3 (ith PDF component)  [-]
+
+    double precision, intent(in) :: &
+      x1_bar,               & ! Mean of x1 (overall)                        [-]
+      x2_alpha_x3_beta_bar    ! Mean of x2^alpha x3^beta                    [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp      ! Exponent beta, corresponding to x3                    [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      trivar_NNL_MM_const_x2
+
+    ! Local Variables
+    integer :: &
+      p, & !
+      q    !
+
+    double precision ::  &
+      sigma_sum    !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do p = 0, a_exp/2, 1
+       do q = 0, b_exp, 1
+
+          sigma_sum  &
+          = sigma_sum  &
+          +   factorial( a_exp )  &
+              / ( factorial( a_exp - 2*p ) * factorial( p ) )  &
+            * factorial( b_exp )  &
+              / ( factorial( b_exp - q ) * factorial( q ) )  &
+            * ( - x2_alpha_x3_beta_bar )**(b_exp-q)  &
+            * mu_x2**(alpha_exp*q)  &
+            * ( 0.5 * sigma_x1**2.0 )**p  &
+            * ( mu_x1 - x1_bar  &
+                + rho_x1x3_n * sigma_x1  &
+                  * sigma_x3_n * beta_exp * q )**(a_exp-2*p)  &
+            * exp( mu_x3_n * beta_exp * q  &
+                   + 0.5 * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0 )
+
+       enddo
+    enddo 
+
+    trivar_NNL_MM_const_x2 = sigma_sum
+
+    return
+
+  end function trivar_NNL_MM_const_x2
+
+  !=============================================================================
+  function trivar_NNL_MM_const_x1_x2( mu_x1, mu_x2, mu_x3_n, sigma_x3_n, &
+                                      x1_bar, x2_alpha_x3_beta_bar, &
+                                      alpha_exp, beta_exp, a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use KK_utilities, only:  &
+        factorial    ! Procedure(s)
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      sigma_x3_n    ! Standard deviation of ln x3 (ith PDF component)       [-]
+
+    double precision, intent(in) :: &
+      x1_bar,               & ! Mean of x1 (overall)                        [-]
+      x2_alpha_x3_beta_bar    ! Mean of x2^alpha x3^beta                    [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp      ! Exponent beta, corresponding to x3                    [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      trivar_NNL_MM_const_x1_x2
+
+    ! Local Variables
+    integer :: &
+      q    !
+
+    double precision ::  &
+      sigma_sum    !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do q = 0, b_exp, 1
+
+       sigma_sum  &
+       = sigma_sum  &
+       + ( mu_x1 - x1_bar )**a_exp  &
+         * factorial( b_exp )  &
+           / ( factorial( b_exp - q ) * factorial( q ) )  &
+         * ( - x2_alpha_x3_beta_bar )**(b_exp-q)  &
+         * mu_x2**(alpha_exp*q)  &
+         * exp( mu_x3_n * beta_exp * q  &
+                + 0.5 * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0 )
+
+    enddo
+
+    trivar_NNL_MM_const_x1_x2 = sigma_sum
+
+    return
+
+  end function trivar_NNL_MM_const_x1_x2
 
 !===============================================================================
 
