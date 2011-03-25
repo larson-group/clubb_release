34d4f0b3d52112865bca9034dc631ffc87223bd1
bmg2@uwm.edu <bmg2@uwm.edu>
2011-03-25 04:15:38 +0000

    I have completed the portion of PDF_integrals_all_MM.F90 dealing with the quadrivariate mixed moments.  All the formulas have been successfully tested against the results produced by my MATLAB programs.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5060 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5d984eb0f0c2738bbe29d271832ed0e190594df8

--- src/KK_micro/PDF_integrals_all_MM.F90
+++ src/KK_micro/PDF_integrals_all_MM.F90
@@ -5,6 +5,9 @@ module PDF_integrals_all_mixed_moments
   private
 
   public :: quadrivar_NNLL_MM, &
+            quadrivar_NNLL_MM_const_x1, &
+            quadrivar_NNLL_MM_const_x2, &
+            quadrivar_NNLL_MM_const_x1_x2, &
             trivar_NNL_MM
 
   contains
@@ -76,6 +79,9 @@ module PDF_integrals_all_mixed_moments
       sigma_sum, & !
       s_cc         !
 
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
     do p = 0, a_exp/2, 1
        do r = 0, (a_exp-2*p), 1
           do q = 0, b_exp, 1
@@ -126,6 +132,279 @@ module PDF_integrals_all_mixed_moments
     return
 
   end function quadrivar_NNLL_MM
+
+  !=============================================================================
+  function quadrivar_NNLL_MM_const_x1( mu_x1, mu_x2, mu_x3_n, mu_x4_n, &
+                                       sigma_x2, sigma_x3_n, sigma_x4_n, &
+                                       rho_x2x3_n, rho_x2x4_n, rho_x3x4_n, &
+                                       x1_bar, x2_alpha_x3_beta_x4_gamma_bar, &
+                                       alpha_exp, beta_exp, gamma_exp, &
+                                       a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use constants_clubb, only:  &
+        pi_dp  ! Constant(s)
+
+    use KK_utilities, only:  &
+        factorial, & ! Procedure(s)
+        Dv_fnc
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      mu_x4_n,    & ! Mean of ln x4 (ith PDF component)                     [-]
+      sigma_x2,   & ! Standard deviation of x2 (ith PDF component)          [-]
+      sigma_x3_n, & ! Standard deviation of ln x3 (ith PDF component)       [-]
+      sigma_x4_n, & ! Standard deviation of ln x4 (ith PDF component)       [-]
+      rho_x2x3_n, & ! Correlation between x2 and ln x3 (ith PDF component)  [-]
+      rho_x2x4_n, & ! Correlation between x2 and ln x4 (ith PDF component)  [-]
+      rho_x3x4_n    ! Correlation between ln x3 & ln x4 (ith PDF component) [-]
+
+    double precision, intent(in) :: &
+      x1_bar,                        & ! Mean of x1 (overall)               [-]
+      x2_alpha_x3_beta_x4_gamma_bar    ! Mean of x2^alpha x3^beta x4^gamma  [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp,   & ! Exponent beta, corresponding to x3                    [-]
+      gamma_exp     ! Exponent gamma, corresponding to x4                   [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      quadrivar_NNLL_MM_const_x1
+
+    ! Local Variables
+    integer :: &
+      q    !
+
+    double precision ::  &
+      sigma_sum, & !
+      s_cc         !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do q = 0, b_exp, 1
+
+       s_cc = ( mu_x2 / sigma_x2 )  &
+              + rho_x2x3_n * sigma_x3_n * beta_exp * q  &
+              + rho_x2x4_n * sigma_x4_n * gamma_exp * q
+
+       sigma_sum  &
+       = sigma_sum  &
+       + 1.0 / sqrt( 2.0*pi_dp )  &
+         * ( mu_x1 - x1_bar )**a_exp  &
+         * factorial( b_exp )  &
+           / ( factorial( b_exp - q ) * factorial( q ) )  &
+         * ( - x2_alpha_x3_beta_x4_gamma_bar )**(b_exp-q)  &
+         * ( - sigma_x2 )**(alpha_exp*q)  &
+         * exp( mu_x3_n * beta_exp * q + mu_x4_n * gamma_exp * q  &
+                + 0.5 * ( 1.0 - rho_x2x3_n**2.0 )  &
+                      * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0  &
+                + 0.5 * ( 1.0 - rho_x2x4_n**2.0 )  &
+                      * sigma_x4_n**2.0 * gamma_exp**2.0 * q**2.0  &
+                + ( rho_x3x4_n - rho_x2x3_n * rho_x2x4_n )  &
+                      * sigma_x3_n * beta_exp  &
+                      * sigma_x4_n * gamma_exp * q**2.0  &
+              )  &
+         * exp( 0.25 * s_cc**2.0 - ( mu_x2 / sigma_x2 ) * s_cc  &
+                + 0.5 * ( mu_x2**2.0 / sigma_x2**2.0 ) )  &
+         * gamma( alpha_exp*q + 1 )  &
+         * Dv_fnc( -(alpha_exp*q + 1), s_cc )
+
+    enddo
+
+    quadrivar_NNLL_MM_const_x1 = sigma_sum
+
+    return
+
+  end function quadrivar_NNLL_MM_const_x1
+
+  !=============================================================================
+  function quadrivar_NNLL_MM_const_x2( mu_x1, mu_x2, mu_x3_n, mu_x4_n, &
+                                       sigma_x1, sigma_x3_n, sigma_x4_n, &
+                                       rho_x1x3_n, rho_x1x4_n, rho_x3x4_n, &
+                                       x1_bar, x2_alpha_x3_beta_x4_gamma_bar, &
+                                       alpha_exp, beta_exp, gamma_exp, &
+                                       a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use KK_utilities, only:  &
+        factorial    ! Procedure(s)
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      mu_x4_n,    & ! Mean of ln x4 (ith PDF component)                     [-]
+      sigma_x1,   & ! Standard deviation of x1 (ith PDF component)          [-]
+      sigma_x3_n, & ! Standard deviation of ln x3 (ith PDF component)       [-]
+      sigma_x4_n, & ! Standard deviation of ln x4 (ith PDF component)       [-]
+      rho_x1x3_n, & ! Correlation between x1 and ln x3 (ith PDF component)  [-]
+      rho_x1x4_n, & ! Correlation between x1 and ln x4 (ith PDF component)  [-]
+      rho_x3x4_n    ! Correlation between ln x3 & ln x4 (ith PDF component) [-]
+
+    double precision, intent(in) :: &
+      x1_bar,                        & ! Mean of x1 (overall)               [-]
+      x2_alpha_x3_beta_x4_gamma_bar    ! Mean of x2^alpha x3^beta x4^gamma  [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp,   & ! Exponent beta, corresponding to x3                    [-]
+      gamma_exp     ! Exponent gamma, corresponding to x4                   [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      quadrivar_NNLL_MM_const_x2
+
+    ! Local Variables
+    integer :: &
+      p, & !
+      q    !
+
+    double precision ::  &
+      sigma_sum    !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do p = 0, a_exp/2, 1
+       do q = 0, b_exp, 1
+
+          sigma_sum  &
+          = sigma_sum  &
+          +   factorial( a_exp )  &
+              / ( factorial( a_exp - 2*p ) * factorial( p ) )  &
+            * factorial( b_exp )  &
+              / ( factorial( b_exp - q ) * factorial( q ) )  &
+            * ( - x2_alpha_x3_beta_x4_gamma_bar )**(b_exp-q)  &
+            * mu_x2**(alpha_exp*q)  &
+            * ( 0.5 * sigma_x1**2.0 )**p  &
+            * ( mu_x1 - x1_bar  &
+                + rho_x1x3_n * sigma_x1  &
+                  * sigma_x3_n * beta_exp * q  &
+                + rho_x1x4_n * sigma_x1  &
+                  * sigma_x4_n * gamma_exp * q )**(a_exp-2*p)  &
+            * exp( mu_x3_n * beta_exp * q + mu_x4_n * gamma_exp * q  &
+                   + 0.5 * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0  &
+                   + 0.5 * sigma_x4_n**2.0 * gamma_exp**2.0 * q**2.0  &
+                   + rho_x3x4_n * sigma_x3_n * beta_exp  &
+                     * sigma_x4_n * gamma_exp * q**2.0  &
+                 )
+
+       enddo
+    enddo
+
+    quadrivar_NNLL_MM_const_x2 = sigma_sum
+
+    return
+
+  end function quadrivar_NNLL_MM_const_x2
+
+  !=============================================================================
+  function quadrivar_NNLL_MM_const_x1_x2( mu_x1, mu_x2, mu_x3_n, mu_x4_n, &
+                                          sigma_x3_n, sigma_x4_n, &
+                                          rho_x3x4_n, x1_bar, &
+                                          x2_alpha_x3_beta_x4_gamma_bar, &
+                                          alpha_exp, beta_exp, gamma_exp, &
+                                          a_exp, b_exp )
+
+    ! Description:
+
+    ! References:
+    !  Griffin, B. M. (2011)
+    !-----------------------------------------------------------------------
+
+    use KK_utilities, only:  &
+        factorial    ! Procedure(s)
+
+    implicit none
+
+    ! Input Variables
+    double precision, intent(in) :: &
+      mu_x1,      & ! Mean of x1 (ith PDF component)                        [-]
+      mu_x2,      & ! Mean of x2 (ith PDF component)                        [-]
+      mu_x3_n,    & ! Mean of ln x3 (ith PDF component)                     [-]
+      mu_x4_n,    & ! Mean of ln x4 (ith PDF component)                     [-]
+      sigma_x3_n, & ! Standard deviation of ln x3 (ith PDF component)       [-]
+      sigma_x4_n, & ! Standard deviation of ln x4 (ith PDF component)       [-]
+      rho_x3x4_n    ! Correlation between ln x3 & ln x4 (ith PDF component) [-]
+
+    double precision, intent(in) :: &
+      x1_bar,                        & ! Mean of x1 (overall)               [-]
+      x2_alpha_x3_beta_x4_gamma_bar    ! Mean of x2^alpha x3^beta x4^gamma  [-]
+    
+    double precision, intent(in) :: &
+      alpha_exp,  & ! Exponent alpha, corresponding to x2                   [-]
+      beta_exp,   & ! Exponent beta, corresponding to x3                    [-]
+      gamma_exp     ! Exponent gamma, corresponding to x4                   [-]
+
+    integer, intent(in) :: &
+      a_exp, & ! Order prime of x1 - < x1 >                                 [-]
+      b_exp    ! Order prime of x2^alpha x3^beta - < x2^alpha x3^beta >     [-]
+
+    ! Return Variable
+    double precision ::  &
+      quadrivar_NNLL_MM_const_x1_x2
+
+    ! Local Variables
+    integer :: &
+      q    !
+
+    double precision ::  &
+      sigma_sum    !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
+
+    do q = 0, b_exp, 1
+
+       sigma_sum  &
+       = sigma_sum  &
+       + ( mu_x1 - x1_bar )**a_exp  &
+         * factorial( b_exp )  &
+           / ( factorial( b_exp - q ) * factorial( q ) )  &
+         * ( - x2_alpha_x3_beta_x4_gamma_bar )**(b_exp-q)  &
+         * mu_x2**(alpha_exp*q)  &
+         * exp( mu_x3_n * beta_exp * q + mu_x4_n * gamma_exp * q  &
+                + 0.5 * sigma_x3_n**2.0 * beta_exp**2.0 * q**2.0  &
+                + 0.5 * sigma_x4_n**2.0 * gamma_exp**2.0 * q**2.0  &
+                + rho_x3x4_n * sigma_x3_n * beta_exp  &
+                      * sigma_x4_n * gamma_exp * q**2.0  &
+              )
+
+    enddo
+
+    quadrivar_NNLL_MM_const_x1_x2 = sigma_sum
+
+    return
+
+  end function quadrivar_NNLL_MM_const_x1_x2
 
   !=============================================================================
   function trivar_NNL_MM( mu_x1, mu_x2, mu_x3_n, &
@@ -186,6 +465,9 @@ module PDF_integrals_all_mixed_moments
     double precision ::  &
       sigma_sum, & !
       s_c          !
+
+    ! Initialize sigma_sum
+    sigma_sum = 0.0
 
     do p = 0, a_exp/2, 1
        do r = 0, (a_exp-2*p), 1
