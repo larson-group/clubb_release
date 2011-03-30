99003418bc646a7320a0aa23852f4e9a90ba7093
roehl@uwm.edu <roehl@uwm.edu>
2011-03-30 19:20:26 +0000

    "Made it so the tuner automatically checks out the LES data it needs, this should prevent the error of it not having the correct LES data.  The tuner also prints out the time that it started so we can see how long it takes for runs.  It prints this to both stdout and a file which makes it easier to check if it is running when ran as a job."
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5071 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 2f606f338d88f5ec05658e262887a0a2875ad8f9

--- ens_tune/mytuner_sequential.bash
+++ ens_tune/mytuner_sequential.bash
@@ -16,7 +16,7 @@ MEMBERMAX=2 # Total number of ensemble members
 CLUBB=$HOME/clubb
 ENSEMBLE_DIR=`dirname $0` # Directory with this script
 CASE=combined_001 # This plus _error.in is the name of tuner namelist file.
-ARCHIVE=$HOME/archive/ensemble
+ARCHIVE=$HOME/archive/ensemble # Directory for the output
 EXPERIMENTS=( bomex dycoms2_rf01 ) # Add additional simulations here
 
 ################################################################################
@@ -34,7 +34,7 @@ function tune ( ) {
 		cat $CLUBB/ens_tune/${EXP}_stats_tune.in >> $CLUBB/ens_tune/${EXP}_hoc.in
 	done
 	# Execute tuning run
-	echo Tuning ensemble member $member on `hostname`
+	echo Tuning ensemble member $member on `hostname` at `date +"%R %D"` | tee $CLUBB/ens_tune/tuner.log 
 	cd $ENSEMBLE_DIR && echo no | ../bin/clubb_tuner &> tune.log
 
 	# Delete GrADS files from tuning run
@@ -73,6 +73,17 @@ fi
 cat "error_"$CASE".in" $CLUBB/input/tunable_parameters/tunable_parameters.in > "error.in"
 
 cd $ENSEMBLE_DIR
+
+#Make the les_data folder if it doesn't exsist
+mkdir $CLUBB/les_data &> $CLUBB/ens_tune/tuner.log
+
+# Download LES data for experiments
+for EXP in "${EXPERIMENTS[@]}"; do
+	if [ ! -e "$CLUBB/les_data/${EXP}_coamps_sm.ctl" ]; then
+		svn export http://carson.math.uwm.edu/repos/clubb_benchmark_runs/les_runs/${EXP}_coamps_sm.ctl $CLUBB/les_data/${EXP}_coamps_sm.ctl
+		svn export http://carson.math.uwm.edu/repos/clubb_benchmark_runs/les_runs/${EXP}_coamps_sm.dat $CLUBB/les_data/${EXP}_coamps_sm.dat
+	fi
+done
 
 # Loop over the number of ensemble points, incrementing by 1
 for (( MEMBER=1; MEMBER <= $MEMBERMAX; MEMBER++ )); do 
