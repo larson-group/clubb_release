d9ec586f9c957104f918e5d1075c11556ca81ffe
dschanen@uwm.edu <dschanen@uwm.edu>
2011-05-16 20:44:19 +0000

    Added a comment to clarify the purpose of the module.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5180 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 0d3eb4bf2056cd7f9b1d2ee8b392adf93c5fddb8

--- src/CLUBB_core/parameters_tunable.F90
+++ src/CLUBB_core/parameters_tunable.F90
@@ -4,10 +4,17 @@
 module parameters_tunable
 
   ! Description:
-  ! Contains tuneable model parameters_tunable.
-
+  !   This module contains tunable model parameters.  The purpose of the module is to make it 
+  !   easier for the clubb_tuner code to use the params vector without "knowing" any information 
+  !   about the individual parameters contained in the vector itself.  It makes it easier to add 
+  !   new parameters to be tuned for, but does not make the CLUBB_core code itself any simpler.
+  !   The parameters within the vector do not need to be the same variables used in the rest of 
+  !   CLUBB_core (see for e.g. nu1_vert_res_dep or lmin_coef). 
+  !   The parameters in the params vector only need to be those parameters for which we're not 
+  !   sure the correct value and we'd like to tune for.
+  !
   ! References:
-  ! None
+  !   None
   !-----------------------------------------------------------------------
 
   use parameter_indices, only: nparams ! Variable(s)
@@ -109,7 +116,7 @@ module parameters_tunable
 
 !$omp threadprivate(beta)
 
-  real :: lmin_coef ! Coefficient of lmin
+  real, private :: lmin_coef ! Coefficient of lmin
 
 !$omp threadprivate(lmin_coef)
 
