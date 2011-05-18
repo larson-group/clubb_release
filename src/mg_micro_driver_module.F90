73f4a46d86b3afd25c0a97a9b0e24a6e7bd37833
vondeyle@uwm.edu <vondeyle@uwm.edu>
2011-05-18 21:06:11 +0000

    Fixed compiler warning caused by too many continuation characters.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5185 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5fa5a41d2a64ca91fe93c6a07560b1bffd4af920

--- src/mg_micro_driver_module.F90
+++ src/mg_micro_driver_module.F90
@@ -150,47 +150,20 @@ module mg_micro_driver_module
     ! otherwise Fortran treats all the variables are if they point to the same memory
     ! location.
     real, dimension(nnzp-1) :: &
-      unused_out01, &
-      unused_out02, &
-      unused_out03, &
-      unused_out04, &
-      unused_out05, &
-      unused_out06, &
-      unused_out07, &
-      unused_out08, &
-      unused_out09, &
-      unused_out10, &
-      unused_out11, &
-      unused_out12, &
-      unused_out13, &
-      unused_out14, &
-      unused_out15, &
-      unused_out16, &
-      unused_out17, &
-      unused_out18, &
-      unused_out19, &
-      unused_out20, &
-      unused_out21, &
-      unused_out22, &
-      unused_out23, &
-      unused_out24, &
-      unused_out25, &
-      unused_out26, &
-      unused_out27, &
-      unused_out28, &
-      unused_out29, &
-      unused_out30, &
-      unused_out31, &
-      unused_out32, &
-      unused_out33, &
-      unused_out34, &
-      unused_out35, &
-      unused_out36, &
-      unused_out37, &
-      unused_out38, &
-      unused_out39, &
-      unused_out40, &
-      unused_out41
+      unused_out01, unused_out02, unused_out03, &
+      unused_out04, unused_out05, unused_out06, &
+      unused_out07, unused_out08, unused_out09, &
+      unused_out10, unused_out11, unused_out12, &
+      unused_out13, unused_out14, unused_out15, &
+      unused_out16, unused_out17, unused_out18, &
+      unused_out19, unused_out20, unused_out21, &
+      unused_out22, unused_out23, unused_out24, &
+      unused_out25, unused_out26, unused_out27, &
+      unused_out28, unused_out29, unused_out30, &
+      unused_out31, unused_out32, unused_out33, &
+      unused_out34, unused_out35, unused_out36, &
+      unused_out37, unused_out38, unused_out39, &
+      unused_out40, unused_out41
       
     real, dimension(nnzp-1,hydromet_dim) :: &
        hydromet_flip, &  ! Hydrometeor species                               [units vary]
