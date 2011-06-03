b52600960d2bcdba78a9ad484979bdeec6cc14c6
roehl@uwm.edu <roehl@uwm.edu>
2011-06-03 16:28:35 +0000

    Changed it so that MPACE_B will indicate that MPACE_B for gfdl is ran at a 5 minute timestep.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5214 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: abc5e351ed63e3260a7860dbd1f839f796d2a39a

--- postprocessing/plotgen/plotgen.pl
+++ postprocessing/plotgen/plotgen.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###########################################################################
-# Plotgen v3.68
+# Plotgen v3.69
 #
 # Documentation is available here:
 # http://larson-group.com/twiki/bin/view.pl/Documentation/CarsonDoc/Plotgen3
