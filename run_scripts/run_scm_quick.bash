ad8fca9c5fd5a05446ed64db5b4fbb672a14a46b
connork@uwm.edu <connork@uwm.edu>
2011-06-08 17:53:41 +0000

    Added a script, run_scm_quick.bash, which omits five cases from run_scm_all.bash to reduce the time it takes for the cases to run. The cases omitted are gabls2, cloud_feedback_s6, cloud_feedback_s11, cloud_feedback_s12, and twp_ice. See ticket #417
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5218 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 869567a54f1a39d52daa7e4f2d79e54324ed4de0

---
+++ run_scripts/run_scm_quick.bash
@@ -0,0 +1,148 @@
+#!/bin/bash
+#######################################################################
+# $Id: run_scm_all.bash 4642 2010-07-09 16:42:57Z vondeyle@uwm.edu $
+#
+# Script to run the standalone CLUBB program for all models.
+# Tested with bash v2.  Might work with Ksh.
+#
+#######################################################################
+# Useful on multiprocessor machines with OpenMP capable Fortran
+#export OMP_NUM_THREADS=2
+#######################################################################
+
+NIGHTLY=false
+OUTPUT_DIR="/home/`whoami`/nightly_tests/output"
+
+# Figure out the directory where the script is located
+scriptPath=`dirname $0`
+
+# Store the current directory location so it can be restored
+restoreDir=`pwd`
+
+# Change directories to the one the script is located in
+cd $scriptPath
+
+OPTIONS=$*
+
+# Note that we use `"$@"' to let each command-line parameter expand to a 
+# separate word. The quotes around `$@' are essential!
+# We need TEMP as the `eval set --' would nuke the return value of getopt.
+TEMP=`getopt -o :nh --long nightly,help -n 'run_scm_all.bash' -- "$@"`
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+while true ; do
+	case "$1" in
+		-n|--nightly) # Use nightly mode
+            NIGHTLY=true
+            shift ;;
+        -h|--help) # Print the help message
+            echo -e "Usage: run_scm_all.bash [OPTION]..."
+            echo -e "\t-n, --nightly\t\t\tRun in nightly mode"
+            echo -e "\t-h, --help\t\t\tPrints this help message"
+
+            # Since the options for run_scm.bash are also valid, print those too:
+            ./run_scm.bash --help | grep -v "Usage: run_scm.bash" | grep -v "\-\-help"
+
+            exit 1 ;;
+		--) shift ; break ;;
+		*) echo "Something bad happened!" ; exit 1 ;;
+	esac
+done
+
+# Declare arrays that will be used later on in this script.
+declare -a RUN_CASE
+declare -a EXIT_CODES
+declare -a IGNORE_CASES
+
+# Populate the list of cases to ingore here. These ignored cases are the
+# cases that take the longest to run.
+IGNORE_CASES[0]=gabls2
+IGNORE_CASES[1]=cloud_feedback_s6
+IGNORE_CASES[2]=cloud_feedback_s11
+IGNORE_CASES[3]=cloud_feedback_s12
+IGNORE_CASES[4]=twp_ice
+
+a=0
+while read line
+do
+    # If the line is not commented out (does not start with '!')
+    if [[ $line != !* ]] && [[ ! -z $line ]] ; then
+        # Check to see if this case should be ignored
+	ignore=0
+	for (( x=0; x<${#IGNORE_CASES[@]}; x++ )); do
+	    if [ $line == ${IGNORE_CASES[$x]} ] ; then
+		ignore=1
+	    fi
+        done
+	
+	# If the case was found in IGNORE_CASES, don't add it.
+	if [ $ignore == 0 ]; then
+	    RUN_CASE[$a]=$line
+	    a=$(($a+1));
+	fi
+    fi
+done < "RUN_CASES"
+
+# Initialize all elements in EXIT_CODES to 0
+# There will be one EXIT_CODE element for each RUN_CASE
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+	EXIT_CODES[$x]=0
+done
+
+if [ $NIGHTLY == true ] ; then
+    echo -e "\nPerforming nightly run...\n"
+
+    # Make the CLUBB_previous and CLUBB_current directories if they don't exist
+    mkdir -p $OUTPUT_DIR"/CLUBB_current"
+    mkdir -p $OUTPUT_DIR"/CLUBB_previous"
+	
+    # Eliminate the previous CLUBB results.
+    # This prevents spurious profile generation resulting from
+    # previous profiles not getting overwritten
+    rm -f $OUTPUT_DIR"/CLUBB_previous/*"
+
+    mv $OUTPUT_DIR/CLUBB_current/*.ctl $OUTPUT_DIR/CLUBB_previous/
+    mv $OUTPUT_DIR/CLUBB_current/*.dat $OUTPUT_DIR/CLUBB_previous/
+else
+    echo -e "\nPerforming standard run\n"
+fi
+
+# This will loop over all runs in sequence 
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); 
+do
+    echo -e "Running ${RUN_CASE[$x]}"
+    
+    if [ $NIGHTLY == true ] ; then
+      RESULT=`./run_scm.bash $OPTIONS ${RUN_CASE[$x]} 2>&1`
+      echo -e "$RESULT"
+    else
+      # Send standard output to the bit bucket so only error output is recorded
+      RESULT=`./run_scm.bash $OPTIONS ${RUN_CASE[$x]} 2>&1 >/dev/null`
+    fi
+
+    RESULT_STATUS=`echo "$RESULT" | grep 'normally'`
+
+    if [ -z "$RESULT_STATUS" ]; then
+        EXIT_CODES[$x]=-1
+       
+        # If there was an error, and this is not running in nightly mode,
+        # it will not be displayed. So, display the error here.
+        if [ $NIGHTLY != true ] ; then
+            echo -e "$RESULT" | tail
+        fi
+    fi
+done
+
+EXIT_STATUS=0
+
+# Print the results and copy files for a nightly run
+for (( x=0; x < "${#RUN_CASE[@]}"; x++ )); do
+	if [ "${EXIT_CODES[$x]}" != 0 ]; then
+		echo "${RUN_CASE[$x]}"' failure'
+        EXIT_STATUS=1
+ 	fi
+done
+
+exit $EXIT_STATUS
