2b51669231d768279fa3498a963d91b10cb75167
roehl@uwm.edu <roehl@uwm.edu>
2011-06-17 18:51:04 +0000

    Prevented plotgen from trying to plot cloud ice concentration for mpace_b, because it needs pf_forc to calculate rho and pf_forc is not available.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5251 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: a4069274fbc387fcfd0f25e37f3b269c054c3dd2

--- postprocessing/plotgen/plotgen.pl
+++ postprocessing/plotgen/plotgen.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###########################################################################
-# Plotgen v3.75
+# Plotgen v3.78
 #
 # Documentation is available here:
 # http://larson-group.com/twiki/bin/view.pl/Documentation/CarsonDoc/Plotgen3
