larson-group/clubb_release

commit d8c87097f488ba2fe6794f54ee690231e2d1c4d0

For ticket #390.  Added upwind differencing for the turbulent advection variance terms (currently disabled).
git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5074 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: 56e3176e6e7524a529023d05fe49c4369800d82e

src/CLUBB_core/clubb_core.F90
@@ -1184,7 +1184,7 @@ module clubb_core
                            sigma_sqd_w, Skw_zm, Kh_zt,    & ! intent(in)
                            rho_ds_zm, rho_ds_zt,          & ! intent(in)
                            invrs_rho_ds_zm, thv_ds_zm,    & ! intent(in)
-                           Lscale, wp3_on_wp2_zt,         & ! intent(in)
+                           Lscale, wp3_on_wp2, wp3_on_wp2_zt, & ! intent(in)
  ! Vince Larson used prognostic timestepping of variances 
  !    in order to increase numerical stability.  17 Jul 2007
  !                          .false., dt,                   & ! intent(in)

src/CLUBB_core/model_flags.F90
@@ -52,8 +52,13 @@ module model_flags
 ! OpenMP directives. These cannot be indented.
 !$omp threadprivate(l_uv_nudge, l_tke_aniso, l_soil_veg)
 
+  ! These 2 flags determine whether we want to use an upwind differencing approximation 
+  ! rather than a centered differencing for turbulent advection terms.
+  ! wpxp = wprtp, wpthlp, & wpsclrp
+  ! xpyp = rtp2, thlp2, up2, vp2, sclrp2, rtpthlp, sclrprtp, & sclrpthlp
   logical, parameter, public :: & 
-    l_upwind_wpxp_ta = .false. ! Whether to use upwind rather than centered differencing for wpxp
+    l_upwind_wpxp_ta = .false., & 
+    l_upwind_xpyp_ta = .false.    
 
   logical, parameter, public :: &
     l_use_boussinesq = .false.  ! Flag to use the Boussinesq form of the

commit 1efe467e97dad118f60cbbb574cc8276a8c42ebd

We noticed an issue in the plots for Cobra and GABLS 2 relating to passive scalars the other day.  After some searching we determined the change occured on 31 March 2011, when the upwind differencing approximation was added for the variance terms.  I found a bug in the arguments being passed to xp2_xpyp_rhs that I introduced in r5074 (mea culpa on that) and corrected it here.
git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5260 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: e4a0933ce1d9b2fb0ba9ffc6c0f50e12bc941a45

src/CLUBB_core/advance_xp2_xpyp_module.F90
@@ -666,14 +666,13 @@ module advance_xp2_xpyp_module
 
         !!!!!***** sclr'^2 *****!!!!!
 
-        call xp2_xpyp_rhs( xp2_xpyp_sclrp2, dt, l_iter, a1, a1_zt, &  ! Intent(in)
-                           wp2_zt, wpsclrp(:,i),  &              ! Intent(in)
-                           wpsclrp_zt, wpsclrp(:,i), wpsclrp_zt,  &   ! Intent(in)
-                           wp3_on_wp2, wp3_on_wp2_zt, &           ! Intent(in)
-                           sclrm(:,i), sclrm(:,i), sclrp2(:,i), &     ! Intent(in)
-                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, &  ! Intent(in)
-                           C2sclr_1d, tau_zm, sclr_tol(i)**2, beta, & ! Intent(in)
-                           sclr_rhs(:,i) )                            ! Intent(out)
+        call xp2_xpyp_rhs( xp2_xpyp_sclrp2, dt, l_iter, a1, a1_zt, & ! In
+                           wp2_zt, wpsclrp(:,i), wpsclrp_zt, & ! In
+                           wp3_on_wp2, wp3_on_wp2_zt, & ! In
+                           wpsclrp(:,i), wpsclrp_zt, sclrm(:,i), sclrm(:,i), sclrp2(:,i), &  ! In
+                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, &  ! In
+                           C2sclr_1d, tau_zm, sclr_tol(i)**2, beta, & ! In
+                           sclr_rhs(:,i) ) ! Out
 
 
         !!!!!***** sclr'r_t' *****!!!!!
@@ -686,14 +685,13 @@ module advance_xp2_xpyp_module
           threshold = 0.0
         end if
 
-        call xp2_xpyp_rhs( xp2_xpyp_sclrprtp, dt, l_iter, a1, a1_zt, & ! Intent(in)
-                           wp2_zt, wpsclrp(:,i),  &             ! Intent(in)
-                           wpsclrp_zt, wprtp, wprtp_zt,  &           ! Intent(in)
-                           wp3_on_wp2, wp3_on_wp2_zt, &           ! Intent(in)
-                           sclrm(:,i), rtm, sclrprtp(:,i),  &        ! Intent(in)
-                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, &  ! Intent(in)
-                           C2sclr_1d, tau_zm, threshold, beta, &     ! Intent(in)
-                           sclr_rhs(:,i+sclr_dim) )                  ! Intent(out)
+        call xp2_xpyp_rhs( xp2_xpyp_sclrprtp, dt, l_iter, a1, a1_zt, & ! In
+                           wp2_zt, wpsclrp(:,i), wpsclrp_zt,  & ! In
+                           wp3_on_wp2, wp3_on_wp2_zt, & ! In
+                           wprtp, wprtp_zt, sclrm(:,i), rtm, sclrprtp(:,i), & ! In
+                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, &  ! In
+                           C2sclr_1d, tau_zm, threshold, beta, &  ! In
+                           sclr_rhs(:,i+sclr_dim) ) ! In
 
 
         !!!!!***** sclr'th_l' *****!!!!!
@@ -706,14 +704,13 @@ module advance_xp2_xpyp_module
           threshold = 0.0
         end if
 
-        call xp2_xpyp_rhs( xp2_xpyp_sclrpthlp, dt, l_iter, a1, a1_zt, & ! Intent(in)
-                           wp2_zt, wpsclrp(:,i),  &             ! Intent(in)
-                           wpsclrp_zt, wpthlp, wpthlp_zt,  &         ! Intent(in)
-                           wp3_on_wp2, wp3_on_wp2_zt, &           ! Intent(in)
-                           sclrm(:,i), thlm, sclrpthlp(:,i), &       ! Intent(in)
-                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, &  ! Intent(in)
-                           C2sclr_1d, tau_zm, threshold, beta, &     ! Intent(in)
-                           sclr_rhs(:,i+2*sclr_dim) )                ! Intent(out)
+        call xp2_xpyp_rhs( xp2_xpyp_sclrpthlp, dt, l_iter, a1, a1_zt, & ! In
+                           wp2_zt, wpsclrp(:,i), wpsclrp_zt, &  ! In
+                           wp3_on_wp2, wp3_on_wp2_zt, & ! In
+                           wpthlp, wpthlp_zt, sclrm(:,i), thlm, sclrpthlp(:,i), & ! In
+                           rho_ds_zm, rho_ds_zt, invrs_rho_ds_zm, & ! In
+                           C2sclr_1d, tau_zm, threshold, beta, & ! In
+                           sclr_rhs(:,i+2*sclr_dim) ) ! Out
       end do ! 1..sclr_dim
 
 

src/CLUBB_core/clubb_core.F90
@@ -1202,7 +1202,7 @@ module clubb_core
                            rtp2, thlp2, rtpthlp,          & ! intent(inout)
                            up2, vp2,                      & ! intent(inout)
                            err_code,                      & ! intent(out)
-                           sclrp2, sclrprtp, sclrpthlp    ) ! intent(out)
+                           sclrp2, sclrprtp, sclrpthlp    ) ! intent(inout)
 
     ! Check stability
     ! Changed from a logical flag to an integer indicating nature of

src/CLUBB_core/stats_type.F90
@@ -288,15 +288,14 @@ module stats_type
 
        else
 
-          write(char_index(1:3),'(i3.3)') var_index
-          call clubb_debug( 1, & 
-          "Beginning an update before finishing previous.  "// & 
-          "Var index = "// char_index   )
-
+          call clubb_debug( 1, &
+            "Beginning an update before finishing previous for variable: "// & 
+            trim( grid_kind%f%var(var_index)%name ) )
        endif
 
     endif
 
+    return
   end subroutine stat_begin_update_pt
 
   !=============================================================================