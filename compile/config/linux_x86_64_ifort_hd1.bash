b0d9d9f4778430f2a4e2389e192bb692ac2c6058
ldgrant@uwm.edu <ldgrant@uwm.edu>
2011-06-27 16:58:13 +0000

    I edited the configure file for compiling CLUBB on hd1 with Intel. I changed the path to the LAPACK libraries for the new Intel installation, Intel Composer XE.  I also had to delete two flags for libraries because they weren't found anymore.  Lastly, I changed the FFLAGS to use optimization instead of debugging so that CLUBB and WRF-CLUBB are consistent. For wrf:#13
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5279 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: c6035927c7feb666a832849917a6a81436b8ce74

--- compile/config/linux_x86_64_ifort_hd1.bash
+++ compile/config/linux_x86_64_ifort_hd1.bash
@@ -27,21 +27,22 @@ ARCH="-fp-model precise" # This should work on carson/steele (AMD Opteron)
 OPTIMIZE="-O3 -vec-report0"
 
 # == NetCDF Location ==
-NETCDF="/sharedapps/LS/vlarson_group/local/netcdf-intel64/"
+NETCDF="/sharedapps/LS/vlarson_group/local/netcdf-intel64"
 
 # == LAPACK libraries ==
 # Intel Math Kernel Library (v10.1)
-MKLPATH="/sharedapps/uwm/common/intel-fortran-compiler/11.1.072-v1/mkl/lib/em64t"
+#MKLPATH="/sharedapps/uwm/common/intel-fortran-compiler/11.1.072-v1/mkl/lib/em64t"
+MKLPATH="/sharedapps/uwm/common/intel-compiler/composerxe-2011.2.137-v1/mkl/lib/intel64"
 
 
-LAPACK="-L$MKLPATH -Wl,-rpath,$MKLPATH -lmkl_intel_lp64 -lmkl_sequential -lmkl_lapack -lmkl_solver_lp64 -lmkl_core -lguide -lpthread"
+LAPACK="-L$MKLPATH -Wl,-rpath,$MKLPATH -lmkl_intel_lp64 -lmkl_sequential -lmkl_solver_lp64 -lmkl_core -lpthread"
 
 # == Linking Flags ==
 # Use -s to strip (no debugging); 
 # Use -L<library path> -l<lib> to link in an external library
 LDFLAGS="-L$NETCDF/lib -lnetcdf $LAPACK"
 
-FFLAGS="$ARCH $DEBUG"
+FFLAGS="$ARCH $OPTIMIZE"
 
 # Preprocessing Directives:
 #   -DNETCDF enables netCDF output
