f91649b4b338c04795bf494bac6bd5ace4c94fd6
dschanen@uwm.edu <dschanen@uwm.edu>
2011-11-22 18:15:25 +0000

    Changed -D__sun to -DNO_LAPACK_ISNAN.  This is should be less confusing for users and make is more obvious how to to deal with a library that lacks the functions sisnan & disnan when compiling CLUBB on a new computer.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5525 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ea510231095af6c924787ba0cead3e26882e0d6d

--- compile/config/linux_x86_64_oracle_debug.bash
+++ compile/config/linux_x86_64_oracle_debug.bash
@@ -53,7 +53,7 @@ FFLAGS="$ARCH $DEBUG"
 #   -Dradoffline and -Dnooverlap (see bugsrad documentation)
 # Define include directories. 
 # Need location of include and *.mod files for the netcdf library
-CPPDEFS="-DNETCDF -Dnooverlap -Dradoffline -D__sun"
+CPPDEFS="-DNETCDF -Dnooverlap -Dradoffline -DNO_LAPACK_ISNAN"
 CPPFLAGS="-M$NETCDF/include"
 
 # == Static library processing ==
