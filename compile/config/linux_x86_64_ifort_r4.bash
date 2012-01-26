e7dc2ab2be2e4022e25d1b056673d601619707ba
dschanen@uwm.edu <dschanen@uwm.edu>
2012-01-26 18:27:54 +0000

    Added a 4 byte real configuration for use with WRF-CLUBB.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5662 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: c94988720106a340404c0a55dadfb350c9416db9

---
+++ compile/config/linux_x86_64_ifort_r4.bash
@@ -0,0 +1,73 @@
+# $Id: linux_x86_64_ifort.bash 5656 2012-01-24 19:53:22Z connork@uwm.edu $
+# Makefile definitions customized for Linux x86_64 using the Intel Fortran 
+# compiler 
+
+
+# Fortran 95 compiler and linker
+FC=ifort
+LD=ifort
+
+# Define path to directories
+dir=`pwd` # dir where this script resides
+bindir="$dir/../bin"  # dir for Makefile and executable
+objdir="$dir/../obj"  # dir for *.o and *.mod files
+libdir="$dir/../lib"  # dir for *.a library files
+srcdir="$dir/../src"  # dir where the source files reside
+
+DEBUG="-g -traceback -check bounds -check uninit"
+
+# == Warnings ==
+WARNINGS="-warn -warn notruncated_source"
+
+# == Machine specific options ==
+ARCH="-msse2 -fp-model precise" # This should work on carson/steele (AMD Opteron)
+#ARCH="-mssse3 -fp-model precise"# Core2 Duo (overlie)
+# == Used to promote all real's to double precision ==
+DOUBLE_PRECISION="-real-size 64"
+
+# == Optimization ==
+OPTIMIZE="-O3 -vec-report0"
+
+# == NetCDF Location ==
+NETCDF="/usr/local/netcdf-intel64"
+
+# == LAPACK libraries ==
+# Intel Math Kernel Library (v11.1)
+MKLPATH="/opt/intel/Compiler/11.1/064/mkl/lib/em64t"
+LAPACK="-L$MKLPATH -Wl,-rpath,$MKLPATH -lmkl_intel_lp64 -lmkl_sequential -lmkl_lapack -lmkl_solver_lp64 -lmkl_core -lguide -lpthread"
+# Generic library
+#LAPACK="-llapack -lblas -lgfortran"
+
+# == Linking Flags ==
+# Use -s to strip (no debugging); 
+# Use -L<library path> -l<lib> to link in an external library
+LDFLAGS="-L$NETCDF/lib -lnetcdf $LAPACK"
+
+FFLAGS="$ARCH $OPTIMIZE"
+
+# Preprocessing Directives:
+#   -DNETCDF enables netCDF output
+#   -Dradoffline and -Dnooverlap (see bugsrad documentation)
+#   -DMKL enables MKL solver (PARDISO/GMRES) support
+# You will need to `make clean' if you change these
+# Use -I<include path> to set a module or header file directory
+# Define include directories. 
+# Need location of include and *.mod files for the netcdf library
+
+CPPDEFS="-DNETCDF -Dnooverlap -Dradoffline -DMKL -DCLUBB_REAL_TYPE=4"
+CPPFLAGS="-I$MKLPATH/../../include -I$NETCDF/include"
+
+# == Static library processing ==
+AR=ar
+ARFLAGS=cru
+RANLIB=ranlib
+
+# == Shared library processing ==
+SHARED=$FC
+SHAREDFLAGS="-fPIC -shared"
+
+# Location of 'mkmf' utility
+mkmf=$dir/mkmf
+
+# gmake command to use and options: '-j 2' enables parallel compilation
+gmake="make"
