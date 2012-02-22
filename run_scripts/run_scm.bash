1942f0571880e1772dc721ae9e53d83e76835680
meyern@uwm.edu <meyern@uwm.edu>
2012-02-22 18:44:57 +0000

    For ticket #486, adding the CGILS cases to run twice for the nightly tests as other cases do to get better timeseries output in the sfc file.  This should make the timeseries plots appear correctly on the nightly test plots.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5746 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 7d01e7cb36d2d131ff366d348593eee2984b1dd9

--- run_scripts/run_scm.bash
+++ run_scripts/run_scm.bash
@@ -288,7 +288,7 @@ then
 			;;
 		* )
 			case $run_case in 
-				rico | astex_a209 | gabls3_night )
+				rico | astex_a209 | gabls3_night | cgils* )
 					# This was added because RICO uses a 300 s timestep
 					# and cannot be run with stats_tout = 60.
 					cat $parameter_file > $NAMELISTS
