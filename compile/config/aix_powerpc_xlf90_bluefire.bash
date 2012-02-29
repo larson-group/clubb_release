e83d7e2347c0146424479d6a47bbeb9e4b1eeacb
dschanen@uwm.edu <dschanen@uwm.edu>
2012-02-29 18:04:00 +0000

    Changed the default precision to be 4 bytes rather than 8.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5757 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: d69be5d7a7834ff485654c219159afbb8180821c

--- compile/config/aix_powerpc_xlf90_bluefire.bash
+++ compile/config/aix_powerpc_xlf90_bluefire.bash
@@ -1,7 +1,6 @@
 # $Id$
-# Makefile definitions customized for IBM AIX on PowerPC using the XL
-# Fortran compiler.
-
+# Makefile definitions customized for IBM AIX on the Power architecture using the XL
+# Fortran compiler on Bluefire.
 
 # Fortran 95 compiler and linker
 F90=xlf90_r
@@ -21,9 +20,7 @@ DEBUG="-g"
 WARNINGS=""
 
 # == Machine specific options ==
-#ARCH="-DNONSTANDARD_SYSTEM_SUBR -DNATIVE_MASSV -DCLUBB" 
 ARCH="-qnoescape" 
-#ARCH="-mssse3 -fp-model precise"# Core2 Duo (overlie)
 
 # == Optimization ==
 OPTIMIZE="-O3"
@@ -32,20 +29,7 @@ OPTIMIZE="-O3"
 NETCDF="/usr/local/netcdf"
 
 # == LAPACK libraries ==
-# Intel Math Kernel Library (v11.1)
-#MKLPATH="/opt/intel/Compiler/11.1/064/mkl/lib/em64t"
-# Intel Math Kernel Library (v10)
-#MKLPATH=/opt/intel/mkl/10.0.5.025/lib/em64t 
-# Intel Math Kernel Library (v10.2)
-#MKLPATH="/opt/intel/mkl/10.2.5.035/lib/em64t"
-#LAPACK="-L$MKLPATH -Wl,-rpath,$MKLPATH -lmkl_intel_lp64 -lmkl_sequential -lmkl_lapack -lmkl_solver_lp64 -lmkl_core -lguide -lpthread"
-# (v8)
-#LAPACK="-L/opt/intel/mkl/8.1/lib/64 -Wl,-rpath,/opt/intel/mkl/8.1/lib/64 -lmkl_lapack64 -lmkl_i2p -lmkl -lmkl_vml_i2p -lmkl_vml -lvml -lguide -lpthread"
-# Generic library
-#LAPACK="-llapack -lblas -lgfortran"
-#LAPACK="-L/usr/local/lib -llapack -L/usr/local/atlas/lib -lf77blas -lcblas -latlas" # ATLAS BLAS (faster then generic library)
-# Use Bluefire's native IBM XL Fortran version of LAPACK: 
-#LAPACK="-qessl"
+# Use Bluefire's version of LAPACK: 
 LAPACK="-L/contrib/lapack/3.2.2/lib"
 BLAS="-L/contrib/blas/lib"
 
@@ -53,27 +37,20 @@ BLAS="-L/contrib/blas/lib"
 # == Linking Flags ==
 # Use -s to strip (no debugging); 
 # Use -L<library path> -l<lib> to link in an external library
-# "-lessl" links the IBM XL Fortran version of LAPACK
-#LDFLAGS="-L$NETCDF/lib -lnetcdf -lessl"
 LDFLAGS="-L$NETCDF/lib -lnetcdf $LAPACK -llapack $BLAS -lblas"
 
 #FFLAGS="$ARCH $DEBUG $LAPACK"
 FFLAGS="$ARCH $OPTIMIZE"
 
 # Preprocessing Directives:
-#   -DNETCDF enables netCDF output
-#   -Dradoffline and -Dnooverlap (see bugsrad documentation)
-#   -DMKL enables MKL solver (PARDISO/GMRES) support
+#   -DNETCDF enables netCDF formet for stats output
+#   -Dradoffline and -Dnooverlap (see BUGSrad documentation)
 # You will need to `make clean' if you change these
 # Use -I<include path> to set a module or header file directory
 # Define include directories. 
 # Need location of include and *.mod files for the netcdf library
-
-#CPPFLAGS="-DNETCDF -I$MKLPATH/../../include -I$NETCDF/include -Dnooverlap -Dradoffline -DMKL"
-#CPPFLAGS="-WF,-DNETCDF,-qmoddir=$NETCDF/include,-Dnooverlap,-Dradoffline"
-#CPPFLAGS="-I$NETCDF/include -WF,-DNETCDF,-Dnooverlap,-Dradoffline"
 CPPFLAGS="-I$NETCDF/include"
-CPPDEFS="-DNETCDF -Dnooverlap -Dradoffline -DCLUBB_REAL_TYPE=8"
+CPPDEFS="-DNETCDF -Dnooverlap -Dradoffline -DCLUBB_REAL_TYPE=4"
 
 # == Static library processing ==
 AR=ar
