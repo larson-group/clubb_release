166023e2c0bcf2373bd60a1b102e51b455c3bf06
dschanen@uwm.edu <dschanen@uwm.edu>
2012-04-13 18:48:15 +0000

    Corrected a bug in how the velocity variables are output.  Since the LH_zt file is the same grid as the zt file, then LH_Vrr and LH_VNr should be on zt grid.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5796 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 077b4c2b4141971489bc3e6192222c1196f85d8f

--- src/microphys_driver.F90
+++ src/microphys_driver.F90
@@ -1445,9 +1445,9 @@ module microphys_driver
 
         if ( l_stats_samp ) then
           ! Latin hypercube estimate for sedimentation velocities
-          call stat_update_var( iLH_Vrr, zt2zm( hydromet_vel_zt(:,iirrainm) ), LH_zt )
+          call stat_update_var( iLH_Vrr, hydromet_vel_zt(:,iirrainm), LH_zt )
 
-          call stat_update_var( iLH_VNr, zt2zm( hydromet_vel_zt(:,iiNrm) ), LH_zt )
+          call stat_update_var( iLH_VNr, hydromet_vel_zt(:,iiNrm), LH_zt )
 
         end if
 
