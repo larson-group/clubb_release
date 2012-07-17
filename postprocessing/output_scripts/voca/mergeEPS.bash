c5641f03af9991cf4ecfe7d6098989e5d8117d72
janhft@uwm.edu <janhft@uwm.edu>
2012-07-17 23:24:09 +0000

    This script combines all eps-files contained in the input directory to one pdf file.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5875 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: b73d7f27dc139dc03e4847c04260f44eeeb29305

---
+++ postprocessing/output_scripts/voca/mergeEPS.bash
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+#created by janhft 07/17/2012
+
+myhelp()
+{ 
+echo "------------------------------------------------------------------------------------"
+echo "This script combines all eps-files contained in the input directory to one pdf file." 
+echo ""
+echo "Usage: ./mergeEPS.bash [-f|h] <indir> <outfile>"
+echo ""
+echo "Options:"
+echo "-h -- show this page"
+echo "-f -- force overwrite if <outfile> already exists"
+echo "------------------------------------------------------------------------------------"
+}
+
+# parse options
+force=false
+
+while getopts fh opt
+do
+  case "$opt" in
+    f) force=true; shift;;
+    h) myhelp; exit;;
+    \?) echo "Error: Unknown option $opt."; myhelp;;
+  esac
+done
+
+
+# check arguments
+indir=$1
+outfile=$2
+overwrite='no'
+
+if [ ! -d $indir ]; then
+	echo "ERROR: Input directory does not exist!"
+	exit
+fi
+
+if [ -f $outfile ] && [ $force = false ]; then
+	echo "Outputfile already exists. Do you want to overwrite it? (yes/no)"
+	read overwrite
+	
+	if [ "$overwrite" != "yes" ] && [ "$overwrite" != "y" ]; then
+		echo "Operation canceled by user. No pdf file has been written."		
+		exit
+	fi
+fi
+
+#get eps graphics from indir
+k=0
+for curFile in $indir*; do
+	if [ "${curFile/*./}" = "eps" ]; then
+		files[$k]=$curFile;
+		let k=k+1;
+	fi	
+done
+
+#write tex-file
+if [ -d mergeEPStmp ]; then
+	rm -r mergeEPStmp
+fi
+mkdir mergeEPStmp
+touch mergeEPStmp/tmp.tex
+
+#header
+echo '\documentclass[letterpaper,12pt]{article}'>>mergeEPStmp/tmp.tex
+echo '\usepackage{graphicx}'>>mergeEPStmp/tmp.tex
+echo '\begin{document}'>>mergeEPStmp/tmp.tex
+echo '\thispagestyle{empty}'>>mergeEPStmp/tmp.tex
+
+#insert figures from eps-graphics 
+#figure 0
+echo '\begin{figure}'>>mergeEPStmp/tmp.tex
+echo '\centering'>>mergeEPStmp/tmp.tex
+echo '\fbox{'>>mergeEPStmp/tmp.tex
+echo '	\includegraphics[width=\textwidth]{'${files[0]%.*}'}'>>mergeEPStmp/tmp.tex
+echo '}'>>mergeEPStmp/tmp.tex
+echo '\end{figure}'>>mergeEPStmp/tmp.tex
+echo '\clearpage'>>mergeEPStmp/tmp.tex
+
+#remaining figures
+let n=k-1
+for i in `seq 1 $n`; do
+	echo '\begin{figure}'>>mergeEPStmp/tmp.tex
+	echo '\centering'>>mergeEPStmp/tmp.tex
+	echo '\fbox{'>>mergeEPStmp/tmp.tex
+	echo '	\includegraphics[width=\textwidth]{'${files[$i]%.*}'}'>>mergeEPStmp/tmp.tex
+	echo '}'>>mergeEPStmp/tmp.tex
+	echo '\end{figure}'>>mergeEPStmp/tmp.tex
+	echo '\clearpage'>>mergeEPStmp/tmp.tex
+done
+echo '\end{document}'>>mergeEPStmp/tmp.tex
+
+#convert tex->pdf
+cd mergeEPStmp
+pdflatex tmp.tex >> /dev/null
+mv tmp.pdf $outfile
+cd ..
+
+#clean up
+rm -r mergeEPStmp
+
+echo "Operation successful. Output has been written to $outfile."
+
