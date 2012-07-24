0e8d14126f2c67efeb2db5517bbc7fc191b0a361
bmg2@uwm.edu <bmg2@uwm.edu>
2012-07-24 17:52:24 +0000

    I am adding the KK integrals unit testing framework to the svn repository. Currently, the program produces all results matching within a very small tolerance.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@5886 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 155200b4fea777963f47481c70bd6d574e18f611

---
+++ src/G_unit_tests.F90
@@ -0,0 +1,26 @@
+! $Id$
+!===============================================================================
+program G_unit_tests
+
+  ! Description:
+
+  ! References:
+  !-------------------------------------------------------------------------
+
+  use KK_integrals_tests, only: &
+      KK_integrals_tests_driver  ! Procedure(s)
+
+  implicit none
+
+  ! Variables
+  logical, parameter :: l_KK_unit_tests = .true.
+
+
+  if ( l_KK_unit_tests ) then
+     call KK_integrals_tests_driver
+  endif
+
+
+!===============================================================================
+
+end program G_unit_tests
