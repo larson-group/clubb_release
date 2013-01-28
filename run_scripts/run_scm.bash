194b7cd49223154dc2e1fc7d65ebd646cd257e6a
raut@uwm.edu <raut@uwm.edu>
2013-01-28 19:46:28 +0000

    Modified run_scm.bash to not use the atrocious "mktemp" command that didn't work properly on some computers.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6027 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 716d5d36bda6d228bec1eb98d072228c4db84320

--- run_scripts/run_scm.bash
+++ run_scripts/run_scm.bash
@@ -347,7 +347,9 @@ else
 	# "cat" the parameter_file and FLAGS_FILE into NAMELISTS for all cases
 	cat $parameter_file $FLAGS_FILE > $NAMELISTS
 	# Create a modified model file that will be edited later
-	MOD_MODEL_FILE=`mktemp`
+	MOD_MODEL_FILE="MOD_MODEL_TEMP_$RANDOM"
+	#TEST
+	echo $MOD_MODEL_FILE
 	cat $model_file > $MOD_MODEL_FILE
 	if [ $TIMESTEP_TEST == true ]; 
 	then
