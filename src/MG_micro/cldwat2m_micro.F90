larson-group/clubb_release

commit 59751d2f9b14344cf1ebe8237f210a9a424b467a

For clubb:ticket:514. I set up {{{setup_pdf_params.F90}}} for the use of the diagnose correlations code. Doing that I split up {{{comp_mean_stdev_corr}}} into {{{comp_mean_stdev}}} and {{{comp_corr}}}. I also cleaned up the code a bit.
git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6310 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: d448f9e37203255dc556034a63cbe4286092244e

src/MG_micro/cldwat2m_micro.F90
@@ -404,7 +404,8 @@ subroutine mmicro_pcond ( sub_column,           &
 
    ! Upscaled KK for autoconversion and accretion
    use setup_clubb_pdf_params, only: &
-       comp_mean_stdev_corr, & ! Procedure(s)
+       comp_mean_stdev, & ! Procedure(s)
+       comp_corr, &
        normalize_pdf_params
 
    use KK_upscaled_means, only: &
@@ -1760,43 +1761,66 @@ subroutine mmicro_pcond ( sub_column,           &
 
                  mixt_frac = pdf_params(k)%mixt_frac
 
-                 call comp_mean_stdev_corr &
-                                ( real( qc(i,k), kind = core_rknd ), & ! In
-                                  real( qric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( nric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( nc(i,k), kind = core_rknd ), &
-                                  real( qric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( qric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( nric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( nric(i,k) * cldmax(i,k), &
-                                        kind = core_rknd ), &
-                                  real( qc(i,k), kind = core_rknd ), &
-                                  real( qc(i,k), kind = core_rknd ), &
-                                  real( lcldm(i,k), kind = core_rknd ), &
-                                  real( lcldm(i,k), kind = core_rknd ), &
-                                  one, one, &
-                                  zero, zero, zero, &
-                                  zero, zero, mixt_frac, &
-                                  pdf_params(k), &
-                                  mu_w_1, mu_w_2, mu_s_1, mu_s_2, mu_t_1, & !Out
-                                  mu_t_2, mu_rr_1, mu_rr_2, mu_Nr_1, mu_Nr_2, &
-                                  mu_Ncn_1, mu_Ncn_2, sigma_w_1, sigma_w_2, &
-                                  sigma_s_1, sigma_s_2, sigma_t_1, sigma_t_2, &
-                                  sigma_rr_1, sigma_rr_2, sigma_Nr_1, &
-                                  sigma_Nr_2, sigma_Ncn_1, sigma_Ncn_2, &
-                                  corr_ws_1, corr_ws_2, corr_wrr_1, &
-                                  corr_wrr_2, corr_wNr_1, corr_wNr_2, &
-                                  corr_wNcn_1, corr_wNcn_2, corr_st_1, &
-                                  corr_st_2, corr_srr_1, corr_srr_2, &
-                                  corr_sNr_1, corr_sNr_2, corr_sNcn_1, &
-                                  corr_sNcn_2, corr_trr_1, corr_trr_2, &
-                                  corr_tNr_1, corr_tNr_2, corr_tNcn_1, &
-                                  corr_tNcn_2, corr_rrNr_1, corr_rrNr_2 )
+                 call comp_mean_stdev( real( qc(i,k), kind = core_rknd ), & ! In
+                                       real( qric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( nric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( nc(i,k), kind = core_rknd ), &
+                                       real( qric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( qric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( nric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( nric(i,k) * cldmax(i,k), &
+                                             kind = core_rknd ), &
+                                       real( qc(i,k), kind = core_rknd ), &
+                                       real( qc(i,k), kind = core_rknd ), &
+                                       real( lcldm(i,k), kind = core_rknd ), &
+                                       real( lcldm(i,k), kind = core_rknd ), &
+                                       one, one, &
+                                       zero, zero, zero, &
+                                       zero, zero, mixt_frac, &
+                                       pdf_params(k), &
+                                       mu_w_1, mu_w_2, mu_s_1, mu_s_2, mu_t_1, & ! Out
+                                       mu_t_2, mu_rr_1, mu_rr_2, mu_Nr_1, mu_Nr_2, &
+                                       mu_Ncn_1, mu_Ncn_2, sigma_w_1, sigma_w_2, &
+                                       sigma_s_1, sigma_s_2, sigma_t_1, sigma_t_2, &
+                                       sigma_rr_1, sigma_rr_2, sigma_Nr_1, &
+                                       sigma_Nr_2, sigma_Ncn_1, sigma_Ncn_2 )
+
+                 call comp_corr( real( qc(i,k), kind = core_rknd ), & ! In
+                                 real( qric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( nric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( nc(i,k), kind = core_rknd ), &
+                                 real( qric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( qric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( nric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( nric(i,k) * cldmax(i,k), &
+                                       kind = core_rknd ), &
+                                 real( qc(i,k), kind = core_rknd ), &
+                                 real( qc(i,k), kind = core_rknd ), &
+                                 real( lcldm(i,k), kind = core_rknd ), &
+                                 real( lcldm(i,k), kind = core_rknd ), &
+                                 one, one, &
+                                 zero, zero, zero, &
+                                 zero, zero, mixt_frac, &
+                                 sigma_rr_1, sigma_Nr_1, sigma_Ncn_1, &
+                                 pdf_params(k), &
+                                 corr_ws_1, corr_ws_2, corr_wrr_1, & ! Out
+                                 corr_wrr_2, corr_wNr_1, corr_wNr_2, &
+                                 corr_wNcn_1, corr_wNcn_2, corr_st_1, &
+                                 corr_st_2, corr_srr_1, corr_srr_2, &
+                                 corr_sNr_1, corr_sNr_2, corr_sNcn_1, &
+                                 corr_sNcn_2, corr_trr_1, corr_trr_2, &
+                                 corr_tNr_1, corr_tNr_2, corr_tNcn_1, &
+                                 corr_tNcn_2, corr_rrNr_1, corr_rrNr_2 )
 
                  call normalize_pdf_params &
                                 ( real( qric(i,k) * cldmax(i,k), & ! In

src/clubb_driver.F90
@@ -1215,10 +1215,6 @@ module clubb_driver
 
       wp2_zt = max( zm2zt( wp2 ), w_tol_sqd ) ! Positive definite quantity
 
-      if ( itime == 50 ) then
-         print *, "Debugging ..."
-      endif
-
       if ( .not. trim( micro_scheme ) == "none" ) then
 
          rrainm = hydromet(:,iirrainm)

src/KK_microphys_module.F90
@@ -362,11 +362,7 @@ module KK_microphys_module
         l_const_Nc_in_cloud    ! Flag to use a const. value of N_c within cloud
 
     use setup_clubb_pdf_params, only: &
-        setup_pdf_parameters, & ! Procedure(s)
-        unpack_pdf_params, &
-        normalize_pdf_params, &
-        pdf_param_hm_stats, &
-        pdf_param_log_hm_stats
+        unpack_pdf_params ! Procedure(s)
 
     use KK_upscaled_means, only: &
         KK_upscaled_means_driver ! Procedure(s)

commit bdf570da1fb78a1aeefdfe0b6c438f36b82c9bc7

For clubb:ticket:514. The nightly tests began to crash due to some bugs introduced in r6310 and 6311. First, there was a call to {{{comp_mean_stdev_corr}}} in {{{cldwat2m_micro.F90}}}. But {{{comp_mean_stdev_corr}}} was split up in r6310. Hence the openf95 compiler crashed while compiling the code. It is pretty confusing that most of the compilers could compile the code even with the bug.
Second, the {{{corr_stats_output}}} was moved to a slightly different spot (after {{{setup_pdf_parameters}}}) in r6309. That somehow resulted in a floating point error, when the code was compiled with the sun compiler. I couldn't yet figure out why this error occured. I moved the call to {{{corr_stats_output}}} back to its old spot (before {{{setup_pdf_parameters}}}). 

Now everything seems to be working fine. I tested the fixed code by using the following compilers: openf95, oracle, gfortran, g95. After each compilation I tested the compiled code for runtime errors by running {{{run_scm_all.bash -i}}}. There were no errors. I also tested the results vs r6309. The results were bit-by-bit identical (using all cases from {{{run_scm_all.bash -i}}}).


git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6314 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: d69b29227a7479e2b0c492de0879efcb48e830de

src/MG_micro/cldwat2m_micro.F90
@@ -2300,7 +2300,7 @@ subroutine mmicro_pcond ( sub_column,           &
 
                  mixt_frac = pdf_params(k)%mixt_frac
 
-                 call comp_mean_stdev_corr &
+                 call comp_mean_stdev &
                                 ( real( qc(i,k), kind = core_rknd ), & ! In
                                   real( qric(i,k) * cldmax(i,k), &
                                         kind = core_rknd ), &
@@ -2328,7 +2328,31 @@ subroutine mmicro_pcond ( sub_column,           &
                                   mu_Ncn_1, mu_Ncn_2, sigma_w_1, sigma_w_2, &
                                   sigma_s_1, sigma_s_2, sigma_t_1, sigma_t_2, &
                                   sigma_rr_1, sigma_rr_2, sigma_Nr_1, &
-                                  sigma_Nr_2, sigma_Ncn_1, sigma_Ncn_2, &
+                                  sigma_Nr_2, sigma_Ncn_1, sigma_Ncn_2 )
+
+                 call comp_corr ( real( qc(i,k), kind = core_rknd ), & ! In
+                                  real( qric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( nric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( nc(i,k), kind = core_rknd ), &
+                                  real( qric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( qric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( nric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( nric(i,k) * cldmax(i,k), &
+                                        kind = core_rknd ), &
+                                  real( qc(i,k), kind = core_rknd ), &
+                                  real( qc(i,k), kind = core_rknd ), &
+                                  real( lcldm(i,k), kind = core_rknd ), &
+                                  real( lcldm(i,k), kind = core_rknd ), &
+                                  one, one, &
+                                  zero, zero, zero, &
+                                  zero, zero, mixt_frac, &
+                                  sigma_rr_1, sigma_Nr_1, sigma_Ncn_1, &
+                                  pdf_params(k), &
                                   corr_ws_1, corr_ws_2, corr_wrr_1, &
                                   corr_wrr_2, corr_wNr_1, corr_wNr_2, &
                                   corr_wNcn_1, corr_wNcn_2, corr_st_1, &

src/clubb_driver.F90
@@ -1233,6 +1233,8 @@ module clubb_driver
 
          Ncnm = Ncnm_initial / rho
 
+         call corr_stat_output( d_variables, gr%nz, corr_array_1 )
+
          !!! Setup the PDF parameters.
          call setup_pdf_parameters( gr%nz, rrainm, Nrm, Ncnm, rho, rcm, & ! In
                                     cloud_frac, sqrt(wp2_zt), wphydrometp, &
@@ -1241,8 +1243,6 @@ module clubb_driver
                                     corr_array_1, corr_array_2, & ! Out
                                     mu_x_1, mu_x_2, sigma_x_1, sigma_x_2, &
                                     hydromet_pdf_params )
-
-         call corr_stat_output( d_variables, gr%nz, corr_array_1 )
 
       endif ! not micro_scheme == "none"
 