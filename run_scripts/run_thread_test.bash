cb7b932c3a0cbfea354370c7b686eec04759baf5
dschanen@uwm.edu <dschanen@uwm.edu>
2013-06-27 01:34:00 +0000

    For clubb:ticket:586.  Added a script to check whether parallel and serial clubb give the same results.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6330 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: e149012e936c253da7952bb8d0cb3202710531f8

---
+++ run_scripts/run_thread_test.bash
@@ -0,0 +1,115 @@
+#!/bin/bash
+###############################################################################
+# $Id$
+#
+# Desciption:
+#   Script to run the test whether CLUBB is still threadsafe.  
+#   We need this because not all versions of Fortran accept command line 
+#   arguments (a Fortran 2003 feature), and GNU Fortran has trouble with
+#   comments in namelists
+# Notes:
+#   This has been tested with GNU Bash v4.1.2 on GNU/Linux.  The sed command
+#   used only works with GNU sed.
+###############################################################################
+
+# Set the number of threads to 
+export OMP_NUM_THREADS=2
+
+RUN_CASES=( fire bomex rico_LH gabls3 )
+NAMELISTS=( 'clubb_1.in' 'clubb_2.in' 'clubb_3.in' 'clubb_4.in' )
+FLAGS_FILE="../input/tunable_parameters/configurable_model_flags.in"
+PARAMS_FILE="../input/tunable_parameters/tunable_parameters.in"
+STATS_FILE="../input/stats/standard_stats.in"
+SERIAL="../output/serial"
+PARALLEL="../output/parallel"
+
+run_serial()
+{
+	for (( x=0;  x < "${#RUN_CASES[@]}"; x++ )); do
+		MODEL_FILE='../input/case_setups/'"${RUN_CASES[$x]}"'_model.in'
+		cat $MODEL_FILE $FLAGS_FILE $PARAMS_FILE $STATS_FILE > 'clubb.in'
+ 
+
+		# Set debug level to 0
+		sed -i 's/debug_level\s*=\s*.*/debug_level = 0/g' 'clubb.in'
+
+		# This is a kluge for Fortran compilers that the can't handle comments in 
+		# a namelist by using the sed command to remove them.
+		# Since -i is a GNU sed extension the command might be 'gsed' on some systems.
+		sed -i -e 's/\!.*//' 'clubb.in'
+
+		if [ -e ../bin/clubb_standalone ]; then
+			../bin/clubb_standalone &> /dev/null
+		else
+			echo "clubb_standalone not found (did you re-compile?)"
+			exit
+		fi
+
+		# Remove the namelist
+		rm -f 'clubb.in'
+	done
+
+}
+
+run_parallel()
+{
+	for (( x=0;  x < "${#RUN_CASES[@]}"; x++ )); do
+		MODEL_FILE='../input/case_setups/'"${RUN_CASES[$x]}"'_model.in'
+		cat $MODEL_FILE $FLAGS_FILE $PARAMS_FILE $STATS_FILE > "${NAMELISTS[$x]}"
+ 
+
+		# Set debug level to 0
+		sed -i 's/debug_level\s*=\s*.*/debug_level = 0/g' "${NAMELISTS[$x]}"
+
+		# This is a kluge for Fortran compilers that the can't handle comments in 
+		# a namelist by using the sed command to remove them.
+		# Since -i is a GNU sed extension the command might be 'gsed' on some systems.
+		sed -i -e 's/\!.*//' "${NAMELISTS[$x]}"
+	done
+
+	# Run the CLUBB thread test
+	if [ -e ../bin/clubb_thread_test ]; then
+		../bin/clubb_thread_test
+	else
+		echo "clubb_thread_test not found (did you re-compile?)"
+		exit
+	fi
+
+	# Remove the namelists
+	for (( x=0;  x < "${#RUN_CASES[@]}"; x++ )); do
+		rm -f "${NAMELISTS[$x]}"
+	done
+}
+
+
+# Run in serial mode
+run_serial
+
+mkdir $SERIAL
+mv ../output/*.??? $SERIAL
+
+# Run in parallel mode
+run_parallel
+
+mkdir $PARALLEL
+mv ../output/*.??? $PARALLEL
+
+for (( x=0;  x < "${#RUN_CASES[@]}"; x++ )); do
+	diff $SERIAL/"${RUN_CASES[$x]}"_zt.dat $PARALLEL/"${RUN_CASES[$x]}"_zt.dat > 'diff.txt'
+	diff $SERIAL/"${RUN_CASES[$x]}"_zm.dat $PARALLEL/"${RUN_CASES[$x]}"_zm.dat >> 'diff.txt'
+	diff $SERIAL/"${RUN_CASES[$x]}"_sfc.dat $PARALLEL/"${RUN_CASES[$x]}"_sfc.dat >> 'diff.txt'
+done
+
+if [[ -s 'diff.txt' ]]; then
+	cat 'diff.txt'
+	RESULT=1
+else
+	echo "No differences found"
+	RESULT=0
+fi
+
+rm -f 'diff.txt'
+rm -rf $SERIAL
+rm -rf $PARALLEL
+
+exit $RESULT
