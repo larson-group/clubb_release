17ffeae8c9fa073a760985b58091cf4149474f53
vlarson@uwm.edu <vlarson@uwm.edu>
2013-10-31 18:57:48 +0000

    I deleted the 2 smallest line widths in plotgen because they're barely visible. Now there are only 4 line widths.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6581 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 4dc38a384d3f8e93c6f2fd0cc5382d6965e01fa5

--- postprocessing/plotgen/plotgen.pl
+++ postprocessing/plotgen/plotgen.pl
@@ -128,7 +128,8 @@ my $darkGreen = "[ 0.00, 0.40, 0.00 ]";
 # Arrays to cycle through when auto is set for lines
 my @lineStyles = ("--", "-", "-.", "-");
 my @lineColors = ($orange, $lt_blue, $purple, "blue", $darkGreen, "cyan", "magenta", "green", "red", $peach, $goldenRod, $grey, "yellow");
-my @lineWidths = (4.5, 3, 2.5, 1.5, 1, 0.5);
+#my @lineWidths = (4.5, 3, 2.5, 1.5, 1, 0.5);
+my @lineWidths = (4.5, 3, 2.5, 1.5);
 
 # Counters for automatic lines
 my $lineStyleCounter = 0;
