larson-group/clubb_release

commit 139ab5b6eea05b3af123cad6d1e1fa32c177a48f

For clubb:ticket:603 and sam:ticket:61, I added budget terms to deal with the so-called instantaneous processes. I also got rid of the REMOVE_ terms, and added those effects to the new _INST terms, since both deal with small amounts of hydrometeors.
git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6665 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: 7249bb8038433548e9ab39db500e71fd52f02082

src/CLUBB_core/stats_zt.F90
@@ -541,12 +541,17 @@ module stats_zt
       iNEGFIX_NI, &
       iNEGFIX_NS, &
       iNEGFIX_NG, &
-      iREMOVE_NR, &
-      iREMOVE_NC, &
-      iREMOVE_NI, &
-      iREMOVE_NS, &
-      iREMOVE_NG, &
       iNIM_MORR_CL, &
+      iQC_INST, &
+      iQR_INST, &
+      iQI_INST, &
+      iQS_INST, & 
+      iQG_INST, &
+      iNC_INST, &
+      iNR_INST, &
+      iNI_INST, &
+      iNS_INST, & 
+      iNG_INST, &
       iT_in_K_mc
 
     use stats_variables, only: &
@@ -2744,53 +2749,104 @@ module stats_zt
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
-      case ('REMOVE_NR')
-        iREMOVE_NR= k
-
-        call stat_assign( var_index=iREMOVE_NR, var_name="REMOVE_NR", &
-             var_description="Remove rain drop # conc. when small mixing rat., -Nrm [(#/kg/s)]",&
-             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
-        k = k + 1
-
-      case ('REMOVE_NC')
-        iREMOVE_NC= k
-
-        call stat_assign( var_index=iREMOVE_NC, var_name="REMOVE_NC", &
-             var_description="Remove cloud drop # conc. when small mixing rat., -Ncm [(#/kg/s)]",&
-             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
-        k = k + 1      
-
-      case ('REMOVE_NI')
-        iREMOVE_NI= k
-
-        call stat_assign( var_index=iREMOVE_NI, var_name="REMOVE_NI", &
-             var_description="Remove ice # conc. when small mixing ratio, -Nim [(#/kg/s)]", &
-             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
-        k = k + 1      
-
-      case ('REMOVE_NS')
-        iREMOVE_NS= k
-
-        call stat_assign( var_index=iREMOVE_NS, var_name="REMOVE_NS", &
-             var_description="Remove snow # conc. when small mixing ratio, -Nsnowm [(#/kg/s)]", &
-             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
-        k = k + 1      
-
-      case ('REMOVE_NG')
-        iREMOVE_NG= k
-
-        call stat_assign( var_index=iREMOVE_NG, var_name="REMOVE_NG", &
-             var_description="Remove graupel # conc. when small mix. rat., -Ngraupelm [(#/kg/s)]",&
-             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
-        k = k + 1
-
       case ('NIM_MORR_CL')
         iNIM_MORR_CL= k
 
         call stat_assign( var_index=iNIM_MORR_CL, var_name="NIM_MORR_CL", &
-             var_description="CLipping of large ice concentrations, -Nim [(#/kg/s)]", &
+             var_description="Clipping of large ice concentrations, -Nim [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
+
+      case ('QC_INST')
+        iQC_INST= k
+
+        call stat_assign( var_index=iQC_INST, var_name="QC_INST", &
+             var_description="Change in mixing ratio due to instantaneous processes, &
+                                +rcm [(kg/kg/s)]", &
+             var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('QR_INST')
+        iQR_INST= k
+
+        call stat_assign( var_index=iQR_INST, var_name="QR_INST", &
+             var_description="Change in mixing ratio from instantaneous processes, &
+                                +rrainm [(kg/kg/s)]", &
+             var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('QI_INST')
+        iQI_INST= k
+
+        call stat_assign( var_index=iQI_INST, var_name="QI_INST", &
+             var_description="Change in mixing ratio from instantaneous processes, &
+                                +ricem [(kg/kg/s)]", &
+             var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('QS_INST')
+        iQS_INST= k
+
+        call stat_assign( var_index=iQS_INST, var_name="QS_INST", &
+             var_description="Change in mixing ratio from instantaneous processes, &
+                                +rsnowm [(kg/kg/s)]", &
+             var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('QG_INST')
+        iQG_INST= k
+
+        call stat_assign( var_index=iQG_INST, var_name="QG_INST", &
+             var_description="Change in mixing ratio from instantaneous processes, &
+                                +rgraupelm [(kg/kg/s)]", &
+             var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('NC_INST')
+        iNC_INST= k
+
+        call stat_assign( var_index=iNC_INST, var_name="NC_INST", &
+             var_description="Change in # conc. from instantaneous processes, &
+                                +Ncm [(#/kg/s)]", &
+             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('NR_INST')
+        iNR_INST= k
+
+        call stat_assign( var_index=iNR_INST, var_name="NR_INST", &
+             var_description="Change in # conc. from instantaneous processes, &
+                                +Nrm [(#/kg/s)]", &
+             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('NI_INST')
+        iNI_INST= k
+
+        call stat_assign( var_index=iNI_INST, var_name="NI_INST", &
+             var_description="Change in # conc. from instantaneous processes, &
+                                +Nim [(#/kg/s)]", &
+             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('NS_INST')
+        iNS_INST= k
+
+        call stat_assign( var_index=iNS_INST, var_name="NS_INST", &
+             var_description="Change in # conc. from instantaneous processes, &
+                                +Nsnowm [(#/kg/s)]", &
+             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
+      case ('NG_INST')
+        iNG_INST= k
+
+        call stat_assign( var_index=iNG_INST, var_name="NG_INST", &
+             var_description="Change in # conc. from instantaneous processes, &
+                                +Ngraupelm [(#/kg/s)]", &
+             var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
+        k = k + 1
+
 
       case ('T_in_K_mc')
         iT_in_K_mc= k

input/stats/all_stats.in
@@ -99,8 +99,8 @@ vars_zt =
 'NPRAI', 'NMULTS', 'NMULTG', 'NMULTR', 'NMULTRG', 'NNUCCD', 'NSUBI', 
 'NGMLTG', 'NSUBG', 'NACT', 'SIZEFIX_NR', 'SIZEFIX_NC', 'SIZEFIX_NI', 
 'SIZEFIX_NS', 'SIZEFIX_NG', 'NEGFIX_NR', 'NEGFIX_NC', 'NEGFIX_NI', 'NEGFIX_NS', 
-'NEGFIX_NG', 'REMOVE_NR','REMOVE_NC', 'REMOVE_NI', 'REMOVE_NS', 'REMOVE_NG', 
-'NIM_MORR_CL', 'T_in_K_mc',
+'NEGFIX_NG', 'NIM_MORR_CL', 'QC_INST', 'QR_INST', 'QI_INST', 'QS_INST', 'QG_INST', 
+'NC_INST', 'NR_INST', 'NI_INST', 'NS_INST', 'NG_INST', 'T_in_K_mc',
 ''
 
 vars_LH_zt =

input/stats/standard_stats.in
@@ -98,8 +98,8 @@ vars_zt =
 'NPRAI', 'NMULTS', 'NMULTG', 'NMULTR', 'NMULTRG', 'NNUCCD', 'NSUBI',
 'NGMLTG', 'NSUBG', 'NACT', 'SIZEFIX_NR', 'SIZEFIX_NC', 'SIZEFIX_NI',
 'SIZEFIX_NS', 'SIZEFIX_NG', 'NEGFIX_NR', 'NEGFIX_NC', 'NEGFIX_NI', 'NEGFIX_NS', 
-'NEGFIX_NG', 'REMOVE_NR','REMOVE_NC', 'REMOVE_NI', 'REMOVE_NS', 'REMOVE_NG', 
-'NIM_MORR_CL', 'T_in_K_mc',
+'NEGFIX_NG', 'NIM_MORR_CL', 'QC_INST', 'QR_INST', 'QI_INST', 'QS_INST', 'QG_INST',
+'NC_INST', 'NR_INST', 'NI_INST', 'NS_INST', 'NG_INST', 'T_in_K_mc',
 ''
 
 vars_LH_zt =

src/CLUBB_core/stats_variables.F90
@@ -841,19 +841,25 @@ module stats_variables
     iNEGFIX_NI = 0, &
     iNEGFIX_NS = 0, &
     iNEGFIX_NG = 0, &
-    iREMOVE_NR = 0, &
-    iREMOVE_NC = 0, &
-    iREMOVE_NI = 0, &
-    iREMOVE_NS = 0, &
-    iREMOVE_NG = 0, &
     iNIM_MORR_CL = 0, &
+    iQC_INST = 0, &
+    iQR_INST = 0, &
+    iQI_INST = 0, &
+    iQS_INST = 0, &
+    iQG_INST = 0, &
+    iNC_INST = 0, &
+    iNR_INST = 0, &
+    iNI_INST = 0, &
+    iNS_INST = 0, &
+    iNG_INST = 0, &
     iT_in_K_mc = 0
 
 !$omp threadprivate(iPCC, iNNUCCC, iNPSACWS, iNPRA, iNPRC, iNPSACWI, iNPSACWG, iNPRAI, &
 !$omp   iNMULTS, iNMULTG, iNMULTR, iNMULTRG, iNNUCCD, iNSUBI, iNGMLTG, iNSUBG, iNACT, &
 !$omp   iSIZEFIX_NR, iSIZEFIX_NC, iSIZEFIX_NI, iSIZEFIX_NS, iSIZEFIX_NG, iNEGFIX_NR, &
-!$omp   iNEGFIX_NC, iNEGFIX_NI, iNEGFIX_NS, iNEGFIX_NG, iREMOVE_NR, iREMOVE_NC, iREMOVE_NI, &
-!$omp   iREMOVE_NS, iREMOVE_NG, iNIM_MORR_CL, iT_in_K_mc  )
+!$omp   iNEGFIX_NC, iNEGFIX_NI, iNEGFIX_NS, iNEGFIX_NG, iNIM_MORR_CL, iQC_INST, iQR_INST, &
+!$omp   iQI_INST, iQS_INST, iQG_INST, iNC_INST, iNR_INST, iNI_INST, iNS_INST, &
+!$omp   iNG_INST, iT_in_K_mc  )
 
   ! Indices for statistics in zm file
   integer, public :: & 

src/morrison_micro_driver_module.F90
@@ -158,12 +158,17 @@ module morrison_micro_driver_module
       iNEGFIX_NI, &
       iNEGFIX_NS, &
       iNEGFIX_NG, &
-      iREMOVE_NR, &
-      iREMOVE_NC, &
-      iREMOVE_NI, &
-      iREMOVE_NS, &
-      iREMOVE_NG, &
       iNIM_MORR_CL, &
+      iQC_INST, & 
+      iQR_INST, &
+      iQI_INST, &
+      iQS_INST, &
+      iQG_INST, &
+      iNC_INST, &
+      iNR_INST, &
+      iNI_INST, & 
+      iNS_INST, &
+      iNG_INST, &
       iT_in_K_mc
 
 
@@ -402,12 +407,18 @@ module morrison_micro_driver_module
       NEGFIX_NC, &    ! Removal of negative cloud drop number concentration 
       NEGFIX_NR, &    ! Removal of negative rain drop number concentration 
       NEGFIX_NG, &    ! Removal of negative graupel number concentration 
-      REMOVE_NI, &    ! Removal of ice number concentration when mixing ratio is small
-      REMOVE_NS, &    ! Removal of snow number concentration when mixing ratio is small
-      REMOVE_NC, &    ! Removal of cloud drop number concentration when mixing ratio is small
-      REMOVE_NR, &    ! Removal of rain drop number concentration when mixing ratio is small
-      REMOVE_NG, &    ! Removal of graupel number concentration when mixing ratio is small
-      NIM_MORR_CL   ! Clipping of large ice number concentrations
+      NIM_MORR_CL, &   ! Clipping of large ice number concentrations
+      QC_INST, & ! Change in cloud mixing ratio due to instantaneous processes
+      QR_INST, & ! Change in rain mixing ratio due to instantaneous processes
+      QI_INST, & ! Change in ice mixing ratio due to instantaneous processes
+      QS_INST, & ! Change in snow mixing ratio due to instantaneous processes
+      QG_INST, & ! Change in graupel mixing ratio due to instantaneous processes
+      NC_INST, & ! Change in cloud number concentration due to instantaneous processes
+      NR_INST, & ! Change in rain number concentration due to instantaneous processes
+      NI_INST, & ! Change in ice number concentration due to instantaneous processes
+      NS_INST, & ! Change in snow number concentration due to instantaneous processes
+      NG_INST    ! Change in graupel number concentration due to instantaneous processes
+
 
     real( kind = core_rknd ), dimension(nz) :: & 
       rcm_in_cloud     ! Liquid water in cloud           [kg/kg]
@@ -574,12 +585,17 @@ module morrison_micro_driver_module
     NEGFIX_NI = 0.0 
     NEGFIX_NS = 0.0 
     NEGFIX_NG = 0.0 
-    REMOVE_NR = 0.0 
-    REMOVE_NC = 0.0 
-    REMOVE_NI = 0.0 
-    REMOVE_NS = 0.0 
-    REMOVE_NG = 0.0
     NIM_MORR_CL = 0.0
+    QC_INST = 0.0
+    QR_INST = 0.0
+    QI_INST = 0.0
+    QS_INST = 0.0
+    QG_INST = 0.0
+    NC_INST = 0.0
+    NR_INST = 0.0
+    NI_INST = 0.0
+    NS_INST = 0.0
+    NG_INST = 0.0
 
 
     hydromet_mc_r4 = real( hydromet_mc )
@@ -623,7 +639,8 @@ module morrison_micro_driver_module
            NMULTS, NMULTG, NMULTR, NMULTRG, NNUCCD, NSUBI, NGMLTG, NSUBG, NACT, &
            SIZEFIX_NR, SIZEFIX_NC, SIZEFIX_NI, SIZEFIX_NS, SIZEFIX_NG, &
            NEGFIX_NR, NEGFIX_NC, NEGFIX_NI, NEGFIX_NS, NEGFIX_NG, &
-           REMOVE_NR, REMOVE_NC, REMOVE_NI, REMOVE_NS, REMOVE_NG, NIM_MORR_CL )
+           NIM_MORR_CL, QC_INST, QR_INST, QI_INST, QS_INST, QG_INST, &
+           NC_INST, NR_INST, NI_INST, NS_INST, NG_INST )
 
     !hydromet_mc = real( hydromet_mc_r4, kind = core_rknd )
     rcm_mc = real( rcm_mc_r4, kind = core_rknd )
@@ -795,13 +812,19 @@ module morrison_micro_driver_module
       call stat_update_var( iNEGFIX_NI, lh_stat_sample_weight*real( NEGFIX_NI,kind=core_rknd ),zt)
       call stat_update_var( iNEGFIX_NS, lh_stat_sample_weight*real( NEGFIX_NS,kind=core_rknd ),zt)
       call stat_update_var( iNEGFIX_NG, lh_stat_sample_weight*real( NEGFIX_NG,kind=core_rknd ),zt)
-      call stat_update_var( iREMOVE_NR, lh_stat_sample_weight*real( REMOVE_NR,kind=core_rknd ),zt)
-      call stat_update_var( iREMOVE_NC, lh_stat_sample_weight*real( REMOVE_NC,kind=core_rknd ),zt)
-      call stat_update_var( iREMOVE_NI, lh_stat_sample_weight*real( REMOVE_NI,kind=core_rknd ),zt)
-      call stat_update_var( iREMOVE_NS, lh_stat_sample_weight*real( REMOVE_NS,kind=core_rknd ),zt)
-      call stat_update_var( iREMOVE_NG, lh_stat_sample_weight*real( REMOVE_NG,kind=core_rknd ),zt)
       call stat_update_var( iNIM_MORR_CL, lh_stat_sample_weight &
                 *real( NIM_MORR_CL,kind=core_rknd ), zt )
+      call stat_update_var( iQC_INST, lh_stat_sample_weight*real( QC_INST,kind=core_rknd ),zt)
+      call stat_update_var( iQR_INST, lh_stat_sample_weight*real( QR_INST,kind=core_rknd ),zt)
+      call stat_update_var( iQI_INST, lh_stat_sample_weight*real( QI_INST,kind=core_rknd ),zt)
+      call stat_update_var( iQS_INST, lh_stat_sample_weight*real( QS_INST,kind=core_rknd ),zt)
+      call stat_update_var( iQG_INST, lh_stat_sample_weight*real( QG_INST,kind=core_rknd ),zt)
+      call stat_update_var( iNC_INST, lh_stat_sample_weight*real( NC_INST,kind=core_rknd ),zt)
+      call stat_update_var( iNR_INST, lh_stat_sample_weight*real( NR_INST,kind=core_rknd ),zt)
+      call stat_update_var( iNI_INST, lh_stat_sample_weight*real( NI_INST,kind=core_rknd ),zt)
+      call stat_update_var( iNS_INST, lh_stat_sample_weight*real( NS_INST,kind=core_rknd ),zt)
+      call stat_update_var( iNG_INST, lh_stat_sample_weight*real( NG_INST,kind=core_rknd ),zt)
+
       call stat_update_var( iT_in_K_mc, lh_stat_sample_weight*real( T_in_K_mc, kind=core_rknd ),zt)
 
     end if ! l_stats_samp

commit 49ff0b41036ace23514e5467f368139450baee3b

For clubb:ticket:654. I eliminated the compiler warnings that were related to estimate_scm_microphys_module.F90 and diagnose_correlations_module.F90. During the process I noticed an error in stats_zt.F90 that was introduced in r6665. I got rid of that error as well.
git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6669 f1ce5500-ea2b-45e0-a110-28dffb978352

Former-commit-id: c25bffdbadca3244a52832bc95a8c9f643a560e8

src/CLUBB_core/diagnose_correlations_module.F90
@@ -33,8 +33,8 @@ module diagnose_correlations_module
     use clubb_precision, only: &
         core_rknd ! Variable(s)
 
-    use corr_matrix_module, only: &
-        iiPDF_w ! Variable(s)
+!    use corr_matrix_module, only: &
+!        iiPDF_w ! Variable(s)
 
     use constants_clubb, only: &
         zero
@@ -110,8 +110,8 @@ module diagnose_correlations_module
     use clubb_precision, only: &
         core_rknd ! Variable(s)
 
-    use parameters_tunable, only:  & 
-        alpha_corr ! Constant(s)
+!    use parameters_tunable, only:  &
+!        alpha_corr ! Constant(s)
 
     use constants_clubb, only: &
       max_mag_correlation
@@ -140,8 +140,8 @@ module diagnose_correlations_module
     integer :: i, j ! Loop iterator
 
     real( kind = core_rknd ) :: &
-      f_ij, &
-      f_ij_o
+      f_ij
+!      f_ij_o
 
     real( kind = core_rknd ), dimension(n_variables) :: &
       s_1j ! s_1j = sqrt(1-c_1j^2)
@@ -152,7 +152,6 @@ module diagnose_correlations_module
     ! Remove compiler warnings about unused variables.
     if ( .false. ) then
        print *, "sqrt_xp2_on_xm2 = ", sqrt_xp2_on_xm2
-       print *, "f_ij_o = ", f_ij_o
     endif
 
     ! calculate all square roots
@@ -988,7 +987,7 @@ module diagnose_correlations_module
     use corr_matrix_module, only: &
         iiPDF_w,        & ! Variable(s)
         iiPDF_s_mellor, &
-        iiPDF_t_mellor, &
+!        iiPDF_t_mellor, &
         iiPDF_Ncn,      &
         iiPDF_rrain,    &
         iiPDF_Nr

src/CLUBB_core/stats_zt.F90
@@ -2761,8 +2761,8 @@ module stats_zt
         iQC_INST= k
 
         call stat_assign( var_index=iQC_INST, var_name="QC_INST", &
-             var_description="Change in mixing ratio due to instantaneous processes, &
-                                +rcm [(kg/kg/s)]", &
+             var_description="Change in mixing ratio due to instantaneous processes," // &
+                             " +rcm [(kg/kg/s)]", &
              var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2770,8 +2770,8 @@ module stats_zt
         iQR_INST= k
 
         call stat_assign( var_index=iQR_INST, var_name="QR_INST", &
-             var_description="Change in mixing ratio from instantaneous processes, &
-                                +rrainm [(kg/kg/s)]", &
+             var_description="Change in mixing ratio from instantaneous processes," // &
+                             " +rrainm [(kg/kg/s)]", &
              var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2779,8 +2779,8 @@ module stats_zt
         iQI_INST= k
 
         call stat_assign( var_index=iQI_INST, var_name="QI_INST", &
-             var_description="Change in mixing ratio from instantaneous processes, &
-                                +ricem [(kg/kg/s)]", &
+             var_description="Change in mixing ratio from instantaneous processes," // &
+                             " +ricem [(kg/kg/s)]", &
              var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2788,8 +2788,8 @@ module stats_zt
         iQS_INST= k
 
         call stat_assign( var_index=iQS_INST, var_name="QS_INST", &
-             var_description="Change in mixing ratio from instantaneous processes, &
-                                +rsnowm [(kg/kg/s)]", &
+             var_description="Change in mixing ratio from instantaneous processes," // &
+                             " +rsnowm [(kg/kg/s)]", &
              var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2797,8 +2797,8 @@ module stats_zt
         iQG_INST= k
 
         call stat_assign( var_index=iQG_INST, var_name="QG_INST", &
-             var_description="Change in mixing ratio from instantaneous processes, &
-                                +rgraupelm [(kg/kg/s)]", &
+             var_description="Change in mixing ratio from instantaneous processes," // &
+                             " +rgraupelm [(kg/kg/s)]", &
              var_units="(kg/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2806,8 +2806,8 @@ module stats_zt
         iNC_INST= k
 
         call stat_assign( var_index=iNC_INST, var_name="NC_INST", &
-             var_description="Change in # conc. from instantaneous processes, &
-                                +Ncm [(#/kg/s)]", &
+             var_description="Change in # conc. from instantaneous processes," // &
+                             " +Ncm [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2815,8 +2815,8 @@ module stats_zt
         iNR_INST= k
 
         call stat_assign( var_index=iNR_INST, var_name="NR_INST", &
-             var_description="Change in # conc. from instantaneous processes, &
-                                +Nrm [(#/kg/s)]", &
+             var_description="Change in # conc. from instantaneous processes," // &
+                             " +Nrm [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2824,8 +2824,8 @@ module stats_zt
         iNI_INST= k
 
         call stat_assign( var_index=iNI_INST, var_name="NI_INST", &
-             var_description="Change in # conc. from instantaneous processes, &
-                                +Nim [(#/kg/s)]", &
+             var_description="Change in # conc. from instantaneous processes," // &
+                             " +Nim [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2833,8 +2833,8 @@ module stats_zt
         iNS_INST= k
 
         call stat_assign( var_index=iNS_INST, var_name="NS_INST", &
-             var_description="Change in # conc. from instantaneous processes, &
-                                +Nsnowm [(#/kg/s)]", &
+             var_description="Change in # conc. from instantaneous processes," // &
+                             " +Nsnowm [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 
@@ -2842,8 +2842,8 @@ module stats_zt
         iNG_INST= k
 
         call stat_assign( var_index=iNG_INST, var_name="NG_INST", &
-             var_description="Change in # conc. from instantaneous processes, &
-                                +Ngraupelm [(#/kg/s)]", &
+             var_description="Change in # conc. from instantaneous processes," // &
+                             " +Ngraupelm [(#/kg/s)]", &
              var_units="(#/kg/s)", l_silhs=.true., grid_kind=zt )
         k = k + 1
 

src/Latin_hypercube/estimate_scm_microphys_module.F90
@@ -39,7 +39,6 @@ module estimate_scm_microphys_module
 
     use parameters_microphys, only: &
       l_lh_cloud_weighted_sampling, & ! Variable(s)
-      l_silhs_KK_convergence_adj_mean, &
       l_const_Nc_in_cloud, &
       l_var_covar_src
 
@@ -73,9 +72,14 @@ module estimate_scm_microphys_module
     use stats_type, only: & 
       stat_update_var ! Procedure(s)
 
+#ifdef SILHS_KK_CONVERGENCE_TEST
+    use parameters_microphys, only: &
+      l_silhs_KK_convergence_adj_mean ! Variable(s)
+
     use array_index, only: &
       iiNrm, & ! Variable(s)
       iirrainm
+#endif
 
     implicit none
 