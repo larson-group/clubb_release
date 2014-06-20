a4cbbc866a23cef65e21cac1ca0f7b1a69e2da4f
raut@uwm.edu <raut@uwm.edu>
2014-06-20 18:30:58 +0000

    For clubb:ticket:703, I made the same change to the other file in Plotgen that had the bug to improve performance.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@6966 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 61fe014a306ec78bb0a6bc114644dfe45f624d41

--- postprocessing/plotgen/read_netcdf_hoc_corr.m
+++ postprocessing/plotgen/read_netcdf_hoc_corr.m
@@ -26,8 +26,8 @@ num_timesteps = zeros(nzmax,1);
 
 % Read in and average profiles of correlations.
 avg_field = zeros(nzmax,1);
+field = netcdf.getVar(fid,varnum);
 for t=t1:t2
-   field = netcdf.getVar(fid,varnum);
    %If there are only 2 dimensions, there is no need to squeeze (used for cloud_feedback LES)
    if max(size(dimids)) == 2
      new_field(1:nzmax,1) = field(1:nzmax, t);
