47ad997b654f067daded90b0ee5b9733b66ebaa1
schemena@uwm.edu <schemena@uwm.edu>
2014-08-07 14:06:10 +0000

    For clubb:ticket:680. Renamed the svn_test_suite to run_full_api_diagnostics.bash and updated it to include the new script. Now, running the script outputs both the API commitment breakdown and the usage analyzer breakdown automagically.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7177 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 881b596e6173da416f64a73f3b09f593374e3de9

--- utilities/api_analysis/run_full_api_diagnostics.bash
+++ utilities/api_analysis/run_full_api_diagnostics.bash
@@ -1,3 +1,9 @@
+# Figure out the directory where the script is located.
+run_dir=`dirname $0`
+
+# Change directories to the one in which the script is located.
+cd $run_dir
+
 echo "Checking out CLUBB"
 svn co http://carson.math.uwm.edu/repos/clubb_repos/trunk CLUBB
 
@@ -13,38 +19,41 @@ svn co https://svn-ccsm-models.cgd.ucar.edu/cam1/branches/subcol_SILHS_UWM CAM
 echo "Moving CLUBB_core"
 mv CLUBB/src/CLUBB_core CLUBB_core
 
-echo "removing .svn folder from CLUBB_core"
-rm -rf CLUBB_core/.svn
+echo "Removing .svn Folders"
+find . -type d -name .svn -exec rm -rf {} \;
 
-echo "removing api from CLUBB_core"
+echo "Running the Usage Analyzer"
+python usage_analyzer.py CLUBB_core/clubb_api_module.F90 SAM/SRC WRF/WRF/phys CAM/models/atm/cam/src > usageAnalyzer_.html
+
+echo "Removing API from CLUBB_core"
 rm  CLUBB_core/clubb_api_module.F90
 
-echo "removing api from SILHS"
+echo "Removing API from SILHS"
 rm  CLUBB/src/SILHS/silhs_api_module.F90
 
-echo "removing G_Unit_Tests from CLUBB"
+echo "Removing G_Unit_Tests from CLUBB"
 rm -rf CLUBB/src/G_unit_test_types
 rm CLUBB/src/G_unit_tests.F90
 
-echo "moving SILHS to CLUBB_core"
+echo "Moving SILHS to CLUBB_core"
 mv CLUBB/src/SILHS/* CLUBB_core/
 
-echo "testing CLUBB_standalone"
+echo "Testing CLUBB_Standalone's API Commitment"
 python api_commitment_test.py -cpu CLUBB_core CLUBB > clubb_standalone_modules.txt
 
-echo "testing CLUBB_core"
+echo "Testing CLUBB_core's API Commitment"
 python api_commitment_test.py -cpu CLUBB_core CLUBB_core > clubb_core_modules.txt
 
-echo "testing SAM"
+echo "Testing SAM's API Commitment"
 python api_commitment_test.py -cpu CLUBB_core SAM/SRC --exclude-dir="CLUBB","SILHS" > sam_modules.txt
 
-echo "testing CAM"
+echo "Testing CAM's API Commitment"
 python api_commitment_test.py -cpu CLUBB_core CAM --exclude-dir="clubb","silhs" > cam_modules.txt
 
-echo "testing WRF"
+echo "Testing WRF's API Commitment"
 python api_commitment_test.py -cpu CLUBB_core WRF --exclude-dir="clubb","silhs" > wrf_modules.txt
 
-python create_module_table.py CLUBB_core> output.html
+python create_module_table.py CLUBB_core> apiCommitment_.html
 
 echo "Removing Dependencies"
 rm -rf CLUBB
