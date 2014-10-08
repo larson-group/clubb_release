6d8c6eb300a80ed30e09488657663c3f75959580
bmg2@uwm.edu <bmg2@uwm.edu>
2014-10-08 19:53:27 +0000

    I am adding more variables to be read into both the CLUBB variable array and the SAM 3D LES variable array for the MATLAB scatter contour plots program.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7320 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 845d1f5097867490428440f4bab6e4ffc4531473

--- postprocessing/matlab_scatter_contour_plots/output_vars_3D.m
+++ postprocessing/matlab_scatter_contour_plots/output_vars_3D.m
@@ -6,13 +6,6 @@ idx_count = 0;
 
 %============= Add SAM Model Field Variables Here =========================
 
-% rr (kg/kg)
-idx_count = idx_count + 1;
-global idx_3D_rr
-idx_3D_rr = idx_count;
-units_corrector_type(idx_3D_rr) = 0;
-varname_sam(idx_3D_rr,1:2) = 'RR';
-
 % w (m/s).
 idx_count = idx_count + 1;
 global idx_3D_w
@@ -20,21 +13,51 @@ idx_3D_w = idx_count;
 units_corrector_type(idx_3D_w) = 0;
 varname_sam(idx_3D_w,1:1) = 'W';
 
+% chi (kg/kg).
+idx_count = idx_count + 1;
+global idx_3D_chi
+idx_3D_chi = idx_count;
+units_corrector_type(idx_3D_chi) = 0;
+varname_sam(idx_3D_chi,1:3) = 'CHI';
+
+% eta (kg/kg).
+idx_count = idx_count + 1;
+global idx_3D_eta
+idx_3D_eta = idx_count;
+units_corrector_type(idx_3D_eta) = 0;
+varname_sam(idx_3D_eta,1:3) = 'ETA';
+
+% rr (kg/kg)
+idx_count = idx_count + 1;
+global idx_3D_rr
+idx_3D_rr = idx_count;
+units_corrector_type(idx_3D_rr) = 0;
+varname_sam(idx_3D_rr,1:2) = 'RR';
+
+% Nr (num/kg)
+idx_count = idx_count + 1;
+global idx_3D_Nr
+idx_3D_Nr = idx_count;
+units_corrector_type(idx_3D_Nr) = 0;
+varname_sam(idx_3D_Nr,1:2) = 'NR';
+
 num_var_sam = idx_count;
 
 %============= Add SAM Model 1-D Variables Here ===========================
 
-% West-East Distance (meters)
+% West-East Distance (degrees)
 idx_count = idx_count + 1;
 global idx_3D_x
 idx_3D_x = idx_count;
+% Need to add a degree-to-meter unit corrector.
 units_corrector_type(idx_3D_x) = 0;
 varname_sam(idx_3D_x,1:1) = 'x';
 
-% South-North Distance (meters)
+% South-North Distance (degrees)
 idx_count = idx_count + 1;
 global idx_3D_y
 idx_3D_y = idx_count;
+% Need to add a degree-to-meter unit corrector.
 units_corrector_type(idx_3D_y) = 0;
 varname_sam(idx_3D_y,1:1) = 'y';
 
--- postprocessing/matlab_scatter_contour_plots/output_vars_clubb.m
+++ postprocessing/matlab_scatter_contour_plots/output_vars_clubb.m
@@ -20,6 +20,20 @@ idx_w_2 = idx_count;
 units_corrector_type(idx_w_2) = 0;
 varname_clubb(idx_w_2,1:3) = 'w_2';
 
+% chi_1 (kg/kg).
+idx_count = idx_count + 1;
+global idx_chi_1
+idx_chi_1 = idx_count;
+units_corrector_type(idx_chi_1) = 0;
+varname_clubb(idx_chi_1,1:5) = 'chi_1';
+
+% chi_2 (kg/kg).
+idx_count = idx_count + 1;
+global idx_chi_2
+idx_chi_2 = idx_count;
+units_corrector_type(idx_chi_2) = 0;
+varname_clubb(idx_chi_2,1:5) = 'chi_2';
+
 % mu_rr_1_n (ln(kg/kg)).
 idx_count = idx_count + 1;
 global idx_mu_rr_1_n
@@ -33,6 +47,20 @@ global idx_mu_rr_2_n
 idx_mu_rr_2_n = idx_count;
 units_corrector_type(idx_mu_rr_2_n) = 0;
 varname_clubb(idx_mu_rr_2_n,1:9) = 'mu_rr_2_n';
+
+% mu_Nr_1_n (ln(num/kg)).
+idx_count = idx_count + 1;
+global idx_mu_Nr_1_n
+idx_mu_Nr_1_n = idx_count;
+units_corrector_type(idx_mu_Nr_1_n) = 0;
+varname_clubb(idx_mu_Nr_1_n,1:9) = 'mu_Nr_1_n';
+
+% mu_Nr_2_n (ln(num/kg)).
+idx_count = idx_count + 1;
+global idx_mu_Nr_2_n
+idx_mu_Nr_2_n = idx_count;
+units_corrector_type(idx_mu_Nr_2_n) = 0;
+varname_clubb(idx_mu_Nr_2_n,1:9) = 'mu_Nr_2_n';
 
 % varnce_w_1 (m^2/s^2).
 idx_count = idx_count + 1;
@@ -48,6 +76,34 @@ idx_varnce_w_2 = idx_count;
 units_corrector_type(idx_varnce_w_2) = 0;
 varname_clubb(idx_varnce_w_2,1:10) = 'varnce_w_2';
 
+% stdev_chi_1 (kg/kg).
+idx_count = idx_count + 1;
+global idx_stdev_chi_1
+idx_stdev_chi_1 = idx_count;
+units_corrector_type(idx_stdev_chi_1) = 0;
+varname_clubb(idx_stdev_chi_1,1:11) = 'stdev_chi_1';
+
+% stdev_chi_2 (kg/kg).
+idx_count = idx_count + 1;
+global idx_stdev_chi_2
+idx_stdev_chi_2 = idx_count;
+units_corrector_type(idx_stdev_chi_2) = 0;
+varname_clubb(idx_stdev_chi_2,1:11) = 'stdev_chi_2';
+
+% stdev_eta_1 (kg/kg).
+idx_count = idx_count + 1;
+global idx_stdev_eta_1
+idx_stdev_eta_1 = idx_count;
+units_corrector_type(idx_stdev_eta_1) = 0;
+varname_clubb(idx_stdev_eta_1,1:11) = 'stdev_eta_1';
+
+% stdev_eta_2 (kg/kg).
+idx_count = idx_count + 1;
+global idx_stdev_eta_2
+idx_stdev_eta_2 = idx_count;
+units_corrector_type(idx_stdev_eta_2) = 0;
+varname_clubb(idx_stdev_eta_2,1:11) = 'stdev_eta_2';
+
 % sigma_rr_1_n (-).
 idx_count = idx_count + 1;
 global idx_sigma_rr_1_n
@@ -62,6 +118,34 @@ idx_sigma_rr_2_n = idx_count;
 units_corrector_type(idx_sigma_rr_2_n) = 0;
 varname_clubb(idx_sigma_rr_2_n,1:12) = 'sigma_rr_2_n';
 
+% sigma_Nr_1_n (-).
+idx_count = idx_count + 1;
+global idx_sigma_Nr_1_n
+idx_sigma_Nr_1_n = idx_count;
+units_corrector_type(idx_sigma_Nr_1_n) = 0;
+varname_clubb(idx_sigma_Nr_1_n,1:12) = 'sigma_Nr_1_n';
+
+% sigma_Nr_2_n (-).
+idx_count = idx_count + 1;
+global idx_sigma_Nr_2_n
+idx_sigma_Nr_2_n = idx_count;
+units_corrector_type(idx_sigma_Nr_2_n) = 0;
+varname_clubb(idx_sigma_Nr_2_n,1:12) = 'sigma_Nr_2_n';
+
+% corr_chi_eta_1_ca (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_eta_1_ca
+idx_corr_chi_eta_1_ca = idx_count;
+units_corrector_type(idx_corr_chi_eta_1_ca) = 0;
+varname_clubb(idx_corr_chi_eta_1_ca,1:17) = 'corr_chi_eta_1_ca';
+
+% corr_chi_eta_2_ca (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_eta_2_ca
+idx_corr_chi_eta_2_ca = idx_count;
+units_corrector_type(idx_corr_chi_eta_2_ca) = 0;
+varname_clubb(idx_corr_chi_eta_2_ca,1:17) = 'corr_chi_eta_2_ca';
+
 % corr_w_rr_1_n (-).
 idx_count = idx_count + 1;
 global idx_corr_w_rr_1_n
@@ -75,6 +159,90 @@ global idx_corr_w_rr_2_n
 idx_corr_w_rr_2_n = idx_count;
 units_corrector_type(idx_corr_w_rr_2_n) = 0;
 varname_clubb(idx_corr_w_rr_2_n,1:13) = 'corr_w_rr_2_n';
+
+% corr_w_Nr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_w_Nr_1_n
+idx_corr_w_Nr_1_n = idx_count;
+units_corrector_type(idx_corr_w_Nr_1_n) = 0;
+varname_clubb(idx_corr_w_Nr_1_n,1:13) = 'corr_w_Nr_1_n';
+
+% corr_w_Nr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_w_Nr_2_n
+idx_corr_w_Nr_2_n = idx_count;
+units_corrector_type(idx_corr_w_Nr_2_n) = 0;
+varname_clubb(idx_corr_w_Nr_2_n,1:13) = 'corr_w_Nr_2_n';
+
+% corr_chi_rr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_rr_1_n
+idx_corr_chi_rr_1_n = idx_count;
+units_corrector_type(idx_corr_chi_rr_1_n) = 0;
+varname_clubb(idx_corr_chi_rr_1_n,1:15) = 'corr_chi_rr_1_n';
+
+% corr_chi_rr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_rr_2_n
+idx_corr_chi_rr_2_n = idx_count;
+units_corrector_type(idx_corr_chi_rr_2_n) = 0;
+varname_clubb(idx_corr_chi_rr_2_n,1:15) = 'corr_chi_rr_2_n';
+
+% corr_chi_Nr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_Nr_1_n
+idx_corr_chi_Nr_1_n = idx_count;
+units_corrector_type(idx_corr_chi_Nr_1_n) = 0;
+varname_clubb(idx_corr_chi_Nr_1_n,1:15) = 'corr_chi_Nr_1_n';
+
+% corr_chi_Nr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_chi_Nr_2_n
+idx_corr_chi_Nr_2_n = idx_count;
+units_corrector_type(idx_corr_chi_Nr_2_n) = 0;
+varname_clubb(idx_corr_chi_Nr_2_n,1:15) = 'corr_chi_Nr_2_n';
+
+% corr_eta_rr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_eta_rr_1_n
+idx_corr_eta_rr_1_n = idx_count;
+units_corrector_type(idx_corr_eta_rr_1_n) = 0;
+varname_clubb(idx_corr_eta_rr_1_n,1:15) = 'corr_eta_rr_1_n';
+
+% corr_eta_rr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_eta_rr_2_n
+idx_corr_eta_rr_2_n = idx_count;
+units_corrector_type(idx_corr_eta_rr_2_n) = 0;
+varname_clubb(idx_corr_eta_rr_2_n,1:15) = 'corr_eta_rr_2_n';
+
+% corr_eta_Nr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_eta_Nr_1_n
+idx_corr_eta_Nr_1_n = idx_count;
+units_corrector_type(idx_corr_eta_Nr_1_n) = 0;
+varname_clubb(idx_corr_eta_Nr_1_n,1:15) = 'corr_eta_Nr_1_n';
+
+% corr_eta_Nr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_eta_Nr_2_n
+idx_corr_eta_Nr_2_n = idx_count;
+units_corrector_type(idx_corr_eta_Nr_2_n) = 0;
+varname_clubb(idx_corr_eta_Nr_2_n,1:15) = 'corr_eta_Nr_2_n';
+
+% corr_rr_Nr_1_n (-).
+idx_count = idx_count + 1;
+global idx_corr_rr_Nr_1_n
+idx_corr_rr_Nr_1_n = idx_count;
+units_corrector_type(idx_corr_rr_Nr_1_n) = 0;
+varname_clubb(idx_corr_rr_Nr_1_n,1:14) = 'corr_rr_Nr_1_n';
+
+% corr_rr_Nr_2_n (-).
+idx_count = idx_count + 1;
+global idx_corr_rr_Nr_2_n
+idx_corr_rr_Nr_2_n = idx_count;
+units_corrector_type(idx_corr_rr_Nr_2_n) = 0;
+varname_clubb(idx_corr_rr_Nr_2_n,1:14) = 'corr_rr_Nr_2_n';
 
 % mixt_frac (-).
 idx_count = idx_count + 1;
