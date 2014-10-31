846e075bb0d4c7ea981bb6ca0227df13295f1b09
raut@uwm.edu <raut@uwm.edu>
2014-10-31 00:32:21 +0000

    This is a necessary addition to clubb:r7356 for clubb:ticket:736 where I actually commit the new test file.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7357 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: e3f8a07f32636989d547d393585f7aac94f7addf

---
+++ src/G_unit_test_types/silhs_category_test.F90
@@ -0,0 +1,120 @@
+!-----------------------------------------------------------------------
+! $Id$
+!-----------------------------------------------------------------------
+module silhs_category_test
+
+  implicit none
+
+  private
+
+  public :: silhs_category_test_driver
+
+  contains
+
+  !=============================================================================
+  function silhs_category_test_driver()
+
+    ! Description:
+    !   This function 
+
+    ! References:
+    !-----------------------------------------------------------------------
+
+    use clubb_precision, only: &
+      core_rknd
+
+    use latin_hypercube_driver_module, only: &
+      generate_strat_uniform_variate, & ! Procedure(s)
+      pick_sample_categories
+
+    use constants_clubb, only: &
+      fstderr
+
+    implicit none
+
+    ! Local Constants
+    integer, parameter :: &
+      num_categories = 8 , &  ! This must be consistent with the definition in
+                              ! latin_hypercube_driver_module for now. Someday,
+                              ! this should be generalized.
+
+      num_samples = 100       ! Number of samples for this test
+
+    ! Output Variable
+    integer :: silhs_category_test_driver ! Returns the exit code of the test
+
+    ! Local Variables
+    integer, dimension(num_categories) :: &
+      n_sample_points_per_category
+
+    integer, dimension(num_samples) :: &
+      int_sample_category
+
+    real( kind = core_rknd ), dimension(num_samples) :: &
+      rand_vect
+
+    real( kind = core_rknd ), dimension(num_categories) :: &
+      category_prescribed_probs
+
+    integer :: isample, icategory
+
+  !-----------------------------------------------------------------------
+
+    !----- Begin Code -----
+
+    silhs_category_test_driver = 0
+
+    print *, "=================================================="
+    print *, ""
+    print *, "Performing SILHS category test"
+    print *, ""
+    print *, "=================================================="
+
+    ! Assign desired numbers for each category. Note: these should add up to
+    ! num_sample_points, otherwise the test will fail!!
+    n_sample_points_per_category(1) = 13
+    n_sample_points_per_category(2) = 12
+    n_sample_points_per_category(3) = 26
+    n_sample_points_per_category(4) = 31
+    n_sample_points_per_category(5) = 5
+    n_sample_points_per_category(6) = 8
+    n_sample_points_per_category(7) = 3
+    n_sample_points_per_category(8) = 2
+
+    category_prescribed_probs(:) = real( n_sample_points_per_category(:), kind=core_rknd ) / &
+                                   real( num_samples, kind=core_rknd )
+
+    rand_vect = generate_strat_uniform_variate( num_samples )
+
+    int_sample_category = pick_sample_categories( num_samples, category_prescribed_probs, &
+                                                  rand_vect )
+
+    ! Here's a sly trick to check that the generated sample points per category equals
+    ! what we prescribed above.
+    do isample=1, num_samples
+      n_sample_points_per_category(int_sample_category(isample)) = &
+        n_sample_points_per_category(int_sample_category(isample)) - 1
+    end do ! isample=1, num_samples
+
+    ! They should all be zero.
+    do icategory=1, num_categories
+      if ( n_sample_points_per_category(icategory) /= 0 ) then
+        silhs_category_test_driver = 1 ! Failure code
+      end if
+    end do
+
+    if ( silhs_category_test_driver /= 0 ) then
+      write(fstderr,*) 'Error: the picked number of samples in a category was not equal to &
+                       &what was prescribed.'
+      write(fstderr,*) 'n_sample_points_per_category(:) = ', n_sample_points_per_category(:)
+
+    else ! silhs_category_test_driver == 1
+
+      print *, 'Success!'
+
+    end if
+
+    return
+  end function silhs_category_test_driver
+
+end module silhs_category_test
