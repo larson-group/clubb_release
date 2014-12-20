3fb6b135f5c2368c44a176ca74bab5da1ee5c5dc
raut@uwm.edu <raut@uwm.edu>
2014-12-20 06:00:51 +0000

    My RMS plotter now has the ability to plot two theoretical lines: a 1-over-N line, and a 1-over-sqrt-N lines.
    clubb:ticket:740
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7429 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: aadfe51189d405ae9a8133643cc1553036834277

--- postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_plot.py
+++ postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_plot.py
@@ -4,16 +4,19 @@ import pylab as pl
 
 sim_points_all = [50, 100, 200, 300, 400, 500, 600, 800, 1000, 1200, 1500, 2000]
 
-clubb_var_str = 'rrm_auto'
-silhs_var_str  = 'lh_rrm_auto'
+clubb_var_str = 'rrm_cond'
+silhs_var_str  = 'lh_rrm_evap'
 
 clubb_var = netCDF4.Dataset('silhs_'+str(sim_points_all[0])+'/rico_lh_zt.nc').variables[clubb_var_str]
 
 k_lh_start = netCDF4.Dataset('silhs_k_lh_start/rico_lh_lh_sfc.nc').variables['k_lh_start']
 
 rms_all = list()
+rms_1_over_n = list()
+rms_1_over_sqrt_n = list()
 
-for num_samples in sim_points_all:
+for n in range(0,len(sim_points_all)):
+    num_samples = sim_points_all[n]
     rms_val = 0.0
     silhs_var = netCDF4.Dataset('silhs_'+str(num_samples)+'/rico_lh_lh_zt.nc').variables[silhs_var_str]
     n_timesteps = silhs_var.shape[1]
@@ -23,6 +26,18 @@ for num_samples in sim_points_all:
     rms_val = rms_val / n_timesteps
     rms_val = np.sqrt(rms_val)
     rms_all.append(rms_val)
+    if n == 0:
+      rms_1_over_n.append(rms_val)
+      rms_1_over_sqrt_n.append(rms_val)
+    else:
+      rms_1_over_n.append(rms_1_over_n[n-1] * (float(sim_points_all[n-1]) / sim_points_all[n]))
+      rms_1_over_sqrt_n.append(rms_1_over_sqrt_n[n-1] * np.sqrt(float(sim_points_all[n-1]) / sim_points_all[n]))
 
-pl.plot(sim_points_all, rms_all)
+pl.plot(sim_points_all, rms_all, label='SILHS')
+pl.plot(sim_points_all, rms_1_over_n, label='1 over N')
+pl.plot(sim_points_all, rms_1_over_sqrt_n, label='1 over sqrt N')
+
+pl.xlabel('Number of Sample Points')
+pl.ylabel('Root Mean Square of Absolute Error')
+pl.legend()
 pl.show()
