9b9969ef1bfc2b5e6ed9e4b797191461f8760a68
raut@uwm.edu <raut@uwm.edu>
2014-12-26 21:58:49 +0000

    I added time ranging ability to the RMS script.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7435 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5fe0a05a0569b912cda0cddcbe6f3a81d95f9f2d

--- postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_simulation.py
+++ postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_simulation.py
@@ -2,21 +2,24 @@ import netCDF4
 import numpy as np
 import pylab as pl
 
-clubb_nc = netCDF4.Dataset('')
-silhs_nc = netCDF4.Dataset('')
+clubb_nc = netCDF4.Dataset('rico_lh_zt.nc')
+silhs_nc = netCDF4.Dataset('rico_lh_lh_zt.nc')
 
-clubb_var_str = 'rrm_auto'
-silhs_var_str = 'lh_rrm_auto'
+clubb_var_str = 'Nrm'
+silhs_var_str = 'lh_Nrm'
 
 clubb_var = clubb_nc.variables[clubb_var_str]
 silhs_var = silhs_nc.variables[silhs_var_str]
 
-k_lh_start = netCDF4.Dataset('').variables['k_lh_start']
+k_lh_start = netCDF4.Dataset('rico_lh_lh_sfc.nc').variables['k_lh_start']
 
 rms_val = 0.0
-n_timesteps = clubb_var.shape[1]
 
-for t in range(0,n_timesteps):
+time1 = 0
+time2 = 4320
+n_timesteps = time2 - time1
+
+for t in range(time1,time2):
     k = int(round(k_lh_start[t,0,0,0]))-1
     rms_val = rms_val + (clubb_var[t,k,0,0]-silhs_var[t,k,0,0])**2
 
