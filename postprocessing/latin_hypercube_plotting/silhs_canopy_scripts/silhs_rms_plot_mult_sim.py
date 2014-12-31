83736e63b501ab8a46cc4d8b3851ed4c9a64e66c
raut@uwm.edu <raut@uwm.edu>
2014-12-31 20:25:46 +0000

    I modified the Python script RMS plotter so that it can do an average over all timesteps.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7442 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 491235e2bc0c4809a47969d815b3bc38d6ee5785

--- postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_plot_mult_sim.py
+++ postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/silhs_rms_plot_mult_sim.py
@@ -2,6 +2,8 @@ import netCDF4
 import numpy as np
 import pylab as pl
 
+l_all_height_avg = True
+
 sim_points_all = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
 
 clubb_var_str = 'rrm_auto'
@@ -11,14 +13,20 @@ silhs_dirs = ["out_default", "out_prescribed"]
 
 clubb_var = netCDF4.Dataset(silhs_dirs[0]+'/silhs_'+str(sim_points_all[0])+'/rico_lh_zt.nc').variables[clubb_var_str]
 
-k_lh_start = netCDF4.Dataset(silhs_dirs[0]+'/silhs_'+str(sim_points_all[0])+'/rico_lh_lh_sfc.nc').variables['k_lh_start']
-
 rms_all = list()
 for d in silhs_dirs:
     rms_all.append(np.empty(len(sim_points_all)))
 
 time1 = 3000
 time2 = 4320
+
+if not l_all_height_avg:
+    k_lh_start = netCDF4.Dataset(silhs_dirs[0]+'/silhs_'+str(sim_points_all[0])+'/rico_lh_lh_sfc.nc').variables['k_lh_start']
+    k_lh_start = k_lh_start[:,0,0,0]
+else:
+    n_heights = clubb_var.shape[1]
+
+clubb_var = clubb_var[:,:,0,0]
 
 for n_i in range(0,len(sim_points_all)):
     num_samples = sim_points_all[n_i]
@@ -28,10 +36,19 @@ for n_i in range(0,len(sim_points_all)):
         rms_val = 0.0
         silhs_var = netCDF4.Dataset(silhs_dir+'/silhs_'+str(num_samples)+'/rico_lh_lh_zt.nc').variables[silhs_var_str]
         n_timesteps = time2-time1
+        silhs_var = silhs_var[:,:,0,0]
+
         for t in range(time1,time2):
-            k = int(round(k_lh_start[t,0,0,0]))-1
-            rms_val = rms_val + (clubb_var[t,k,0,0]-silhs_var[t,k,0,0])**2
+            if l_all_height_avg:
+                for k in range(0,n_heights):
+                    rms_val = rms_val + (clubb_var[t,k]-silhs_var[t,k])**2
+            else:
+                k = int(round(k_lh_start[t]))-1
+                rms_val = rms_val + (clubb_var[t,k]-silhs_var[t,k])**2
+
         rms_val = rms_val / n_timesteps
+        if l_all_height_avg:
+            rms_val = rms_val / n_heights
         rms_val = np.sqrt(rms_val)
         rms_all[d_i][n_i] = rms_val
 
