52ffcb4fc5dba9c94ac0d91eb978c5db0ca1b5a2
weberjk@uwm.edu <weberjk@uwm.edu>
2015-02-10 14:54:58 +0000

    Some refactoring
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7487 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 39aa0b21dd7b00a5e45af64787a2f0602d44145f

--- postprocessing/python_sam_clubb_pdf_compare/plot_sam_clubb_marginal.py
+++ postprocessing/python_sam_clubb_pdf_compare/plot_sam_clubb_marginal.py
@@ -10,34 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4
 
-#----------------------------------------------------------------------------------------
-# Definitions
-#----------------------------------------------------------------------------------------
-def find_sams_timestep(filename):
-    # User passes in SAM's statistical file. Return the timestep of the SAM 
-    # simulation
-    nc = netCDF4.Dataset(filename)
-    time, units = pull_samples(nc, 'time')
-    nc.close()
-    return time[1]-time[0]
-    
-def pull_samples(nc, varname):
-    var = nc.variables[varname]
-    var_u = nc.variables[varname].units
-    var = np.squeeze(var)
-    return var, var_u
-
-def reshape_sam_slice(var, idx_z0):
-    var = np.squeeze(var)
-    var = np.swapaxes(var,2,0)
-    var = var[:,:,idx_z0]
-    var = np.reshape(var,(np.size(var,0)*np.size(var,1)))
-    return var
-    
-def reshape_clubb_samples(var, idx_z0, idx_t0):
-    var = var[idx_t0,idx_z0,:]
-    return var
-
 #----------------------------------------------------------------------------------------    
 # User input
 #----------------------------------------------------------------------------------------
@@ -57,7 +29,7 @@ sam_dt = 6. # [s] SAM's model timestep, not output frequency.
 sam_stat_file = '/home/weberjk/Plot_CRM/SAM_LBA/Data/LBA.nc'
 
 # Directory for CLUBB's sample points file
-clubb_3D_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/'
+clubb_3D_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/prescribe_thl_thlp2_rt_rtp2/'
 
 # CLUBB's file names
 clb_nl_file = clubb_3D_dir + 'lba_nl_lh_sample_points_2D.nc'
@@ -71,13 +43,13 @@ clubb_stat_file = clubb_3D_dir + 'lba_zt.nc' # The statistics file and sample po
 SAM_hist_var_name = 'CHI'   # SAM's name for variable
 CLUBB_hist_var_name = 'chi' # CLUBB's name for variable
 
-out_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/' # Figure output directory
+out_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/prescribe_thl_thlp2_rt_rtp2/' # Figure output directory
 out_name = 'hist_chi.png'                                # and and name
 
 plot_var_name = '$\chi$'         # How the variable looks when it is plotted
 plot_var_units = '$kg\ kg^{-1}$' # The plotted units
                       
-z0 = 1125                  # Analysis height [m]
+z0 = 1800                  # Analysis height [m]
 t0_in_min = 225            # Analysis time [min] 
                             # Note: This exact time must exist for SAM's output. 
                             # CLUBB will use the closest available.
@@ -85,6 +57,27 @@ t0_in_min = 225            # Analysis time [min]
 #----------------------------------------------------------------------------------------
 # Should not have to edit below this line
 #----------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------
+# Definitions
+#----------------------------------------------------------------------------------------
+def pull_samples(nc, varname):
+    var = nc.variables[varname]
+    var_u = nc.variables[varname].units
+    var = np.squeeze(var)
+    return var, var_u
+
+def reshape_sam_slice(var, idx_z0):
+    var = np.squeeze(var)
+    var = np.swapaxes(var,2,0)
+    var = var[:,:,idx_z0]
+    var = np.reshape(var,(np.size(var,0)*np.size(var,1)))
+    return var
+    
+def reshape_clubb_samples(var, idx_z0, idx_t0):
+    var = var[idx_t0,idx_z0,:]
+    return var
+
 
 # Find the correct SAM 3D file.
 t0_in_s = t0_in_min*60.
