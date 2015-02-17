217602c9f913e84f7137eb770adaba7e8c7095de
raut@uwm.edu <raut@uwm.edu>
2015-02-17 22:27:52 +0000

    In the interest of defensive programming, I am refusing to access the `isilhs_variance_category` array unless it is explicitly allocated. This should always be the case in our code when `l_stats_samp` is `.true.`, but maybe future confusion could be prevented this way.
    clubb:ticket:752
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7499 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 194c0600a090550b799a1a1a084833706b4fc523

--- src/estimate_scm_microphys_module.F90
+++ src/estimate_scm_microphys_module.F90
@@ -294,12 +294,16 @@ module estimate_scm_microphys_module
 
     if ( l_stats_samp ) then
 
-      if ( isilhs_variance_category(1) > 0 ) then
-        call silhs_category_variance_driver &
-             ( nz, num_samples, d_variables, X_nl_all_levs, X_mixt_comp_all_levs,  & ! Intent(in)
-               irrm_auto, microphys_stats_zt_all, microphys_stats_zt_avg,          & ! Intent(in)
-               lh_sample_point_weights )                                             ! Intent(in)
-      end if ! isilhs_variance_category(1) > 0
+      if ( allocated( isilhs_variance_category ) ) then
+
+        if( isilhs_variance_category(1) > 0 ) then
+          call silhs_category_variance_driver &
+               ( nz, num_samples, d_variables, X_nl_all_levs, X_mixt_comp_all_levs,  & ! Intent(in)
+                 irrm_auto, microphys_stats_zt_all, microphys_stats_zt_avg,          & ! Intent(in)
+                 lh_sample_point_weights )                                             ! Intent(in)
+        end if ! isilhs_variance_category(1) > 0
+
+      end if ! allocated( isilhs_variance_category ) 
 
     end if ! l_stats_samp
 
