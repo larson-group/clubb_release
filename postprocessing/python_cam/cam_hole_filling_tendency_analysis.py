0bb717f810f7aa9259fc1fbfeb436aa94ea4a813
janhft@uwm.edu <janhft@uwm.edu>
2015-03-04 03:58:43 +0000

    For cpt:ticket:198. I added new plots and adapted to the changes in cam:r68669.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7519 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 3c045fe0dfee4bba2538898e54c6f4ecb28c3c35

--- postprocessing/python_cam/cam_hole_filling_tendency_analysis.py
+++ postprocessing/python_cam/cam_hole_filling_tendency_analysis.py
@@ -62,6 +62,7 @@ def plot_t_profile(title, data_label, data, out_filepath):
     plt.ylabel(data_label)
     plt.xlabel('Time [days]')
     fig.savefig(out_filepath)
+    plt.close()
 #----------------------------------------------------------
 
 ### Main script starts here ###
@@ -73,16 +74,24 @@ nc = netCDF4.Dataset(nc_file_path)
 
 # Grid cell altitudes
 lev = nc.variables['lev']
+nlev = len(lev)
+print(nlev)
 
 # Calculate the inverse of each grid cell height (for averaging over z)
-rho = range(0,len(lev))
+invers_dz = range(0,nlev)
+invers_dz[0] = 1/lev[0]
+for i in range(1,nlev):
+    invers_dz[i] = 1/(lev[i]-lev[i-1])
 
-rho[0] = 1./lev[0]
-for i in range(1,len(lev)):
-    rho[i] = 1./(lev[i]-lev[i-1])
+#rho = get_2d_profile(nc,'rho_ds_zt')
+#
+#print(len(rho))
 
-print(np.squeeze(lev))
-print(np.squeeze(rho))
+# It should be weights = rho_ds/invers_dz like in vertical_integral
+wghts = invers_dz
+
+print('---- Test: weights should add to 1 ----')
+print(np.sum(wghts))
 
 ice_clip_tend = get_2d_profile(nc,'INEGCLPTEND')
 ice_clip_tend_tavg = np.average(ice_clip_tend, axis=0)
@@ -116,16 +125,16 @@ if ( l_show_plots ):
 
 
 # Plot timelines of height averages
-levavg_sum = np.average(ice_clip_tend+liq_clip_tend+vap_clip_tend, axis=1, weights=rho)
-levavg_vap = np.average(vap_clip_tend, axis=1, weights=rho)
-levavg_liq = np.average(liq_clip_tend, axis=1, weights=rho)
-levavg_ice = np.average(ice_clip_tend, axis=1, weights=rho)
+levavg_sum = np.average(ice_clip_tend+liq_clip_tend+vap_clip_tend, axis=1, weights=wghts)
+levavg_vap = np.average(vap_clip_tend, axis=1, weights=wghts)
+levavg_liq = np.average(liq_clip_tend, axis=1, weights=wghts)
+levavg_ice = np.average(ice_clip_tend, axis=1, weights=wghts)
 
 print("---- Total ----")
-print(np.sum(levavg_sum))
-print(np.sum(levavg_vap))
-print(np.sum(levavg_liq))
-print(np.sum(levavg_ice))
+print("Sum: "+str(np.sum(levavg_sum)))
+print("Vap: "+str(np.sum(levavg_vap)))
+print("Liq: "+str(np.sum(levavg_liq)))
+print("Ice: "+str(np.sum(levavg_ice)))
 
 title = 'Sum of Vapor/Ice/Liquid mixing ratio tendencies due to hole filling (total)'
 label = 'Mixing ratios (Sum)'
@@ -152,14 +161,14 @@ plot_t_profile(title, label, levavg_ice, out_filepath)
 ice_clip_tend_vhf = get_2d_profile(nc,'INEGCLPTEND_VHF')
 liq_clip_tend_vhf = get_2d_profile(nc,'LNEGCLPTEND_VHF')
 
-levavg_sum_vhf = np.average(ice_clip_tend_vhf+liq_clip_tend_vhf, axis=1, weights=rho)
-levavg_liq_vhf = np.average(liq_clip_tend_vhf, axis=1, weights=rho)
-levavg_ice_vhf = np.average(ice_clip_tend_vhf, axis=1, weights=rho)
+levavg_sum_vhf = np.average(ice_clip_tend_vhf+liq_clip_tend_vhf, axis=1, weights=wghts)
+levavg_liq_vhf = np.average(liq_clip_tend_vhf, axis=1, weights=wghts)
+levavg_ice_vhf = np.average(ice_clip_tend_vhf, axis=1, weights=wghts)
 
 print("---- VertHF ----")
-print(np.sum(levavg_sum_vhf))
-print(np.sum(levavg_liq_vhf))
-print(np.sum(levavg_ice_vhf))
+print("Sum: "+str(np.sum(levavg_sum_vhf)))
+print("Liq: "+str(np.sum(levavg_liq_vhf)))
+print("Ice: "+str(np.sum(levavg_ice_vhf)))
 
 title = 'Sum of Vapor/Ice/Liquid mixing ratio tendencies due to vertical hole filling'
 label = 'Mixing ratios (Sum)'
@@ -181,14 +190,14 @@ plot_t_profile(title, label, levavg_ice_vhf, out_filepath)
 ice_clip_tend_whf = get_2d_profile(nc,'INEGCLPTEND_WHF')
 liq_clip_tend_whf = get_2d_profile(nc,'LNEGCLPTEND_WHF')
 
-levavg_sum_whf = np.average(ice_clip_tend_whf+liq_clip_tend_whf, axis=1, weights=rho)
-levavg_liq_whf = np.average(liq_clip_tend_whf, axis=1, weights=rho)
-levavg_ice_whf = np.average(ice_clip_tend_whf, axis=1, weights=rho)
+levavg_sum_whf = np.average(ice_clip_tend_whf+liq_clip_tend_whf, axis=1, weights=wghts)
+levavg_liq_whf = np.average(liq_clip_tend_whf, axis=1, weights=wghts)
+levavg_ice_whf = np.average(ice_clip_tend_whf, axis=1, weights=wghts)
 
 print("---- WVHF ----")
-print(np.sum(levavg_sum_whf))
-print(np.sum(levavg_liq_whf))
-print(np.sum(levavg_ice_whf))
+print("Sum: "+str(np.sum(levavg_sum_whf)))
+print("Liq: "+str(np.sum(levavg_liq_whf)))
+print("Ice: "+str(np.sum(levavg_ice_whf)))
 
 title = 'Sum of Vapor/Ice/Liquid mixing ratio tendencies due to water vapor hole filling'
 label = 'Mixing ratios (Sum)'
@@ -209,14 +218,14 @@ plot_t_profile(title, label, levavg_ice_whf, out_filepath)
 ice_clip_tend_clp = get_2d_profile(nc,'INEGCLPTEND_CLP')
 liq_clip_tend_clp = get_2d_profile(nc,'LNEGCLPTEND_CLP')
 
-levavg_sum_clp = np.average(ice_clip_tend_clp+liq_clip_tend_clp, axis=1, weights=rho)
-levavg_liq_clp = np.average(liq_clip_tend_clp, axis=1, weights=rho)
-levavg_ice_clp = np.average(ice_clip_tend_clp, axis=1, weights=rho)
+levavg_sum_clp = np.average(ice_clip_tend_clp+liq_clip_tend_clp, axis=1, weights=wghts)
+levavg_liq_clp = np.average(liq_clip_tend_clp, axis=1, weights=wghts)
+levavg_ice_clp = np.average(ice_clip_tend_clp, axis=1, weights=wghts)
 
 print("---- CLIP ----")
-print(np.sum(levavg_sum_clp))
-print(np.sum(levavg_liq_clp))
-print(np.sum(levavg_ice_clp))
+print("Sum: "+str(np.sum(levavg_sum_clp)))
+print("Liq: "+str(np.sum(levavg_liq_clp)))
+print("Ice: "+str(np.sum(levavg_ice_clp)))
 
 title = 'Sum of Vapor/Ice/Liquid mixing ratio tendencies due to clipping'
 label = 'Mixing ratios (Sum)'
@@ -233,17 +242,80 @@ label = 'Cloud ice mixing ratio'
 out_filepath = outdir+out_filename_zavg+"_ice_clp"+out_filetype
 plot_t_profile(title, label, levavg_ice_clp, out_filepath)
 
-print('---- TEST (should be all zero) ----')
-print(np.sum(levavg_sum_vhf)+np.sum(levavg_sum_whf)+np.sum(levavg_sum_clp)-np.sum(levavg_sum)+np.sum(levavg_vap))
-print(np.sum(levavg_liq_vhf)+np.sum(levavg_liq_whf)+np.sum(levavg_liq_clp)-np.sum(levavg_liq))
-print(np.sum(levavg_ice_vhf)+np.sum(levavg_ice_whf)+np.sum(levavg_ice_clp)-np.sum(levavg_ice))
+# Plot profiles of clipping tendencies due to rest
+ice_clip_tend_rst = get_2d_profile(nc,'INEGCLPTEND_RST')
+liq_clip_tend_rst = get_2d_profile(nc,'LNEGCLPTEND_RST')
 
-# Those two profiles should match
+levavg_sum_rst = np.average(ice_clip_tend_rst+liq_clip_tend_rst, axis=1, weights=wghts)
+levavg_liq_rst = np.average(liq_clip_tend_rst, axis=1, weights=wghts)
+levavg_ice_rst = np.average(ice_clip_tend_rst, axis=1, weights=wghts)
+
+print("---- REST ----")
+print("Sum: "+str(np.sum(levavg_sum_rst)))
+print("Liq: "+str(np.sum(levavg_liq_rst)))
+print("Ice: "+str(np.sum(levavg_ice_rst)))
+
+title = 'Sum of Vapor/Ice/Liquid mixing ratio tendencies due to clipping'
+label = 'Mixing ratios (Sum)'
+out_filepath = outdir+out_filename_zavg+"_sum_rst"+out_filetype
+plot_t_profile(title, label, levavg_sum_rst, out_filepath)
+
+title = 'Cloud liquid mixing ratio tendencies due to clipping'
+label = 'Cloud liquid mixing ratio'
+out_filepath = outdir+out_filename_zavg+"_liq_rst"+out_filetype
+plot_t_profile(title, label, levavg_liq_rst, out_filepath)
+
+title = 'Cloud ice mixing ratio tendencies due to clipping'
+label = 'Cloud ice mixing ratio'
+out_filepath = outdir+out_filename_zavg+"_ice_rst"+out_filetype
+plot_t_profile(title, label, levavg_ice_rst, out_filepath)
+
+print('---- TEST (should be all zero) ----')
+print(np.sum(levavg_sum_vhf)+np.sum(levavg_sum_whf)+np.sum(levavg_sum_clp)+np.sum(levavg_sum_rst)-np.sum(levavg_sum)+np.sum(levavg_vap))
+print(np.sum(levavg_liq_vhf)+np.sum(levavg_liq_whf)+np.sum(levavg_liq_clp)+np.sum(levavg_liq_rst)-np.sum(levavg_liq))
+print(np.sum(levavg_ice_vhf)+np.sum(levavg_ice_whf)+np.sum(levavg_ice_clp)+np.sum(levavg_ice_rst)-np.sum(levavg_ice))
+
+# Those profiles should match
 fig = plt.figure()
 fig.text(.5,.95,title,horizontalalignment='center',)
 plt.plot(range(2001), levavg_vap)
-plt.plot(range(2001), -levavg_sum_whf, 'r--')
+plt.plot(range(2001), -levavg_sum_whf, 'ro')
 fig.savefig(outdir+out_filename_zavg+"_test"+out_filetype)
+
+
+fig = plt.figure()
+fig.text(.5,.95,title,horizontalalignment='center',)
+plt.plot(range(2001), levavg_sum-levavg_vap)
+plt.plot(range(2001), levavg_sum_vhf+levavg_sum_whf+levavg_sum_clp+levavg_sum_rst, 'ro')
+fig.savefig(outdir+out_filename_zavg+"_test2"+out_filetype)
+
+
+fig = plt.figure()
+fig.text(.5,.95,title,horizontalalignment='center',)
+
+liq_clip_tend_vhf_tavg = np.average(liq_clip_tend_vhf, axis=0)
+liq_clip_tend_whf_tavg = np.average(liq_clip_tend_whf, axis=0)
+liq_clip_tend_clp_tavg = np.average(liq_clip_tend_clp, axis=0)
+liq_clip_tend_rst_tavg = np.average(liq_clip_tend_rst, axis=0)
+
+plt.plot(liq_clip_tend_tavg, range(nlev))
+plt.plot(liq_clip_tend_vhf_tavg+liq_clip_tend_whf_tavg+liq_clip_tend_clp_tavg+liq_clip_tend_rst_tavg, range(nlev),'r--')
+
+fig.savefig(outdir+out_filename_tavg+"_compare_liq"+out_filetype)
+
+fig = plt.figure()
+fig.text(.5,.95,title,horizontalalignment='center',)
+
+ice_clip_tend_vhf_tavg = np.average(ice_clip_tend_vhf, axis=0)
+ice_clip_tend_whf_tavg = np.average(ice_clip_tend_whf, axis=0)
+ice_clip_tend_clp_tavg = np.average(ice_clip_tend_clp, axis=0)
+ice_clip_tend_rst_tavg = np.average(ice_clip_tend_rst, axis=0)
+
+plt.plot(ice_clip_tend_tavg, range(nlev))
+plt.plot(ice_clip_tend_vhf_tavg+ice_clip_tend_whf_tavg+ice_clip_tend_clp_tavg+ice_clip_tend_rst_tavg, range(nlev),'r--')
+
+fig.savefig(outdir+out_filename_tavg+"_compare_ice"+out_filetype)
+
 
 if ( l_show_plots ):
    plt.show()
