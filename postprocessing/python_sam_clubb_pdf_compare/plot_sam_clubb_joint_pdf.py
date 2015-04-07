5b67614092cc2a41b074b3e04cc6f5e38d3f8298
weberjk@uwm.edu <weberjk@uwm.edu>
2015-04-07 15:40:14 +0000

    adding some features
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7615 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 3ae1d33208a8d1930255a592b21f0c72d0c05efb

--- postprocessing/python_sam_clubb_pdf_compare/plot_sam_clubb_joint_pdf.py
+++ postprocessing/python_sam_clubb_pdf_compare/plot_sam_clubb_joint_pdf.py
@@ -12,6 +12,8 @@ import netCDF4
 # User input
 #----------------------------------------------------------------------------------------
 
+case_dir = 'ADG2_best'
+
 # Directory for SAM's 3D files 
 sam_3D_dir = '/home/weberjk/Plot_CRM/SAM_LBA/Data/3D/1km/'
 
@@ -27,7 +29,7 @@ sam_dt = 6. # [s] SAM's model timestep, not output frequency.
 sam_stat_file = '/home/weberjk/Plot_CRM/SAM_LBA/Data/LBA.nc'
 
 # Directory for CLUBB's sample points file
-clubb_3D_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/prescribe_thl_thlp2_rt_rtp2/'
+clubb_3D_dir = '/home/weberjk/Ticket764/output/%s/'%(case_dir)
 
 # CLUBB's file names
 clb_nl_file = clubb_3D_dir + 'lba_nl_lh_sample_points_2D.nc'
@@ -37,25 +39,25 @@ clubb_stat_file = clubb_3D_dir + 'lba_zt.nc' # The statistics file and sample po
                                            # same directory by default
 
 # Variable name, time, and height
-sam_abs_var_name = 'CHI'         # SAM's name for abscissa
-sam_abs_file_suffix = '_micro.nc' # SAM outputs 2 different 3D files. One is '.nc' 
+sam_abs_var_name = 'QR'         # SAM's name for abscissa
+sam_abs_file_suffix = '.nc' # SAM outputs 2 different 3D files. One is '.nc' 
                                  # and the other is written out from Morrison 
                                  # Micro and has a '_micro.nc'
 
-sam_ord_var_name = 'QR'   # SAM's name for ordinate
+sam_ord_var_name = 'NR'   # SAM's name for ordinate
 sam_ord_file_suffix = '.nc'
 
-sam_colorby_var = 'PRA' # Color SAM's points by anther variable
+sam_colorby_var = 'NRAGG' # Color SAM's points by anther variable
 sam_colorby_file_suffix = '_micro.nc'
 
 sam_abs_unit_conversion = 1.
 sam_ord_unit_conversion = 1./1000.
 
-clubb_abs_var_name = 'chi' # CLUBB's name for abscissa
-clubb_ord_var_name = 'rr' # CLUBB's name for ordinate
+clubb_abs_var_name = 'rr' # CLUBB's name for abscissa
+clubb_ord_var_name = 'Nr' # CLUBB's name for ordinate
 
 # Figure output directory and name
-out_dir = '/home/weberjk/Summarize_Progress/CLUBB/output/prescribe_thl_thlp2_rt_rtp2/' 
+out_dir = '/home/weberjk/Ticket764/output/%s/'%(case_dir) 
 out_name = 'joint_pdf_%s_%s.png'%(clubb_abs_var_name,clubb_ord_var_name) 
 
 abs_name = '$\chi$' # Fancy names for title
@@ -65,8 +67,8 @@ abs_units = '$kg\ kg^{-1}$'  # How the variable looks when it is plotted
 ord_units = '$kg\ kg^{-1}$'  # The plotted units
 
 # Analysis height and time                   
-z0 = 1800                  # Analysis height [m]
-t0_in_min = 225            # Analysis time [min] 
+z0_array = [4000]                # Analysis height [m]
+t0_in_min = 330            # Analysis time [min] 
                             # Note: This exact time must exist for SAM's output. 
                             # CLUBB will use the closest available.
 
@@ -98,102 +100,127 @@ def calc_precip_frac(rain, threshold):
     precip_frac = float(rain_num) / float(total_num)
     return precip_frac    
 
-# Find the correct SAM 3D file.
-t0_in_s = t0_in_min*60.
+for i in np.arange(0,len(z0_array)):
+    z0 = z0_array[i]
 
-# Find which NSTEP corrosponds to our desired t0
-t0_filename = str(int((t0_in_s)/sam_dt))
+    # Find the correct SAM 3D file.
+    t0_in_s = t0_in_min*60.
 
-# SAM's nstep counter is a fixed 10 digits long.
-sam_nstep_counter='0000000000'
+    # Find which NSTEP corrosponds to our desired t0
+    t0_filename = str(int((t0_in_s)/sam_dt))
 
-# Find how many digits the desire NSTEP takes up
-place = len(t0_filename)
+    # SAM's nstep counter is a fixed 10 digits long.
+    sam_nstep_counter='0000000000'
 
-# From the beginning of the nstep counter, find how many places until we insert our 
-# NSTEP
-end_zeros = len(sam_nstep_counter) - place
+    # Find how many digits the desire NSTEP takes up
+    place = len(t0_filename)
 
-# Combine SAM's nstep counter and our desired NSTEP
-t0_filename = sam_nstep_counter[0:end_zeros] + t0_filename
+    # From the beginning of the nstep counter, find how many places until we insert our 
+    # NSTEP
+    end_zeros = len(sam_nstep_counter) - place
 
-# Paste the strings together and call it a filename.
-sam_abs_file = sam_3D_dir+sam_3D_file+t0_filename+sam_abs_file_suffix
-sam_ord_file = sam_3D_dir+sam_3D_file+t0_filename+sam_ord_file_suffix
-sam_colorby_file = sam_3D_dir+sam_3D_file+t0_filename+sam_colorby_file_suffix
+    # Combine SAM's nstep counter and our desired NSTEP
+    t0_filename = sam_nstep_counter[0:end_zeros] + t0_filename
+    
+    # Paste the strings together and call it a filename.
+    sam_abs_file = sam_3D_dir+sam_3D_file+t0_filename+sam_abs_file_suffix
+    sam_ord_file = sam_3D_dir+sam_3D_file+t0_filename+sam_ord_file_suffix
+    sam_colorby_file = sam_3D_dir+sam_3D_file+t0_filename+sam_colorby_file_suffix
 
-print "Extract SAM's RHO for this level"
-nc = netCDF4.Dataset(sam_stat_file)
-time, time_u = pull_samples(nc, 'time')
-z, z_u = pull_samples(nc, 'z')
-rho, rho_u = pull_samples(nc, 'RHO')
-idx_z0 = (np.abs(z[:] - z0)).argmin()
-idx_t0 = (np.abs(time[:] - t0_in_min)).argmin()
-rho = rho[idx_t0, idx_z0]
+    print "Extract SAM's RHO for this level"
+    nc = netCDF4.Dataset(sam_stat_file)
+    time, time_u = pull_samples(nc, 'time')
+    z, z_u = pull_samples(nc, 'z')
+    rho, rho_u = pull_samples(nc, 'RHO')
+    idx_z0 = (np.abs(z[:] - z0)).argmin()
+    idx_t0 = (np.abs(time[:] - t0_in_min)).argmin()
+    rho = rho[idx_t0, idx_z0]
+    
+    print "Extract SAM abscissa"
+    nc = netCDF4.Dataset(sam_abs_file)
+    sam_z, z_u = pull_samples(nc, 'z')
+    sam_abs, sam_abs_u = pull_samples(nc, sam_abs_var_name)
+    nc.close()
+    
+    print "Extract SAM ordinate"
+    nc = netCDF4.Dataset(sam_ord_file)
+    sam_ord, sam_ord_u = pull_samples(nc, sam_ord_var_name)
+    nc.close()
+    
+    print "Extract SAM color-by"
+    nc = netCDF4.Dataset(sam_colorby_file)
+    sam_colorby, sam_colorby_u = pull_samples(nc, sam_colorby_var)
+    nc.close()
+    
+    print "Find Time and Height Indicies for SAM Slice"
+    idx_z0 = (np.abs(sam_z[:] - z0)).argmin()
+    
+    sam_abs = reshape_sam_slice(sam_abs, idx_z0)
+    sam_ord = reshape_sam_slice(sam_ord,  idx_z0)
+    sam_colorby = reshape_sam_slice(sam_colorby,  idx_z0)
+    
 
-print "Extract SAM abscissa"
-nc = netCDF4.Dataset(sam_abs_file)
-sam_z, z_u = pull_samples(nc, 'z')
-sam_abs, sam_abs_u = pull_samples(nc, sam_abs_var_name)
-nc.close()
+    print "Extract CLUBB Data"
+    nc = netCDF4.Dataset(clb_nl_file)
+    clb_z, z_u = pull_samples(nc, 'altitude')
+    clb_time, clb_time_u = pull_samples(nc, 'time')
+    clb_abs, clb_abs_u = pull_samples(nc, clubb_abs_var_name)
+    clb_ord, clb_ord_u = pull_samples(nc, clubb_ord_var_name)
+    nc.close()
 
-print "Extract SAM ordinate"
-nc = netCDF4.Dataset(sam_ord_file)
-sam_ord, sam_ord_u = pull_samples(nc, sam_ord_var_name)
-nc.close()
+    nc = netCDF4.Dataset(clubb_stat_file)
+    mixt_frac, mixt_frac_u = pull_samples(nc, 'mixt_frac')
+    nc.close()
 
-print "Extract SAM color-by"
-nc = netCDF4.Dataset(sam_colorby_file)
-sam_colorby, sam_colorby_u = pull_samples(nc, sam_colorby_var)
-nc.close()
+    nc = netCDF4.Dataset(clb_u_file)
+    dp1, dp1_u = pull_samples(nc, 'dp1')
+    nc.close()
+    
+    idx_t0 = (np.abs(clb_time[:] - t0_in_s)).argmin()
+    idx_z0 = (np.abs(clb_z[:] - z0)).argmin()
+    
+    clb_abs = reshape_clubb_samples(clb_abs, idx_z0,idx_t0)
+    clb_ord = reshape_clubb_samples(clb_ord, idx_z0,idx_t0)
+    dp1 = dp1[idx_t0,idx_z0]
+    mixt_frac = mixt_frac[idx_t0,idx_z0]
+    
+    idx_comp_1 = np.where(dp1 < mixt_frac)
+    idx_comp_2 = np.where(dp1 >= mixt_frac)
+    clb_abs_1 = clb_abs[idx_comp_1]
+    clb_abs_2 = clb_abs[idx_comp_2]
 
-print "Find Time and Height Indicies for SAM Slice"
-idx_z0 = (np.abs(sam_z[:] - z0)).argmin()
+    clb_ord_1 = clb_ord[idx_comp_1]
+    clb_ord_2 = clb_ord[idx_comp_2]
+    
+    print "Convert Units"
+    sam_abs = sam_abs*sam_abs_unit_conversion
+    sam_ord = sam_ord*sam_ord_unit_conversion
+    
 
-sam_abs = reshape_sam_slice(sam_abs, idx_z0)
-sam_ord = reshape_sam_slice(sam_ord,  idx_z0)
-sam_colorby = reshape_sam_slice(sam_colorby,  idx_z0)
+    print "Plot"
+    out_name = '%s_joint_pdf_%s_%s_%smin_%sm.png'%(case_dir,clubb_abs_var_name,clubb_ord_var_name,t0_in_min,z0)
 
+    f, (ax1, ax2) = plt.subplots(1,2,sharey=True, sharex=True)
+    f.subplots_adjust(bottom=.2)
+    
+    ax1.scatter(sam_abs[0::],sam_ord[0::],c='gray', alpha=.3)
+    ax1.scatter(sam_abs[0::16],sam_ord[0::16], c=sam_colorby[0::16])
+    ax1.set_xlim([.9*min(min(clb_abs),min(sam_abs)),1.1*max(max(clb_abs),max(sam_abs))])
+    ax1.set_ylim([.9*min(min(clb_ord),min(sam_ord)),1.1*max(max(clb_ord),max(sam_ord))])
+    ax1.grid()
+    ax1.set_title('SAM')
+    plt.setp(ax1.get_xticklabels(),rotation=45)
+    
+    ax2.scatter(clb_abs_1,clb_ord_1,c='r',label='Comp.-1')
+    ax2.scatter(clb_abs_2,clb_ord_2,c='b',label='Comp.-2')
+    plt.legend()
+    plt.setp(ax2.get_xticklabels(),rotation=45)
+    ax2.set_title('CLUBB')
+    ax2.grid()
 
-print "Extract CLUBB Data"
-nc = netCDF4.Dataset(clb_nl_file)
-clb_z, z_u = pull_samples(nc, 'altitude')
-clb_time, clb_time_u = pull_samples(nc, 'time')
-clb_abs, clb_abs_u = pull_samples(nc, clubb_abs_var_name)
-clb_ord, clb_ord_u = pull_samples(nc, clubb_ord_var_name)
-nc.close()
-
-idx_t0 = (np.abs(clb_time[:] - t0_in_s)).argmin()
-idx_z0 = (np.abs(clb_z[:] - z0)).argmin()
-
-clb_abs = reshape_clubb_samples(clb_abs, idx_z0,idx_t0)
-clb_ord = reshape_clubb_samples(clb_ord, idx_z0,idx_t0)
-
-
-
-print "Convert Units"
-sam_abs = sam_abs*sam_abs_unit_conversion
-sam_ord = sam_ord*sam_ord_unit_conversion
-
-
-print "Plot"
-f, (ax1, ax2) = plt.subplots(1,2,sharey=True, sharex=True)
-f.subplots_adjust(bottom=.15)
-
-ax1.scatter(sam_abs[0::],sam_ord[0::],c='gray', alpha=.3)
-ax1.scatter(sam_abs[0::16],sam_ord[0::16], c=sam_colorby[0::16])
-ax1.set_xlim([min(min(clb_abs),min(sam_abs)),1.1*max(max(clb_abs),max(sam_abs))])
-ax1.set_ylim([min(min(clb_ord),min(sam_ord)),1.1*max(max(clb_ord),max(sam_ord))])
-ax1.grid()
-plt.setp(ax1.get_xticklabels(),rotation=45)
-
-ax2.scatter(clb_abs,clb_ord)
-plt.setp(ax2.get_xticklabels(),rotation=45)
-ax2.grid()
-
-ax1.set_xlabel(abs_name+', '+ abs_units)
-ax1.set_ylabel(ord_name+', '+ord_units)
-text_str = 'Joint PDF of %s and %s at z=%d m and t=%d min.'%(abs_name, ord_name, z0, t0_in_min)
-plt.suptitle(text_str)
-plt.savefig(out_dir + out_name)
-plt.close()
+    ax1.set_xlabel(abs_name+', '+ abs_units)
+    ax1.set_ylabel(ord_name+', '+ord_units)
+    text_str = 'Joint PDF of %s and %s at z=%d m and t=%d min.'%(abs_name, ord_name, z0, t0_in_min)
+    plt.suptitle(text_str)
+    plt.savefig(out_dir + out_name)
+    plt.close()
