c49af21f58113df48c9048140dc472e69ea65d31
raut@uwm.edu <raut@uwm.edu>
2015-05-16 01:44:34 +0000

    Minor change to script.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7675 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 0935e0ac27e90ab76c681113b3515be40f5c6e47

--- postprocessing/latin_hypercube_plotting/variance_analysis_scripts/rms_vs_sample_points/silhs_varying_sp_output.sh
+++ postprocessing/latin_hypercube_plotting/variance_analysis_scripts/rms_vs_sample_points/silhs_varying_sp_output.sh
@@ -12,15 +12,23 @@ MODEL_FILE="$CLUBB_DIR/input/case_setups/rico_lh_model.in"
 # The different numbers of sample points to use, separated by spaces
 SAMPLE_POINT_VALUES="2 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200"
 
+# Sanity check
+if [[ ! -f $CLUBB_DIR/run_scripts/run_scm.bash ]]
+then
+  >&2 echo "The CLUBB run script was not found. Please check the CLUBB_DIR" \
+           "variable in the script."
+  exit 1
+fi
+
 for num_samples in $SAMPLE_POINT_VALUES
 do
   sed 's/lh_num_samples\s*=\s*[0-9]*/lh_num_samples = '"$num_samples"'/g' \
     -i $MODEL_FILE
   echo "Running with $num_samples samples"
   $CLUBB_DIR/run_scripts/run_scm.bash rico_lh -o $OUTPUT_DIR/silhs_$num_samples &>/dev/null
-  if [ ! $? -eq 0 ]
+  if [[ ! $? -eq 0 ]]
   then
-    echo 'A run failed!'
-    exit 1
+    >&2 echo 'A run failed!'
+    exit 2
   fi
 done
