1676594df40f86464a3d64ac0542fa6e1f5c75a6
bmg2@uwm.edu <bmg2@uwm.edu>
2015-06-02 23:15:41 +0000

    This is the first step in updating the scatter/contour plotter to be able to plot multiple CLUBB lines on one plot!
    This will require vectorizing many places in the scatter/contour plotter code.
    
    However, in this first step, the run_scatter_contour_plots.bash script is
    updated to be able to read in multiple CLUBB data sets (files).
    
    As before, if less than two files (and paths) are given at the command line,
    the script reverts to the "default" configuration listed in the
    PDF_scatter_contour_plotter.m file, which is a specific RICO LES 3D output file
    and one specific CLUBB RICO output file.
    
    However, if at least two files (and paths) are given at the command line, the
    script now has an enhancement.  Just as before, the first filename listed is the
    SAM LES 3D output file.  However, after that, one or more CLUBB files can be
    listed at the command line.  The script will read in the filenames into an array,
    print them to the screen, and then pass them into the MATLAB code.  As of now,
    only the first CLUBB file listed is used in the actual program.  It will remain
    that way until the entire program has been updated.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7700 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 898015bc34828d4d467310a6717d0242985165fc

--- postprocessing/matlab_scatter_contour_plots/run_scatter_contour_plots.bash
+++ postprocessing/matlab_scatter_contour_plots/run_scatter_contour_plots.bash
@@ -15,10 +15,11 @@ cd $run_dir
 # all users. 
 chmod 777 output
 
-# Check for files (and paths) passed in through the command line.  The first
-# one is the SAM LES 3D file (in netCDF format).  The second one is the CLUBB
-# file (also in netCDF format).  When both files aren't present in the command
-# line, use the value of 'default' for both.  The default setting is found in
+# Check for files (and paths) passed in through the command line.  The first one
+# is the SAM LES 3D file (in netCDF format).  Every subsequent file is a CLUBB
+# file (also in netCDF format).  When at least two files aren't present in the
+# command line, use the value of 'default' for both SAM and CLUBB (the CLUBB
+# input file array will have a size of 1).  The default setting is found in
 # PDF_scatter_contour_plotter.m.
 if [ -z $2 ]; then
 
@@ -32,13 +33,34 @@ if [ -z $2 ]; then
 
 else
 
+   # The first file listed is always the SAM LES 3D output file.
    SAM_LES_3D_file=$1
-   CLUBB_file=$2
+
+   # All the remaining files listed are CLUBB files, which will be placed into
+   # the CLUBB_file array.
+   shift
+   CLUBB_file_index=-1
+   while (( "$#" )); do
+      CLUBB_file_index=$((CLUBB_file_index+1))
+      CLUBB_file[$((CLUBB_file_index))]=$1
+      shift
+   done
 
    echo ""
    echo "SAM LES 3D file:  "$SAM_LES_3D_file
-   echo ""
-   echo "CLUBB_file:  "$CLUBB_file
+
+   if [ ${#CLUBB_file[@]} -gt 1 ]
+   then
+      for (( indx=1; indx<=${#CLUBB_file[@]}; indx++ ))
+      do
+         echo ""
+         echo "CLUBB file "$indx":  "${CLUBB_file[$((indx-1))]}
+      done
+   else
+      echo ""
+      echo "CLUBB file:  "${CLUBB_file[0]}
+   fi
+
    echo ""
 
 fi
