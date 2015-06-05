90b8fe9b7346fef589b171babe8d8b97e7f610c8
raut@uwm.edu <raut@uwm.edu>
2015-06-05 22:35:41 +0000

    I am continuing my quest to create an automated script to generate plots for the paper!
    In this commit, to avoid creating giant output data sets, I added a `stats.in` file that specifies the small number of statistics that are actually needed for the plots that will be put in the paper. I also modified the SILHS run script to accept an option for the statistics file.
    
    This is for clubb:ticket:740.
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7712 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ea740dabd0b3b4cdc06d58ce883cde68e076c053

--- postprocessing/latin_hypercube_plotting/variance_analysis_scripts/rms_vs_sample_points/silhs_varying_sp_output.sh
+++ postprocessing/latin_hypercube_plotting/variance_analysis_scripts/rms_vs_sample_points/silhs_varying_sp_output.sh
@@ -8,6 +8,7 @@
 CLUBB_DIR="`readlink -f \`dirname $0\``/../../../.."
 OUTPUT_DIR="$CLUBB_DIR/rms_plot_output"
 CASE_NAME="rico_lh"
+STATS_FILE=""
 
 # The different numbers of sample points to use, separated by spaces
 SAMPLE_POINT_VALUES="2 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200"
@@ -26,6 +27,9 @@ do
         shift
         CASE_NAME=$1
         ;;
+    "--stats_file")
+        shift
+        STATS_FILE=$1
     *)
         >&2 echo "Invalid argument: $1"
         exit 1
@@ -61,12 +65,20 @@ fi
 
 mkdir -p $OUTPUT_DIR
 
+# Statistics command line argument string
+STATS_CMD_STRING=""
+if [[ -n $STATS_FILE ]]
+then
+    STATS_CMD_STRING="-s $STATS_FILE"
+fi
+
 for num_samples in $SAMPLE_POINT_VALUES
 do
   sed 's/lh_num_samples\s*=\s*[0-9]*/lh_num_samples = '"$num_samples"'/g' \
     -i $MODEL_FILE
   echo "Running with $num_samples samples"
-  $CLUBB_DIR/run_scripts/run_scm.bash $CASE_NAME -o $OUTPUT_DIR/silhs_$num_samples &>/dev/null
+  $CLUBB_DIR/run_scripts/run_scm.bash $CASE_NAME $STATS_CMD_STRING \
+    -o $OUTPUT_DIR/silhs_$num_samples --netcdf &>/dev/null
   if [[ ! $? -eq 0 ]]
   then
     >&2 echo 'A run failed!'
