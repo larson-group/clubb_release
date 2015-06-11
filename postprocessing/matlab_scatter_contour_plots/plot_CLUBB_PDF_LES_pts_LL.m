6a0e90b4f9a94c77a36dd763dc94cb378db4fd39
bmg2@uwm.edu <bmg2@uwm.edu>
2015-06-11 04:50:31 +0000

    I am updating the scatter/contour plotter, this time with significant progress.
    I have updated all of the plot utility files to be able to plot multiple CLUBB
    data sets (files).  As of now, the LES and multiple CLUBB files can be plotted
    successfully in all two-variable and one-variable (marginal) plots.
    
    However, there is still more work to do.  Right now, all the CLUBB lines are
    exactly the same (solid, blue lines on the marginal plots, etc.) and the
    legends need entries for multiple CLUBB files.  But at least it works, which
    means in runs all the way through without crashing.
    
    Additionally, the traditional plots (one LES file and one CLUBB run) haven't
    changed a bit.  The .png output files from the previous revision are an exact
    match to the .png output files from this revision for all 18 files in my test
    output.  In other words, bin-diff identical .png files!
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7746 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 2c27f7bc4555e4e068e67f96aa083300e7b0f5f5

--- postprocessing/matlab_scatter_contour_plots/plot_CLUBB_PDF_LES_pts_LL.m
+++ postprocessing/matlab_scatter_contour_plots/plot_CLUBB_PDF_LES_pts_LL.m
@@ -9,6 +9,7 @@ function plot_CLUBB_PDF_LES_pts_LL( var_x_LES, ...
                                     sigma_y_2_n, corr_x_y_1_n, ...
                                     corr_x_y_2_n, precip_frac_1, ...
                                     precip_frac_2, mixt_frac, ...
+                                    num_clubb_files, ...
                                     var_x_label, var_y_label, ...
                                     case_name, fields_plotted, ...
                                     time_plotted, ...
@@ -39,15 +40,18 @@ for i = 1:1:nx_LES_grid*ny_LES_grid
    end
 end
 precip_frac_x_LES = pos_count_x / ( nx_LES_grid * ny_LES_grid );
-precip_frac_clubb ...
-= mixt_frac * precip_frac_1 + ( 1.0 - mixt_frac ) * precip_frac_2;
+for clubb_idx = 1:1:num_clubb_files
+   precip_frac_clubb(clubb_idx) ...
+   = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+     + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx);
+end % clubb_idx = 1:1:num_clubb_files
 min_x = min( var_x_LES_pos );
 max_x = max( var_x_LES_pos );
 num_x_divs = num_x_pts + 1;
 delta_x = ( max_x - min_x ) / num_x_pts;
 x_divs = zeros( num_x_divs, 1 );
 x = zeros( num_x_pts, 1 );
-P_x = zeros( num_x_pts, 1 );
+P_x = zeros( num_clubb_files, num_x_pts );
 % Calculate the x "division points" (edges of the bins).
 for i = 1:1:num_x_divs
    x_divs(i) = min_x + delta_x * ( i - 1 );
@@ -57,13 +61,17 @@ for i = 1:1:num_x_pts
    x(i) = 0.5 * ( x_divs(i) + x_divs(i+1) );
 end
 % CLUBB's PDF marginal for x (lognormal or in-precip portion).
-for i = 1:1:num_x_pts
-   P_x(i) ...
-   = mixt_frac * precip_frac_1 ...
-     * PDF_comp_Lognormal( x(i), mu_x_1_n, sigma_x_1_n ) ...
-     + ( 1.0 - mixt_frac ) * precip_frac_2 ...
-       * PDF_comp_Lognormal( x(i), mu_x_2_n, sigma_x_2_n );
-end
+for clubb_idx = 1:1:num_clubb_files
+   for i = 1:1:num_x_pts
+      P_x(clubb_idx,i) ...
+      = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+        * PDF_comp_Lognormal( x(i), mu_x_1_n(clubb_idx), ...
+                              sigma_x_1_n(clubb_idx) ) ...
+        + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx) ...
+          * PDF_comp_Lognormal( x(i), mu_x_2_n(clubb_idx), ...
+                                sigma_x_2_n(clubb_idx) );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 % Centerpoints and counts for each bin (from LES results) for x, where
 % x > 0.
 binranges_x = x;
@@ -80,27 +88,31 @@ if ( precip_frac_x_LES < 1.0 )
                'Color', 'r', 'LineStyle', '-', 'LineWidth', 3, ...
                'HeadLength', 11, 'HeadWidth', 11 );
 end
-hold on
 % Plot PDF of x for CLUBB, where x > 0.
-plot( x, P_x, '-b', 'LineWidth', 2 )
-if ( precip_frac_clubb < 1.0 )
+for clubb_idx = 1:1:num_clubb_files
    hold on
-   % Plot PDF of x for CLUBB, where x = 0.
-   arrow_vec = get( gca, 'Position' );
-   annotation( 'arrow', [ arrow_vec(1) arrow_vec(1) ], ...
-               [ arrow_vec(2) arrow_vec(2)+arrow_vec(4) ], ...
-               'Color', 'b', 'LineStyle', '-', 'LineWidth', 2, ...
-               'HeadLength', 7, 'HeadWidth', 7 );
-end
+   plot( x, P_x(clubb_idx,:), '-b', 'LineWidth', 2 )
+end % clubb_idx = 1:1:num_clubb_files
+% Plot PDF of x for CLUBB, where x = 0.
+for clubb_idx = 1:1:num_clubb_files
+   if ( precip_frac_clubb(clubb_idx) < 1.0 )
+      hold on
+      arrow_vec = get( gca, 'Position' );
+      annotation( 'arrow', [ arrow_vec(1) arrow_vec(1) ], ...
+                  [ arrow_vec(2) arrow_vec(2)+arrow_vec(4) ], ...
+                  'Color', 'b', 'LineStyle', '-', 'LineWidth', 2, ...
+                  'HeadLength', 7, 'HeadWidth', 7 );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 hold off
 % Set the range of the plot on both the x-axis and y-axis.
-if ( precip_frac_clubb < 1.0 || precip_frac_x_LES < 1.0 )
+if ( any( precip_frac_clubb < 1.0 ) || precip_frac_x_LES < 1.0 )
    xlim([0 max_x]);
 else
    xlim([min_x max_x]);
 end
 ylim( [ 0 max( max(bincounts_x) / ( nx_LES_grid * ny_LES_grid * delta_x ), ...
-               max(P_x) ) ] );
+               max(max(P_x)) ) ] );
 %xlabel( var_x_label )
 legend( 'LES', 'CLUBB', 'Location', 'NorthEast' );
 grid on
@@ -128,7 +140,7 @@ num_y_divs = num_y_pts + 1;
 delta_y = ( max_y - min_y ) / num_y_pts;
 y_divs = zeros( num_y_divs, 1 );
 y = zeros( num_y_pts, 1 );
-P_y = zeros( num_y_pts, 1 );
+P_y = zeros( num_clubb_files, num_y_pts );
 % Calculate the y "division points" (edges of the bins).
 for j = 1:1:num_y_divs
    y_divs(j) = min_y + delta_y * ( j - 1 );
@@ -138,13 +150,17 @@ for j = 1:1:num_y_pts
    y(j) = 0.5 * ( y_divs(j) + y_divs(j+1) );
 end
 % CLUBB's PDF marginal for y (lognormal or in-precip portion).
-for j = 1:1:num_y_pts
-   P_y(j) ...
-   = mixt_frac * precip_frac_1 ...
-     * PDF_comp_Lognormal( y(j), mu_y_1_n, sigma_y_1_n ) ...
-     + ( 1.0 - mixt_frac ) * precip_frac_2 ...
-       * PDF_comp_Lognormal( y(j), mu_y_2_n, sigma_y_2_n );
-end
+for clubb_idx = 1:1:num_clubb_files
+   for j = 1:1:num_y_pts
+      P_y(clubb_idx,j) ...
+      = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+        * PDF_comp_Lognormal( y(j), mu_y_1_n(clubb_idx), ...
+                              sigma_y_1_n(clubb_idx) ) ...
+        + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx) ...
+          * PDF_comp_Lognormal( y(j), mu_y_2_n(clubb_idx), ...
+                                sigma_y_2_n(clubb_idx) );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 % Centerpoints and counts for each bin (from LES results) for y, where
 % y > 0.
 binranges_y = y;
@@ -161,27 +177,31 @@ if ( precip_frac_y_LES < 1.0 )
                'LineStyle', '-', 'LineWidth', 3, ...
                'HeadLength', 11, 'HeadWidth', 11 );
 end
-hold on
 % Plot PDF of y for CLUBB, where y > 0.
-plot( y, P_y, '-b', 'LineWidth', 2 );
-if ( precip_frac_clubb < 1.0 )
+for clubb_idx = 1:1:num_clubb_files
    hold on
-   % Plot PDF of y for CLUBB, where y = 0.
-   arrow_vec = get( gca, 'Position' );
-   annotation( 'arrow', [ arrow_vec(1) arrow_vec(1)+arrow_vec(3) ], ...
-               [ arrow_vec(2) arrow_vec(2) ], 'Color', 'b', ...
-               'LineStyle', '-', 'LineWidth', 2, ...
-               'HeadLength', 7, 'HeadWidth', 7 );
-end
+   plot( y, P_y(clubb_idx,:), '-b', 'LineWidth', 2 );
+end % clubb_idx = 1:1:num_clubb_files
+% Plot PDF of y for CLUBB, where y = 0.
+for clubb_idx = 1:1:num_clubb_files
+   if ( precip_frac_clubb(clubb_idx) < 1.0 )
+      hold on
+      arrow_vec = get( gca, 'Position' );
+      annotation( 'arrow', [ arrow_vec(1) arrow_vec(1)+arrow_vec(3) ], ...
+                  [ arrow_vec(2) arrow_vec(2) ], 'Color', 'b', ...
+                  'LineStyle', '-', 'LineWidth', 2, ...
+                  'HeadLength', 7, 'HeadWidth', 7 );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 hold off
 % Set the range of the plot on both the x-axis and y-axis.
-if ( precip_frac_clubb < 1.0 || precip_frac_y_LES < 1.0 )
+if ( any( precip_frac_clubb < 1.0 ) || precip_frac_y_LES < 1.0 )
    xlim([0 max_y]);
 else
    xlim([min_y max_y]);
 end
 ylim( [ 0 max( max(bincounts_y) / ( nx_LES_grid * ny_LES_grid * delta_y ), ...
-               max(P_y) ) ] );
+               max(max(P_y)) ) ] );
 %xlabel( var_y_label )
 % Reverse the plot and turn the plot 90 degrees clockwise.
 set(gca, 'xdir', 'reverse')
@@ -192,126 +212,165 @@ box on
 subplot( 'Position', [ 0.1 0.1 0.55 0.55 ] )
 
 % Scatterplot and contour plot for x and y.
-P_xy = zeros( num_y_pts, num_x_pts );
+P_xy = zeros( num_clubb_files, num_y_pts, num_x_pts );
 % CLUBB's PDF of x and y, where y > 0.
-for i = 1:1:num_x_pts
-   for j = 1:1:num_y_pts
-      P_xy(j,i) ...
-      = mixt_frac * precip_frac_1 ...
-        * PDF_comp_bivar_LL( x(i), y(j), mu_x_1_n, mu_y_1_n, ...
-                             sigma_x_1_n, sigma_y_1_n, corr_x_y_1_n ) ...
-        + ( 1.0 - mixt_frac ) * precip_frac_2 ...
-          * PDF_comp_bivar_LL( x(i), y(j), mu_x_2_n, mu_y_2_n, ...
-                               sigma_x_2_n, sigma_y_2_n, corr_x_y_2_n );
+for clubb_idx = 1:1:num_clubb_files
+   for i = 1:1:num_x_pts
+      for j = 1:1:num_y_pts
+         P_xy(clubb_idx,j,i) ...
+         = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+           * PDF_comp_bivar_LL( x(i), y(j), ...
+                                mu_x_1_n(clubb_idx), ...
+                                mu_y_1_n(clubb_idx), ...
+                                sigma_x_1_n(clubb_idx), ...
+                                sigma_y_1_n(clubb_idx), ...
+                                corr_x_y_1_n(clubb_idx) ) ...
+           + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx) ...
+             * PDF_comp_bivar_LL( x(i), y(j), ...
+                                  mu_x_2_n(clubb_idx), ...
+                                  mu_y_2_n(clubb_idx), ...
+                                  sigma_x_2_n(clubb_idx), ...
+                                  sigma_y_2_n(clubb_idx), ...
+                                  corr_x_y_2_n(clubb_idx) );
+      end
    end
-end
+end % clubb_idx = 1:1:num_clubb_files
 % Find the minimum contour to plot for CLUBB results.
 % Sample a value that is a set number of standard deviations away from the
 % bivariate mean for each variable for each component.
 num_std_devs = num_std_devs_min_contour / sqrt(2.0);
-% For the 1st PDF component, sample P_xy at sqrt(2)*num_std_devs, which is
-% num_std_devs away from the 1st PDF component bivariate mean in both x
-% and y.
-if ( sigma_x_1_n > 0.0 && sigma_y_1_n > 0.0 )
-   mu_x_1 = exp( mu_x_1_n + 0.5 * sigma_x_1_n^2 );
-   sigma_x_1 = sqrt( ( exp( sigma_x_1_n^2 ) - 1.0 ) ...
-                     * exp( 2.0 * mu_x_1_n + sigma_x_1_n^2 ) );
-   x_1_test = min( mu_x_1 + num_std_devs * sigma_x_1, max_x );
-   mu_y_1 = exp( mu_y_1_n + 0.5 * sigma_y_1_n^2 );
-   sigma_y_1 = sqrt( ( exp( sigma_y_1_n^2 ) - 1.0 ) ...
-                     * exp( 2.0 * mu_y_1_n + sigma_y_1_n^2 ) );
-   y_1_test = min( mu_y_1 + num_std_devs * sigma_y_1, max_y );
-   P_low_1 ...
-   = mixt_frac * precip_frac_1 ...
-     * PDF_comp_bivar_LL( x_1_test, y_1_test, mu_x_1_n, mu_y_1_n, ...
-                          sigma_x_1_n, sigma_y_1_n, corr_x_y_1_n ) ...
-     + ( 1.0 - mixt_frac ) * precip_frac_2 ...
-       * PDF_comp_bivar_LL( x_1_test, y_1_test, mu_x_2_n, mu_y_2_n, ...
-                            sigma_x_2_n, sigma_y_2_n, corr_x_y_2_n );
-else
-   P_low_1 = -1.0;
-end
-% For the 2nd PDF component, sample P_xy at sqrt(2)*num_std_devs, which is
-% num_std_devs away from the 2nd PDF component bivariate mean in both x
-% and y.
-if ( sigma_x_2_n > 0.0 && sigma_y_2_n > 0.0 )
-   mu_x_2 = exp( mu_x_2_n + 0.5 * sigma_x_2_n^2 );
-   sigma_x_2 = sqrt( ( exp( sigma_x_2_n^2 ) - 1.0 ) ...
-                     * exp( 2.0 * mu_x_2_n + sigma_x_2_n^2 ) );
-   x_2_test = min( mu_x_2 + num_std_devs * sigma_x_2, max_x );
-   mu_y_2 = exp( mu_y_2_n + 0.5 * sigma_y_2_n^2 );
-   sigma_y_2 = sqrt( ( exp( sigma_y_2_n^2 ) - 1.0 ) ...
-                     * exp( 2.0 * mu_y_2_n + sigma_y_2_n^2 ) );
-   y_2_test = min( mu_y_2 + num_std_devs * sigma_y_2, max_y );
-   P_low_2 ...
-   = mixt_frac * precip_frac_1 ...
-     * PDF_comp_bivar_LL( x_2_test, y_2_test, mu_x_1_n, mu_y_1_n, ...
-                          sigma_x_1_n, sigma_y_1_n, corr_x_y_1_n ) ...
-     + ( 1.0 - mixt_frac ) * precip_frac_2 ...
-       * PDF_comp_bivar_LL( x_2_test, y_2_test, mu_x_2_n, mu_y_2_n, ...
-                            sigma_x_2_n, sigma_y_2_n, corr_x_y_2_n );
-else
-   P_low_2 = -1.0;
-end
-% The smallest contour is the smaller of P_low_1 and P_low_2.
-if ( P_low_1 > 0.0 && P_low_2 > 0.0 )
-   contour_low = min( P_low_1, P_low_2 );
-elseif ( P_low_1 > 0.0 ) % P_low_2 <= 0.0
-   contour_low = P_low_1;
-elseif ( P_low_2 > 0.0 ) % P_low_1 <= 0.0
-   contour_low = P_low_2;
-else % P_low_1 <= 0.0 && P_low_2 <= 0.0
-   contour_low = 0.0;
-end
-% In a scenario where contour_low is so far out that it doesn't plot well,
-% use an alternative.  Sort all the values of P_xy and plot the lowest
-% contour at the 10th percentile of P_xy.
-P_xy_vals = zeros( num_x_pts*num_y_pts, 1 );
-idx = 0;
-for i = 1:1:num_x_pts
-   for j = 1:1:num_y_pts
-      idx = idx + 1;
-      P_xy_vals(idx) = P_xy(j,i);
+for clubb_idx = 1:1:num_clubb_files
+   % For the 1st PDF component, sample P_xy at sqrt(2)*num_std_devs, which
+   % is num_std_devs away from the 1st PDF component bivariate mean in both
+   % x and y.
+   if ( sigma_x_1_n(clubb_idx) > 0.0 && sigma_y_1_n(clubb_idx) > 0.0 )
+      mu_x_1 = exp( mu_x_1_n(clubb_idx) + 0.5 * sigma_x_1_n(clubb_idx)^2 );
+      sigma_x_1 = sqrt( ( exp( sigma_x_1_n(clubb_idx)^2 ) - 1.0 ) ...
+                        * exp( 2.0 * mu_x_1_n(clubb_idx) ...
+                               + sigma_x_1_n(clubb_idx)^2 ) );
+      x_1_test = min( mu_x_1 + num_std_devs * sigma_x_1, max_x );
+      mu_y_1 = exp( mu_y_1_n(clubb_idx) + 0.5 * sigma_y_1_n(clubb_idx)^2 );
+      sigma_y_1 = sqrt( ( exp( sigma_y_1_n(clubb_idx)^2 ) - 1.0 ) ...
+                        * exp( 2.0 * mu_y_1_n(clubb_idx) ...
+                               + sigma_y_1_n(clubb_idx)^2 ) );
+      y_1_test = min( mu_y_1 + num_std_devs * sigma_y_1, max_y );
+      P_low_1 ...
+      = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+        * PDF_comp_bivar_LL( x_1_test, y_1_test, ...
+                             mu_x_1_n(clubb_idx), ...
+                             mu_y_1_n(clubb_idx), ...
+                             sigma_x_1_n(clubb_idx), ...
+                             sigma_y_1_n(clubb_idx), ...
+                             corr_x_y_1_n(clubb_idx) ) ...
+        + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx) ...
+          * PDF_comp_bivar_LL( x_1_test, y_1_test, ...
+                               mu_x_2_n(clubb_idx), ...
+                               mu_y_2_n(clubb_idx), ...
+                               sigma_x_2_n(clubb_idx), ...
+                               sigma_y_2_n(clubb_idx), ...
+                               corr_x_y_2_n(clubb_idx) );
+   else
+      P_low_1 = -1.0;
    end
-end
-P_xy_sort = sort( P_xy_vals );
-P_xy_low = P_xy_sort( floor( 0.10*num_x_pts*num_y_pts ) );
-if ( contour_low < P_xy_low )
-   contour_low = P_xy_low;
-end
-% Find the maximum contour to plot for CLUBB results.
-P_xy_high = max( max( P_xy ) );
-contour_high = P_xy_high;
-% Set the contour vector.
-delta_contour = ( contour_high - contour_low ) / ( num_contours - 1 );
-for ci = 1:1:num_contours
-   contours(ci) = contour_low + delta_contour * ( ci - 1 );
-end
+   % For the 2nd PDF component, sample P_xy at sqrt(2)*num_std_devs, which
+   % is num_std_devs away from the 2nd PDF component bivariate mean in both
+   % x and y.
+   if ( sigma_x_2_n(clubb_idx) > 0.0 && sigma_y_2_n(clubb_idx) > 0.0 )
+      mu_x_2 = exp( mu_x_2_n(clubb_idx) + 0.5 * sigma_x_2_n(clubb_idx)^2 );
+      sigma_x_2 = sqrt( ( exp( sigma_x_2_n(clubb_idx)^2 ) - 1.0 ) ...
+                        * exp( 2.0 * mu_x_2_n(clubb_idx) ...
+                               + sigma_x_2_n(clubb_idx)^2 ) );
+      x_2_test = min( mu_x_2 + num_std_devs * sigma_x_2, max_x );
+      mu_y_2 = exp( mu_y_2_n(clubb_idx) + 0.5 * sigma_y_2_n(clubb_idx)^2 );
+      sigma_y_2 = sqrt( ( exp( sigma_y_2_n(clubb_idx)^2 ) - 1.0 ) ...
+                        * exp( 2.0 * mu_y_2_n(clubb_idx) ...
+                               + sigma_y_2_n(clubb_idx)^2 ) );
+      y_2_test = min( mu_y_2 + num_std_devs * sigma_y_2, max_y );
+      P_low_2 ...
+      = mixt_frac(clubb_idx) * precip_frac_1(clubb_idx) ...
+        * PDF_comp_bivar_LL( x_2_test, y_2_test, ...
+                             mu_x_1_n(clubb_idx), ...
+                             mu_y_1_n(clubb_idx), ...
+                             sigma_x_1_n(clubb_idx), ...
+                             sigma_y_1_n(clubb_idx), ...
+                             corr_x_y_1_n(clubb_idx) ) ...
+        + ( 1.0 - mixt_frac(clubb_idx) ) * precip_frac_2(clubb_idx) ...
+          * PDF_comp_bivar_LL( x_2_test, y_2_test, ...
+                               mu_x_2_n(clubb_idx), ...
+                               mu_y_2_n(clubb_idx), ...
+                               sigma_x_2_n(clubb_idx), ...
+                               sigma_y_2_n(clubb_idx), ...
+                               corr_x_y_2_n(clubb_idx) );
+   else
+      P_low_2 = -1.0;
+   end
+   % The smallest contour is the smaller of P_low_1 and P_low_2.
+   if ( P_low_1 > 0.0 && P_low_2 > 0.0 )
+      contour_low = min( P_low_1, P_low_2 );
+   elseif ( P_low_1 > 0.0 ) % P_low_2 <= 0.0
+      contour_low = P_low_1;
+   elseif ( P_low_2 > 0.0 ) % P_low_1 <= 0.0
+      contour_low = P_low_2;
+   else % P_low_1 <= 0.0 && P_low_2 <= 0.0
+      contour_low = 0.0;
+   end
+   % In a scenario where contour_low is so far out that it doesn't plot
+   % well, use an alternative.  Sort all the values of P_xy and plot the
+   % lowest contour at the 10th percentile of P_xy.
+   P_xy_vals = zeros( num_x_pts*num_y_pts, 1 );
+   idx = 0;
+   for i = 1:1:num_x_pts
+      for j = 1:1:num_y_pts
+         idx = idx + 1;
+         P_xy_vals(idx) = P_xy(clubb_idx,j,i);
+      end
+   end
+   P_xy_sort = sort( P_xy_vals );
+   P_xy_low = P_xy_sort( floor( 0.10*num_x_pts*num_y_pts ) );
+   if ( contour_low < P_xy_low )
+      contour_low = P_xy_low;
+   end
+   % Find the maximum contour to plot for CLUBB results.
+   P_xy_high = max( max( max( P_xy ) ) );
+   contour_high = P_xy_high;
+   % Set the contour vector.
+   delta_contour = ( contour_high - contour_low ) / ( num_contours - 1 );
+   for ci = 1:1:num_contours
+      contours(clubb_idx,ci) = contour_low + delta_contour * ( ci - 1 );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 % Scatterplot of LES results for x and y.
 scatter( var_x_LES, var_y_LES, ...
          'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r' )
-hold on
 % Contour plot of the PDF of x and y for CLUBB, where both x and y > 0.
-contour( x, y, P_xy, contours, 'Linewidth', 1.5 )
-legend( 'LES', 'CLUBB', 'Location', 'NorthEast' )
-hold on
+for clubb_idx = 1:1:num_clubb_files
+   hold on
+   contour( x, y, reshape( P_xy(clubb_idx,:,:), num_y_pts, num_x_pts), ...
+            contours(clubb_idx,:), 'Linewidth', 1.5 )
+end % clubb_idx = 1:1:num_clubb_files
 % Plot of the PDF of x and y for CLUBB, where x = 0.
-if ( precip_frac_clubb < 1.0 )
-   plot( [ 0 0 ], [ min_y max_y ], '-b', 'LineWidth', 1.5 );
-end
-hold on
+for clubb_idx = 1:1:num_clubb_files
+   hold on
+   if ( precip_frac_clubb(clubb_idx) < 1.0 )
+      plot( [ 0 0 ], [ min_y max_y ], '-b', 'LineWidth', 1.5 );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 % Plot of the PDF of x and y for CLUBB, where y = 0.
-if ( precip_frac_clubb < 1.0 )
-   plot( [ min_x max_x ], [ 0 0 ], '-b', 'LineWidth', 1.5 );
-end
+for clubb_idx = 1:1:num_clubb_files
+   hold on
+   if ( precip_frac_clubb(clubb_idx) < 1.0 )
+      plot( [ min_x max_x ], [ 0 0 ], '-b', 'LineWidth', 1.5 );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 hold off
+legend( 'LES', 'CLUBB', 'Location', 'NorthEast' )
 % Set the range of the plot on both the x-axis and y-axis.
-if ( precip_frac_clubb < 1.0 || precip_frac_x_LES < 1.0 )
+if ( any( precip_frac_clubb ) < 1.0 || precip_frac_x_LES < 1.0 )
    xlim([0 max_x])
 else
    xlim([min_x max_x])
 end
-if ( precip_frac_clubb < 1.0 || precip_frac_y_LES < 1.0 )
+if ( any( precip_frac_clubb ) < 1.0 || precip_frac_y_LES < 1.0 )
    ylim([0 max_y]);
 else
    ylim([min_y max_y])
--- postprocessing/matlab_scatter_contour_plots/plot_CLUBB_PDF_LES_pts_N.m
+++ postprocessing/matlab_scatter_contour_plots/plot_CLUBB_PDF_LES_pts_N.m
@@ -2,6 +2,7 @@
 function plot_CLUBB_PDF_LES_pts_N( var_x_LES, ...
                                    nx_LES_grid, ny_LES_grid, ...
                                    num_x_pts, log_Px_plot, ...
+                                   num_clubb_files, ...
                                    mu_x_1, mu_x_2, ...
                                    sigma_x_1, sigma_x_2, ...
                                    mixt_frac, var_x_label, ...
@@ -28,7 +29,7 @@ num_x_divs = num_x_pts + 1;
 delta_x = ( max_x - min_x ) / num_x_pts;
 x_divs = zeros( num_x_divs, 1 );
 x = zeros( num_x_pts, 1 );
-P_x = zeros( num_x_pts, 1 );
+P_x = zeros( num_clubb_files, num_x_pts );
 % Calculate the x "division points" (edges of the bins).
 for i = 1:1:num_x_divs
    x_divs(i) = min_x + delta_x * ( i - 1 );
@@ -38,11 +39,17 @@ for i = 1:1:num_x_pts
    x(i) = 0.5 * ( x_divs(i) + x_divs(i+1) );
 end
 % CLUBB's PDF marginal for x.
-for i = 1:1:num_x_pts
-   P_x(i) ...
-   = mixt_frac * PDF_comp_Normal( x(i), mu_x_1, sigma_x_1 ) ...
-     + ( 1.0 - mixt_frac ) * PDF_comp_Normal( x(i), mu_x_2, sigma_x_2 );
-end
+for clubb_idx = 1:1:num_clubb_files
+   for i = 1:1:num_x_pts
+      P_x(clubb_idx,i) ...
+      = mixt_frac(clubb_idx) ...
+        * PDF_comp_Normal( x(i), mu_x_1(clubb_idx), ...
+                           sigma_x_1(clubb_idx) ) ...
+        + ( 1.0 - mixt_frac(clubb_idx) ) ...
+          * PDF_comp_Normal( x(i), mu_x_2(clubb_idx), ...
+                             sigma_x_2(clubb_idx) );
+   end
+end % clubb_idx = 1:1:num_clubb_files
 % Centerpoints and counts for each bin (from LES results) for x.
 binranges_x = x;
 [bincounts_x] = histc( var_x_LES, binranges_x );
@@ -57,13 +64,15 @@ else
         bincounts_x / ( nx_LES_grid * ny_LES_grid * delta_x ), ...
         1.0, 'r', 'EdgeColor', 'r' );
 end
-hold on
 % Plot PDF of x for CLUBB.
-if ( log_Px_plot )
-   semilogy( x, P_x, '-b', 'LineWidth', 2 )
-else
-   plot( x, P_x, '-b', 'LineWidth', 2 )
-end
+for clubb_idx = 1:1:num_clubb_files
+   hold on
+   if ( log_Px_plot )
+      semilogy( x, P_x(clubb_idx,:), '-b', 'LineWidth', 2 )
+   else
+      plot( x, P_x(clubb_idx,:), '-b', 'LineWidth', 2 )
+   end
+end % clubb_idx = 1:1:num_clubb_files
 hold off
 % Set the range of the plot on both the x-axis and y-axis.
 xlim( [ min_x max_x ] )
@@ -78,11 +87,11 @@ if ( log_Px_plot )
                    / ( nx_LES_grid * ny_LES_grid * delta_x ) ...
            max( max(bincounts_x) ...
                 / ( nx_LES_grid * ny_LES_grid * delta_x ), ...
-                max(P_x) ) ] );
+                max(max(P_x)) ) ] );
 else
    ylim( [ 0 max( max(bincounts_x) ...
                   / ( nx_LES_grid * ny_LES_grid * delta_x ), ...
-                  max(P_x) ) ] );
+                  max(max(P_x)) ) ] );
 end
 xlabel( var_x_label )
 ylabel( [ 'P( ', field_plotted, ' )' ] )
