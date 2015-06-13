1f5ad7d0124bf1351a3093dcd1a341d36b5f37fa
bmg2@uwm.edu <bmg2@uwm.edu>
2015-06-13 23:50:10 +0000

    I updated the scatter/contour plotter again.  This time, I took the code that finds the CLUBB statistical output timestep at or closest to the time of the SAM 3D statistical output time and placed it in its own function and file.  Again, this allows it to be referenced by other MATLAB programs.
    The .png output file results from this revision are an exact match to the .png
    output file results from the previous revision for all .png output files in my
    test case.
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7757 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 118ce51a2ed8a48329c508d446a75f30c84c7468

--- postprocessing/matlab_scatter_contour_plots/PDF_scatter_contour_plotter.m
+++ postprocessing/matlab_scatter_contour_plots/PDF_scatter_contour_plotter.m
@@ -288,31 +288,7 @@ end % clubb_idx = 1:1:num_clubb_files
 
 % Find the time in the CLUBB zt output file that is equal (or closest) to
 % the SAM LES output time.
-time_sam_sec = time_sam * 86400.0;
-time_diff_clubb_sam_sec = abs( time_clubb - time_sam_sec );
-% Initialize the minimum time difference and its (CLUBB) index.
-idx_min_time_diff = 1;
-min_time_diff = time_diff_clubb_sam_sec(idx_min_time_diff);
-% Find the index of the minimum time difference between CLUBB output time
-% and the requested SAM 3D file output time.
-for iter = 2:1:num_t_clubb
-   if ( time_diff_clubb_sam_sec(iter) < min_time_diff )
-      min_time_diff = time_diff_clubb_sam_sec(iter);
-      idx_min_time_diff = iter;
-   end
-end
-% The CLUBB output index is the index that corresponds with the minimum
-% time difference between the CLUBB output time and the SAM LES 3D file
-% output time.
-clubb_time_idx = idx_min_time_diff;
-
-% Print the time of the SAM LES output.
-fprintf( 'Time of SAM LES output (seconds): %g\n', time_sam_sec );
-
-% Print the CLUBB output time index and the associated time.
-fprintf( [ 'Time index of CLUBB output: %g;', ...
-           ' Time of CLUBB output (seconds): %g\n' ], ...
-           clubb_time_idx, time_clubb(clubb_time_idx) );
+clubb_time_idx = get_clubb_time_index( time_sam, time_clubb, num_t_clubb );
 
 % Use SAM 3D data from the SAM level that is at the same altitude as the
 % requested CLUBB level (z_clubb(clubb_height_idx)).  If SAM does not have
