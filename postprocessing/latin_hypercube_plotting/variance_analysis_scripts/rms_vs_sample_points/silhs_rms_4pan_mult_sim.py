53259a2bdd0140acddb1a5879d8e96f2483ed3a9
raut@uwm.edu <raut@uwm.edu>
2015-07-09 00:07:46 +0000

    For clubb:ticket:740, I started work on an RMS plotter that will create panels. I am not done.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7805 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ddde963dd41162f3963b1cd7b6b083b3ad87b2aa

---
+++ postprocessing/latin_hypercube_plotting/variance_analysis_scripts/rms_vs_sample_points/silhs_rms_4pan_mult_sim.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import netCDF4
+import numpy as np
+import pylab as pl
+import sys
+import os
+
+#######################################################################
+# TODO: NOT FINISHED!!
+#
+# This script will take several directories output from the scipt
+# silhs_varying_sp_output.sh, given as command line parameters to
+# the script, and compute the average RMSE for each number of sample
+# points for each of the directories, and plot this information.
+#
+# This script is to be used only with non-interactive SILHS runs.
+#
+# Usage:
+#  ./silhs_rms_plot_mult_sim.py [options] path1 [path2 [...]]
+#######################################################################
+
+case_name = 'rico_lh'
+time1 = 0
+time2 = 4320
+
+# Average over all height levels. If false, RMSE will be computed only at
+# k_lh_start
+l_all_height_avg = False
+
+clubb_var_strs  = [ 'rrm_auto',    'rrm_accr',    'rrm_cond',    'rrm_mc_nonadj' ]
+silhs_var_strs  = [ 'lh_rrm_auto', 'lh_rrm_accr', 'lh_rrm_evap', 'lh_rrm_mc_nonadj' ]
+
+plot_title = ''
+
+output_file = ''
+
+#-------------------------------------------------------------------------
+
+silhs_dirs = []
+
+# Read command line arguments
+i = 1
+while i < len(sys.argv):
+    if sys.argv[i] == '--plot_title':
+        i = i + 1
+        plot_title = sys.argv[i]
+    elif sys.argv[i] == '--time1':
+        i = i + 1
+        time1 = int(sys.argv[i])
+    elif sys.argv[i] == '--time2':
+        i = i + 1
+        time2 = int(sys.argv[i])
+    elif sys.argv[i] == '--case_name':
+        i = i + 1
+        case_name = sys.argv[i]
+    elif sys.argv[i] == '--output_file':
+        i = i + 1
+        output_file = sys.argv[i]
+    else:
+        silhs_dirs.append(sys.argv[i])
+
+    i = i + 1
+
+if len(silhs_dirs) == 0:
+    print("Usage: ./silhs_rms_plot_mult_sim.py [options] "
+          "path1 [path2 [...]]", file=sys.stderr)
+    sys.exit(1)
+
+if plot_title == '':
+    plot_title = clubb_var_str
+
+sim_points_all = list()
+for entry in os.listdir(silhs_dirs[0]):
+    if (entry[:6] == 'silhs_'):
+        sim_points_all.append(int(entry[6:]))
+sim_points_all.sort()
+
+clubb_var = netCDF4.Dataset(silhs_dirs[0]+'/silhs_'+str(sim_points_all[0])+ \
+    '/'+case_name+'_zt.nc').variables[clubb_var_str]
+
+rms_all = list()
+for i in range(len(silhs_dirs)):
+    rms_all.append(np.empty(len(sim_points_all)))
+
+n_timesteps = time2-time1
+
+if not l_all_height_avg:
+    k_lh_start = netCDF4.Dataset(silhs_dirs[0]+'/silhs_'+str(sim_points_all[0])+ \
+      '/'+case_name+'_lh_sfc.nc').variables['k_lh_start']
+    k_lh_start = k_lh_start[:,0,0,0]
+else:
+    n_heights = clubb_var.shape[1]
+
+clubb_var = clubb_var[:,:,0,0]
+
+for n_i in range(0,len(sim_points_all)):
+    num_samples = sim_points_all[n_i]
+    print("Trying " + str(num_samples) + " sample points.")
+    for d_i in range(0,len(silhs_dirs)):
+        silhs_dir = silhs_dirs[d_i]
+        rms_val = 0.0
+        silhs_var = netCDF4.Dataset(silhs_dir+'/silhs_'+str(num_samples)+ \
+            '/'+case_name+'_lh_zt.nc').variables[silhs_var_str]
+        # Copy to memory for better performance
+        silhs_var = silhs_var[:,:,0,0]
+
+        for t in range(time1,time2):
+            if l_all_height_avg:
+                # Exclude the model lower level
+                for k in range(1,n_heights):
+                    rms_val = rms_val + (clubb_var[t,k]-silhs_var[t,k])**2
+            else:
+                k = int(round(k_lh_start[t]))-1
+                rms_val = rms_val + (clubb_var[t,k]-silhs_var[t,k])**2
+
+        rms_val = rms_val / n_timesteps
+        if l_all_height_avg:
+            rms_val = rms_val / n_heights
+        rms_val = np.sqrt(rms_val)
+        rms_all[d_i][n_i] = rms_val
+
+for d_i in range(len(silhs_dirs)):
+    pl.plot(sim_points_all, rms_all[d_i], label=os.path.basename(silhs_dirs[d_i]))
+
+pl.xlabel('Number of Sample Points')
+pl.ylabel('RMSE of SILHS Estimate')
+pl.xscale('log')
+pl.yscale('log')
+pl.title(plot_title)
+# This will change the axes so that the plot 'tight'ly hugs the data
+#pl.axis('tight')
+
+pl.legend()
+
+if output_file == '':
+    pl.show()
+else:
+    # Output to disk
+    pl.savefig(output_file)
