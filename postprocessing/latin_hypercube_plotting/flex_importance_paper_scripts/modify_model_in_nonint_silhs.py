2cbac6e1d2ba59db47e12d7c58cac32dc07a5ad4
raut@uwm.edu <raut@uwm.edu>
2015-07-18 00:43:44 +0000

    For clubb:ticket:740, I am committing the changes that make our RICO-LH simulation more like RICO.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7845 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: ffc4d019fc8dd5ad431abec548e94ea15d531c20

--- postprocessing/latin_hypercube_plotting/flex_importance_paper_scripts/modify_model_in_nonint_silhs.py
+++ postprocessing/latin_hypercube_plotting/flex_importance_paper_scripts/modify_model_in_nonint_silhs.py
@@ -13,11 +13,9 @@ with open(model_in_file, 'r') as f_in:
             if state == 0 and line.rstrip() == '&microphysics_setting':
                 state = 1
             elif state == 1 and line.rstrip() == '/':
-                f_out.write('l_var_covar_src = .false.\n\n')
                 f_out.write('! Addition for using SILHS\n')
                 f_out.write('lh_microphys_type = "non-interactive",\n')
                 f_out.write('l_local_kk = .false.,\n')
-                f_out.write('l_fix_chi_eta_correlations = .true.,\n')
                 f_out.write('l_lh_importance_sampling = .true.,\n')
                 f_out.write('l_lh_clustered_sampling = .true.,\n')
                 f_out.write('l_rcm_in_cloud_k_lh_start = .true.,\n')
@@ -27,7 +25,5 @@ with open(model_in_file, 'r') as f_in:
                 f_out.write('lh_num_samples = 8,\n')
                 f_out.write('lh_seed = 1,\n')
                 f_out.write('lh_sequence_length = 1\n\n')
-                if os.path.basename(model_in_file) == 'rico_lh_model.in':
-                    f_out.write('l_silhs_KK_convergence_adj_mean = .true.\n')
                 state = 2
             f_out.write(line)
