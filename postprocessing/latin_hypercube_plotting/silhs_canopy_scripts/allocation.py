f333df7ed158828c99d7d4c88d60884a60734dad
raut@uwm.edu <raut@uwm.edu>
2015-08-12 21:14:34 +0000

    I added a script to plot or print sample allocation in SILHS.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7883 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 5592c483aad5093380ec406cb46f599ea0065fdc

---
+++ postprocessing/latin_hypercube_plotting/silhs_canopy_scripts/allocation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import netCDF4
+import sys
+import numpy
+import matplotlib
+import matplotlib.pyplot as plt
+
+silhs_dir = sys.argv[1]
+
+l_plot = True
+
+k_lh_start = netCDF4.Dataset(silhs_dir+'/silhs_256_1/rico_lh_lh_sfc.nc') \
+             .variables['k_lh_start'][:,0,0,0]
+n_timesteps = len(k_lh_start)
+
+samp_fracs = numpy.empty((n_timesteps,8))
+for i in range(8):
+    samp_frac_var = netCDF4.Dataset(silhs_dir+'/silhs_256_1/rico_lh_lh_zt.nc') \
+                    .variables['lh_samp_frac_'+str(i+1)][:,:,0,0]
+    for t in range(0,n_timesteps):
+        samp_fracs[t,i] = samp_frac_var[t,k_lh_start[t]]
+
+if not l_plot:
+    samp_fracs_time_avg = numpy.average(samp_fracs, axis=0)
+    for i in range(8):
+        print(str(i+1) + ": " + str(samp_fracs_time_avg[i]))
+else:
+    labels = ['c_p_1', 'c_p_2', 'nc_p_1', 'nc_p_2', 'c_np_1', 'c_np_2', 'nc_np_1', 'nc_np_2']
+    colors = ['red', 'cyan', 'magenta', 'yellow', 'black', 'orange', 'brown', 'pink']
+    for i in range(8):
+        plt.plot(range(n_timesteps), samp_fracs[:,i], colors[i], label=labels[i])
+    plt.legend()
+    plt.show()
