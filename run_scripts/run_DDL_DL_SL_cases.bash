9cd229a0cf0a4f70fc048701188b8923caeae231
bmg2@uwm.edu <bmg2@uwm.edu>
2015-09-25 21:37:50 +0000

    I added code to run the matlab scatter/contour plots to produce marginal PDF plots for RICO (rr and Nr), DYCOMS-II RF02 DS (rr and Nr), and LBA (rr, Nr, rs, Ns, rg, Ng, ri, and Ni).  I am able to run the script (with the -p option enabled) and produce .png output files that are an exact match to the .png output files I included in the presentation for LLNL.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/branches/Brian_special_input_fields_r7465@7906 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 62a23160154d18a94d0c2b7e900c731cf9920575

--- run_scripts/run_DDL_DL_SL_cases.bash
+++ run_scripts/run_DDL_DL_SL_cases.bash
@@ -136,6 +136,181 @@ rm ../src/CLUBB_core/model_flags.F90.prev
 # Options
 
 # Produce plots of the PDFs.
+if [ $PDF_PLOTS == true ];
+then
+
+   # Change directory to the matlab_scatter_contour_plots directory.
+   cd ../postprocessing/matlab_scatter_contour_plots
+
+   # Copy PDF_scatter_contour_plotter.m back one directory.
+   cp PDF_scatter_contour_plotter.m ../PDF_scatter_contour_plotter.m
+
+   # Setup the PDF plots for the RICO case (rr and Nr).
+
+   # Copy PDF_scatter_contour_plotter.m to PDF_scatter_contour_plotter.m.prev.
+   cp PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m.prev
+
+   # Edit PDF_scatter_contour_plotter.m.
+   sed "s/clubb_height_idx =/%clubb_height_idx =/g; \
+        s/print_note =/%print_note =/g; s/%%/%/g; \
+        s/%clubb_height_idx = 11/clubb_height_idx = 11/g; \
+        s/%print_note = ''/print_note = ''/g; \
+        s/plot_rt_thl  = true/plot_rt_thl  = false/g; \
+        s/plot_chi_eta = true/plot_chi_eta = false/g; \
+        s/plot_w_rr    = true/plot_w_rr    = false/g; \
+        s/plot_w_Nr    = true/plot_w_Nr    = false/g; \
+        s/plot_chi_rr  = true/plot_chi_rr  = false/g; \
+        s/plot_chi_Nr  = true/plot_chi_Nr  = false/g; \
+        s/plot_eta_rr  = true/plot_eta_rr  = false/g; \
+        s/plot_eta_Nr  = true/plot_eta_Nr  = false/g; \
+        s/plot_rr_Nr   = true/plot_rr_Nr   = false/g; \
+        s/plot_w       = true/plot_w       = false/g; \
+        s/plot_rt      = true/plot_rt      = false/g; \
+        s/plot_thl     = true/plot_thl     = false/g; \
+        s/plot_chi     = true/plot_chi     = false/g; \
+        s/plot_eta     = true/plot_eta     = false/g; \
+        s/plot_ri      = true/plot_ri      = false/g; \
+        s/plot_Ni      = true/plot_Ni      = false/g; \
+        s/plot_ln_ri   = true/plot_ln_ri   = false/g; \
+        s/plot_ln_Ni   = true/plot_ln_Ni   = false/g; \
+        s/plot_rs      = true/plot_rs      = false/g; \
+        s/plot_Ns      = true/plot_Ns      = false/g; \
+        s/plot_ln_rs   = true/plot_ln_rs   = false/g; \
+        s/plot_ln_Ns   = true/plot_ln_Ns   = false/g; \
+        s/plot_rg      = true/plot_rg      = false/g; \
+        s/plot_Ng      = true/plot_Ng      = false/g; \
+        s/plot_ln_rg   = true/plot_ln_rg   = false/g; \
+        s/plot_ln_Ng   = true/plot_ln_Ng   = false/g; \
+        s/auto_legend_text = true/auto_legend_text = false/g; \
+        s/1:9/1:3/g; s/1:8/1:2/g; s/CLUBB DDL/DDL/g; \
+        s/CLUBB DL/DL/g; s/CLUBB SL/SL/g;" \
+        PDF_scatter_contour_plotter.m.prev > PDF_scatter_contour_plotter.m
+
+   # Remove PDF_scatter_contour_plotter.m.prev.
+   rm PDF_scatter_contour_plotter.m.prev
+
+   # Run the plotter for the RICO case.
+   ./run_scatter_contour_plots.bash \
+      ../../sam_benchmark_runs/RICO_256x256x100_drizzle/3D_output/RICO_256x256x100_drizzle_128_0000252000_micro.nc \
+      ../../output/$date_string/DDL/rico_zt.nc \
+      ../../output/$date_string/DL/rico_zt.nc \
+      ../../output/$date_string/SL/rico_zt.nc
+
+   # Setup the PDF plots for the DYCOMS-II RF02 DS case (rr and Nr).
+
+   # Copy PDF_scatter_contour_plotter.m to PDF_scatter_contour_plotter.m.prev.
+   cp PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m.prev
+
+   # Edit PDF_scatter_contour_plotter.m.
+   sed "s/clubb_height_idx =/%clubb_height_idx =/g;
+        s/casename =/%casename =/g; s/%%/%/g; \
+        s/%clubb_height_idx = 28/clubb_height_idx = 28/g; \
+        s/%casename = 'RF02'/casename = 'RF02'/g;" \
+        PDF_scatter_contour_plotter.m.prev > PDF_scatter_contour_plotter.m
+
+   # Remove PDF_scatter_contour_plotter.m.prev.
+   rm PDF_scatter_contour_plotter.m.prev
+
+   # Run the plotter for the DYCOMS-II RF02 DS case.
+   ./run_scatter_contour_plots.bash \
+      ../../sam_benchmark_runs/DYCOMS_RF02_128x128x96_dr_sed/3D_output/DYCOMS_RF02_128x128x96_dr_sed_128_0000039600_micro.nc \
+      ../../output/$date_string/DDL/dycoms2_rf02_ds_zt.nc \
+      ../../output/$date_string/DL/dycoms2_rf02_ds_zt.nc \
+      ../../output/$date_string/SL/dycoms2_rf02_ds_zt.nc
+
+   # Setup the PDF plots for the LBA case (rr and Nr).
+
+   # Copy PDF_scatter_contour_plotter.m to PDF_scatter_contour_plotter.m.prev.
+   cp PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m.prev
+
+   # Edit PDF_scatter_contour_plotter.m.
+   sed "s/clubb_height_idx =/%clubb_height_idx =/g;
+        s/casename =/%casename =/g; s/%%/%/g; \
+        s/%clubb_height_idx = 25/clubb_height_idx = 25/g; \
+        s/%casename = 'LBA'/casename = 'LBA'/g;" \
+        PDF_scatter_contour_plotter.m.prev > PDF_scatter_contour_plotter.m
+
+   # Remove PDF_scatter_contour_plotter.m.prev.
+   rm PDF_scatter_contour_plotter.m.prev
+
+   # Run the plotter for the LBA case.
+   ./run_scatter_contour_plots.bash \
+      ../../sam_benchmark_runs/LBA_r1663_128x128x128_1km_Morrison/3D_output/LBA_128kmx128kmx128_1km_Morrison_64_0000003300_micro.nc \
+      ../../output/$date_string/DDL/lba_zt.nc \
+      ../../output/$date_string/DL/lba_zt.nc \
+      ../../output/$date_string/SL/lba_zt.nc
+
+   # Setup the PDF plots for the LBA case (rs, Ns, rg, and Ng).
+
+   # Copy PDF_scatter_contour_plotter.m to PDF_scatter_contour_plotter.m.prev.
+   cp PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m.prev
+
+   # Edit PDF_scatter_contour_plotter.m.
+   sed "s/clubb_height_idx =/%clubb_height_idx =/g; s/%%/%/g; \
+        s/%clubb_height_idx = 44/clubb_height_idx = 44/g; \
+        s/plot_rr      = true/plot_rr      = false/g; \
+        s/plot_Nr      = true/plot_Nr      = false/g; \
+        s/plot_ln_rr   = true/plot_ln_rr   = false/g; \
+        s/plot_ln_Nr   = true/plot_ln_Nr   = false/g; \
+        s/plot_rs      = false/plot_rs      = true/g; \
+        s/plot_Ns      = false/plot_Ns      = true/g; \
+        s/plot_ln_rs   = false/plot_ln_rs   = true/g; \
+        s/plot_ln_Ns   = false/plot_ln_Ns   = true/g; \
+        s/plot_rg      = false/plot_rg      = true/g; \
+        s/plot_Ng      = false/plot_Ng      = true/g; \
+        s/plot_ln_rg   = false/plot_ln_rg   = true/g; \
+        s/plot_ln_Ng   = false/plot_ln_Ng   = true/g;" \
+        PDF_scatter_contour_plotter.m.prev > PDF_scatter_contour_plotter.m
+
+   # Remove PDF_scatter_contour_plotter.m.prev.
+   rm PDF_scatter_contour_plotter.m.prev
+
+   # Run the plotter for the LBA case.
+   ./run_scatter_contour_plots.bash \
+      ../../sam_benchmark_runs/LBA_r1663_128x128x128_1km_Morrison/3D_output/LBA_128kmx128kmx128_1km_Morrison_64_0000003600_micro.nc \
+      ../../output/$date_string/DDL/lba_zt.nc \
+      ../../output/$date_string/DL/lba_zt.nc \
+      ../../output/$date_string/SL/lba_zt.nc
+
+   # Setup the PDF plots for the LBA case (ri and Ni).
+
+   # Copy PDF_scatter_contour_plotter.m to PDF_scatter_contour_plotter.m.prev.
+   cp PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m.prev
+
+   # Edit PDF_scatter_contour_plotter.m.
+   sed "s/clubb_height_idx =/%clubb_height_idx =/g; s/%%/%/g; \
+        s/%clubb_height_idx = 60/clubb_height_idx = 60/g; \
+        s/plot_rs      = true/plot_rs      = false/g; \
+        s/plot_Ns      = true/plot_Ns      = false/g; \
+        s/plot_ln_rs   = true/plot_ln_rs   = false/g; \
+        s/plot_ln_Ns   = true/plot_ln_Ns   = false/g; \
+        s/plot_rg      = true/plot_rg      = false/g; \
+        s/plot_Ng      = true/plot_Ng      = false/g; \
+        s/plot_ln_rg   = true/plot_ln_rg   = false/g; \
+        s/plot_ln_Ng   = true/plot_ln_Ng   = false/g; \
+        s/plot_ri      = false/plot_ri      = true/g; \
+        s/plot_Ni      = false/plot_Ni      = true/g; \
+        s/plot_ln_ri   = false/plot_ln_ri   = true/g; \
+        s/plot_ln_Ni   = false/plot_ln_Ni   = true/g;" \
+        PDF_scatter_contour_plotter.m.prev > PDF_scatter_contour_plotter.m
+
+   # Remove PDF_scatter_contour_plotter.m.prev.
+   rm PDF_scatter_contour_plotter.m.prev
+
+   # Run the plotter for the LBA case.
+   ./run_scatter_contour_plots.bash \
+      ../../sam_benchmark_runs/LBA_r1663_128x128x128_1km_Morrison/3D_output/LBA_128kmx128kmx128_1km_Morrison_64_0000003600_micro.nc \
+      ../../output/$date_string/DDL/lba_zt.nc \
+      ../../output/$date_string/DL/lba_zt.nc \
+      ../../output/$date_string/SL/lba_zt.nc
+
+   # Restore original PDF_scatter_contour_plotter.m.
+   mv ../PDF_scatter_contour_plotter.m PDF_scatter_contour_plotter.m
+
+   # Change directories back to the one where the run script is located.
+   cd ../../run_scripts
+
+fi # Produce plots of the PDFs.
 
 # Run the goodness-of-fit tests.
 
