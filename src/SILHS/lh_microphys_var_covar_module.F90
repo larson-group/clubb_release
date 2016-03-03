03f2842c61caa9e881911c2cfa3d48d375c7aa9a
raut@uwm.edu <raut@uwm.edu>
2016-03-03 00:04:01 +0000

    For clubb:ticket:753:
    I refactored the code in `lh_microphys_var_covar_module` to compute the SILHS variance and covariance tendencies by using the terms given in clubb:ticket:753#comment:20. This is the first step in making SILHS agree with the analytic solution.
    
    I overplotted the tendencies produced by this code with the tendencies produced by the prior code, and visually they look identical. Nevertheless, output was not bin-diff identical (likely due to round-off error).
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@7998 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 8b95edc889d65111b47e8e4314e7bfbde748f650

--- src/SILHS/lh_microphys_var_covar_module.F90
+++ src/SILHS/lh_microphys_var_covar_module.F90
@@ -38,6 +38,17 @@ module lh_microphys_var_covar_module
     use stats_type_utilities, only: &
       stat_update_var ! Procedure
 
+    use math_utilities, only: &
+      compute_sample_mean,        & ! Procedure(s)
+      compute_sample_variance,    &
+      compute_sample_covariance
+
+    use grid_class, only: &
+      zt2zm
+
+    use constants_clubb, only: &
+      two    ! Constant
+
     implicit none
 
     ! Input Variables!
@@ -68,41 +79,69 @@ module lh_microphys_var_covar_module
       lh_rtpthlp_mc                    ! SILHS microphys. est. tendency of <rt'thl'> [K*(kg/kg)/s]
 
     ! Local Variables
-    real( kind = core_rknd ), dimension(nz) :: &
-      lh_rtp2_before_microphys,    & ! <rt'^2> before microphys_sub                  [(kg/kg)^2]
-      lh_rtp2_after_microphys,     & ! <rt'^2> after microphys_sub                   [(kg/kg)^2]
-      lh_thlp2_before_microphys,   & ! <thl'^2> before microphys_sub                 [K^2]
-      lh_thlp2_after_microphys,    & ! <thl'^2> after microphys_sub                  [K^2]
-      lh_wprtp_before_microphys,   & ! <w'rt'> before microphys_sub                  [m*(kg/kg)/s]
-      lh_wprtp_after_microphys,    & ! <w'rt'> after microphys_sub                   [m*(kg/kg)/s]
-      lh_wpthlp_before_microphys,  & ! <w'thl'> before microphys_sub                 [m*K/s]
-      lh_wpthlp_after_microphys,   & ! <w'thl'> after microphys_sub                  [m*K/s]
-      lh_rtpthlp_before_microphys, & ! <rt'thl'> before microphys_sub                [K*(kg/kg)/s]
-      lh_rtpthlp_after_microphys     ! <rt'thl'> after microphys_sub                 [K*(kg/kg)/s]
+    real( kind = core_rknd ), dimension(nz,num_samples) :: &
+      lh_rt_mc_all
 
+    real( kind = core_rknd ), dimension(nz) :: &
+      mean_rt,         &
+      mean_rt_mc,      &
+      covar_rt_rtmc,   &
+      mean_thl,        &
+      mean_thl_mc,     &
+      covar_thl_thlmc, &
+      mean_w,          &
+      covar_w_rtmc,    &
+      covar_w_thlmc,   &
+      covar_thl_rtmc,  &
+      covar_rt_thlmc
+
+    ! For timestep-dependent terms
+    real( kind = core_rknd ), dimension(nz) :: &
+      var_rt_mc, &
+      var_thl_mc, &
+      covar_rtmc_thlmc
 
   !-----------------------------------------------------------------------
 
     !----- Begin Code -----
-    call lh_moments ( num_samples, lh_sample_point_weights, nz, &              ! Intent (in)
-                      lh_rt_all, lh_thl_all, lh_w_all, &                       ! Intent (in)
-                      lh_rtp2_before_microphys, lh_thlp2_before_microphys, &   ! Intent (out)
-                      lh_wprtp_before_microphys, lh_wpthlp_before_microphys, & ! Intent (out)
-                      lh_rtpthlp_before_microphys )                            ! Intent (out)
+    lh_rt_mc_all = lh_rcm_mc_all + lh_rvm_mc_all
 
-    call lh_moments ( num_samples, lh_sample_point_weights, nz, &              ! Intent (in)
-                      lh_rt_all + dt * ( lh_rcm_mc_all + lh_rvm_mc_all ), &    ! Intent (in)
-                      lh_thl_all + dt * lh_thlm_mc_all, &                      ! Intent (in)
-                      lh_w_all, &                                              ! Intent (in)
-                      lh_rtp2_after_microphys, lh_thlp2_after_microphys, &     ! Intent (out)
-                      lh_wprtp_after_microphys, lh_wpthlp_after_microphys, &   ! Intent (out)
-                      lh_rtpthlp_after_microphys )                             ! Intent (out)
+    ! Calculate means, variances, and covariances needed for the tendency terms
+    mean_rt = compute_sample_mean( nz, num_samples, lh_sample_point_weights, lh_rt_all )
+    mean_rt_mc = compute_sample_mean( nz, num_samples, lh_sample_point_weights, lh_rt_mc_all )
+    covar_rt_rtmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                               lh_rt_all, mean_rt, lh_rt_mc_all, mean_rt_mc )
+    mean_thl = compute_sample_mean( nz, num_samples, lh_sample_point_weights, lh_thl_all )
+    mean_thl_mc = compute_sample_mean( nz, num_samples, lh_sample_point_weights, lh_thlm_mc_all )
+    covar_thl_thlmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                                 lh_thl_all, mean_thl, lh_thlm_mc_all, mean_thl_mc )
+    mean_w = compute_sample_mean( nz, num_samples, lh_sample_point_weights, lh_w_all )
+    covar_w_rtmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                              lh_w_all, mean_w, lh_rt_mc_all, mean_rt_mc )
+    covar_w_thlmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                              lh_w_all, mean_w, lh_thlm_mc_all, mean_thl_mc )
+    covar_thl_rtmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                                lh_thl_all, mean_thl, lh_rt_mc_all, mean_rt_mc )
+    covar_rt_thlmc = compute_sample_covariance( nz, num_samples, lh_sample_point_weights, &
+                                                lh_rt_all, mean_rt, lh_thlm_mc_all, mean_thl_mc )
 
-    lh_wpthlp_mc = ( lh_wpthlp_after_microphys - lh_wpthlp_before_microphys ) / dt
-    lh_wprtp_mc = ( lh_wprtp_after_microphys - lh_wprtp_before_microphys ) / dt
-    lh_rtp2_mc = ( lh_rtp2_after_microphys - lh_rtp2_before_microphys ) / dt
-    lh_thlp2_mc = ( lh_thlp2_after_microphys - lh_thlp2_before_microphys) / dt
-    lh_rtpthlp_mc = ( lh_rtpthlp_after_microphys - lh_rtpthlp_before_microphys) / dt
+    ! Variances and covariances for time-dependent terms
+    var_rt_mc  = compute_sample_variance( nz, num_samples, lh_rt_mc_all, lh_sample_point_weights, &
+                                          mean_rt_mc  )
+    var_thl_mc = compute_sample_variance &
+                                ( nz, num_samples, lh_thlm_mc_all, lh_sample_point_weights, &
+                                  mean_thl_mc )
+    covar_rtmc_thlmc = compute_sample_covariance &
+                              ( nz, num_samples, lh_sample_point_weights, &
+                                lh_rt_mc_all, mean_rt_mc, lh_thlm_mc_all, mean_thl_mc )
+
+    ! Compute the microphysical variance and covariance tendencies
+    lh_rtp2_mc    = zt2zm( two*covar_rt_rtmc   + dt*var_rt_mc  )
+    lh_thlp2_mc   = zt2zm( two*covar_thl_thlmc + dt*var_thl_mc )
+    lh_wprtp_mc   = zt2zm( covar_w_rtmc  )
+    lh_wpthlp_mc  = zt2zm( covar_w_thlmc )
+    lh_rtpthlp_mc = zt2zm( covar_thl_rtmc + covar_rt_thlmc + dt*covar_rtmc_thlmc )
+
 
     ! Stats sampling
     if ( l_stats_samp ) then
@@ -115,93 +154,5 @@ module lh_microphys_var_covar_module
 
     return
   end subroutine lh_microphys_var_covar_driver
-
   !-----------------------------------------------------------------------
-  subroutine lh_moments ( n_samples, lh_weights, nz, & 
-                 rt_all_samples, thl_all_samples, w_all_samples, &
-                 lh_rtp2, lh_thlp2, &
-                 lh_wprtp, lh_wpthlp, &
-                 lh_rtpthlp )
-
-  ! Description:
-  !   Calculates variances and covariances using LH sample columns
-  !
-  ! References:
-  !   None
-  !
-  ! TODO: This code assumes nz == gr%nnzp since it references zt2zm;  this is
-  ! not necessarily the case.
-  !-----------------------------------------------------------------------------
-
-    use grid_class, only: &
-      zt2zm    ! Procedures
-
-    use math_utilities, only: &
-      compute_sample_mean, & ! functions
-      compute_sample_variance, &
-      compute_sample_covariance
-
-    use clubb_precision, only: &
-      core_rknd ! Constants
-
-    implicit none
-
-    ! Input Variables
-    integer, intent(in) :: &
-      nz,            & ! Number of vertical levels
-      n_samples        ! Number of sample columns from latin hypercube
-
-    real( kind = core_rknd ), dimension(n_samples), intent(in) :: &
-      lh_weights   ! Sample  point weights                   [-]
-
-    real( kind = core_rknd ), dimension(nz,n_samples), intent(in) :: &
-      rt_all_samples, &  ! rt columns from latin hypercube   [kg/kg]
-      thl_all_samples, & ! thl columns from latin hypercube  [K]
-      w_all_samples      ! w columns from latin hypercube    [m/s]
-
-    ! Output Variables
-    real( kind = core_rknd ), dimension(nz), intent(out) :: &
-      lh_rtp2, &    ! Latin hypercube estimate of <rt'^2>     [(kg/kg)^2]
-      lh_thlp2, &   ! Latin hypercube estimate of <thl'^2>    [K^2]
-      lh_wprtp, &   ! Latin hypercube estimate of <w'rt'>     [m*(kg/kg)/s]
-      lh_wpthlp, &  ! Latin hypercube estimate of <w'thl'>    [m*K/s]
-      lh_rtpthlp    ! Latin hypercube estimate of <rt'thl'>   [K*(kg/kg)]
-
-    ! Local variables
-    real( kind = core_rknd ), dimension(nz) :: &  
-      rt_mean, &    ! Latin hypercube estimate of rtm         [kg/kg]
-      thl_mean, &   ! Latin hypercube estimate of thlm        [K]
-      w_mean, &     ! Latin hypercube estimate of wm          [m/s]
-      rtp2_zt, &    ! Estimate of <rt'^2> on the zt grid      [(kg/kg)^2]
-      thlp2_zt, &   ! Estimate of <thl'^2> on the zt grid     [K^2]
-      wprtp_zt, &   ! Estimate of <w'rt'> on the zt grid      [m*(kg/kg)/s]
-      wpthlp_zt, &  ! Estimate of <w'thl'> on the zt grid     [m*K/s]
-      rtpthlp_zt    ! Estimate of <rt'thl'> on the zt grid    [K*(kg/kg)]
-
-
-    ! ---- Begin code ----
-
-    rt_mean = compute_sample_mean( nz, n_samples, lh_weights, rt_all_samples )
-    thl_mean = compute_sample_mean( nz, n_samples, lh_weights, thl_all_samples )
-    w_mean = compute_sample_mean( nz, n_samples, lh_weights, w_all_samples )
-
-    rtp2_zt = compute_sample_variance( nz, n_samples, rt_all_samples, lh_weights, rt_mean )
-    thlp2_zt = compute_sample_variance( nz, n_samples, thl_all_samples, lh_weights, thl_mean )
-  
-    wprtp_zt = compute_sample_covariance( nz, n_samples, lh_weights, &
-                   w_all_samples, w_mean, rt_all_samples, rt_mean ) 
-    wpthlp_zt = compute_sample_covariance( nz, n_samples, lh_weights, &
-                   w_all_samples, w_mean, thl_all_samples, thl_mean )
-    rtpthlp_zt = compute_sample_covariance( nz, n_samples, lh_weights, &
-                   rt_all_samples, rt_mean, thl_all_samples, thl_mean ) 
-
-    lh_rtp2 = zt2zm( rtp2_zt )
-    lh_thlp2 = zt2zm( thlp2_zt )
-    lh_wprtp = zt2zm( wprtp_zt )
-    lh_wpthlp = zt2zm( wpthlp_zt )
-    lh_rtpthlp = zt2zm( rtpthlp_zt )
-
-    return
-  end subroutine lh_moments
-  !-----------------------------------------------------------------------------
 end module lh_microphys_var_covar_module
