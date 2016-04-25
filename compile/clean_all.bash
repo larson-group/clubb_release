3a70903a6505fbd7f9e6dde0f27bc3b80ec13838
raut@uwm.edu <raut@uwm.edu>
2016-04-25 20:24:29 +0000

    I am making CLUBB's `clean_all.bash` script able to be run from any directory. It will clean a compilation in whatever working copy of CLUBB it is in.
    This is vaguely related to clubb:ticket:786.
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8045 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 84ddc0a77a0967b8f22fbb14891b4edef26eba83

--- compile/clean_all.bash
+++ compile/clean_all.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 # $Id: compile.bash 5079 2011-04-01 17:25:38Z dschanen@uwm.edu $
-cd ../bin && make distclean
+
+# What directory am I running from?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR/../bin" && make distclean
