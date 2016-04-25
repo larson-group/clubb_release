a70675bfc319ab1fc8b67130d02b717241391bbd
raut@uwm.edu <raut@uwm.edu>
2016-04-25 20:40:06 +0000

    This is a more accurate comment.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8046 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 822efac298098555d1c8ae645d34c19131bde5c0

--- compile/clean_all.bash
+++ compile/clean_all.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $Id: compile.bash 5079 2011-04-01 17:25:38Z dschanen@uwm.edu $
 
-# What directory am I running from?
+# What directory am I located in?
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/../bin" && make distclean
