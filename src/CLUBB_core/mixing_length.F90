9ac52e72f5c7bb005b146df2cc5b2495973f4980
raut@uwm.edu <raut@uwm.edu>
2016-07-07 23:13:56 +0000

    For cam:ticket:67:
    I added the ability to use ice in the computation of rsatl_par.
    
    This is wrapped in a flag, `l_include_ice`, which is F by default, keeping output bin-diff.
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8182 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: f69754e5f7c966f12ce435d22662adcb41959f50

--- src/CLUBB_core/mixing_length.F90
+++ src/CLUBB_core/mixing_length.F90
@@ -811,34 +811,76 @@ module mixing_length
   !-----------------------------------------------------------------------
 
     use clubb_precision, only: &
-      core_rknd
+      core_rknd ! Precision
 
     use model_flags, only: &
       l_sat_mixrat_lookup ! Variable(s)
 
     use saturation, only:  & 
-        sat_mixrat_liq, & ! Procedure(s)
-        sat_mixrat_liq_lookup
+      sat_mixrat_liq, & ! Procedure(s)
+      sat_mixrat_liq_lookup, &
+      sat_mixrat_ice
+
+    use constants_clubb, only: &
+      zero, &
+      one, &
+      T_freeze_K
 
     implicit none
+
+    ! Parameters
+    logical, parameter :: &
+      l_include_ice = .false. ! Include ice in calculation of rsat_par
+
+    real( kind = core_rknd ), parameter :: &
+      T_all_ice = 233.15_core_rknd ! [K]
 
     ! Input Variables
     real( kind = core_rknd ), intent(in) :: &
       p_in_Pa, &    ! Pressure at this grid level           [Pa]
-      tl_par        ! tl at this grid level                 [?]
+      tl_par        ! tl at this grid level                 [K]
 
     ! Result variable
     real( kind = core_rknd ) :: &
       rsatl_par
 
-  !-----------------------------------------------------------------------
+    real( kind = core_rknd ) :: &
+      sat_ice_ratio, &    ! Ratio of interpolation between sat_mixrat_liq and sat_mixrat_ice.
+                          ! sat_ice_ratio=1 ---> rsatl_par = sat_mixrat_ice
+      sat_mixrat_liq_res, &
+      sat_mixrat_ice_res
 
+  !-----------------------------------------------------------------------
     !----- Begin Code -----
-    if ( l_sat_mixrat_lookup ) then
-      rsatl_par = sat_mixrat_liq_lookup( p_in_Pa, tl_par )
+
+    if ( l_include_ice ) then
+      if ( tl_par >= T_freeze_K ) then
+        sat_ice_ratio = zero
+      else if ( tl_par <= T_all_ice ) then
+        sat_ice_ratio = one
+      else
+        ! Linear interpolation
+        sat_ice_ratio = ( T_freeze_K - tl_par ) / ( T_freeze_K - T_all_ice )
+      end if
     else
-      rsatl_par = sat_mixrat_liq( p_in_Pa, tl_par )
+      sat_ice_ratio = zero
+    end if ! l_include_ice
+
+    if ( sat_ice_ratio < one ) then
+      ! Include liquid.
+      if ( l_sat_mixrat_lookup ) then
+        sat_mixrat_liq_res = sat_mixrat_liq_lookup( p_in_Pa, tl_par )
+      else
+        sat_mixrat_liq_res = sat_mixrat_liq( p_in_Pa, tl_par )
+      end if
     end if
+
+    if ( sat_ice_ratio > zero ) then
+      ! Include ice.
+      sat_mixrat_ice_res = sat_mixrat_ice( p_in_Pa, tl_par )
+    end if
+
+    rsatl_par = sat_mixrat_liq_res + ( sat_mixrat_ice_res - sat_mixrat_liq_res ) * sat_ice_ratio
 
     return
   end function compute_rsat_parcel
