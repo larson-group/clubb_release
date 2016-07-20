5ad1c9fe02102087f63c7893d20ea5f7bfbfe62c
raut@uwm.edu <raut@uwm.edu>
2016-07-20 15:27:43 +0000

    For clubb:ticket:788, I added a threadprivate entry for `iwp3_pr3`, which will hopefully resolve the failure of the OpenMP test (build:16010).
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8212 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 41002aa1be883eea7f5cee997cf5384d81c4a333

--- src/CLUBB_core/stats_variables.F90
+++ src/CLUBB_core/stats_variables.F90
@@ -383,7 +383,7 @@ module stats_variables
      iwp3_cl  = 0
 
 !$omp   threadprivate(iwp3_bt, iwp3_ma, iwp3_ta, iwp3_tp, iwp3_ac, iwp3_bp1)
-!$omp   threadprivate(iwp3_bp2, iwp3_pr1, iwp3_pr2, iwp3_dp1, iwp3_cl)
+!$omp   threadprivate(iwp3_bp2, iwp3_pr1, iwp3_pr2, iwp3_pr3, iwp3_dp1, iwp3_cl)
 
   ! Rain mixing ratio budgets
   integer, public :: & 
