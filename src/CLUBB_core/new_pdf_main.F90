243d759ef9923ca42c2700c4b5f9a87ba3595c68
bmg2@uwm.edu <bmg2@uwm.edu>
2017-11-08 07:22:43 +0000

    I have set the tunable parameters F_x and zeta_x to skewness functions for the new PDF.  Coefficients are currently declared in the driver subroutine and then passed into subroutines and functions.
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8496 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: c22cd4b53994328617fc67e53a7790cf1126a814

--- src/CLUBB_core/new_pdf_main.F90
+++ src/CLUBB_core/new_pdf_main.F90
@@ -155,6 +155,14 @@ module new_pdf_main
       coef_wp2thlp_implicit, & ! Coef. that is multiplied by <w'thl'>      [m/s]
       term_wp2thlp_explicit    ! Term that is on the RHS             [m^2/s^2 K]
 
+    real ( kind = core_rknd ) :: &
+      coefficient_w,   & ! Coefficient for w                                 [-]
+      lambda_w,        & ! Param. that increases or decreases Skw dependence [-]
+      coefficient_rt,  & ! Coefficient for rt                                [-]
+      lambda_rt,       & ! Param. that increases/decreases Skrt dependence   [-]
+      coefficient_thl, & ! Coefficient for thl                               [-]
+      lambda_thl         ! Param. that increases/decreases Skthl dependence  [-]
+
     logical, parameter :: &
       l_use_w_setter_var = .false. ! Flag to always use w as the setter variable
 
@@ -177,6 +185,13 @@ module new_pdf_main
        sgn_wpthlp = -one
     endif ! wpthlp >= 0
 
+    coefficient_w = 35.0_core_rknd 
+    lambda_w = 1.0_core_rknd
+    coefficient_rt = 0.75_core_rknd
+    lambda_rt = 0.1_core_rknd
+    coefficient_thl = 0.75_core_rknd
+    lambda_thl = 0.1_core_rknd
+
 
     ! The variable with the greatest magnitude of skewness will be the setter
     ! variable and the other variables will be responder variables.  When the
@@ -187,9 +202,10 @@ module new_pdf_main
        ! The variable w has the greatest magnitude of skewness or the
        ! l_use_w_setter_var flag is enabled.
 
-       call calc_F_x_zeta_x_setter( Skw, 200.0_core_rknd, & ! In
-                                    F_w, zeta_w,          & ! Out
-                                    min_F_w, max_F_w      ) ! Out
+       call calc_F_x_zeta_x_setter( Skw, sgn_wp2,            & ! In
+                                    coefficient_w, lambda_w, & ! In
+                                    F_w, zeta_w,             & ! Out
+                                    min_F_w, max_F_w         ) ! Out
 
        ! Calculate the PDF parameters, including mixture fraction, for the
        ! setter variable, w.
@@ -212,7 +228,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable rt.
        call calc_responder_var( rtm, rtp2, sgn_wprtp, mixt_frac, & ! In
-                                200.0_core_rknd,                 & ! In
+                                coefficient_rt, lambda_rt,       & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,       & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,       & ! In
                                 Skrt,                            & ! In/Out
@@ -224,7 +240,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable thl.
        call calc_responder_var( thlm, thlp2, sgn_wpthlp, mixt_frac, & ! In
-                                200.0_core_rknd,                    & ! In
+                                coefficient_thl, lambda_thl,        & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,          & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,          & ! In
                                 Skthl,                              & ! In/Out
@@ -238,9 +254,10 @@ module new_pdf_main
 
        ! The variable rt has the greatest magnitude of skewness.
 
-       call calc_F_x_zeta_x_setter( Skrt, 200.0_core_rknd,  & ! In
-                                    F_rt, zeta_rt,          & ! Out
-                                    min_F_rt, max_F_rt      ) ! Out
+       call calc_F_x_zeta_x_setter( Skrt, sgn_wprtp,           & ! In
+                                    coefficient_rt, lambda_rt, & ! In
+                                    F_rt, zeta_rt,             & ! Out
+                                    min_F_rt, max_F_rt         ) ! Out
 
        ! Calculate the PDF parameters, including mixture fraction, for the
        ! setter variable, rt.
@@ -263,7 +280,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable w.
        call calc_responder_var( wm, wp2, sgn_wp2, mixt_frac,  & ! In
-                                200.0_core_rknd,              & ! In
+                                coefficient_w, lambda_w,      & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,    & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,    & ! In
                                 Skw,                          & ! In/Out
@@ -275,7 +292,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable thl.
        call calc_responder_var( thlm, thlp2, sgn_wpthlp, mixt_frac, & ! In
-                                200.0_core_rknd,                    & ! In
+                                coefficient_thl, lambda_thl,        & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,          & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,          & ! In
                                 Skthl,                              & ! In/Out
@@ -289,9 +306,10 @@ module new_pdf_main
 
        ! The variable thl has the greatest magnitude of skewness.
 
-       call calc_F_x_zeta_x_setter( Skthl, 200.0_core_rknd,   & ! In
-                                    F_thl, zeta_thl,          & ! Out
-                                    min_F_thl, max_F_thl      ) ! Out
+       call calc_F_x_zeta_x_setter( Skthl, sgn_wpthlp,           & ! In
+                                    coefficient_thl, lambda_thl, & ! In
+                                    F_thl, zeta_thl,             & ! Out
+                                    min_F_thl, max_F_thl         ) ! Out
 
        ! Calculate the PDF parameters, including mixture fraction, for the
        ! setter variable, thl.
@@ -314,7 +332,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable w.
        call calc_responder_var( wm, wp2, sgn_wp2, mixt_frac,  & ! In
-                                200.0_core_rknd,              & ! In
+                                coefficient_w, lambda_w,      & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,    & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,    & ! In
                                 Skw,                          & ! In/Out
@@ -326,7 +344,7 @@ module new_pdf_main
 
        ! Calculate the PDF parameters for responder variable rt.
        call calc_responder_var( rtm, rtp2, sgn_wprtp, mixt_frac, & ! In
-                                200.0_core_rknd,                 & ! In
+                                coefficient_rt, lambda_rt,       & ! In
                                 max_Skx2_pos_Skx_sgn_wpxp,       & ! In
                                 max_Skx2_neg_Skx_sgn_wpxp,       & ! In
                                 Skrt,                            & ! In/Out
@@ -390,7 +408,7 @@ module new_pdf_main
 
   !=============================================================================
   subroutine calc_responder_var( xm, xp2, sgn_wpxp, mixt_frac, & ! In
-                                 coefficient,                  & ! In
+                                 coefficient, lambda,          & ! In
                                  max_Skx2_pos_Skx_sgn_wpxp,    & ! In
                                  max_Skx2_neg_Skx_sgn_wpxp,    & ! In
                                  Skx,                          & ! In/Out
@@ -424,11 +442,12 @@ module new_pdf_main
 
     ! Input Variables
     real( kind = core_rknd ), intent(in) :: &
-      xm,          & ! Mean of x (overall)                  [units vary]
+      xm,          & ! Mean of x (overall)                      [units vary]
       xp2,         & ! Variance of x (overall)              [(units vary)^2]
-      sgn_wpxp,    & ! Sign of the covariance of w and x    [-]
-      mixt_frac,   & ! Mixture fraction                     [-]
-      coefficient    ! Coefficient                          [-]
+      sgn_wpxp,    & ! Sign of the covariance of w and x                 [-]
+      mixt_frac,   & ! Mixture fraction                                  [-]
+      coefficient, & ! Coefficient                                       [-]
+      lambda         ! Param. that incr. or decr. Skx dependence of F_x  [-]
 
     real( kind = core_rknd ), intent(in) :: &
       max_Skx2_pos_Skx_sgn_wpxp, & ! Maximum Skx^2 when Skx*sgn(<w'x'>) >= 0 [-]
@@ -481,7 +500,8 @@ module new_pdf_main
                                     min_F_x, max_F_x )           ! Out
 
     ! F_x must have a value between min_F_x and max_F_x.
-    F_x = calc_F_x_Skx_fnc( Skx, coefficient, min_F_x, max_F_x )
+    F_x = calc_F_x_Skx_fnc( Skx, coefficient, lambda, &
+                            min_F_x, max_F_x )
 
     call calc_responder_params( xm, xp2, Skx, sgn_wpxp,       & ! In
                                 F_x, mixt_frac,               & ! In
@@ -496,13 +516,41 @@ module new_pdf_main
   end subroutine calc_responder_var
 
   !=============================================================================
-  subroutine calc_F_x_zeta_x_setter( Skx, coefficient, & ! In
-                                     F_x, zeta_x,      & ! Out
-                                     min_F_x, max_F_x  ) ! Out
+  subroutine calc_F_x_zeta_x_setter( Skx, sgn_wpxp,       & ! In
+                                     coefficient, lambda, & ! In
+                                     F_x, zeta_x,         & ! Out
+                                     min_F_x, max_F_x     ) ! Out
 
     ! Description:
     ! Calculates the values of F_x and zeta_x for the setter variable (which is
     ! the variable that sets the mixture fraction).
+    !
+    ! The value of F_x is calculated between 0 (min_F_x) and 1 (max_F_x).  The
+    ! equation is:
+    !
+    ! F_x = max_F_x + ( min_F_x - max_F_x ) * exp{ -|Skx|^lambda / coef };
+    !
+    ! which reduces to:
+    !
+    ! F_x = 1 - exp{ -|Skx|^lambda / coef };
+    !
+    ! where lambda > 0 and coef > 0.  As |Skx| goes toward 0, the value of F_x
+    ! goes toward 0, and as |Skx| becomes large, the value of F_x goes toward 1.
+    ! When coef is small, the value of F_x tends toward 1, and when coef is
+    ! large, the value of F_x tends toward 0.  When lambda is small, the value
+    ! of F_x is less dependent on Skx, and when lambda is large, the value of
+    ! F_x is more dependent on Skx.
+    !
+    ! The value of zeta_x is also dependent on Skx, in a way that produces a
+    ! symmetric PDF.  The equation for zeta_x is:
+    !
+    ! zeta_x
+    !    = | lambda * |Skx|,                     when Skx * sgn( <w'x'> ) >= 0;
+    !      | ( 1 / ( 1 + lambda * |Skx| ) ) - 1, when Skx * sgn( <w'x'> ) < 0.
+    !
+    ! The value of zeta_x is dependent on |Skx|.  When lambda is small, the
+    ! value of zeta_x does not depend much on Skx (it remains close to 0), and
+    ! when lambda is large, the value of zeta_x depends more on Skx.
 
     ! References:
     !-----------------------------------------------------------------------
@@ -518,8 +566,10 @@ module new_pdf_main
 
     ! Input Variables
     real( kind = core_rknd ), intent(in) :: &
-      Skx,         & ! Skewness of x (overall)      [-]
-      coefficient    ! Coefficient                  [-]
+      Skx,         & ! Skewness of x (overall)                             [-]
+      sgn_wpxp,    & ! Sign of the covariance of w and x                   [-]
+      coefficient, & ! Coefficient                                         [-]
+      lambda         ! Param. that increases or decreases Skx dependence   [-]
 
     ! Output Variables
     real( kind = core_rknd ), intent(out) :: &
@@ -529,11 +579,21 @@ module new_pdf_main
       max_F_x    ! Maximum allowable value of parameter F_x                  [-]
 
 
+    ! Set min_F_x to 0 and max_F_x to 1 for the setter variable.
     min_F_x = zero
     max_F_x = one
 
-    F_x = one - exp( -Skx**2 / coefficient )
-    zeta_x = zero
+    ! F_x must have a value between min_F_x and max_F_x, which are 0 and 1,
+    ! respectively.
+    F_x = calc_F_x_Skx_fnc( Skx, coefficient, lambda, &
+                            min_F_x, max_F_x )
+
+    ! The value of zeta_x must be greater than -1.
+    if ( Skx * sgn_wpxp >= zero ) then
+       zeta_x = lambda * abs( Skx )
+    else ! Skx * sgn( <w'x'> ) < 0
+       zeta_x = one / ( one + lambda * abs( Skx ) ) - one 
+    endif ! Skx * sgn( <w'x'> ) >= 0
 
 
     return
@@ -541,15 +601,40 @@ module new_pdf_main
   end subroutine calc_F_x_zeta_x_setter
 
   !=============================================================================
-  function calc_F_x_Skx_fnc( Skx, coefficient, min_F_x, max_F_x ) &
+  function calc_F_x_Skx_fnc( Skx, coefficient, lambda, &
+                             min_F_x, max_F_x ) &
   result( F_x )
 
     ! Description:
     ! Calculates the value of F_x as a function of Skx between min_F_x and
     ! max_F_x.
+    !
+    ! The value of F_x is calculated between min_F_x and max_F_x.  The equation
+    ! is:
+    !
+    ! F_x = max_F_x + ( min_F_x - max_F_x ) * exp{ -|Skx|^lambda / coef };
+    !
+    ! where lambda > 0 and coef > 0.  As |Skx| goes toward 0, the value of F_x
+    ! goes toward min_F_x, and as |Skx| becomes large, the value of F_x goes
+    ! toward max_F_x.  When coef is small, the value of F_x tends toward
+    ! max_F_x, and when coef is large, the value of F_x tends toward min_F_x.
+    ! When lambda is small, the value of F_x is less dependent on Skx, and when
+    ! lambda is large, the value of F_x is more dependent on Skx.
+    !
+    ! Mathematically, this equation will always produce a value of F_x that
+    ! falls between min_F_x and max_F_x.  However, in order to prevent a value
+    ! of F_x from being calculated outside the bounds of min_F_x and max_F_x
+    ! owing to numerical underflow or loss of precision, this equation can be
+    ! rewritten as:
+    !
+    ! F_x = min_F_x * exp{ -|Skx|^lambda / coef }
+    !       + max_F_x * ( 1 - exp{ -|Skx|^lambda / coef } ).
 
     ! References:
     !-----------------------------------------------------------------------
+
+    use constants_clubb, only: &
+        one    ! Variable(s)
 
     use clubb_precision, only: &
         core_rknd    ! Variable(s)
@@ -558,18 +643,26 @@ module new_pdf_main
 
     ! Input Variables
     real( kind = core_rknd ), intent(in) :: &
-      Skx,         & ! Skewness of x (overall)                          [-]
-      coefficient, & ! Coefficient                                      [-]
-      min_F_x,     & ! Minimum allowable value of parameter F_x         [-]
-      max_F_x        ! Maximum allowable value of parameter F_x         [-]
+      Skx,         & ! Skewness of x (overall)                             [-]
+      coefficient, & ! Coefficient                                         [-]
+      lambda,      & ! Param. that increases or decreases Skx dependence   [-]
+      min_F_x,     & ! Minimum allowable value of parameter F_x            [-]
+      max_F_x        ! Maximum allowable value of parameter F_x            [-]
 
     ! Return Variable
     real( kind = core_rknd ) :: &
-      F_x    ! Parameter for the spread of the PDF component means of x  [-]
+      F_x    ! Parameter for the spread of the PDF component means of x    [-]
+
+    ! Local Variable
+    real( kind = core_rknd ) :: &
+      exp_Skx_interp_factor    ! Function to interp. between min. and max. [-]
 
 
     ! F_x must have a value between min_F_x and max_F_x.
-    F_x = max_F_x + ( min_F_x - max_F_x ) * exp( -Skx**2 / coefficient )
+    exp_Skx_interp_factor = exp( -abs(Skx)**lambda / coefficient )
+
+    F_x = min_F_x * exp_Skx_interp_factor &
+          + max_F_x * ( one - exp_Skx_interp_factor )
 
 
     return
