b60d5b72b8f98f7142cb0934a0d41004b7a72e04
huebler@uwm.edu <huebler@uwm.edu>
2018-05-25 01:07:06 +0000

    Removed failure_test cases from input/case_setups, they neither work properly nor are needed anymore. Added test_fatal_error_handling.bash script which tests for various errors defined by an identifier in the code, more description available in the bash file. Also added more descriptive error output to some parts of code. This change focuses only on output and comments and is BIT IDENTICAL clubb:ticket:800
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8687 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: 9756b76bfe8ee00d683ca42f1a87abf7a89e382e

---
+++ run_scripts/test_fatal_error_handling.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+###############################################################################
+# $Id$
+#
+#   Description:
+#       This script injects errors into clubb then runs a case and captures the 
+#       error output. The injected errors need to be labeled in the fortran code
+#       with a name, then added to the testName array in this script along with
+#       the name of the variable to be tested to the testVariable array.
+#
+#   Example:
+#
+#       In fortran code:    
+#                           ! advance_wp2_wp3_bad_wp2
+#
+#       In this script:
+#                           testName[1]="advance_wp2_wp3_bad_wp2"
+#                           testVariable[1]="wp2"
+#
+#       This results in "! advance_wp2_wp3_bad_wp2" in the code being replaced
+#       with a line that sets "wp2" to NaN, then the error messages clubb
+#       produces are saved in a file named "advance_wp2_wp3_bad_wp2"
+#
+#   Notes:
+#       To add error tests, simply add the lines that are explained in the example
+#       above, changing the test name to something unique andthe variable to the one
+#       that will be tested. Remeber to change the index as well. 
+#
+###############################################################################
+
+
+declare -a testName
+declare -a testVariable
+
+########### TEST DEFINITIONS  ##################
+testName[1]="advance_wp2_wp3_bad_wp2"
+testVariable[1]="wp2"
+
+testName[2]="advance_xm_wpxp_bad_wp2"
+testVariable[2]="wp2"
+
+
+
+#change compiler flags so the compliation ignores the NaN
+sed -i "s:^DEBUG=\"-g -fbounds-check:DEBUG=\"-g -fno-range-check -fbounds-check:g" ../compile/config/linux_x86_64_gfortran.bash
+
+for(( j=1; j<=${#testName[@]}; j++)); do
+
+    #find test name and replace with line that sets test variable to NaN
+    sed -i "s:! ${testName[j]}:${testVariable[j]} = transfer( 2143289344, 1.0 ):g" ../src/CLUBB_core/advance_clubb_core_module.F90
+
+    #compile
+    ./../compile/compile.bash
+
+    #run a case (any non error causing case will work) and write error output to a file
+    ./run_scm.bash args = "arm_97" > /dev/null 2> ${testName[j]}
+
+    #undo previous sed 
+    sed -i "s:${testVariable[j]} = transfer( 2143289344, 1.0 ):! ${testName[j]}:g" ../src/CLUBB_core/advance_clubb_core_module.F90
+
+done
+
+#restore compiler flags
+sed -i "s:^DEBUG=\"-g -fno-range-check -fbounds-check:DEBUG=\"-g -fbounds-check:g" ../compile/config/linux_x86_64_gfortran.bash
