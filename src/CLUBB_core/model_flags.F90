a560f1a2e3554dfc7cc7dd9da60cc50b3dad4686
bmg2@uwm.edu <bmg2@uwm.edu>
2018-08-14 22:18:52 +0000

    I am turning on the l_predict_upwp_vpwp code!
    This revision is BIT_CHANGING, in the best way possible!
    
    
    
    git-svn-id: file:///usr/local/src/svnroot/clubb_repos/trunk@8778 f1ce5500-ea2b-45e0-a110-28dffb978352
    
    Former-commit-id: fbf124c85d68eda019777aac6790e369e30e829c

--- src/CLUBB_core/model_flags.F90
+++ src/CLUBB_core/model_flags.F90
@@ -62,7 +62,7 @@ module model_flags
   ! approximated by eddy diffusivity when <u> and <v> are advanced in
   ! subroutine advance_windm_edsclrm.
   logical, public :: &
-    l_predict_upwp_vpwp = .false.
+    l_predict_upwp_vpwp = .true.
 
 !$omp threadprivate( l_predict_upwp_vpwp )
 
