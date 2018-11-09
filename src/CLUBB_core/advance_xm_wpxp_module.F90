65fb7174f57783131e5fa9ca6a790f412d3d7df6
mcfadd <mcfadd34@uwm.edu>
2018-11-09 09:32:21 -0600

    introducing error into clubb core for testing larson-group/sys_admin#533

--- src/CLUBB_core/advance_xm_wpxp_module.F90
+++ src/CLUBB_core/advance_xm_wpxp_module.F90
@@ -485,8 +485,13 @@ module advance_xm_wpxp_module
     ! Damp C6 as a function of Lscale in stably stratified regions
     C6rt_Skw_fnc = damp_coefficient( C6rt, C6rt_Skw_fnc, &
                                      C6rt_Lscale0, wpxp_L_thresh, Lscale )
-    C6thl_Skw_fnc = damp_coefficient( C6thl, C6thl_Skw_fnc, &
-                                      C6thl_Lscale0, wpxp_L_thresh, Lscale )
+
+    ! Commented out on 11/9/2018 for testing bittens sys_admin#533
+    !C6thl_Skw_fnc = damp_coefficient( C6thl, C6thl_Skw_fnc, &
+    !                                  C6thl_Lscale0, wpxp_L_thresh, Lscale )
+
+     C6thl_Skw_fnc = damp_milwaukee_weather( C6thl, C6thl_Skw_fnc, &
+				       C6thl_Lscale0, wpxp_L_thresh, Lscale )
 
     !        C6rt_Skw_fnc = C6rt
     !        C6thl_Skw_fnc = C6thl
