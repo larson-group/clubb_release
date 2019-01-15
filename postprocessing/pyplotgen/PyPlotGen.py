58fe2ee6e7f1f3d089b597af8dc3d7d20c4e72ce
Nic Strike <strike@uwm.edu>
2019-01-14 18:09:35 -0600

    Initializing folder and files for PyPlotGen (the new python version of python).

---
+++ postprocessing/pyplotgen/PyPlotGen.py
@@ -0,0 +1,19 @@
+'''
+This file is the main driver for PyPlotGen. It handles the overall
+process of retrieving the user's input and dictating the loading
+of files, generation of plots, and then the export of plots, though
+these processes are mostly carried out by other classes/files.
+
+Author: Nicolas Strike
+Date: Jan 2019
+'''
+
+
+def main():
+'''
+This is the main method that starts the program
+'''
+
+# run main if file is run by interpreter
+if __name__ == "__main__":
+    main()
