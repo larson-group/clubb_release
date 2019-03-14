5db85c2ab0c4bdca02dbccf6d64b49b1d7c59ad7
mcfadd <mcfadd34@uwm.edu>
2019-03-14 10:41:41 -0500

    updated run_full_api_diagnostics.bash. Replaced svn with git. Changed repo locations from http://carson.math.uwm.edu to https://github.com. Added log file argument for bitten tests

--- utilities/api_analysis/run_full_api_diagnostics.bash
+++ utilities/api_analysis/run_full_api_diagnostics.bash
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 ####################################################################################
-# run_full_api_diagnostics v1.0
+# run_full_api_diagnostics v1.2
 #
 # Run every api/* test.
 #
 # File History:
+#  v1.2: Converted svn commands to git
 #  v1.1: Merged the bitten and nightly version of this script
 #  v1.0: Initial Release
 #
 # Required Arguments
 #  -None
 # Optional Arguments
-#  -nightly
-#       With this argument, the tables are produced using the nightly paths.
-#       Without this argument, no tables are produced (bitten requires no tables).
+#  $1 = "-nightly"
+#         With this argument, the tables are produced using the nightly paths.
+#         Without this argument, no tables are produced (bitten requires no tables).
+#  $1 = <log file>
+#         $outputLog will be set to the log file specified (used by bitten tests).
 ####################################################################################
 
-#This variable allows the script to reurn to where it was run from.
+#This variable allows the script to return to where it was run from.
 restoreLoc=`pwd`
 
 # Figure out the directory where the script is located.
@@ -29,108 +32,119 @@ cd $run_dir
 # Checkout CLUBB and the Host Models
 if [ "$1" == "-nightly" ]
 then
+    # The output log file
+    outputLog=$logLocation/current/nightly_tests.log
+
     # Setup the checkout scripts
-    . ../nightly_config.sh
-    echo "Checking out CLUBB"
+    echo "Checking out CLUBB" >> $outputLog
     ../checkout_clubb_fresh.bash
-    echo "Checking out SAM"
+    echo "Checking out SAM" >> $outputLog
     ../checkout_sam_fresh.bash
-    echo "Checking out WRF"
+    echo "Checking out WRF" >> $outputLog
     ../checkout_wrf_fresh.bash
-    echo "Checking out CAM"
+    echo "Checking out CAM" >> $outputLog
     ../checkout_cam_fresh.bash
 else
-    echo "Checking out CLUBB"
-    svn co http://carson.math.uwm.edu/repos/clubb_repos/trunk CLUBB
-    echo "Checking out SAM"
-    svn co http://carson.math.uwm.edu/repos/sam_repos/trunk SAM
-    echo "Checking out WRF"
-    svn co http://carson.math.uwm.edu/repos/wrf/trunk WRF
-    echo "Checking out CAM"
-    svn co https://svn-ccsm-models.cgd.ucar.edu/cam1/branches/subcol_SILHS_UWM CAM
+    # The output log file
+    outputLog=$1
+
+    echo "Checking out CLUBB" >> $outputLog
+    git clone https://github.com/larson-group/clubb.git CLUBB
+    echo "Checking out SAM" >> $outputLog
+    git clone https://github.com/larson-group/sam_clubb.git SAM
+    echo "Checking out WRF" >> $outputLog
+    git clone https://github.com/larson-group/wrf.git WRF
+    echo "Checking out CAM" >> $outputLog
+    git clone https://github.com/larson-group/cam.git CAM
 fi
 
 # Setup the paths to the host models
 if [ "$1" == "-nightly" ]
 then
-    clubbDir="../../../clubb"
-    samDir="../../../SAM_CLUBB"
-    wrfDir="../../../WRF_CLUBB"
-    camDir="../../../CAM"
+    clubbDir=$clubbSource
+    samDir=$samSource
+    wrfDir=$wrfSource
+    camDir=$camSource
 else
     clubbDir="CLUBB"
     samDir="SAM"
-    wrfDir="WRF"
+    wrfDir="WRF/WRF"
     camDir="CAM"
 fi
 
-echo "Moving CLUBB_core"
+echo "Moving CLUBB_core" >> $outputLog
 mv $clubbDir/src/CLUBB_core CLUBB_core
 
-echo "Removing .svn Folders"
-find CLUBB_core -type d -name .svn -exec rm -rf {} \;
-find $clubbDir -type d -name .svn -exec rm -rf {} \;
-find $samDir -type d -name .svn -exec rm -rf {} \;
-find $wrfDir -type d -name .svn -exec rm -rf {} \;
-find $camDir -type d -name .svn -exec rm -rf {} \;
+echo "Removing .git Folders" >> $outputLog
+find CLUBB_core -type d -name .git -exec rm -rf {} \; >> $outputLog
+find $clubbDir -type d -name .git -exec rm -rf {} \; >> $outputLog
+find $samDir -type d -name .git -exec rm -rf {} \; >> $outputLog
+find $wrfDir -type d -name .git -exec rm -rf {} \; >> $outputLog
+find $camDir -type d -name .git -exec rm -rf {} \; >> $outputLog
+
+find CLUBB_core -type d -name .gitignore -exec rm -rf {} \; >> $outputLog
+find $clubbDir -type d -name .gitignore -exec rm -rf {} \; >> $outputLog
+find $samDir -type d -name .gitignore -exec rm -rf {} \; >> $outputLog
+find $wrfDir -type d -name .gitignore -exec rm -rf {} \; >> $outputLog
+find $camDir -type d -name .gitignore -exec rm -rf {} \; >> $outputLog
 
 if [ "$1" == "-nightly" ]
 then
-    echo "Copying the SILHS API into the CLUBB API"
+    echo "Copying the SILHS API into the CLUBB API" >> $outputLog
     cat $clubbDir/src/SILHS/silhs_api_module.F90 CLUBB_core/clubb_api_module.F90 > tempApiFile
     mv tempApiFile CLUBB_core/clubb_api_module.F90
 
-    echo "Running the Usage Analyzer"
+    echo "Running the Usage Analyzer" >> $outputLog
     python usage_analyzer.py CLUBB_core/clubb_api_module.F90 $samDir $wrfDir $camDir > ../text_output/usageAnalyzerTable.html
 fi
 
-echo "Removing API from CLUBB_core"
-rm  CLUBB_core/clubb_api_module.F90
+echo "Removing API from CLUBB_core" >> $outputLog
+rm  CLUBB_core/clubb_api_module.F90 >> $outputLog
 
-echo "Removing API from SILHS"
+echo "Removing API from SILHS" >> $outputLog
 rm  $clubbDir/src/SILHS/silhs_api_module.F90
 
-echo "Removing G_Unit_Tests from CLUBB"
-rm -rf $clubbDir/src/G_unit_test_types
-rm $clubbDir/src/G_unit_tests.F90
+echo "Removing G_Unit_Tests from CLUBB" >> $outputLog
+rm -rf $clubbDir/src/G_unit_test_types >> $outputLog
+rm $clubbDir/src/G_unit_tests.F90 >> $outputLog
 
-echo "Moving SILHS to CLUBB_core"
-mv $clubbDir/src/SILHS/* CLUBB_core/
+echo "Moving SILHS to CLUBB_core" >> $outputLog
+mv $clubbDir/src/SILHS/* CLUBB_core/ >> $outputLog
 
-echo "Testing CLUBB_Standalone's API Commitment"
+echo "Testing CLUBB_Standalone's API Commitment" >> $outputLog
 python api_commitment_test.py -cpu CLUBB_core $clubbDir > clubb_standalone_modules.txt
 
-echo "Testing CLUBB_core's API Commitment"
+echo "Testing CLUBB_core's API Commitment" >> $outputLog
 python api_commitment_test.py -cpu CLUBB_core CLUBB_core > clubb_core_modules.txt
 
-echo "Testing SAM's API Commitment"
+echo "Testing SAM's API Commitment" >> $outputLog
 python api_commitment_test.py -cpu CLUBB_core $samDir --exclude-dir="CLUBB","SILHS" > sam_modules.txt
 
-echo "Testing CAM's API Commitment"
+echo "Testing CAM's API Commitment" >> $outputLog
 python api_commitment_test.py -cpu CLUBB_core $camDir --exclude-dir="spcam","cime","clubb","silhs" > cam_modules.txt
 
-echo "Testing WRF's API Commitment"
+echo "Testing WRF's API Commitment" >> $outputLog
 python api_commitment_test.py -cpu CLUBB_core $wrfDir --exclude-dir="clubb","silhs" > wrf_modules.txt
 
 if [ "$1" == "-nightly" ]
 then
     python create_module_table.py CLUBB_core> ../text_output/apiCommitmentTable.html
-    rm -rf CLUBB_core
+    rm -rf CLUBB_core     
 else
-    echo "Removing Checkouts"
+    echo "Removing Checkouts" >> $outputLog
     rm -rf $clubbDir
     rm -rf $samDir
     rm -rf $wrfDir
     rm -rf $camDir
     rm -rf CLUBB_core
     
-    echo "Testing API Commitment"
+    echo "Testing API Commitment" >> $outputLog
     sam_modules="sam_modules.txt"
     wrf_modules="wrf_modules.txt"
     cam_modules="cam_modules.txt"
     if [ -s "$sam_modules" ] || [ -s "$wrf_modules" ] || [ -s "$cam_modules" ] ; then
-        echo "ERROR: A host model is circumventing the API." 
-        echo "Please inspect the nightly test API Commitment Table."
+        echo "ERROR: A host model is circumventing the API." >> $outputLog
+        echo "Please inspect the nightly test API Commitment Table." >> $outputLog
         echo "MODELS AT FAULT INCLUDE:"
 
                 if [ -s "$sam_modules" ] ; then
@@ -146,14 +160,15 @@ else
                         cat cam_modules.txt
                 fi
 
+
         exitCode=1
     else 
-        echo "All host models passed."
+        echo "All host models passed." >> $outputLog
         exitCode=0
     fi
 fi
 
-echo "Removing Dependencies"
+echo "Removing Dependencies" >> $outputLog
 rm -rf clubb_standalone_modules.txt
 rm -rf clubb_core_modules.txt
 rm -rf sam_modules.txt
@@ -163,3 +178,4 @@ rm -rf cam_modules.txt
 cd $restoreLoc
 
 exit $exitCode
+
