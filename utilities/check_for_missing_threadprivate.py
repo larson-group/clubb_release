ef84fd13bdb6de5a3777d28dee98784461b3e7d1
mcfadd <mcfadd34@uwm.edu>
2019-05-21 12:35:53 -0500

    I added check_for_missing_threadprivate.py to compile.bash so it runs before CLUBBStandardsCheck.pl only if python is installed, formatted the script according to PEP8 conventions, added command line args for CLUBB_core and SILHS directories, and made output text red if the check fails. I found that the script does not take longer than 1 second to run and is compatible with python 2.7 and python 3.6. #854

--- utilities/check_for_missing_threadprivate.py
+++ utilities/check_for_missing_threadprivate.py
@@ -2,11 +2,14 @@
 declared as threadprivate.  This is necessary to ensure thread safety
 in CAM-CLUBB runs that use openmp shared-memory parallelization. """
 
-import pdb # debugging tools
+import pdb  # debugging tools
 
-import re # regex tools
+import re  # regex tools
 
-import os # for traversing directories
+import os  # for traversing directories
+
+import sys  # for getting cmd line args
+
 
 # pdb.set_trace()
 
@@ -14,20 +17,20 @@ def extractLine(fileName):
     """  Extract a logical line that declares a real 
     or integer.  The logical line may be continued onto other lines. """
     with open(fileName) as srcFile:
-        parts = [] # parts of a line that may have a continuation
+        parts = []  # parts of a line that may have a continuation
         # Are we in a line of code that declares a real or integer variable? Not yet.
-        isPartOfDecLine = False 
-        isPartOfTypeDec = False # is the current line part of a type declaration? Not yet.
+        isPartOfDecLine = False
+        isPartOfTypeDec = False  # is the current line part of a type declaration? Not yet.
         for line in srcFile:
-            line = line.split("!")[0] # Remove comment from end of line
-            if line.isspace() or line=='':
+            line = line.split("!")[0]  # Remove comment from end of line
+            if line.isspace() or line == '':
                 # The line is only a comment and nothing else; skip
                 continue
-            if "contains" in line: 
+            if "contains" in line:
                 break  # There are no more module variables after "contains"
-            if re.search(r"\btype\b", line, re.IGNORECASE) is not None\
-                and re.search(r"\bend\s*\btype\b", line, re.IGNORECASE) is None \
-                and re.search(r"\btype\s*\(", line, re.IGNORECASE) is None:
+            if re.search(r"\btype\b", line, re.IGNORECASE) is not None \
+                    and re.search(r"\bend\s*\btype\b", line, re.IGNORECASE) is None \
+                    and re.search(r"\btype\s*\(", line, re.IGNORECASE) is None:
                 # this is the beginning of a type declaration; 
                 #   stop search until 'end type'
                 isPartOfTypeDec = True
@@ -37,42 +40,45 @@ def extractLine(fileName):
                 #   resume search for declarations
                 isPartOfTypeDec = False
                 continue
-            if    re.search(r"\breal\b", line, re.IGNORECASE) is None \
-                and re.search(r"\binteger\b", line, re.IGNORECASE) is None \
-                and re.search(r"\blogical\b", line, re.IGNORECASE) is None \
-                and re.search(r"\bcharacter\b", line, re.IGNORECASE) is None \
-                and re.search(r"\btype\s*\(\b", line, re.IGNORECASE) is None \
-                and not isPartOfDecLine \
-                or isPartOfTypeDec:
+            if re.search(r"\breal\b", line, re.IGNORECASE) is None \
+                    and re.search(r"\binteger\b", line, re.IGNORECASE) is None \
+                    and re.search(r"\blogical\b", line, re.IGNORECASE) is None \
+                    and re.search(r"\bcharacter\b", line, re.IGNORECASE) is None \
+                    and re.search(r"\btype\s*\(\b", line, re.IGNORECASE) is None \
+                    and not isPartOfDecLine \
+                    or isPartOfTypeDec:
                 # this line is not part of a variable declaration, so skip it
                 continue
             else:
                 # this line is part of a declaration
-                if "&" in line: # then line continues
+                if "&" in line:  # then line continues
                     splitLine = line.split("&")
                     parts.append(splitLine[0])
                     isPartOfDecLine = True
-                else: # line ends here
-                    yield ''.join(parts) + line # output line
+                else:  # line ends here
+                    yield ''.join(parts) + line  # output line
                     parts = []
                     isPartOfDecLine = False
-        if parts: yield ''.join(parts) # return any declarations at end
+        if parts:
+            yield ''.join(parts)  # return any declarations at end
+
 
 def extractVarNames(decLine):
     """Given a logical line that declares one or more variables,
     return the variable names in a list."""
-    
+
     # a Fortran parameter does not need a threadprivate declaration
     if re.search(r"\bparameter\b(?=.*::)", decLine, re.IGNORECASE) is not None:
         return []
     else:
-        #print(decLine)
-        decLine = decLine.split("::")[1] # Remove declaration text
-        decLine = decLine.split("!")[0] # Remove code comment
-        listOfVars = decLine.split(",") # Separate variable names
+        # print(decLine)
+        decLine = decLine.split("::")[1]  # Remove declaration text
+        decLine = decLine.split("!")[0]  # Remove code comment
+        listOfVars = decLine.split(",")  # Separate variable names
         listOfVars = [x.split("=")[0] for x in listOfVars]  # Remove equations
-        listOfVars = [x.strip() for x in listOfVars] # Remove extra whitespace
+        listOfVars = [x.strip() for x in listOfVars]  # Remove extra whitespace
     return listOfVars
+
 
 def findDecVars(fileName):
     """ Given a file, return all declared variables in the file. 
@@ -82,19 +88,22 @@ def findDecVars(fileName):
         listOfAllVars.extend(extractVarNames(decLine))
     return listOfAllVars
 
+
 def findThreadPrivates(fileName):
     """ Given a file, return a list of all variables 
     that are declared threadprivate. """
     with open(fileName) as srcFile:
-        srcString = srcFile.read().replace('\n', '') # Make file into one big string
+        srcString = srcFile.read().replace('\n', '')  # Make file into one big string
         srcString = srcString.split("contains\s*\n")[0]  # Chop off everything after "contains"
         listOfThreadPrivates = re.findall(r"(?si)!\$omp\s*threadprivate\s*(\(.*?\))", srcString)
-        listOfThreadPrivates = [x.strip("()") for x in listOfThreadPrivates] # Eliminate parentheses
-        listOfThreadPrivates = [x.split(",") for x in listOfThreadPrivates] # Separate variables
-        listOfThreadPrivates = sum(listOfThreadPrivates,[]) # Convert nested list to simple list
-        listOfThreadPrivates = [re.sub(r"(?i)&\s*!\$omp","",x) for x in listOfThreadPrivates] # Remove openmp directives
-        listOfThreadPrivates = [x.strip() for x in listOfThreadPrivates] # Strip off whitespace around variable names
+        listOfThreadPrivates = [x.strip("()") for x in listOfThreadPrivates]  # Eliminate parentheses
+        listOfThreadPrivates = [x.split(",") for x in listOfThreadPrivates]  # Separate variables
+        listOfThreadPrivates = sum(listOfThreadPrivates, [])  # Convert nested list to simple list
+        listOfThreadPrivates = [re.sub(r"(?i)&\s*!\$omp", "", x) for x in
+                                listOfThreadPrivates]  # Remove openmp directives
+        listOfThreadPrivates = [x.strip() for x in listOfThreadPrivates]  # Strip off whitespace around variable names
     return listOfThreadPrivates
+
 
 # Code taken from usage_analyzer.py
 # Returns an array of every file in a directory
@@ -103,50 +112,61 @@ def findThreadPrivates(fileName):
 def findFiles(dir):
     """ Find all files in a directory.
     """
-    foundFiles=[] # The returned full files locations and names
+    foundFiles = []  # The returned full files locations and names
     subdirs = [x[0] for x in os.walk(dir)]  # Walk through each directory (including the given) 
-                                            #    and find subdirectories recursively
-    for subdir in subdirs:                  # Go through each subdirectory (including this one)
-        currentFiles = next(os.walk(subdir))[2]    # Add the found files to a list
-        if (len(currentFiles) > 0):                 # If files were found
-            for file in currentFiles:                   # go through each file
+    #    and find subdirectories recursively
+    for subdir in subdirs:  # Go through each subdirectory (including this one)
+        currentFiles = next(os.walk(subdir))[2]  # Add the found files to a list
+        if (len(currentFiles) > 0):  # If files were found
+            for file in currentFiles:  # go through each file
                 foundFiles.append(subdir + file)  # Add its location and the filename 
-                                                        #    to the returned files list
-    return foundFiles                       # Return the found files
+                #    to the returned files list
+    return foundFiles  # Return the found files
+
 
 def main():
     """ Test whether all module variables in a set of directories
     are properly listed as threadprivate.
     """
-    
-    clubbFileNames = findFiles("/home/vlarson/cam_issue_121/src/CLUBB_core/")
-    silhsFileNames = findFiles("/home/vlarson/cam_issue_121/src/SILHS/")
+
+    print("check_for_missing_threadprivate.py has begun.")
+
+    # get command line args where argv[1] is clubb core path and argv[2] is silhs path
+    clubbFileNames = findFiles(sys.argv[1])
+    silhsFileNames = findFiles(sys.argv[2])
     fileNames = clubbFileNames + silhsFileNames
 
-    #outputFile = open("/home/vlarson/Downloads/threadprivate_output.txt","w+")
+    # outputFile = open("/home/vlarson/Downloads/threadprivate_output.txt","w+")
     failedFiles = []
     passedTest = True
     for fileName in fileNames:
-        print(fileName)
+        # print(fileName)
         listOfVars = findDecVars(fileName)
-        #print("listOfVars=")
-        #print(listOfVars)
+        # print("listOfVars=")
+        # print(listOfVars)
         listOfThreadPrivates = findThreadPrivates(fileName)
-        #print("listOfThreadPrivates=")
-        #print(listOfThreadPrivates)
-        varsWoThreadprivates = list(set(listOfVars)-set(listOfThreadPrivates))
-        threadprivatesWoVars = list(set(listOfThreadPrivates)-set(listOfVars))
+        # print("listOfThreadPrivates=")
+        # print(listOfThreadPrivates)
+        varsWoThreadprivates = list(set(listOfVars) - set(listOfThreadPrivates))
+        threadprivatesWoVars = list(set(listOfThreadPrivates) - set(listOfVars))
         if varsWoThreadprivates != [] or threadprivatesWoVars != []:
             passedTest = False
             failedFiles.append(fileName)
-        #outputFile.write("%s\n" % fileName)
-        #outputFile.write("%s\n" % str(varsWoThreadprivates))
-        #outputFile.write("%s\n\n" % str(threadprivatesWoVars))
-    #outputFile.close()
-    print("passedTest = %s" %passedTest)
-    print("failedFiles = %s" %str(failedFiles))
+        # outputFile.write("%s\n" % fileName)
+        # outputFile.write("%s\n" % str(varsWoThreadprivates))
+        # outputFile.write("%s\n\n" % str(threadprivatesWoVars))
+    # outputFile.close()
+    if passedTest:
+        print("check_for_missing_threadprivate.py passed.")
+    else:
+        failed_color = '\033[91m'
+        end_color = '\033[0m'
+        print(failed_color + "check_for_missing_threadprivate.py did not pass." + end_color)
+        print(failed_color + "failedFiles = " + str(failedFiles) + end_color)
+
     return passedTest
-    
+
+
 # Standard boilerplate to call the main() function to begin
 # the program.
 if __name__ == '__main__':
