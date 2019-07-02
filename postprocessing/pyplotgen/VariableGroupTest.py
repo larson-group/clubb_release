ab6564c2e8e90a65668fe823575e286625473fe4
Nicolas Strike <strike@uwm.edu>
2019-07-02 14:24:04 -0500

    Begins implementing an easy string table for defining the variables in a variable group. Variables can now be defined in a simple dictionary along with their titles, labels, etc. Polishing is still needed such as autoimporting titles from netcdf. https://github.com/larson-group/sys_admin/issues/542

--- postprocessing/pyplotgen/VariableGroupTest.py
+++ postprocessing/pyplotgen/VariableGroupTest.py
@@ -49,57 +49,21 @@ class VariableGroupBase(VariableGroup):
                                                                                self.height_max_value)
             self.z_sam.data = self.z_sam.data[self.z_sam_min_idx:self.z_sam_max_idx]
 
-        ### Initialize plots in order to make panels later ###
-        # Step 1) Generate current clubb plots using super().getVarLinePlots()
-        # Step 2) If plotting SAM and variable needs to be calculated, create and call a calulating function
-        # Step 3) Append the SAM plotAll from step 2 into the list generated from step 1
-        thlm_plots = super().getVarLinePlots('thlm', ncdf_files, averaging_start_time=self.averaging_start_time,
-                                             averaging_end_time=self.averaging_end_time, label="current clubb",
-                                             line_format='r--')
-        if sam_file != None:
-            thlm_sam = self.getThlmSamPlot()
-            thlm_plots.append(thlm_sam)
+        variables = [
+            {'clubb_name': 'thlm', 'title': 'Liquid Water Potential Temperature, Theta l', 'axis_title':'thlm [K]', 'sam_calc':self.getThlmSamPlot},
+            {'clubb_name': 'rtm', 'title': 'Turbulent Flux of Theta l', 'axis_title': 'wpthlp [K m/s]', 'sam_calc':self.getRtmSamPlot},
+            {'clubb_name': 'wpthlp', 'title': 'Turbulent Flux of Theta l', 'axis_title': 'wpthlp [K m/s]'},
+            {'clubb_name': 'wprtp', 'title': 'Turbulent Flux of rt', 'axis_title': 'wprtp [(kg/kg) m/s]'},
+            {'clubb_name': 'wp2', 'title': 'Variance of w', 'axis_title': 'wp2 [m^2/s^2]'},
+            {'clubb_name': 'wp2_vert_avg', 'title': 'Density-Weighted Vertically Averaged wp2', 'axis_title': 'wp2 [m^2/s^2]', 'type':Panel.TYPE_TIMESERIES},
+            {'clubb_name': 'cloud_frac', 'title': 'Cloud Frac', 'axis_title': 'cloud_frac[%]'}
 
-        rtm_plots = super().getVarLinePlots('rtm', ncdf_files, averaging_start_time=self.averaging_start_time,
-                                            averaging_end_time=self.averaging_end_time, label="current clubb",
-                                            line_format='r--')
-        if sam_file != None:
-            rtm_sam = self.getRtmSamPlot()
-            rtm_plots.append(rtm_sam)
+        ]
 
-        # SAM variables that don't require calculation beyond a conversion factor can be plotted by defining the
-        # variable name conversion in the file VarnameConversions.py
-        wpthlp_plots = super().getVarLinePlots('wpthlp', ncdf_files['zm'], averaging_start_time=self.averaging_start_time,
-                                               averaging_end_time=self.averaging_end_time, sam_file=sam_file,
-                                               label="current clubb", line_format='r--')
-        wprtp_plots = super().getVarLinePlots('wprtp', ncdf_files, averaging_start_time=self.averaging_start_time,
-                                              averaging_end_time=self.averaging_end_time, sam_file=sam_file,
-                                              label="current clubb", line_format='r--')
-        wp2_plots = super().getVarLinePlots('wp2', ncdf_files, averaging_start_time=self.averaging_start_time,
-                                            averaging_end_time=self.averaging_end_time, sam_file=sam_file,
-                                            label="current clubb", line_format='r--')
-        den_wgt_avg_plots = super().getVarLinePlots('wp2_vert_avg', ncdf_files['sfc'],
-                                                    averaging_start_time=self.averaging_start_time,
-                                                    averaging_end_time=self.averaging_end_time,
-                                                    override_panel_type=Panel.TYPE_TIMESERIES, label="current clubb",
-                                                    sam_conv_factor=1 / 1000, line_format='r--')
-        cloud_frac_plots = super().getVarLinePlots('cloud_frac', ncdf_files, sam_conv_factor=100, conversion_factor=100,
-                                                   averaging_start_time=self.averaging_start_time,
-                                                   averaging_end_time=self.averaging_end_time, sam_file=sam_file,
-                                                   label="current clubb", line_format='r--')
+        for variable in variables:
+            super().addClubbVariable(variable)
+        super().generatePanels()
 
-        ### Initialize Panels using plots from above ###
-        liq_pot_temp = Panel(thlm_plots, title="Liquid Water Potential Temperature, Theta l", dependant_title="thlm [K]")
-        turbulent_flux__theta = Panel(wpthlp_plots, title="Turbulent Flux of Theta l", dependant_title='wpthlp [K m/s]')
-        turbulent_flux_r = Panel(wprtp_plots, title="Turbulent Flux of rt", dependant_title='wprtp [(kg/kg) m/s]')
-        wp2 = Panel(wp2_plots, title="Variance of w", dependant_title='wp2 [m^2/s^2]')
-        den_wgt_avg = Panel(den_wgt_avg_plots, title="Density-Weighted Vertically Averaged wp2",
-                            dependant_title="wp2 [m^2/s^2]", panel_type=Panel.TYPE_TIMESERIES)
-        rtm_panel = Panel(rtm_plots, title="Total Water Mixing Ratio, _t", dependant_title="rtm [kg/kg]")
-        cld_panel = Panel(cloud_frac_plots, title="Cloud Fraction", dependant_title="cloud_frac[%]")
-
-        ### Build panels list ###
-        self.panels = [turbulent_flux__theta, turbulent_flux_r, wp2, den_wgt_avg, liq_pot_temp, rtm_panel, cld_panel]
 
     def plot(self):
         '''
