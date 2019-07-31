80b7dc6b2d943ea474b6b9d43d14b80fcccb9513
Gunther Huebler <huebler@uwm.edu>
2019-07-31 17:20:14 -0500

    Pulling the calculation of p_in_Pa(_zm) out of the vertical loop in update_pressure, and precalculates one/kappa. This improved vectorization capabilities of the calculation of exponents, which is slow so that's important. Also added the option to use MKL_VML functions if -DMKL is specified, which is slightly more efficient. This saves about 1.5-2.5% and is bit identical if not using MKL. larson-group/clubb#855

--- src/CLUBB_core/calc_pressure.F90
+++ src/CLUBB_core/calc_pressure.F90
@@ -118,7 +118,8 @@ module calc_pressure
       rvm_nz    ! Water vapor mixing ratio; uppermost thermo. level  [kg/kg]
 
     real( kind = core_rknd ), parameter :: &
-      g_ov_Cp = grav / Cp  ! g / Cp  [K/m]
+      g_ov_Cp = grav / Cp, &     ! g / Cp  [K/m]
+      invrs_kappa = one / kappa  ! 1 / kappa
 
     ! Flag to calculate pressure and exner on momentum levels.  Otherwise,
     ! linear interpolation of exner will be used.
@@ -127,6 +128,7 @@ module calc_pressure
 
     integer :: k  ! Vertical level index
 
+    ! ---- Begin code ----
 
     ! Calculate thvm on thermodynamic grid levels.
     thvm = calculate_thvm( thlm, rtm, rcm, exner, thv_ds_zt )
@@ -186,7 +188,7 @@ module calc_pressure
     endif ! l_calc_p_exner_m_levs
 
     ! Calculate pressure on the uppermost momentum level.
-    p_in_Pa_zm(gr%nz) = p0 * exner_zm(gr%nz)**(one/kappa)
+    p_in_Pa_zm(gr%nz) = p0 * exner_zm(gr%nz)**invrs_kappa
 
     ! Calculate exner at all other thermodynamic and momentum grid levels,
     ! which are all located below the uppermost thermodynamic grid level.
@@ -212,9 +214,6 @@ module calc_pressure
           exner(k) = exner(k+1) + g_ov_Cp * ( gr%zt(k+1) - gr%zt(k) ) / thvm(k)
 
        endif
-
-       ! Calculate pressure on thermodynamic levels.
-       p_in_Pa(k) = p0 * exner(k)**(one/kappa)
 
        if ( l_calc_p_exner_m_levs ) then
 
@@ -242,10 +241,27 @@ module calc_pressure
 
        endif ! l_calc_p_exner_m_levs
 
-       ! Calculate pressure on momentum levels.
-       p_in_Pa_zm(k) = p0 * exner_zm(k)**(one/kappa)
-
     enddo ! k = gr%nz-1, 2, -1
+
+#ifdef MKL
+    ! MKL VML functions available. vdpowx(n,a,b,y) computes a(1:n)^b = y(1:n)
+    ! This temporarily store exner(_zm)**invrs_kappa in p_in_Pa(_zm), before
+    ! multiplying p_in_Pa(_zm) by p0 to complete the calculation.
+
+    ! Calculate pressure on thermodynamic levels
+    call vdpowx( gr%nz-2, exner(2:gr%nz-1), invrs_kappa, p_in_Pa(2:gr%nz-1) )
+    p_in_Pa(2:gr%nz-1) =  p_in_Pa(2:gr%nz-1) * p0
+
+    ! Calculate pressure on momentum levels.
+    call vdpowx( gr%nz-2, exner_zm(2:gr%nz-1), invrs_kappa, p_in_Pa_zm(2:gr%nz-1) )
+    p_in_Pa_zm(2:gr%nz-1) =  p_in_Pa_zm(2:gr%nz-1) * p0
+#else
+    ! Calculate pressure on thermodynamic levels.
+    p_in_Pa(2:gr%nz-1) = p0 * exner(2:gr%nz-1)**invrs_kappa
+
+    ! Calculate pressure on momentum levels.
+    p_in_Pa_zm(2:gr%nz-1) = p0 * exner_zm(2:gr%nz-1)**invrs_kappa
+#endif
 
     ! Calculate exner the model lower boundary or surface.
     ! exner1
@@ -270,7 +286,7 @@ module calc_pressure
     endif
 
     ! Calculate pressure at the model lower boundary or surface.
-    p_in_Pa_zm(1) = p0 * exner_zm(1)**(one/kappa)
+    p_in_Pa_zm(1) = p0 * exner_zm(1)**invrs_kappa
 
     ! For the lowest thermodynamic level, which is below the model lower
     ! boundary, set pressure and exner to the pressure and exner found at the
