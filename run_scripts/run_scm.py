a0f4d96f3e921688131a1016f50d5b0c96fbcdd8
Chris Vogl <vogl2@llnl.gov>
2020-04-22 17:04:47 -0700

    added custom run scripts

---
+++ run_scripts/run_scm.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+modifiable_parameters = ['dt', 'dt_output', 'microphysics', 'format', 'prefix']
+
+# TODO: check that this is being run from the run_scripts directory
+os.chdir('../output')
+
+if (len(sys.argv) < 2):
+  print('usage: ./run_scm.py model_name ... option_name option value ...')
+  print('valid options:')
+  print(modifiable_parameters)
+  sys.exit(1)
+
+model_file_name = sys.argv[1] + '_model.in'
+
+# read in default configuration from model file
+model_file = open('../input/case_setups/' + model_file_name, 'r')
+model_config = model_file.readlines()
+model_file.close()
+
+# read in parameters to modify from command line
+parameters = {}
+for n in range(2,len(sys.argv)-1):
+  parameters[sys.argv[n]] = sys.argv[n+1]
+
+# create concatenated model file name (and check parameters are valid)
+model_file_name = model_file_name.replace('_model.in', '')
+for parameter in parameters:
+  model_file_name += '_' + parameter + '-' + parameters[parameter]
+  if (parameter not in modifiable_parameters):
+    print(paramter + ' is not modifiable')
+    print('valid options:')
+    print(modifiable_parameters)
+    sys.exit(1)
+model_file_name += '.in'
+
+# create modified configuration model file
+model_file = open(model_file_name, 'w')
+
+# set microphysics to "none" unless user has specified something else
+if ('microphysics' not in parameters):
+  parameters['microphysics'] = '"none"'
+
+# set output to netcdf unless user has specified something else
+if ('format' not in parameters):
+  parameters['format'] = '"netcdf"'
+
+# set file prefix to modified model name unless user has specified something else
+if ('prefix' not in parameters):
+  parameters['prefix'] = '"' + model_file_name.replace('.in','') + '"'
+
+# match dt_output to dt unless it is specifically provided
+if ('dt' in parameters and 'dt_output' not in parameters):
+  parameters['dt_output'] = parameters['dt']
+
+modified_lines = []
+for line in model_config:
+  modified = False
+  if (not line.strip().startswith('!')):
+    for parameter in parameters:
+      # adjust timestep sizes
+      if (parameter == 'dt'):
+        if (line.startswith('dt_main') or line.startswith('dt_rad')):
+          default_dt = line.split()[2]
+          line = line.replace(default_dt, parameters[parameter])
+          modified = True
+      # adjust output timestep
+      if (parameter == 'dt_output'):
+        if (line.startswith('stats_tsamp') or line.startswith('stats_tout')):
+          default_dt = line.split()[2]
+          line = line.replace(default_dt, parameters[parameter])
+          modified = True
+      # adjust microphysics scheme
+      if (parameter == 'microphysics'):
+        if (line.startswith('microphys_scheme')):
+          default_scheme = line.split()[2]
+          line = line.replace(default_scheme, parameters[parameter])
+          modified = True
+      # adjust output file name prefix
+      if (parameter == 'prefix'):
+        if (line.startswith('fname_prefix')):
+          default_prefix = line.split()[2]
+          line = line.replace(default_prefix, parameters[parameter])
+          modified = True
+      # adjust output file format
+      if (parameter == 'format'):
+        if (line.startswith('stats_fmt')):
+          default_format = line.split()[2]
+          line = line.replace(default_format, parameters[parameter])
+          modified = True
+
+  # write line to model configuration (and record if modified)
+  model_file.write(line)
+  if (modified):
+    modified_lines.append(line)
+
+model_file.close()
+
+print('Wrote ' + model_file_name + ' file with the following modified lines:\n')
+for line in modified_lines:
+  print(line),
+
+# concatenate all other input namelist files to one file
+line_collections = []
+# "parameter" file
+input_file = open('../input/tunable_parameters/tunable_parameters.in', 'r')
+line_collections.append(input_file.readlines())
+input_file.close()
+# "SILHS_PARAMS" file
+input_file = open('../input/tunable_parameters/silhs_parameters.in', 'r')
+line_collections.append(input_file.readlines())
+input_file.close()
+# "FLAGS" file
+input_file = open('../input/tunable_parameters/configurable_model_flags.in', 'r')
+line_collections.append(input_file.readlines())
+input_file.close()
+# "MOD_MODEL" file
+input_file = open(model_file_name, 'r')
+line_collections.append(input_file.readlines())
+input_file.close()
+# "stats" file
+input_file = open('../input/stats/standard_stats.in', 'r')
+line_collections.append(input_file.readlines())
+input_file.close()
+# write out to namelist file
+namelist_file = open('clubb.in', 'w')
+for line_collection in line_collections:
+  for line in line_collection:
+    ind = line.find('!')
+    if (ind != -1):
+      line = line[0:ind] + '\n'
+    namelist_file.write(line)
+namelist_file.close()
+
+# execute CLUBB
+print('Running CLUBB...')
+os.system('../bin/clubb_standalone')
