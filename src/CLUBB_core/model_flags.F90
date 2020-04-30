af240ac0623b62934d04ee0a012c71adaa3531ea
Vincent Larson <vlarson@users.noreply.github.com>
2020-04-30 17:00:59 -0500

    I set `l_diag_Lscale_from_tau = .true.` in source code
    in order to match the value in configurable_model_flags.in.  
    I did so to avoid confusion, but also to check whether doing so
    will allow the WRF-CLUBB lines nightly plots to better match the CLUBB standalone lines.
    
    For larson-group/wrf#105.

--- src/CLUBB_core/model_flags.F90
+++ src/CLUBB_core/model_flags.F90
@@ -449,7 +449,7 @@ module model_flags
     l_damp_wp2_using_em = .true.
     l_do_expldiff_rtm_thlm = .false.
     l_Lscale_plume_centered = .false.
-    l_diag_Lscale_from_tau = .false.
+    l_diag_Lscale_from_tau = .true.
     l_use_ice_latent = .false.
     l_use_C7_Richardson = .true.
     l_use_C11_Richardson = .false.
