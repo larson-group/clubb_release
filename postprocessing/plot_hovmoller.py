813ffbce48695c24fb1bc38b23ba6f4993c85229
Chris Vogl <vogl2@llnl.gov>
2020-06-04 13:26:48 -0700

    updated plot hovmoller script to plot both wp2 and wp3 with new colorscheme

--- postprocessing/plot_hovmoller.py
+++ postprocessing/plot_hovmoller.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as pyplot
+import matplotlib.colors as colors
 from netCDF4 import Dataset
 import numpy as np
 import sys
@@ -11,23 +12,25 @@ if (M < 1):
   print("usage: python plot_hovmoller.py <run 1> [run 2] ...")
   sys.exit(1)
 
-f, ax = pyplot.subplots(M, 1, sharex=True, figsize=(8,4*M))
-if (M == 1):
-  ax = [ax,]
-for m in range(M):
-  nc_file = sys.argv[1+m] + '_zm.nc'
-  data = Dataset(nc_file)
-  t = data['time'][:]
-  z = data['altitude'][:]
-  wp2 = data['wp2'][:]
-  pcr = ax[m].pcolor(t/60.0, z, np.transpose(wp2[:,:,0,0]), cmap='gist_heat_r', vmin=0, vmax=1.0)
-  f.colorbar(pcr, ax=ax[m])
-  ax[m].set_ylabel(sys.argv[1+m])
-  ax[m].grid(True, which='major', axis='x')
+for (suffix, var) in (('zm','wp2'),('zt','wp3')):
+  f, ax = pyplot.subplots(M, 1, sharex=True, figsize=(8,4*M))
+  if (M == 1):
+    ax = [ax,]
+  for m in range(M):
+    nc_file = sys.argv[1+m] + '_{}.nc'.format(suffix)
+    data = Dataset(nc_file)
+    t = data['time'][:]
+    z = data['altitude'][:]
+    val = data[var][:]
+    #norm = colors.TwoSlopeNorm(vmin=min(0.0,np.amin(val))-1e-12, vcenter=0.0, vmax=np.amax(val))
+    valmax = min(2.0, np.amax(val))
+    pcr = ax[m].contourf(t/60.0, z, np.transpose(val[:,:,0,0]), cmap='gist_stern_r', levels=np.linspace(0.0,valmax,10))
+    f.colorbar(pcr, ax=ax[m])
+    ax[m].set_ylabel(sys.argv[1+m])
+    ax[m].grid(True, which='major', axis='x')
+  pyplot.setp(ax, xlim=(1000,1440), ylim=(0,5000))
+  ax[0].set_title(var)
+  ax[M-1].set_xlabel('time (min)')
+  f.tight_layout()
 
-pyplot.setp(ax, xlim=(0,1000), ylim=(0,5000))
-#pyplot.grid(True, which='major', axis='x')
-ax[0].set_title('wp2')
-ax[M-1].set_xlabel('time (min)')
-f.tight_layout()
 pyplot.show()
