ebd95914ee63dc0cb167690c345d154f49fd707d
bmg929 <bmg2@uwm.edu>
2020-06-25 10:32:58 -0500

    I updated the namelist definition and namelist defaults to include a bunch of CLUBB tunable parameters that are normally hardwired by E3SM developers directly in CLUBB.  This is a better method because it does not require changing CLUBB code, making CLUBB updates easier in the future.
    E3SM Issue Reference: #22.

--- components/cam/src/physics/cam/clubb_intr.F90
+++ components/cam/src/physics/cam/clubb_intr.F90
@@ -537,13 +537,6 @@ end subroutine clubb_init_cnst
 
     !  From the CLUBB libraries
     use clubb_api_module, only: &
-        iC2rtthl, iC4, iC6rt, iC6thl, iC8b, iC15, iC_wp2_splat, &
-        ic_K1, ic_K2, inu2, ic_K8, ic_K9, inu9, ic_K_hmb, &
-        ibeta, ilmin_coef, imult_coef, iSkw_denom_coef, ic_K10h, &
-        iup2_vp2_factor, iSkw_max_mag
-
-    !  From the CLUBB libraries
-    use clubb_api_module, only: &
          setup_clubb_core_api, &
          print_clubb_config_flags_api, &
          time_precision, &
@@ -750,30 +743,6 @@ end subroutine clubb_init_cnst
        zt_g(k) = ((k-1)*1000._r8)-500._r8  !  this is dummy garbage
        zi_g(k) = (k-1)*1000._r8            !  this is dummy garbage
     enddo
-
-    ! Overwrite default values of CLUBB tunable parameters with values that have
-    ! been tuned for use in the E3SM model.
-    clubb_params(iC2rtthl) = 2.275_r8
-    clubb_params(iC4) = 5.2_r8
-    clubb_params(iC6rt) = 4.0_r8
-    clubb_params(iC6thl) = clubb_params(iC6rt)
-    clubb_params(iC8b) = 0.0_r8
-    clubb_params(iC15) = 0.4_r8
-    clubb_params(iC_wp2_splat) = 0.0_r8
-    clubb_params(ic_K1) = 0.75_r8
-    clubb_params(ic_K2) = 0.125_r8
-    clubb_params(inu2) = 5.0_r8
-    clubb_params(ic_K8) = 1.25_r8
-    clubb_params(ic_K9) = 0.25_r8
-    clubb_params(inu9) = 20.0_r8
-    clubb_params(ic_K_hmb) = 0.1_r8
-    clubb_params(ibeta) = 2.4_r8
-    clubb_params(ilmin_coef) = 0.1_r8
-    clubb_params(imult_coef) = 1.0_r8
-    clubb_params(iSkw_denom_coef) = 0.0_r8
-    clubb_params(ic_K10h) = 0.35_r8
-    clubb_params(iup2_vp2_factor) = 2.0_r8
-    clubb_params(iSkw_max_mag) = 4.5_r8
 
     call init_clubb_config_flags( clubb_config_flags ) ! In/Out
 
