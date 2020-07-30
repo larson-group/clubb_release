4e035e6539946638b920f2e7cd13cb8fc41eab1e
IsenfireLDC <isenfireldc@gmail.com>
2020-07-30 11:33:02 -0500

    Removed unused import [-Wunused-variable] Unchanged
    Issue #878

--- src/SILHS/silhs_importance_sample_module.F90
+++ src/SILHS/silhs_importance_sample_module.F90
@@ -393,9 +393,6 @@ module silhs_importance_sample_module
     use clubb_precision, only: &
       core_rknd     ! Constant
 
-    use constants_clubb, only: & ! Constant
-      unused_var
-
     implicit none
 
     ! Input Variables
