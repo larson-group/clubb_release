ac41258af461eb7969f84011f774b53d4812446e
Nicolas Strike <strike@malan.math.uwm.edu>
2020-08-25 14:40:52 -0500

    Updating updateDocs to be more friendly with new computers  https://github.com/larson-group/sys_admin/issues/542

--- postprocessing/pyplotgen/docs/updateDocs.sh
+++ postprocessing/pyplotgen/docs/updateDocs.sh
@@ -3,13 +3,24 @@
 echo "Updating HTML Reference Documentation for PyPlotgen"
 echo "-------------------------"
 
+pip install --user virtualenv
+virtualenv sphinx-venv
+
 # if there's a venv, try to use it
-source ../venv/bin/activate
+source sphinx-venv/bin/activate
 
 echo "Ensuring python dependencies are installed"
+pip3 install -r ../requirements.txt
 pip3 install sphinx sphinx_rtd_theme
+
+mkdir _static
+mkdir _templates
+
+sphinx-build -o . ..
 
 make clean
 make html
 
 echo "If the build succeeded, open '_build/html/index.html' to view the reference docs."
+deactivate
+
