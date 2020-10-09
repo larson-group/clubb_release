* Fix the variable priority rotation not working as expected with calculate functions due to an issue of switching to np.any() * Add manual logic for halding missing qi var in rtm sam calculations
https://github.com/larson-group/sys_admin/issues/542

postprocessing/pyplotgen/config/VariableGroupBase.py
@@ -635,11 +635,13 @@ class VariableGroupBase(VariableGroup):
         dataset = self.sam_benchmark_dataset
         if dataset_override is not None:
             dataset = dataset_override
-        # z,z, dataset = self.getVarForCalculations('z', self.sam_benchmark_dataset)
         qt, indep, dataset = self.getVarForCalculations('QT', dataset)
         qi, indep, dataset = self.getVarForCalculations('QI', dataset)
 
         rtm = (qt - qi) / 1000
+        if np.any(np.isnan(qi)):
+            rtm = qt / 1000
+
         return rtm, indep
 
     def getSkwZtLesCalc(self, dataset_override=None):
@@ -859,7 +861,6 @@ class VariableGroupBase(VariableGroup):
         if not np.isnan(wprtp_sgs[0]):
             wprtp += wprtp_sgs
 
-        # z,z, dataset = self.getVarForCalculations(['z', 'lev', 'altitude'], self.sam_benchmark_dataset)
         return wprtp, indep
 
     def getWpthvpSamCalc(self, dataset_override=None):

postprocessing/pyplotgen/src/DataReader.py
@@ -259,7 +259,11 @@ class NetCdfVariable:
             else:
                 dependent_data, independent_data = varname_element(dataset_override=all_datasets)
 
-            if np.isnan(dependent_data.any()) or np.isnan(independent_data.any()):
+            # When plotting subcolumns, dependent_data can be multidimentional. This accounts for that.
+            if dependent_data.ndim > 1:
+                if (np.isnan(dependent_data[0,0])) or np.isnan(np.isnan(dependent_data[0,0])):
+                    continue
+            elif dependent_data.ndim == 1 and (np.isnan(dependent_data[0]) or (np.isnan(dependent_data[0]))):
                 continue
             else:
                 break