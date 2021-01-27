fc11fcae7158ad83e4bceadd3c779fa901b64de6
bstephens82 <benjamin.andrew.stephens@gmail.com>
2021-01-27 13:38:10 -0600

    Updating advance_xm_wpxp_module so that the upwp_pr4 term uses the parameter C_uu_shr instead of the hard-coded value 0.7. Also updating the value of C_uu_shr from 0.3 to 0.4 after testing priority-cases with the new upwp_pr4 term.
    This commit is BIT_CHANGING.
    
    See #901.

--- src/CLUBB_core/advance_xm_wpxp_module.F90
+++ src/CLUBB_core/advance_xm_wpxp_module.F90
@@ -2344,8 +2344,8 @@ module advance_xm_wpxp_module
       sclr_dim, &  ! Variable(s)
       sclr_tol
 
-!    use parameters_tunable, only: &
-!      C_uu_shr    ! Parameter(s)
+    use parameters_tunable, only: &
+      C_uu_shr    ! Parameter(s)
         
     use clubb_precision, only:  & 
         core_rknd ! Variable(s)
@@ -2629,26 +2629,14 @@ module advance_xm_wpxp_module
        endif ! .not. l_implemented
 
        ! Add "extra term" and optional Coriolis term for <u'w'> and <v'w'>.
-       !upwp_forcing = C7_Skw_fnc * wp2 * ddzt( um )
-       !vpwp_forcing = C7_Skw_fnc * wp2 * ddzt( vm )
-       upwp_forcing = 0.7_core_rknd * wp2 * ddzt( um )
-       vpwp_forcing = 0.7_core_rknd * wp2 * ddzt( vm )
-!       upwp_forcing = C_uu_shr * wp2 * ddzt( um )
-!       vpwp_forcing = C_uu_shr * wp2 * ddzt( vm )
+       upwp_forcing = C_uu_shr * wp2 * ddzt( um )
+       vpwp_forcing = C_uu_shr * wp2 * ddzt( vm )
 
        if ( l_stats_samp ) then
-          !call stat_update_var( iupwp_pr4, C7_Skw_fnc * wp2 * ddzt( um ), &
-          !                      stats_zm )
-          !call stat_update_var( ivpwp_pr4, C7_Skw_fnc * wp2 * ddzt( vm ), &
-          !                      stats_zm )
-          call stat_update_var( iupwp_pr4, 0.7_core_rknd * wp2 * ddzt( um ), &
+          call stat_update_var( iupwp_pr4, C_uu_shr * wp2 * ddzt( um ), &
                                 stats_zm )
-          call stat_update_var( ivpwp_pr4, 0.7_core_rknd * wp2 * ddzt( vm ), &
+          call stat_update_var( ivpwp_pr4, C_uu_shr * wp2 * ddzt( vm ), &
                                 stats_zm )
-!          call stat_update_var( iupwp_pr4, C_uu_shr * wp2 * ddzt( um ), &
-!                                stats_zm )
-!          call stat_update_var( ivpwp_pr4, C_uu_shr * wp2 * ddzt( vm ), &
-!                                stats_zm )
        endif ! l_stats_samp
 
        ! need tau_C6_zm for these calls
