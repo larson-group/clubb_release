b0ef19af47e735da455b4ac23ec245492a1f3bc6
IsenfireLDC <isenfireldc@gmail.com>
2021-02-11 15:58:33 -0600

    Changes error handling in error.f90 for the fire tuner
    Adds a statement to flag l_error if l_file_error is flagged
    Changes a blank stop to an error stop with a message
    
    This is a recommit of 612ed898 on a new branch
    
    larson-group/sys_admin#725

--- src/error.F90
+++ src/error.F90
@@ -861,6 +861,9 @@ module error
         if (les_stats_file(c_run)(len_file-2: len_file) == ".nc") then
           call open_netcdf_read( les_v(i), les_stats_file(c_run), netcdf_file, l_file_error);
           call close_netcdf_read(netcdf_file);
+          if (l_file_error) then
+            l_error = .true. ! This may or may not be necessary
+          end if
         else
           l_file_error = .true. ! This will cause the following assertion check to be skipped
         end if
@@ -909,7 +912,7 @@ module error
           end if
           write(fstderr,*) "The specified LES variable "//trim( les_v(i) )//" was invalid, "// &
             "or the GrADS file did not exist."
-          stop
+          error stop "Missing or improperly formatted file"
         end if
 
         ! Verify that the domain that we're tuning CLUBB over is fully defined in
