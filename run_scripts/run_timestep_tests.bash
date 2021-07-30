70e26ca1acd06149d3b7b996235769e5e0509c39
pbroels <pbroels@uwm.edu>
2021-07-30 09:39:41 -0500

    Modifying run_timestep_tests.bash to run only 10 minute and above timesteps and exit immediately if the 10 mintue timestep fails. larson-group/sys_admin#749

--- run_scripts/run_timestep_tests.bash
+++ run_scripts/run_timestep_tests.bash
@@ -20,17 +20,27 @@ restoreDir=`pwd`
 # Change directories to the one the script is located in
 cd $scriptPath
 
-TEST_TIMESTEP[0]=300.0   # 5 minute time step.
-TEST_TIMESTEP[1]=600.0   # 10 minute time step.
-TEST_TIMESTEP[2]=900.0   # 15 minute time step.
-TEST_TIMESTEP[3]=1200.0  # 20 minute time step.
-TEST_TIMESTEP[4]=1800.0  # 30 minute time step.
-TEST_TIMESTEP[5]=2700.0  # 45 minute time step.
-TEST_TIMESTEP[6]=3600.0  # 60 minute (one hour) time step.
+TEST_TIMESTEP[0]=600.0   # 10 minute time step.
+TEST_TIMESTEP[1]=900.0   # 15 minute time step.
+TEST_TIMESTEP[2]=1200.0  # 20 minute time step.
+TEST_TIMESTEP[3]=1800.0  # 30 minute time step.
+TEST_TIMESTEP[4]=2700.0  # 45 minute time step.
+TEST_TIMESTEP[5]=3600.0  # 60 minute (one hour) time step.
 
 for (( x=0; x < "${#TEST_TIMESTEP[@]}"; x++ )); do
     echo -e "\nRunning all cases at a(n) "${TEST_TIMESTEP[$x]}" second time step."
    ./run_scm_all.bash --timestep_test ${TEST_TIMESTEP[$x]}
+   #Check the exit code of the last run
+   if [ $? -ne 0 ] 
+   then
+      #If it is the first run (the 10 minute timestep) and it failed, exit the script immediately
+      if [ $x -eq 0 ]
+      then 
+         echo "10 minute timestep failed, exiting test with exit code 1."
+         echo "See above output for error troubleshooting."
+         exit 1
+      fi
+   fi
    # Move all output files from a given timestep to their own directory.
    # This code assumes that this script is being run from the run_scripts directory
    #   and that the output directory is empty before this script is run.
