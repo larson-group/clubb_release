7eb659d596d4c75f2da95a56428657ed23fb41e5
bmg929 <bmg2@uwm.edu>
2023-04-20 12:19:36 -0500

    Added a new file to compile CLUBB on Anvil.

---
+++ compile/config/linux_x86_64_ifort_anvil.bash
@@ -0,0 +1,84 @@
+# $Id$
+# Makefile definitions customized for Linux x86_64 using the Intel Fortran 
+# compiler 
+
+
+module purge
+module load python
+module load intel
+module load netcdf-fortran/4.5.3
+module load netcdf-c/4.7.4
+module load intel-mkl/2019.5.281
+
+# Fortran 95 compiler and linker
+FC=ifort
+LD=ifort
+
+# Define path to directories
+dir=`pwd` # dir where this script resides
+bindir="$dir/../bin"  # dir for Makefile and executable
+objdir="$dir/../obj"  # dir for *.o and *.mod files
+libdir="$dir/../lib"  # dir for *.a library files
+srcdir="$dir/../src"  # dir where the source files reside
+
+
+# == Debug ==
+DEBUG=""
+#DEBUG="-debug full -traceback -check bounds -check uninit -fpe0 -ftz -ftrapuv"
+
+# == Warnings ==
+WARNINGS="-warn -warn notruncated_source"
+
+# == Machine specific options ==
+ARCH="-xHost" # This should work on most modern AMD/Intel computers
+# == Used to promote all real's to double precision ==
+DOUBLE_PRECISION="-real-size 64"
+
+# == Optimization ==
+OPTIMIZE="-O3"
+#OPTIMIZE="-O3 -ipo" # Interprocedural optimization
+
+# == NetCDF Location ==
+NETCDF=$NETCDF_ROOT
+
+# == LAPACK libraries ==
+# AMD Core Math Library
+#ACML="/opt/acml5.1.0/ifort64/lib"
+#LAPACK="-L$ACML -Wl,-rpath,$ACML -lacml"
+# Intel MKL
+
+LAPACK="-mkl=sequential"
+
+# == Linking Flags ==
+# Use -s to strip (no debugging); 
+# Use -L<library path> -l<lib> to link in an external library
+LDFLAGS="-L$NETCDF/lib -lnetcdff -lnetcdf -L$MKLROOT/lib/intel64 $LAPACK"
+
+FFLAGS="$ARCH $OPTIMIZE $DEBUG -fp-model strict"
+
+# Preprocessing Directives:
+#   -DNETCDF enables netCDF output
+#   -DMKL enables MKL solver (PARDISO/GMRES) support
+# You will need to `make clean' if you change these
+# Use -I<include path> to set a module or header file directory
+# Define include directories. 
+# Need location of include and *.mod files for the netcdf library
+
+CPPDEFS="-DNETCDF -DCLUBB_REAL_TYPE=8"
+CPPFLAGS="-I$MKLPATH/../../include -I$NETCDF/include"
+
+# == Static library processing ==
+AR=ar
+#AR=xiar # Interprocedural optimization archive tool
+ARFLAGS=cru
+RANLIB=ranlib
+
+# == Shared library processing ==
+SHARED=$FC
+SHAREDFLAGS="-fPIC -shared"
+
+# Location of 'mkmf' utility
+mkmf=$dir/mkmf
+
+# gmake command to use and options: '-j 2' enables parallel compilation
+gmake="make -j5"
