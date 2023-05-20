b75bf29871855c7e2bb56532f1fb82354a55729e
cernikt <tjcernik@uwm.edu>
2023-05-20 11:52:45 -0500

    Added comments to the script to explain ambiguous portions of my code

--- utilities/autocommit_message_maker/autocommit_message_maker.py
+++ utilities/autocommit_message_maker/autocommit_message_maker.py
@@ -7,9 +7,16 @@ import re
 Author: Tyler Cernik
 Date: 1/16/2023
 This program will be used to create autocommit messages of all the commits that have been made to clubb since the last autocommit.
-The program will be called from the autocommit script and will return the autocommit message to standard output.
-The autocommit script will then take that message and use it to create the autocommit.
-This method will also take in the path to the repo that we are getting the autocommit messages from.
+This is done over a few steps:
+    1. First we will read in arguments from the command line where this script is run from:
+        - The path to the host repo that we will be accessing to view the last autocommit in it
+        - The path to the internal directory of clubb that we will be looking for commits in
+    
+    2. Then we will put all commit information into a dictionary where the key is the commit hash and the value is the entire commit
+    3. Then we will get the commit hash of the newest clubb commit in the host repo's latest autocommit message
+    4. Then we will get all the commits in clubb that are newer than the commit hash we got in step 3
+
+
 TODO:
     - set up program to be able to take in an argument for which repo we are getting the autocommit messages from
        (wrf, e3sm, cam) (or maybe just the path to the repo)
@@ -26,8 +33,10 @@ log_dict = {} # This will be a dictionary that will hold the commit hash as the 
 # clubb_internal_directory = "src/CLUBB_core"
 
 def get_arguments():
-    # This method will get the arguments from the command line and set the values of the variables
-    # external_repo and clubb_interal_directory
+    """
+    This method will get the arguments from the command line and set the values of the variables
+    external_repo and clubb_interal_directory
+    """
 
     # if there is no argv[2], then we will assume that the internal directory is src/CLUBB_core
 
@@ -45,17 +54,16 @@ def get_arguments():
         print("the path to the repo you want to get the autocommit messages from is incorrect, exiting")
         sys.exit(1)
 
-
     # print("external_repo: " + external_repo)
     # print("clubb_internal_directory: " + clubb_internal_directory)
 
     return external_repo, clubb_internal_directory
 
 
-
-
 def run(external_repo, clubb_internal_directory, log_dict):
-    #global log_dict
+    """
+    This method will get the git log from the clubb repo and put it into a dictionary where the key is the commit hash and the value is the entire commit
+    """
     # This method will be called from the base of the clubb repo, otherwise the clubb_internal_directory will need to be changed
     # to reflect as such.
 
@@ -64,37 +72,18 @@ def run(external_repo, clubb_internal_directory, log_dict):
 
     # get the git log from the clubb repo
     git_log = subprocess.run(["git", "log", format, "--", clubb_internal_directory], stdout=subprocess.PIPE)
-    #"--pretty[format:Commit %H %nMessage: <full-commit-message>], stdout=subprocess.PIPE)
-    # print(type(git_log))
-    #git_log = git_log.stdout.decode("utf-8")
-    #print(type(git_log.stdout))
-    #print(str(git_log.stdout).split("\n\n"))
-
 
     # decode the git log from bytes to a string, and split it into a list of strings
     # git_log = list(map(lambda x: x.strip("\n") ,str(git_log.stdout.decode("utf-8")).split("commit_hash:")))
 
     git_log = list(str(git_log.stdout.decode("utf-8")).strip("\"").split("Start_of_Commit\n"))
 
-
-
-
-    # print(len(git_log))
-
-
     # clean all empty strings from the list
     git_log = list(filter(None, git_log))
 
-
     for commit in git_log:
-        # print(commit)
-        # print(commit[7:48])
         hash = re.search('Commit (.+)', commit).group(1)
-        # commit[7:48].strip("\n")
         log_dict[hash] = commit
-
-
-
 
     # Testing code - writes all items in the git_log list to a file
     # out_file = open("/home/cernikt/Documents/autocommit_message_maker/clubb/utilities/git_log.txt", "w")
@@ -104,7 +93,12 @@ def run(external_repo, clubb_internal_directory, log_dict):
     #     out_file.write("\n")
     # out_file.close()
 
+
 def get_autommit_message_commit(external_repo, clubb_internal):
+    """
+    This method will get the latest autocommit message from the external repo and return the commit hash of the latest clubb commit
+    If it cannot find a clubb commit in the latest autocommit message, it will return -1
+    """
     commit_format = "--pretty=\"commit_hash:%H %s Body: %b\""
     # get the latest autocommit message
     # Gets it in the format commit_hash:hash commit_title
@@ -134,12 +128,17 @@ def get_autommit_message_commit(external_repo, clubb_internal):
     for line in repo_git_log:
         if ("Autoupdated " + clubb_internal_src) in line:
 
+            # this method looks through the git log of the host repo and finds the latest autocommit message
+
+
             # print(line)
             # print(len(line))
+            # this offset is here to account for the length of the "Autoupdated src/CLUBB_core" string
             if(len(line) <= (len("Autoupdated " + clubb_internal_src)+80)):
                 return -1
             offset = line.index("Autoupdated " + clubb_internal_src) + len("Autoupdated " + clubb_internal_src)
 
+            # 54 is the length of the string "Commit " plus the length of the hash
             hash = re.search('Commit (.+)', line[:offset + 54]).group(1).strip()
             # print(hash)
             # once we find the latest autocommit, we can break out of the loop
@@ -152,11 +151,13 @@ def get_autommit_message_commit(external_repo, clubb_internal):
 
 
 def get_new_clubb_commits(log_dict, external_repo, clubb_internal_directory):
+    """
+    This method will run previously defined methods to get the latest autocommit message from the external repo
+    This is done by first calling teh get autommit message commit method, which will return the commit hash of the latest clubb commit in the host repo
+    Then, based on what that is, we will return a string that contains the commit message for the newest auto commit message,
+    which contains the commit messages that make up that autocommit.
+    """
     # get the latest clubb commit from the latest autocommit message in the external repo
-
-    #global log_dict
-
-    # external_repo, clubb_internal_directory = get_arguments()
 
     internal = clubb_internal_directory.split("/")[1].strip()
 
@@ -165,30 +166,34 @@ def get_new_clubb_commits(log_dict, external_repo, clubb_internal_directory):
     hash = get_autommit_message_commit(external_repo, clubb_internal_directory)
     # print(hash)
     if(hash == -1):
+        # this case occurs when there are no clubb commit message in the latest autocommit message in the host repo
+        # This occurs when either:
+        # 1. The external repo is not properly specified in the arguments (This likely won't happen, since this script is run in another script)
+        # 2. There are no autocommit messages in the external repo (This can occur if it is the first time this script is run during the autocommit process)
+
+        # In this case, we will just return a message that contains the latest clubb commit message that was made.
+        # Because of this, once the script is run again, it will be able to find the latest autocommit message, and will be able to get the latest clubb commit message from that
+        # it also adds a message that says that there are no autocommit messages found, and that the latest clubb commit message was printed instead
+
         key = list(log_dict.keys())[0]
-        #print(list(log_dict[key]))
         temp = (("Autoupdated " + internal + "\n") + ("").join(list(log_dict[key])).replace('"', ''))
         
         temp += ("No autocommit messages found, printed latest commit from " + internal + " instead. \n Please make sure the that there are previous autocommit messages, or that the external repo is properly configured.")
         return temp
     else:
-        
+        # We did find a clubb commit in the latest autocommit message in the host repo, so we can proceed as normal
+
         # get all of the clubb commits in the internal repo
     
         # filter the list of clubb commits to only include commits that are after the latest autocommit clubb hash
         new_clubb_commits = []
 
-        # print(log_dict.keys())
-
-        # print(log_dict[hash])
-
+        # Gettting all of the clubb commits that are after the latest autocommit clubb commit
         for key in list(log_dict.keys()):
 
             if(key in hash):
                 return ("Autoupdated " + internal + "\n") + ("\n").join(new_clubb_commits).replace('"', '')
             new_clubb_commits.append(log_dict[key])
-
-
 
         # return the list of new clubb commits as a string
         return ("Autoupdated " + internal + "\n") + ("\n").join(new_clubb_commits).replace('"', '')
@@ -199,7 +204,6 @@ if __name__=="__main__":
     try:
         external_repo, clubb_internal_directory = get_arguments()
         out = get_new_clubb_commits(log_dict, external_repo, clubb_internal_directory)
-
 
         sys.stdout.flush()
 
