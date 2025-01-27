f3c5bfa75630b6277c79cc2c8c107ac8bb9950fc
st3113n <0-freundlich-adenin@icloud.com>
2025-01-27 14:09:38 -0600

    remove new gpu commands

--- src/Benchmark_cases/atex.F90
+++ src/Benchmark_cases/atex.F90
@@ -251,7 +251,6 @@ module atex
 
       ! calculate z_inversion for dycore grid (z_inversion_dycore) with interpolated rtm values on 
       ! dycore grid
-      !$acc parallel loop gang vector default(present)
       do i = 1, ngrdcol
         z_lev_dycore(i) = 1
         do while ( z_lev_dycore(i) <= dycore_gr%nzt .and. rtm_dycore(i,z_lev_dycore(i)) > 6.5e-3_core_rknd )
@@ -260,8 +259,6 @@ module atex
       end do
 
       if ( clubb_at_least_debug_level(2) ) then
-
-        !$acc update host( z_lev_dycore, rtm_dycore )
 
         do i = 1, ngrdcol
           if ( z_lev_dycore(i) == dycore_gr%nzt+1 .or. z_lev_dycore(i) == 1 ) then
@@ -275,7 +272,6 @@ module atex
         end do
       end if
 
-      !$acc parallel loop gang vector default(present)
       do i = 1, ngrdcol
         z_inversion_dycore(i) = dycore_gr%zt(i,z_lev_dycore(i)-1)
       end do
@@ -335,7 +331,6 @@ module atex
     end do
 
     if (interp_from_dycore_grid_method > 0 ) then
-      !$acc parallel loop gang vector collapse(2) default(present)
       do k = 1, dycore_gr%nzt
         do i = 1, ngrdcol
           thlm_forcing_dycore(i,k) = 0._core_rknd
