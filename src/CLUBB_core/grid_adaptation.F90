3f057b824b3c314dac14860fc37027f565185c98
st3113n <0-freundlich-adenin@icloud.com>
2025-02-06 15:45:25 -0600

    add functions for adaptive grid generation and grid density function normalization

--- src/CLUBB_core/grid_adaptation.F90
+++ src/CLUBB_core/grid_adaptation.F90
@@ -1297,6 +1297,403 @@ module grid_adaptation_module
     return
   end function vertical_integral_conserve_mass
 
+  function calc_integral( g_idx, g_x, g_y )
+    ! Description:
+    ! Computes the exact integral, assuming the given points build a piecewise linear function.
+    ! g_x and g_y_idx must both have g_idx elements
+
+    ! References:
+    ! None
+    !-----------------------------------------------------------------------
+
+    use clubb_precision, only: &
+        core_rknd ! Variable(s)
+
+    implicit none
+
+    !--------------------- Input Variables ---------------------
+    integer, intent(in) :: & 
+      g_idx  ! The total numer of indices of g_x and g_y
+
+    real( kind = core_rknd ), dimension(g_idx), intent(in) ::  &
+      g_x,  &    ! the x coordinates of the connection points of the piecewise linear function
+      g_y        ! the y coordinates of the connection points of the piecewise linear function
+
+    !--------------------- Local Variables ---------------------
+    integer :: i
+
+    real( kind = core_rknd ) :: &
+      calc_integral ! The integral
+
+    !--------------------- Begin Code ---------------------
+    ! Initializing calc_integral to avoid a compiler warning.
+    calc_integral = 0.0_core_rknd
+
+    ! calculate each linear segments exact integral and add them up
+    do i = 1, (g_idx-1)
+        calc_integral = calc_integral + (g_y(i) + g_y(i+1))/2 * (g_x(i+1) - g_x(i))
+    end do
+
+    return
+
+  end function calc_integral
+
+  subroutine normalize_density( gd_idx, gd_x, gd_y, &
+                                min_dens, num_levels, &
+                                gd_norm_x, gd_norm_y )
+
+    ! Description:
+    ! Takes the linear piecewise grid density function (gd_x, gd_y) each of size gd_idx and
+    ! normalizes this density, such that the integral is the
+    ! number of desired grid levels (num_levels) -1 and the minimum is above the minimal
+    ! density (min_dens)
+
+    ! References:
+    ! None
+    !-----------------------------------------------------------------------
+
+    implicit none
+
+    !--------------------- Input Variables ---------------------
+    integer, intent(in) :: &
+      gd_idx, &   ! number of elements in gd_x and gd_y
+      num_levels  ! desired number of grid levels
+
+    real( kind = core_rknd ), dimension(gd_idx), intent(in) :: &
+      gd_x, & ! the grid density x coordinates
+      gd_y    ! the grid density y coordinates
+      ! gd_x and gd_y should be ordered from the bottom to the top
+
+    real( kind = core_rknd ), intent(in) :: &
+      min_dens   ! the desired minimum grid density
+
+    !--------------------- Output Variables ---------------------
+    real( kind = core_rknd ), dimension(gd_idx), intent(out) :: &
+      gd_norm_x, & ! the normalized grid density x coordinates
+      gd_norm_y    ! the normalized grid density y coordinates
+
+    !--------------------- Local Variables ---------------------
+    integer :: i
+
+    real( kind = core_rknd ) :: &
+      grid_sfc, &               ! the surface height of the grid
+      grid_top, &               ! the highest point of the grid
+      delta_s, &                ! delta by which the function first gets shifted,
+                                ! so the minimum of the function is above min_dens
+      integral_before_shift, &  ! the integral of the shifted density function before normalizing it
+      equi_dens, &              ! the grid density for an equi-distant grid
+      factor, &                 ! factor to multiply shifted function with to end up with an
+                                ! integral of num_levels-1
+      h, &                      ! absolut distance from lowest to highest point of the grid
+      diff, zero
+
+    !--------------------- Begin Code ---------------------
+    grid_sfc = gd_x(1)
+    grid_top = gd_x(gd_idx)
+    diff = (min_dens - minval(gd_y))
+    zero = 0.0_core_rknd
+    delta_s = maxval([diff, zero])
+    h = grid_top - grid_sfc
+    equi_dens = (num_levels-1)/h
+
+    ! first shift function such that minimum is above min_dens, then shift, so that min_dens is zero
+    do i = 1, gd_idx
+        gd_norm_x(i) = gd_x(i)
+        gd_norm_y(i) = gd_y(i) + delta_s - min_dens
+    end do
+
+    integral_before_shift = calc_integral(gd_idx, gd_norm_x, gd_norm_y)
+
+    ! if the integral is zero, then all gd_norm_y were the same and smaller or equal to min_dens
+    ! so in that case we have an equi-distant grid
+    if (integral_before_shift < 1.0e-6_core_rknd) then
+        ! set to equi-distant grid
+        do i = 1, gd_idx
+            gd_norm_y(i) = equi_dens
+        end do
+    else
+        factor = (num_levels-1-h*min_dens)/integral_before_shift
+        do i = 1, gd_idx
+            gd_norm_y(i) = factor*(gd_norm_y(i)) + min_dens
+        end do
+    end if
+
+    if ( clubb_at_least_debug_level( 2 ) ) then
+      ! check if minimum of function is actually bigger or equal to min_dens
+      if (minval(gd_norm_y) < min_dens) then
+        write(fstderr,*) "Warning! The minimum of the normalized function is below min_dens."
+      end if
+      ! check if the integral of the normalized function is actually num_levels-1
+      if (abs(calc_integral(gd_idx, gd_norm_x, gd_norm_y) - (num_levels-1)) > 0.000001) then
+        write(fstderr,*) "Warning! The integral of the normalized function is not num_levels-1."
+      end if
+    end if
+
+  end subroutine normalize_density
+
+  function create_grid_from_normalized_grid_density_func( num_levels, &
+                                                          gd_norm_idx, &
+                                                          gd_norm_x, &
+                                                          gd_norm_y ) &
+                                                        result (grid_heights)
+    ! Description:
+    ! Creates the grid from the normalized grid density function following the equi-distribution
+    ! principle.
+
+    ! References:
+    ! None
+    !-----------------------------------------------------------------------
+
+    use clubb_precision, only: &
+        core_rknd ! Variable(s)
+
+    implicit none
+
+    !--------------------- Input Variables ---------------------
+    integer, intent(in) :: & 
+      gd_norm_idx, &  ! The total numer of indices of gd_norm_x and gd_norm_y
+      num_levels      ! number of levels the new grid should have
+
+    real( kind = core_rknd ), dimension(gd_norm_idx), intent(in) ::  &
+      gd_norm_x,  &    ! the x coordinates of the connection points of the normalized piecewise
+                       ! linear grid density function
+      gd_norm_y        ! the y coordinates of the connection points of the normalized piecewise
+                       ! linear grid density function
+      ! The grid density function needs to be normalized with the number of grid levels num_levels,
+      ! before the function can be applied here
+
+    !--------------------- Output Variable ---------------------
+    real( kind = core_rknd ), dimension(num_levels) :: &
+      grid_heights ! the heights of the newly created grid, from bottom to top
+
+    !--------------------- Local Variables ---------------------
+    integer :: i, &
+               prev_x_ind  ! the index of the last used x coordinate
+
+    real( kind = core_rknd ) :: &
+      grid_sfc, &               ! the surface height of the grid
+      grid_top, &               ! the highest point of the grid
+      area_up_to_prev_x, &      ! the integral of the function up to the last used x coordinate
+      new_x_area_increment, &   ! integral from grid density function on [prev_x_ind, prev_x_ind+1]
+      desired_area_up_to_ith_level, & ! the desired area up to the ith grid level, should be exactly
+                                      ! i, since the grid density function is normalized so that the
+                                      ! integral of the whole function is num_levels-1 and since we
+                                      ! follow the equi-distribution approach
+      A, grid_level, m, b, p, q
+
+    logical :: grid_level_found
+
+    !--------------------- Begin Code ---------------------
+    grid_sfc = gd_norm_x(1)
+    grid_top = gd_norm_x(gd_norm_idx)
+
+    grid_heights(1) = grid_sfc
+
+    ! Initializing calc_integral to avoid a compiler warning.
+    prev_x_ind = 1
+    area_up_to_prev_x = 0.0_core_rknd
+
+    do i = 2, num_levels-1
+        grid_level_found = .false.
+        do while (.not. grid_level_found)
+            new_x_area_increment = (gd_norm_y(prev_x_ind) + gd_norm_y(prev_x_ind+1))/2 &
+                                   * (gd_norm_x(prev_x_ind+1) - gd_norm_x(prev_x_ind))
+            desired_area_up_to_ith_level = i-1
+            if (area_up_to_prev_x + new_x_area_increment >= desired_area_up_to_ith_level) then
+                A = desired_area_up_to_ith_level - area_up_to_prev_x
+                ! check if gd_norm_y(prev_x_ind+1) == gd_norm_y(prev_x_ind)
+                if (abs(gd_norm_y(prev_x_ind+1) - gd_norm_y(prev_x_ind)) < 1.0e-6_core_rknd) then
+                    ! gd_norm_y(prev_x_ind+1) == gd_norm_y(prev_x_ind)
+                    grid_level = A/gd_norm_y(prev_x_ind) + gd_norm_x(prev_x_ind)
+                else
+                    ! calculate slope and absolute value of linear function
+                    m = (gd_norm_y(prev_x_ind+1) - gd_norm_y(prev_x_ind)) &
+                        /(gd_norm_x(prev_x_ind+1) - gd_norm_x(prev_x_ind))
+                    b = gd_norm_y(prev_x_ind) - gd_norm_x(prev_x_ind)*m
+                    ! calculate p and q of second-order polynomial to solve for x
+                    p = 2*b/m
+                    q = -1*gd_norm_x(prev_x_ind)**2 - 2/m*b*gd_norm_x(prev_x_ind) - 2/m*A
+                    ! since we have second-order polynomial, we have two roots, so we need to
+                    ! identify the right solution - use pq formula to get solutions
+                    grid_level = -1*p/2 + sqrt((p/2)**2 - q)
+
+                    if ((grid_level < gd_norm_x(prev_x_ind)) &
+                        .or. (grid_level > gd_norm_x(prev_x_ind+1)) & 
+                        .or. (grid_level < grid_heights(i-1))) then
+                        ! first solution was not the one we were looking for
+                        grid_level = -1*p/2 - sqrt((p/2)**2 - q)
+                        if ((grid_level < gd_norm_x(prev_x_ind)) &
+                            .or. (grid_level > gd_norm_x(prev_x_ind+1)) & 
+                            .or. (grid_level < grid_heights(i-1))) then
+                            write(fstderr,*) "None of the two solutions works. ", &
+                                             "Something went wrong."
+                        end if
+                    end if
+                end if
+
+                grid_heights(i) = grid_level
+                grid_level_found = .true.
+            else
+                area_up_to_prev_x = area_up_to_prev_x + new_x_area_increment
+                prev_x_ind = prev_x_ind + 1
+            end if
+        end do
+    end do
+
+    grid_heights(num_levels) = grid_top
+
+    return
+
+  end function create_grid_from_normalized_grid_density_func
+  
+  subroutine check_grid( grid_heights_idx, grid_heights, &
+                         desired_num_levels, desired_min_dens, &
+                         desired_grid_sfc, desired_grid_top )
+
+    ! Description:
+    ! Checks if the grid is valid.
+
+    ! References:
+    ! None
+    !-----------------------------------------------------------------------
+
+    implicit none
+
+    !--------------------- Input Variables ---------------------
+    integer, intent(in) :: &
+      grid_heights_idx, &   ! number of elements in grid_heights
+      desired_num_levels    ! desired number of grid levels
+    ! Note: if everything worked fine, those two integers should be the same
+
+    real( kind = core_rknd ), dimension(grid_heights_idx), intent(in) :: &
+      grid_heights ! the grid heights ordered from bottom to top
+
+    real( kind = core_rknd ), intent(in) :: &
+      desired_min_dens, &   ! the desired minimum grid density
+      desired_grid_sfc, &   ! desired surface height of the grid
+      desired_grid_top      ! desired top height of the grid
+
+    !--------------------- Local Variables ---------------------
+    integer :: i
+
+    real( kind = core_rknd ) :: &
+      max_dist  ! the maximum distance between two grid levels, depending on desired_min_dens
+
+    !--------------------- Begin Code ---------------------
+    ! check if first grid element is grid_sfc
+    if (abs(grid_heights(1) - desired_grid_sfc) > 0.000001) then
+        write(fstderr,*) "Warning! The first grid level is not correct. It should be the grids", &
+                         " surface: ", desired_grid_sfc, " and not ", grid_heights(1)
+    end if
+
+    max_dist = 1/desired_min_dens
+
+    do i = 1, (grid_heights_idx-1)
+        ! check if grid heights are getting bigger
+        if (grid_heights(i) >= grid_heights(i+1)) then
+            write(fstderr,*) "Warning! The grid levels are not in the right order. ", &
+                             "They should get bigger with the index."
+        end if
+        ! check if grid fulfills min_dense
+        if ((grid_heights(i+1) - grid_heights(i)) > max_dist) then
+            write(fstderr,*) "Warning! The grid has a distance, ", &
+                             "that is bigger than defined by min_dens."
+        end if
+    end do
+
+    ! check if grid has the correct number of levels
+    if (grid_heights_idx /= desired_num_levels) then
+        write(fstderr,*) "Warning! The grid has not the right number of levels. There are ", &
+                         grid_heights_idx, " level, but there should be ", desired_num_levels, &
+                         " level."
+    end if
+
+    ! check if last grid level is grid_top
+    if (abs(grid_heights(desired_num_levels) - desired_grid_top) > 0.000001) then
+        write(fstderr,*) "Warning! The last grid level is not correct. It should be the grids", &
+                         " top: ", desired_grid_top, " and not ", grid_heights(desired_num_levels)
+    end if
+
+  end subroutine check_grid
+
+  function create_grid_from_grid_density_func( gd_idx, &
+                                               gd_x, gd_y, &
+                                               min_dens, &
+                                               num_levels ) &
+                                             result (grid_heights)
+    ! Description:
+    ! Creates the grid from the unnormalized grid density function following the equi-distribution
+    ! principle.
+
+    ! References:
+    ! None
+    !-----------------------------------------------------------------------
+
+    use clubb_precision, only: &
+        core_rknd ! Variable(s)
+
+    implicit none
+
+    !--------------------- Input Variables ---------------------
+    integer, intent(in) :: & 
+      gd_idx, &       ! total numer of indices of gd_x and gd_y
+      num_levels      ! number of levels the new grid should have
+
+    real( kind = core_rknd ), intent(in) ::  &
+      min_dens  ! the minimum density the new grid should have
+
+    real( kind = core_rknd ), dimension(gd_idx), intent(in) ::  &
+      gd_x,  &    ! the x coordinates of the connection points of the piecewise linear
+                  ! grid density function
+      gd_y        ! the y coordinates of the connection points of the piecewise linear
+                  ! grid density function
+
+    !--------------------- Output Variable ---------------------
+    real( kind = core_rknd ), dimension(num_levels) :: &
+      grid_heights ! the heights of the newly created grid, from bottom to top
+
+    !--------------------- Local Variables ---------------------
+    integer :: grid_heights_idx
+
+    real( kind = core_rknd ), dimension(gd_idx) ::  &
+      gd_norm_x,  &    ! the x coordinates of the connection points of the normalized piecewise
+                       ! linear grid density function
+      gd_norm_y        ! the y coordinates of the connection points of the normalized piecewise
+                       ! linear grid density function
+
+    real( kind = core_rknd ) ::  &
+      grid_sfc,  &    ! height of the grids surface
+      grid_top        ! height of the top of the grid
+
+    !--------------------- Begin Code ---------------------
+    grid_sfc = gd_x(1)
+    grid_top = gd_x(gd_idx)
+
+    call normalize_density( gd_idx, gd_x, gd_y, &
+                            min_dens, num_levels, &
+                            gd_norm_x, gd_norm_y )
+
+    grid_heights = create_grid_from_normalized_grid_density_func( num_levels, &
+                                                                  gd_idx, &
+                                                                  gd_norm_x, &
+                                                                  gd_norm_y )
+
+    ! TODO how to read may change if we incorporate ngrdcol!,
+    ! TODO since size does not work then, but should be size/ngrdcol?,
+    ! TODO since all columns should have same number of grid levels?
+    !grid_heights_idx = shape(grid_heights)
+    grid_heights_idx = size(grid_heights) ! counts the scalar values
+
+    if ( clubb_at_least_debug_level( 2 ) ) then
+        call check_grid( grid_heights_idx, grid_heights, &
+                         num_levels, min_dens, &
+                         grid_sfc, grid_top )
+    end if 
+
+    return
+
+  end function create_grid_from_grid_density_func
+
 !===============================================================================
 
 end module grid_adaptation_module
