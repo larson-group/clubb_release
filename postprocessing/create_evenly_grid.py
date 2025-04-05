31a8545c29781c0bb7c70e9fed7854c4a261f903
st3113n <0-freundlich-adenin@icloud.com>
2025-04-04 20:26:18 -0500

    update create_evenly_grid script

--- postprocessing/create_evenly_grid.py
+++ postprocessing/create_evenly_grid.py
@@ -4,9 +4,10 @@ def write_file(file_name):
     num_levels = 33
     with open(file_name, "w") as f:
         altitude = alt_sfc
-        while altitude <= alt_top:
+        while altitude < alt_top:
             f.write(str(altitude) + '\n')
             altitude += (alt_top - alt_sfc)/(num_levels-1)
+        f.write(str(alt_top))
 
 def main():
     write_file('test.grd')
